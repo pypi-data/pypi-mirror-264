96f07dc99f09b7d8b628c4cc87eb1408619e029a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-24 14:03:27 +0000

    Add package mockmaster/0.0.1/mockmaster-0.0.1.tar.gz

---
+++ packages/mockmaster/mockmaster-0.0.1.tar.gz/mockmaster-0.0.1/setup.py
@@ -0,0 +1,31 @@
+import os
+from setuptools import setup
+
+
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+
+setup(
+    name="mockmaster",
+    version="0.0.1",
+    author="Deepu Krishnareddy , Harish Mohan",
+    author_email="deepukreddy007@gmail.com , Harishmohan1598@gmail.com",
+    description=("A CLI tool to generate dummy data "),
+    license="MIT",
+    keywords="data dummy",
+    packages=['mockmaster'],
+    long_description=read('README.md'),
+
+    classifiers=[
+        "Development Status :: 2 - Pre-Alpha",
+        "Topic :: Utilities",
+    ],
+    install_requires=['openai', 'pandas',
+                      "jsonschema", "python-dotenv", "pyfiglet", "tabulate", "termcolor"],
+    entry_points={
+        'console_scripts': [
+            'mockmaster = mockmaster.cli:main',
+        ],
+    }
+)
