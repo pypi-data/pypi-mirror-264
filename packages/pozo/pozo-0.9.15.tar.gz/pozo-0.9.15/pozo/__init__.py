588b9a8d9d3a66a5560ef1f7fbc4ae4ced88a1cb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-24 14:03:05 +0000

    Add package pozo/0.9.15/pozo-0.9.15.tar.gz

---
+++ packages/pozo/pozo-0.9.15.tar.gz/pozo-0.9.15/pozo/__init__.py
@@ -0,0 +1,35 @@
+import ood
+
+ood.exceptions.NameConflictException.default_level = ood.exceptions.ErrorLevel.IGNORE
+ood.exceptions.MultiParentException.default_level = ood.exceptions.ErrorLevel.IGNORE
+from .traces import Trace
+from .axes import Axis
+from .tracks import Track
+from .graphs import Graph
+
+import pozo.themes as themes
+import pozo.renderers as renderers
+import pozo.units as units
+
+def deLASio(mnemonic):
+    return mnemonic.split(":", 1)[0] if ":" in mnemonic else mnemonic
+
+# Error on dimensionless
+#
+
+
+class HasLog(ood.selectors.Selector):
+    def __init__(self, mnemonic):
+        self.mnemonic = mnemonic
+    def _process(self, parent):
+        ret_items = []
+        for item in parent.get_items():
+            if hasattr(item, 'get_mnemonic'):
+                if item.get_mnemonic() == self.mnemonic:
+                    ret_items.append(item)
+                    break
+            if isinstance(item, ood.Observer):
+                if item.has_item(self):
+                    ret_items.append(item)
+                    break
+        return ret_items
