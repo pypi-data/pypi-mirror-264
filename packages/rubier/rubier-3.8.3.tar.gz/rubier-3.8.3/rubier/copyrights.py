a64a3ae2675784bff122b4d0a645e1dfd01ef243
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-24 03:03:22 +0000

    Add package rubier/3.8.3/rubier-3.8.3.tar.gz

---
+++ packages/rubier/rubier-3.8.3.tar.gz/rubier-3.8.3/rubier/copyrights.py
@@ -0,0 +1,8 @@
+import colors
+import os
+
+os.system("")
+
+class ConsoleCopyrights(object):
+    def consolePrinter():
+        print(f"{colors.colors.MAGENTA}Rubier Library 3.8.3\n{colors.colors.CYAN}Rubino Framework\n\n{colors.colors.END}Started {colors.colors.ORANGE}Rubier{colors.colors.END} as Soon ...\n{colors.colors.LRED}-------------------------{colors.colors.END}\n\n")
