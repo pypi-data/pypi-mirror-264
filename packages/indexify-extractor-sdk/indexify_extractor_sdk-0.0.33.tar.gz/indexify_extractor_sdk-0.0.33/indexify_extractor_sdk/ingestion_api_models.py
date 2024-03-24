e01f175e3f2ccb1679fb4bdf22b93f95c60a3fe0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-24 04:03:21 +0000

    Add package indexify-extractor-sdk/0.0.33/indexify_extractor_sdk-0.0.33.tar.gz

---
+++ packages/indexify-extractor-sdk/indexify_extractor_sdk-0.0.33.tar.gz/indexify_extractor_sdk-0.0.33/indexify_extractor_sdk/ingestion_api_models.py
@@ -0,0 +1,55 @@
+from pydantic import BaseModel, Json
+from typing import List, Dict
+
+
+class ApiFeature(BaseModel):
+    feature_type: str
+    name: str
+    data: Json
+
+
+class ApiContent(BaseModel):
+    content_type: str
+    bytes: List[int]
+    features: List[ApiFeature] = []
+    labels: Dict[str, str] = {}
+
+
+class BeginExtractedContentIngest(BaseModel):
+    task_id: str
+    namespace: str
+    output_to_index_table_mapping: Dict[str, str]
+    parent_content_id: str
+    executor_id: str
+    task_outcome: str
+    extraction_policy: str
+    extractor: str
+
+
+class ExtractedFeatures(BaseModel):
+    content_id: str
+    features: List[ApiFeature]
+
+
+class ExtractedContent(BaseModel):
+    content_list: List[ApiContent]
+
+
+class FinishExtractedContentIngest(BaseModel):
+    num_extracted_content: int
+
+
+class ApiBeginExtractedContentIngest(BaseModel):
+    BeginExtractedContentIngest: BeginExtractedContentIngest
+
+
+class ApiExtractedContent(BaseModel):
+    ExtractedContent: ExtractedContent
+
+
+class ApiFinishExtractedContentIngest(BaseModel):
+    FinishExtractedContentIngest: FinishExtractedContentIngest
+
+
+class ApiExtractedFeatures(BaseModel):
+    ExtractedFeatures: ExtractedFeatures
