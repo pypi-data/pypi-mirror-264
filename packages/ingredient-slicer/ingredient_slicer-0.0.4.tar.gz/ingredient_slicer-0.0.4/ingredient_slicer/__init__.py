c581e8d14e58107825be34bd8396827051ee5f5d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-24 14:02:59 +0000

    Add package ingredient-slicer/0.0.4/ingredient_slicer-0.0.4.tar.gz

---
+++ packages/ingredient-slicer/ingredient_slicer-0.0.4.tar.gz/ingredient_slicer-0.0.4/ingredient_slicer/__init__.py
@@ -0,0 +1,52 @@
+# __init__.py
+
+__version__ = "0.0.4"
+
+from ._constants import NUMBER_WORDS,  NUMBER_PREFIX_WORDS, \
+    MULTI_FRACTION_WORDS, FRACTION_WORDS, DENOMINATOR_WORDS, UNICODE_FRACTIONS, \
+    UNITS, BASIC_UNITS, VOLUME_UNITS, WEIGHT_UNITS, DIMENSION_UNITS, \
+    CASUAL_QUANTITIES, CASUAL_UNITS, UNITS_SET, \
+    BASIC_UNITS_SET, NON_BASIC_UNITS_SET, SIZE_MODIFIERS_SET, \
+    VOLUME_UNITS_SET, WEIGHT_UNITS_SET, DIMENSION_UNITS_SET, CASUAL_QUANTITIES_SET, CASUAL_UNITS_SET, \
+    UNIT_MODIFIERS, PREP_WORDS, APPROXIMATE_STRINGS
+
+from ._utils import _make_int_or_float_str, _fraction_str_to_decimal, \
+    _find_substring_indices, _find_and_remove_hyphens_around_substring
+    # _replace_and_with_hyphen, _replace_to_or_with_hyphen, _replace_to_with_hyphen, _replace_or_with_hyphen
+    
+
+from ._regex_patterns import IngredientRegexPatterns
+from ._ingredient_slicer import IngredientSlicer
+
+__all__ = [
+    # Constants
+    'NUMBER_WORDS', 
+    'NUMBER_PREFIX_WORDS',
+    'MULTI_FRACTION_WORDS',
+    'FRACTION_WORDS',
+    'DENOMINATOR_WORDS',
+    'UNICODE_FRACTIONS', 
+    'UNITS', 
+    'BASIC_UNITS', 
+    'VOLUME_UNITS',
+    'WEIGHT_UNITS',
+    'DIMENSION_UNITS',
+    'CASUAL_UNITS',
+    'CASUAL_QUANTITIES',
+    'UNITS_SET',
+    'BASIC_UNITS_SET', 
+    'NON_BASIC_UNITS_SET', 
+    'SIZE_MODIFIERS_SET', 
+    'VOLUME_UNITS_SET',
+    'WEIGHT_UNITS_SET',
+    'DIMENSION_UNITS_SET',
+    'CASUAL_UNITS_SET',
+    'CASUAL_QUANTITIES_SET',
+    'UNIT_MODIFIERS', 
+    'PREP_WORDS', 
+    'APPROXIMATE_STRINGS', 
+
+    # Recipes regex and parser classes
+    'IngredientRegexPatterns', 
+    'IngredientSlicer'
+    ]
