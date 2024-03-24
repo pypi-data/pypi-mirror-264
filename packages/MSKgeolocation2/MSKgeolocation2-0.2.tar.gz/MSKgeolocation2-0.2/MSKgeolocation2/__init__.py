fa186886ec24c0a75a837f83278a5b30c33af241
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-24 04:03:28 +0000

    Add package MSKgeolocation2/0.2/MSKgeolocation2-0.2.tar.gz

---
+++ packages/MSKgeolocation2/MSKgeolocation2-0.2.tar.gz/MSKgeolocation2-0.2/MSKgeolocation2/__init__.py
@@ -0,0 +1,1 @@
+from .MSKgeolocation2 import *
---
+++ packages/MSKgeolocation2/MSKgeolocation2-0.2.tar.gz/MSKgeolocation2-0.2/setup.py
@@ -0,0 +1,30 @@
+from setuptools import setup
+
+setup(
+    name = 'MSKgeolocation2',
+    version = '0.2',
+    author = 'Massaki Igarashi',
+    author_email = 'prof.massaki@gmail.com',
+    packages = ['MSKgeolocation2'],
+    description = 'Biblioteca teste desenvolvida pelo Prof. Massaki de O. Igarashi',
+    long_description =  'Funções e operações básicas: soma, sub, mult, div. \n'
+                        + 'exemplo de uso: '
+                        + 'soma(x,y) '
+                        + ' sub(x,y) '
+                        + 'mult(x,y) '
+                        + ' div(x,y) ',
+
+    url = 'https://github.com/massakiigarashi2/MSKlib',
+    project_urls = {
+        'Código fonte': 'https://github.com/massakiigarashi2/MSKlib',
+        'Download': 'https://github.com/massakiigarashi2/MSKlib/raw/main/MSK002-0.2.tar.gz'},
+    license = 'MIT',
+    keywords = 'Funções Matemáticas Básicas',
+    classifiers = [
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Natural Language :: Portuguese (Brazilian)',
+        'Operating System :: OS Independent',
+        'Topic :: Software Development :: Internationalization',
+        'Topic :: Scientific/Engineering :: Physics'])
