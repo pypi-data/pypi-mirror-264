36cfe01af73d22ebc24b9a629fa6b31fe12adc91
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-24 03:03:06 +0000

    Add package django-icon-picker/0.0.9/django-icon-picker-0.0.9.tar.gz

---
+++ packages/django-icon-picker/django-icon-picker-0.0.9.tar.gz/django-icon-picker-0.0.9/django_icon_picker/widgets.py
@@ -0,0 +1,26 @@
+from django.forms import TextInput
+from django.conf import settings
+
+
+class IconPicker(TextInput):
+    template_name = "django_icon_picker/icon_picker.html"
+
+    def get_context(self, name, value, attrs):
+        context = super().get_context(name, value, attrs)
+        try:
+            context["save_path"] = getattr(
+                settings, "DJANGO_ICON_PICKER_SVG_FILES_SAVE_PATH"
+            )
+        except:
+            pass
+        return context
+
+    class Media:
+        css = {
+            "all": (
+                "https://cdn.jsdelivr.net/gh/mdbassit/Coloris@latest/dist/coloris.min.css",
+            )
+        }
+        js = (
+            "https://cdn.jsdelivr.net/gh/mdbassit/Coloris@latest/dist/coloris.min.js",
+        )
