708653f635bb5f217e78b1d25b80d5f44141d35c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-24 04:02:56 +0000

    Add package wagtail-heroicons/0.2.0/wagtail_heroicons-0.2.0.tar.gz

---
+++ packages/wagtail-heroicons/wagtail_heroicons-0.2.0.tar.gz/wagtail_heroicons-0.2.0/tests/conftest.py
@@ -0,0 +1,36 @@
+from __future__ import annotations
+
+import logging
+
+from django.conf import settings
+
+from .settings import DEFAULT_SETTINGS
+
+pytest_plugins = []  # type: ignore
+
+
+def pytest_configure(config):
+    logging.disable(logging.CRITICAL)
+
+    settings.configure(
+        **DEFAULT_SETTINGS,
+        **TEST_SETTINGS,
+    )
+
+
+TEST_SETTINGS = {
+    "INSTALLED_APPS": [
+        "wagtail_heroicons",
+        "django.contrib.auth",
+        "django.contrib.contenttypes",
+        "wagtail",
+    ],
+    "STATIC_URL": "/static/",
+    "TEMPLATES": [
+        {
+            "BACKEND": "django.template.backends.django.DjangoTemplates",
+            "DIRS": [],
+            "APP_DIRS": True,
+        },
+    ],
+}
