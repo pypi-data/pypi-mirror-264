215d4356eb29b70a18a765e66951ef72992d239f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-24 15:03:37 +0000

    Add package Streamlit-Functions/0.1/Streamlit_Functions-0.1.tar.gz

---
+++ packages/Streamlit-Functions/Streamlit_Functions-0.1.tar.gz/Streamlit_Functions-0.1/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='Streamlit_Functions',
+    version="0.01",
+    author='Zahid',
+    author_email='zahidpichen1@gmail.com',
+    description='mostly used functions for Streamlit app',
+    packages=find_packages(),
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent'
+    ],
+    python_requires='>=3.6'
+)
