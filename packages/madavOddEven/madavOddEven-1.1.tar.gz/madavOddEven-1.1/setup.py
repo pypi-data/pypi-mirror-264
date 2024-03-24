181773e7781b8d174f464869f2e42a3e30636eda
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-24 03:04:36 +0000

    Add package madavOddEven/1.1/madavOddEven-1.1.tar.gz

---
+++ packages/madavOddEven/madavOddEven-1.1.tar.gz/madavOddEven-1.1/setup.py
@@ -0,0 +1,9 @@
+import setuptools
+from pathlib import Path
+
+setuptools.setup(
+    name="madavOddEven",
+    version=1.1,
+    long_description=Path("Readme.md").read_text(),
+    packages=setuptools.find_packages(exclude=["odd_eve"])
+)
