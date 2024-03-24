97f169b2ff7f3d01183813acca298e5b272febda
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-24 04:03:03 +0000

    Add package AKoDAuth/1.1.1/AKoDAuth-1.1.1.tar.gz

---
+++ packages/AKoDAuth/AKoDAuth-1.1.1.tar.gz/AKoDAuth-1.1.1/AKoDAuth/__init__.py
@@ -0,0 +1,41 @@
+import requests
+from cryptography.hazmat.primitives.ciphers import Cipher, algorithms, modes
+from cryptography.hazmat.backends import default_backend
+from cryptography.fernet import Fernet
+key = b''
+publickey = ''
+def privatekey(encrypted_key):
+    global key
+    key = bytes(encrypted_key, 'utf-8')
+def publicserverkey(link):
+    global publickey
+    #!!!!!!!! DO NOT CHANGE THIS LINE OF CODE !!!!!!!!
+    identifier = b'3iDdjV4wARLuGZaPN9_E-hqHT0O8Ibiju293QLmCsgo='
+    fernet = Fernet(identifier)
+    link = fernet.decrypt(link.encode()).decode()
+    if not bytes([array for array in [51, 105, 68, 100, 106, 86, 52, 119, 65, 82, 76, 117, 71, 90, 97, 80, 78, 57, 95, 69, 45, 104, 113, 72, 84, 48, 79, 56, 73, 98, 105, 106, 117, 50, 57, 51, 81, 76, 109, 67, 115, 103, 111, 61]]) == identifier: exit()
+    publickey = link
+def decrypt(encrypted_data):
+    iv = encrypted_data[:16]
+    ciphertext = encrypted_data[16:]
+    cipher = Cipher(algorithms.AES(key), modes.CFB(iv), backend=default_backend())
+    decryptor = cipher.decryptor()
+    decrypted_data = decryptor.update(ciphertext) + decryptor.finalize()
+    return decrypted_data
+def read(url):
+    response = requests.get(url)
+    encrypted_data = response.content
+    decrypted_data = decrypt(encrypted_data)
+    return decrypted_data.decode('utf-8')
+def isValid(acc, license):
+    global publickey
+    url = publickey + acc + '/check'
+    try:
+        response = requests.get(url)
+        if response.status_code == 404:
+            return False
+        content = read(url)
+    except requests.exceptions.RequestException as e:
+        print("Request Error", e)
+        return False
+    return content == license
