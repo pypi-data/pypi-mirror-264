dd8715cee1b805cc3116aad9282f36b54232dd87
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-24 03:03:49 +0000

    Add package tina4-python/0.1.33/tina4_python-0.1.33.tar.gz

---
+++ packages/tina4-python/tina4_python-0.1.33.tar.gz/tina4_python-0.1.33/tina4_python/Env.py
@@ -0,0 +1,25 @@
+#
+# Tina4 - This is not a 4ramework.
+# Copy-right 2007 - current Tina4
+# License: MIT https://opensource.org/licenses/MIT
+#
+import os
+
+
+# check .env for information
+def load_env(path: str = '.env'):
+    env_vars = []
+    if not os.path.isfile(path):
+        with open(path, 'w') as f:
+            f.write("[Project]")
+            f.write("\n")
+
+    with open(path, 'r') as f:
+        for line in f.readlines():
+            if not line.startswith('#') and not line.startswith("["):
+                key_value = line.replace('\n', '').split('=')
+                env_vars = dict([key_value])
+    if len(env_vars) > 0:
+        os.environ.update(env_vars)
+        print("Loaded environment variables from .env file")
+        print(env_vars)
