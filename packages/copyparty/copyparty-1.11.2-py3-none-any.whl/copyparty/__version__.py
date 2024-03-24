bc729d669c4ba119ef7e44f82609b28a6766e1e5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-24 04:03:00 +0000

    Add package copyparty/1.11.2/copyparty-1.11.2-py3-none-any.whl

---
+++ packages/copyparty/copyparty-1.11.2-py3-none-any.whl/copyparty/__version__.py
@@ -0,0 +1,13 @@
+# coding: utf-8
+
+VERSION = (1, 11, 2)
+CODENAME = "You Can (Not) Proceed"
+BUILD_DT = (2024, 3, 23)
+
+S_VERSION = ".".join(map(str, VERSION))
+S_BUILD_DT = "{0:04d}-{1:02d}-{2:02d}".format(*BUILD_DT)
+
+__version__ = S_VERSION
+__build_dt__ = S_BUILD_DT
+
+# I'm all ears
