a10ef62733629d63ea20ae3c5ed59e96f780cbd9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-24 14:03:21 +0000

    Add package apollo-orm/2.0.0/apollo_orm-2.0.0-py3-none-any.whl

---
+++ packages/apollo-orm/apollo_orm-2.0.0-py3-none-any.whl/apollo_orm/domains/models/entities/concurrent/result_process/entity.py
@@ -0,0 +1,10 @@
+from datetime import datetime
+from typing import Any, Optional
+
+
+class ResultProcess:
+    def __init__(self, message: str, query: Optional[str], params: Any, processed_date: datetime = datetime.now()):
+        self.message = message
+        self.query = query
+        self.params = params
+        self.processed_date = processed_date.strftime('%Y-%m-%d %H:%M:%S:%f')
