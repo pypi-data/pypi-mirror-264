8951ed68190548fe526ae4fb6df45f79df1488f5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-24 14:03:34 +0000

    Add package dailytasks/2.1.2/dailytasks-2.1.2-py3-none-any.whl

---
+++ packages/dailytasks/dailytasks-2.1.2-py3-none-any.whl/daily_tasks/commands/__init__.py
@@ -0,0 +1,5 @@
+from daily_tasks.commands.main_commands import *
+from daily_tasks.commands.modification_commands import *
+from daily_tasks.commands.filter_commands import *
+from daily_tasks.commands.removal_commands import *
+from daily_tasks.commands.utilities import *
