caf018f0b9fd88f81489904f3e69dc6a6d68ee4b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-24 03:03:20 +0000

    Add package kappamodules/0.1.51/kappamodules-0.1.51-py3-none-any.whl

---
+++ packages/kappamodules/kappamodules-0.1.51-py3-none-any.whl/kappamodules/attention/__init__.py
@@ -0,0 +1,12 @@
+from .dot_product_attention import (
+    DotProductAttention1d,
+    DotProductAttention2d,
+    DotProductAttention3d,
+)
+from .dot_product_attention_slow import DotProductAttentionSlow
+from .linformer_attention import (
+    LinformerAttention1d,
+    LinformerAttention2d,
+    LinformerAttention3d,
+)
+from .perceiver_attention import PerceiverAttention1d, PerceiverAttention
