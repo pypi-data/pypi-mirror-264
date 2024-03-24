ed331f7ec4d95b49c6db556ef4fbaea22ca7fc42
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-24 03:04:44 +0000

    Add package RandomForest1/0.1/RandomForest1-0.1.tar.gz

---
+++ packages/RandomForest1/RandomForest1-0.1.tar.gz/RandomForest1-0.1/setup.py
@@ -0,0 +1,16 @@
+
+from setuptools import setup, find_packages
+
+setup(
+    name='RandomForest1',
+    version='0.1',
+    packages=find_packages(),
+    install_requires=[
+        'numpy',
+    ],
+    author='Boobalan-Ashokan',
+    author_email='boobalanashokan2002@email.com',
+    description='A custom implementation of Random Forest Classifier',
+    url='',
+    license='MIT',  # Change this to the appropriate license
+)
