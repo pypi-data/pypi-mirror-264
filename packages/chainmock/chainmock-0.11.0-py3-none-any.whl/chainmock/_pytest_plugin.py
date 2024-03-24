9b35f6c5beebf44f687fe714e6bc58a1d85c1767
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-24 03:03:32 +0000

    Add package chainmock/0.11.0/chainmock-0.11.0-py3-none-any.whl

---
+++ packages/chainmock/chainmock-0.11.0-py3-none-any.whl/chainmock/_pytest_plugin.py
@@ -0,0 +1,28 @@
+"""Pytest plugin."""
+
+from typing import Generator, Optional
+
+import pytest
+from _pytest.runner import CallInfo, ExceptionInfo, Item, TestReport
+
+from ._api import State
+
+
+@pytest.hookimpl(hookwrapper=True)
+def pytest_runtest_makereport(
+    item: Item,  # pylint: disable=unused-argument
+    call: CallInfo[None],
+) -> Generator[None, None, None]:
+    """Hook into test execution and execute teardown after a test."""
+    if call.when == "call":
+        State.reset_mocks()
+        if call.excinfo is None:
+            try:
+                State.validate_mocks()
+            except BaseException:  # pylint: disable=broad-except
+                call.excinfo = ExceptionInfo.from_current()
+    elif call.when == "teardown":
+        State.reset_mocks()
+        State.reset_state()
+
+    _test_report: Optional[TestReport] = yield
