9f0e1eb85b1f0b3528ffa2244d743c37c2344104
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-24 15:03:35 +0000

    Add package jmcwatch/1.5.8/jmcwatch-1.5.8.tar.gz

---
+++ packages/jmcwatch/jmcwatch-1.5.8.tar.gz/jmcwatch-1.5.8/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='jmcwatch',
+    version='1.5.8',
+    packages=find_packages(),
+    entry_points={
+        'console_scripts': [
+            'jmcwatch=jmcwatch.__main__:main',
+        ],
+    },
+    install_requires=[
+        'click',
+        'watchdog',
+        'jmcfunction'
+    ],
+    author='amandin',
+    author_email='amawwdin@email.com',
+    description='Watch and compile JMC files. Simply type `jmcwatch` to use.',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    license='MIT',
+)
