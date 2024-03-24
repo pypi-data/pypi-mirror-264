b2ba014a859a097db8861c1575800ab8666df894
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-24 04:03:03 +0000

    Add package RyzenTest/1.0/RyzenTest-1.0.tar.gz

---
+++ packages/RyzenTest/RyzenTest-1.0.tar.gz/RyzenTest-1.0/setup.py
@@ -0,0 +1,13 @@
+import setuptools as st
+st.setup(
+    name='RyzenTest',
+    version='1.0',
+    author='Ryzen ENCGO',
+    description='une Librairie Simple pour le Test Formation Python',
+    packages=st.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+        "License :: OSI Approved :: MIT License"
+    ]
+)
