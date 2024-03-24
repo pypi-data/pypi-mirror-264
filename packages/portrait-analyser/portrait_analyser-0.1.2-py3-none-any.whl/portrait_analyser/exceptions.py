a7cf0e57304df5f7f06f0eb178aac5fda7a79ac6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-24 15:03:27 +0000

    Add package portrait-analyser/0.1.2/portrait_analyser-0.1.2-py3-none-any.whl

---
+++ packages/portrait-analyser/portrait_analyser-0.1.2-py3-none-any.whl/portrait_analyser/exceptions.py
@@ -0,0 +1,18 @@
+class ExifValidationFailed(Exception):
+    pass
+
+
+class NoDepthMapFound(Exception):
+    pass
+
+
+class UnknownExtension(Exception):
+    pass
+
+
+class NoFacesDetected(Exception):
+    pass
+
+
+class MultipleFacesDetected(Exception):
+    pass
