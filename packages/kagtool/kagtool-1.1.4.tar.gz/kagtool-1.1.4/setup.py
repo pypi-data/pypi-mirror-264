5376f7848318e30c863fea137f10da61f4ee3e27
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-24 14:03:31 +0000

    Add package kagtool/1.1.4/kagtool-1.1.4.tar.gz

---
+++ packages/kagtool/kagtool-1.1.4.tar.gz/kagtool-1.1.4/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+setup(
+    name='kagtool',
+    version='1.1.4',
+    description='Helpers For Kaggle Competition',
+    author='Your Name',
+    author_email='your@email.com',
+    packages=['kagtool'],
+    install_requires=[
+        'kaggle',
+        'sentencepiece',
+        'transformers',
+    ],
+)
