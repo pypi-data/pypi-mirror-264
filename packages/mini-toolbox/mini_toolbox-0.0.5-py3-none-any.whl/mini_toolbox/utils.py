2e700a199ae23f997c06041fe3eacc9bf46dbdc7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-24 03:02:37 +0000

    Add package mini-toolbox/0.0.5/mini_toolbox-0.0.5-py3-none-any.whl

---
+++ packages/mini-toolbox/mini_toolbox-0.0.5-py3-none-any.whl/mini_toolbox/utils.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding:utf-8 -*-
+# @Mail: haha@haha.com
+# @File: mini_toolbox/utils.py
+# @Date: 2024-03-19 10:42:12
+# @Desc:
+
+
+def add_one(number):
+    return number + 1
