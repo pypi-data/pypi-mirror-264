b9cc99d5e2228522d1a832cbce762abb7ea28fa4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-24 14:03:51 +0000

    Add package cx-Freeze/7.0.0rc0/cx_Freeze-7.0.0rc0-cp38-cp38-win_amd64.whl

---
+++ packages/cx-Freeze/cx_Freeze-7.0.0rc0-cp38-cp38-win_amd64.whl/cx_Freeze/_typing.py
@@ -0,0 +1,17 @@
+"""The internal _typing module."""
+
+from __future__ import annotations
+
+from pathlib import Path, PurePath
+
+from typing_extensions import TypeAlias
+
+from cx_Freeze.module import Module
+
+DeferredList: TypeAlias = list[tuple[Module, Module, list[str]]]
+
+IncludesList: TypeAlias = list[
+    str | Path | tuple[str | Path, str | Path | None]
+]
+
+InternalIncludesList: TypeAlias = list[tuple[Path, PurePath]]
