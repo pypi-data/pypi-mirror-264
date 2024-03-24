020fa9ea53d31eca63ae5a5478c54e2df09b4bfc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-24 04:02:59 +0000

    Add package ikctl/1.2.1/ikctl-1.2.1.tar.gz

---
+++ packages/ikctl/ikctl-1.2.1.tar.gz/ikctl-1.2.1/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='ikctl',
+    version='1.2.1',
+    description="App to installer packages on remote servers",
+    author="David Moya López",
+    author_email="3nueves@gmail.com",
+    license="Apache v2.0",
+    packages=find_packages(include=['ikctl','ikctl.*']),
+    install_requires=[
+        # 'paramiko',
+        # 'pyaml',
+        # 'envyaml'
+    ],
+    entry_points={
+        'console_scripts': [
+            # 'ikctl=ikctl.main:create_parser'
+            'ikctl=.main:create_parser'
+        ]
+    }
+)
