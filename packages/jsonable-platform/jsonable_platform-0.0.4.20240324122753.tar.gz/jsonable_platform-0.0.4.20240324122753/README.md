f5b8827a9082c89903573ebfd14c343c15984775
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-24 14:03:32 +0000

    Add package jsonable-platform/0.0.4.20240324122753/jsonable_platform-0.0.4.20240324122753.tar.gz

---
+++ packages/jsonable-platform/jsonable_platform-0.0.4.20240324122753.tar.gz/jsonable_platform-0.0.4.20240324122753/README.md
@@ -0,0 +1,13 @@
+# jsonable-platform
+Pipy package for customizing convert to JSON
+
+> [!Important]
+> This package is still Beta, please **DO NOT** use it in production environment!
+
+> [!NOTE]
+> 如果多语言文件与 中文 (简体) 冲突或差异, 以 中文 (简体) 为准
+> 
+> If multi-language files conflict or different from Chinese (Simplified), in Chinese (Simplified), prevail
+
+### Please choose your language which you want to read
+* [zh-CN, 中文 (简体), Chinese](./docs/zh-CN/README.md)
---
+++ packages/jsonable-platform/jsonable_platform-0.0.4.20240324122753.tar.gz/jsonable_platform-0.0.4.20240324122753/jsonable_platform/type.py
@@ -0,0 +1,85 @@
+from sys import version_info
+from typing import TypedDict, TypeAlias, TypeVar, Literal, Union, Generic, Any, Callable, Iterable
+
+JSONAbleClassID: TypeAlias = str
+
+JSONSupportedBases: TypeAlias = Union[bytes, str, int, float, bool, None]
+JSONSupportedIterables: TypeAlias = Union[
+    list[Union[JSONSupportedBases, 'JSONSupportedIterables', 'JSONAbleABC']],
+    tuple[Union[JSONSupportedBases, 'JSONSupportedIterables', 'JSONAbleABC']],
+    dict[
+        Union[JSONSupportedBases, 'JSONSupportedIterables'],
+        Union[JSONSupportedBases, 'JSONSupportedIterables', 'JSONAbleABC']
+    ]
+]
+JSONSupportedEditableIters: TypeAlias = Union[list, dict]
+
+JSONSupportedTypes: TypeAlias = Union[JSONSupportedBases, JSONSupportedIterables]
+
+# `Self` is Python 3.11+ Only (https://docs.python.org/zh-cn/3/library/typing.html#typing.Self)
+if version_info >= (3, 11):
+    from typing import Self  # type: ignore
+else:
+    from typing_extensions import Self
+
+
+class JSONAbleABC:
+    @classmethod
+    def __jsonable_hash__(cls) -> str | None:
+        """
+        Get the hash of the jsonable object
+        :return: Any str or None. if return None, will use the `__name__` of your class
+        """
+        return
+
+    @classmethod
+    def __jsonable_encode__(cls, obj: Self) -> JSONSupportedTypes:
+        """
+        Encode to json, return native jsonable type
+        :parma obj: Any Python object you want to encode
+        :return: *The type of return is the same as the `obj` param at function `__jsonable_decode__`
+        """
+        raise NotImplemented
+
+    @classmethod
+    def __jsonable_decode__(cls, data: JSONSupportedTypes) -> Self:
+        """
+        Decode from json, return any Python object
+        :param data: The data of the object, as same as the return of function `__jsonable_encode__`
+        :return: Any Python object as same as given at the `obj` param at function `__jsonable_encode__`
+        """
+        raise NotImplemented
+
+
+JSONAbleABCType = type[JSONAbleABC]
+RequirementsType = Iterable[JSONAbleABCType]
+
+
+class DefinedClassesData(TypedDict):
+    cls: JSONAbleABCType
+    requirements: RequirementsType
+
+
+class DefinedClasses(TypedDict):
+    defaults: dict[JSONAbleClassID, DefinedClassesData]
+    customs: dict[JSONAbleClassID, DefinedClassesData]
+
+
+DefinedClassesKeysType = Literal['defaults', 'customs']
+
+
+class JSONAbleEncodedDict(TypedDict):
+    hash: str
+    data: JSONSupportedTypes
+    source: DefinedClassesKeysType
+
+
+class RequiredEncodedDict(JSONAbleEncodedDict):
+    parent: str
+
+
+JSONAbleEncodedType = Union[JSONAbleABCEncodedType, RequiredEncodedDict]
+
+HashMethods = Literal['default', 'custom']
+EncoderFallbackType = Callable[[Any], JSONAbleEncodedDict]
+DecoderFallbackType = Callable[[JSONAbleEncodedDict], Any]
