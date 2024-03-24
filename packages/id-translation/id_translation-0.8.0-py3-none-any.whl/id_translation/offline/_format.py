227fb3e1a8445200a72558b24ee453015a45bbeb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-24 03:03:01 +0000

    Add package id-translation/0.8.0/id_translation-0.8.0-py3-none-any.whl

---
+++ packages/id-translation/id_translation-0.8.0-py3-none-any.whl/id_translation/offline/_format.py
@@ -0,0 +1,146 @@
+from collections.abc import Iterable, Mapping
+from typing import Any
+
+from rics.misc import tname
+
+from . import parse_format_string
+from .types import FormatType, PlaceholdersTuple
+
+
+class Format:
+    r"""Format specification for translations strings.
+
+    Translation formats are similar to regular f-strings, with two important exceptions:
+
+    1. Positional placeholders (``'{}'``) may not be used; correct form is ``'{placeholder-name}'``.
+    2. Placeholders surrounded by ``'[]'`` denote an optional element. Optional elements are rendered...
+
+       * Only if `all` of its placeholders are defined.
+       * Without delimiting brackets.
+       * As literal text (with brackets) if there is no placeholder in the block.
+
+    .. hint::
+
+       Double the wanted bracket character to render as a literal, analogous to ``'{{'`` and ``'}}'``
+       in plain Python f-strings. See the example below for a demonstration.
+
+    Args:
+        fmt: A translation fstring.
+
+    Examples:
+        A format string using literal angle brackets and an optional element.
+
+        >>> from id_translation.offline import Format
+        >>> fmt = Format("{id}:[[{name}]][, nice={is_nice}]")
+
+        The ``Format`` class when used directly only returns required placeholders by default...
+
+        >>> fmt.fstring()
+        '{id}:[{name}]'
+        >>> fmt.fstring().format(id=0, name="Tarzan")
+        '0:[Tarzan]'
+
+        ...but the :attr:`placeholders` attribute can be used to retrieve all placeholders, required and optional:
+
+        >>> fmt.placeholders
+        ('id', 'name', 'is_nice')
+        >>> fmt.fstring(fmt.placeholders).format(id=1, name="Morris", is_nice=True)
+        '1:[Morris], nice=True'
+
+    The :class:`.Translator` will automatically add optional placeholders, if they are present in the source.
+
+    .. note::
+       Python format specifications and conversions are preserved.
+
+    This is especially useful for long values such as UUIDs.
+
+    >>> from uuid import UUID
+    >>> uuid = UUID("550e8400-e29b-41d4-a716-446655440000")
+
+    Convert to string and truncate to eight characters.
+
+    >>> Format("{id!s:.8}:{name!r}").fstring().format(id=uuid, name="Sofia")
+    "550e8400:'Sofia'"
+
+    See the official :py:ref:`formatspec` documentation for details.
+    """
+
+    def __init__(self, fmt: str) -> None:
+        self._fmt = fmt
+        self._elements: list[parse_format_string.Element] = parse_format_string.get_elements(fmt)
+
+    def fstring(self, placeholders: Iterable[str] | None = None, *, positional: bool = False) -> str:
+        """Create a format string for the given placeholders.
+
+        Args:
+            placeholders: Keys to keep. Passing ``None`` is equivalent to passing :attr:`required_placeholders`.
+            positional: If ``True``, remove names to return a positional fstring.
+
+        Returns:
+            An fstring with optional elements removed unless included in `placeholders`.
+
+        Raises:
+            KeyError: If required placeholders are missing.
+        """
+        placeholders = placeholders or self.required_placeholders
+        missing_required_placeholders = set(self.required_placeholders).difference(placeholders)
+        if missing_required_placeholders:
+            raise KeyError(f"Required key(s) {missing_required_placeholders} missing from {placeholders=}.")
+
+        return self._make_fstring(placeholders, positional=positional)
+
+    def _make_fstring(self, placeholders: Iterable[str], positional: bool) -> str:
+        def predicate(e: parse_format_string.Element) -> bool:
+            return e.required or set(placeholders).issuperset(e.placeholders)
+
+        return "".join(e.positional_part if positional else e.part for e in filter(predicate, self._elements))
+
+    def partial(self, defaults: Mapping[str, Any]) -> "Format":
+        """Get a partially formatted :meth:`fstring`.
+
+        Args:
+            defaults: Keys which should be replaced with real values. Keys which are **not** part of `defaults` will
+                be left as-is.
+
+        Returns:
+            A partially formatted fstring.
+        """
+        parts, _ = parse_format_string.Element.parse_block(self._fmt, defaults=defaults)
+        return Format("".join(parts))
+
+    @staticmethod
+    def parse(fmt: FormatType) -> "Format":
+        """Parse a format.
+
+        Args:
+            fmt: Input to parse.
+
+        Returns:
+            A ``Format`` instance.
+        """
+        return fmt if isinstance(fmt, Format) else Format(fmt)
+
+    @property
+    def placeholders(self) -> PlaceholdersTuple:
+        """All placeholders in the order in which they appear."""
+        return self._extract_placeholders(self._elements)
+
+    @property
+    def required_placeholders(self) -> PlaceholdersTuple:
+        """All required placeholders in the order in which they appear."""
+        return self._extract_placeholders(filter(lambda e: e.required, self._elements))
+
+    @property
+    def optional_placeholders(self) -> PlaceholdersTuple:  # pragma: no cover
+        """All optional placeholders in the order in which they appear."""
+        return self._extract_placeholders(filter(lambda e: not e.required, self._elements))
+
+    @staticmethod
+    def _extract_placeholders(elements: Iterable[parse_format_string.Element]) -> PlaceholdersTuple:
+        ans = []
+        for e in elements:
+            ans.extend(e.placeholders)
+        return tuple(ans)
+
+    def __repr__(self) -> str:
+        return f"{tname(self)}({self._fmt!r})"
---
+++ packages/id-translation/id_translation-0.8.0-py3-none-any.whl/id_translation/offline/_magic_dict.py
@@ -0,0 +1,149 @@
+import logging
+from collections.abc import Iterator, MutableMapping
+from typing import Any
+
+from rics.misc import tname
+
+from .. import _uuid_utils
+from ..transform.types import Transformer, TransformerStop
+from ..types import IdType
+from .types import TranslatedIds
+
+
+class MagicDict(MutableMapping[IdType, str]):
+    """Dictionary type for translated IDs.
+
+    If `default_value` is given, it is used as the default answer for any calls to ``__getitem__`` where the key is
+    not in `translated_ids`.
+
+    Args:
+        real_translations: A dict holding real translations.
+        default_value: A string with exactly one or zero placeholders.
+        enable_uuid_heuristics: Enabling may improve matching when :py:class:`~uuid.UUID`-like IDs are in use.
+        transformer: Initialized :class:`.Transformer` instance.
+
+    Examples:
+        Behaviour with :attr:`default_value`.
+
+        >>> magic = MagicDict(
+        ...     {1999: "1999:Sofia", 1991: "1991:Richard"},
+        ...     default_value="<Failed: id={!r}>",
+        ... )
+        >>> magic
+        {1999: '1999:Sofia', 1991: '1991:Richard'}
+
+        Calls to ``__getitem__`` and ``__contains__`` will never return ``False``.
+
+        >>> magic[1999], 1999 in magic
+        ('1999:Sofia', True)
+        >>> magic["1999"], "1999" in magic
+        ("<Failed: id='1999'>", True)
+
+        Special handling for :py:class:`uuid.UUID` and ``UUID``-like strings improve matching.
+
+        >>> from uuid import UUID
+        >>> string_uuid = "550e8400-e29b-41d4-a716-446655440000"
+        >>> magic = MagicDict(
+        ...     {string_uuid: "Found!"},
+        ...     enable_uuid_heuristics=True,
+        ... )
+        >>> magic
+        {UUID('550e8400-e29b-41d4-a716-446655440000'): 'Found!'}
+
+        >>> magic[string_uuid], magic[UUID(string_uuid)]
+        ('Found!', 'Found!')
+
+        Converting to a regular ``dict``.
+
+        >>> dict(magic)
+        {UUID('550e8400-e29b-41d4-a716-446655440000'): 'Found!'}
+
+        Casting to ``dict`` removes all special handling.
+    """
+
+    LOGGER = logging.getLogger(__package__).getChild("MagicDict")
+
+    def __init__(
+        self,
+        real_translations: TranslatedIds[IdType],
+        default_value: str | None = None,
+        enable_uuid_heuristics: bool = True,
+        transformer: Transformer[IdType] = None,
+    ) -> None:
+        if enable_uuid_heuristics and real_translations:
+            real_translations, enable_uuid_heuristics = _try_stringify_many(real_translations)
+
+        self._real: TranslatedIds[IdType] = real_translations
+        self._default = default_value
+        self._cast_key = enable_uuid_heuristics
+
+        self._try_add_missing_key = None
+        if transformer is not None:
+            transformer.update_translations(real_translations)
+            self._try_add_missing_key = transformer.try_add_missing_key
+
+    @property
+    def default_value(self) -> str | None:
+        """Return the default string value to return for unknown keys, if any."""
+        return self._default
+
+    def _try_stringify(self, key: IdType) -> Any:
+        return _uuid_utils.try_cast_one(key) if self._cast_key else key
+
+    def __getitem__(self, key: IdType) -> str:
+        key = self._on_read(key)
+        if key in self._real or self.default_value is None:
+            return self._real[key]
+
+        return self.default_value.format(key)
+
+    def __contains__(self, key: Any) -> bool:
+        key = self._on_read(key)
+        return self.default_value is not None or key in self._real
+
+    def _on_read(self, key: IdType) -> IdType:
+        key = self._try_stringify(key)
+        if self._try_add_missing_key and key not in self._real:
+            try:
+                self._try_add_missing_key(key, translations=self)
+            except TransformerStop as e:
+                if self.LOGGER.isEnabledFor(logging.DEBUG):
+                    call = f"{tname(self._try_add_missing_key, prefix_classname=True)}({key!r}, translations=self)"
+                    self.LOGGER.debug(f"try_add_missing_key: {call} raised {e!r}. Dropping this callback function.")
+                self._try_add_missing_key = None
+        return key
+
+    def __setitem__(self, key: IdType, value: str) -> None:
+        key = self._on_write(key)
+        self._real[key] = value
+
+    def __delitem__(self, key: IdType) -> None:
+        key = self._on_write(key)
+        del self._real[key]
+
+    def _on_write(self, key: IdType) -> IdType:
+        key = self._try_stringify(key)
+        return key
+
+    def __len__(self) -> int:
+        return len(self._real)
+
+    def __iter__(self) -> Iterator[IdType]:
+        return iter(self._real)
+
+    def __repr__(self) -> str:
+        return repr(self._real)
+
+
+def _try_stringify_many(real_translations: TranslatedIds[IdType]) -> tuple[TranslatedIds[IdType], bool]:
+    original_ids = list(real_translations)
+    try:
+        new_keys = _uuid_utils.cast_many(original_ids)
+    except (ValueError, AttributeError, TypeError):
+        return real_translations, False  # Keys are not UUID-like.
+
+    uuid_translations = {uuid: real_translations[idx] for uuid, idx in zip(new_keys, original_ids)}
+    if len(real_translations) != len(uuid_translations):
+        raise TypeError("Duplicate UUIDs found. Verify translation sources or set enable_uuid_heuristics=False.")
+
+    return uuid_translations, True
