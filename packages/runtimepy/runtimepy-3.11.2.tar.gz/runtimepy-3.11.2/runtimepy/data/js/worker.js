8b84fff39aab85423c4447b076799fb9d29f8e18
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-24 14:03:06 +0000

    Add package runtimepy/3.11.2/runtimepy-3.11.2.tar.gz

---
+++ packages/runtimepy/runtimepy-3.11.2.tar.gz/runtimepy-3.11.2/runtimepy/data/js/worker.js
@@ -0,0 +1,37 @@
+function create_connections(config) {
+  /* This business logic could use some work. */
+  const conn_factories = {"json" : JsonConnection, "data" : DataConnection};
+
+  let worker_cfg = config["worker"];
+  let conns = {};
+  for (let name in conn_factories) {
+    conns[name] = new conn_factories[name](name, worker_cfg[name]);
+  }
+
+  return conns;
+}
+
+/* Worker entry. */
+async function start(config) {
+  console.log(config);
+
+  conns = create_connections(config);
+
+  /* Wait for both connections to be established. */
+  for (const key in conns) {
+    await conns[key].connected;
+  }
+
+  /* Forward all other messages to the server. */
+  onmessage =
+      async (event) => { conns["json"].send_json({"ui" : event.data}); };
+
+  /* Add message handler to forward UI messages to the main thread. */
+  conns["json"].message_handlers["ui"] = (data) => { postMessage(data); };
+
+  /* Tell main thread we're ready to go. */
+  postMessage(0);
+}
+
+/* Handle first message from the main thread. */
+onmessage = async (event) => { await start(event.data); };
---
+++ packages/runtimepy/runtimepy-3.11.2.tar.gz/runtimepy-3.11.2/runtimepy/mixins/environment.py
@@ -0,0 +1,48 @@
+"""
+A module implementing a channel-environment class mixin.
+"""
+
+# internal
+from runtimepy import METRICS_NAME
+from runtimepy.channel.environment import ChannelEnvironment
+from runtimepy.metrics import ConnectionMetrics, PeriodicTaskMetrics
+
+
+class ChannelEnvironmentMixin:
+    """A simple channel-environment mixin."""
+
+    env: ChannelEnvironment
+
+    def __init__(self, env: ChannelEnvironment = None, **kwargs) -> None:
+        """Initialize this instance."""
+
+        if not hasattr(self, "env"):
+            if env is None:
+                env = ChannelEnvironment(**kwargs)
+            self.env = env
+
+    def register_task_metrics(
+        self, metrics: PeriodicTaskMetrics, namespace: str = METRICS_NAME
+    ) -> None:
+        """Register periodic task metrics."""
+
+        with self.env.names_pushed(namespace):
+            self.env.channel("dispatches", metrics.dispatches)
+            self.env.channel("rate_hz", metrics.rate_hz)
+            self.env.channel("average_s", metrics.average_s)
+            self.env.channel("max_s", metrics.max_s)
+            self.env.channel("min_s", metrics.min_s)
+            self.env.channel("overruns", metrics.overruns)
+
+    def register_connection_metrics(
+        self, metrics: ConnectionMetrics, namespace: str = METRICS_NAME
+    ) -> None:
+        """Register connection metrics."""
+
+        with self.env.names_pushed(namespace):
+            for name, direction in [("tx", metrics.tx), ("rx", metrics.rx)]:
+                with self.env.names_pushed(name):
+                    self.env.channel("messages", direction.messages)
+                    self.env.channel("messages_rate", direction.message_rate)
+                    self.env.channel("bytes", direction.bytes)
+                    self.env.channel("kbps", direction.kbps)
