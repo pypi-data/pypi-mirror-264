2dc6c421bf400ead565ecb2da14213f01a41f153
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-24 03:04:47 +0000

    Add package unibo-dtm-se-calculator/0.1.1/unibo_dtm_se_calculator-0.1.1.tar.gz

---
+++ packages/unibo-dtm-se-calculator/unibo_dtm_se_calculator-0.1.1.tar.gz/unibo_dtm_se_calculator-0.1.1/calculator/ui/cli.py
@@ -0,0 +1,31 @@
+from calculator import Calculator
+import sys
+
+
+class CalculatorCLI:
+    def __init__(self, args, channel=sys.stdout):
+        self._args = args
+        self._calc = Calculator()
+        self._channel = channel
+
+    def _print(self, *args, **kwargs):
+        print(*args, **kwargs, file=self._channel)
+
+    def run(self):
+        if not self._args:
+            self._print("Usage: python -m calculator.ui.cli <expression>")
+            return
+        self._calc.expression = " ".join(self._args)
+        try:
+            result = self._calc.compute_result()
+            self._print(result)
+        except ValueError as e:
+            self._print(e)
+
+
+def start_app():
+    CalculatorCLI(sys.argv[1:]).run()
+
+
+if __name__ == '__main__':
+    start_app()
