42b027d0d017ef95d37757a7442ac73f2b22a74b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-24 04:03:12 +0000

    Add package recom/0.0.1/recom-0.0.1.tar.gz

---
+++ packages/recom/recom-0.0.1.tar.gz/recom-0.0.1/recom/__init__.py
@@ -0,0 +1,6 @@
+from recom.device import RecomDevice, DeviceException
+from recom.interface import RecomInterface
+
+__version__ = "0.0.1"   # This will be replaced by setuptools_scm during installation
+
+__all__ = ["RecomDevice", "RecomInterface", "DeviceException"]
