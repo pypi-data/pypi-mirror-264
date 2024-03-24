bd16b79fcae9033a0133b8a4978485b22fe5893e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-24 04:03:04 +0000

    Add package imswitchclient/0.1.1/imswitchclient-0.1.1.tar.gz

---
+++ packages/imswitchclient/imswitchclient-0.1.1.tar.gz/imswitchclient-0.1.1/imswitchclient/__init__.py
@@ -0,0 +1,7 @@
+"""Top-level package for ImSwitchClient."""
+from imswitchclient import ImSwitchClient
+from imswitchclient import positionersManager
+
+__author__ = """Benedict Diederich"""
+__email__ = 'benedictdied@gmail.com'
+__version__ = "v0.1.1"#'0.1.1'
