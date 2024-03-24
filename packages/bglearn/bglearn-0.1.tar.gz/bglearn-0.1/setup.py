f2a98ea7a0955c07cc329be8cd64162dbc308db4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-24 03:03:00 +0000

    Add package bglearn/0.1/bglearn-0.1.tar.gz

---
+++ packages/bglearn/bglearn-0.1.tar.gz/bglearn-0.1/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='bglearn',
+    version='0.1',
+    packages=find_packages(),
+    install_requires=[
+        'numpy',
+    ],
+    author='gautham-balraj',
+    author_email='gauthambalraj07@email.com',
+    description='A custom implementation of Random Forest Classifier',
+    url='',
+    license='MIT',  # Change this to the appropriate license
+)
