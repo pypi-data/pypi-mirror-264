8e5ebd87895d9e555c0b7cc011087d0034c11fe2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-24 03:03:05 +0000

    Add package cognite-pygen/0.99.15/cognite_pygen-0.99.15-py3-none-any.whl

---
+++ packages/cognite-pygen/cognite_pygen-0.99.15-py3-none-any.whl/cognite/pygen/__init__.py
@@ -0,0 +1,15 @@
+"""
+This is the main entry point for the pygen package. It contains the main functions for
+generating SDKs.
+"""
+
+from ._generator import generate_sdk, generate_sdk_notebook
+from ._version import __version__
+from .utils.cdf import load_cognite_client_from_toml
+
+__all__ = [
+    "__version__",
+    "generate_sdk",
+    "generate_sdk_notebook",
+    "load_cognite_client_from_toml",
+]
