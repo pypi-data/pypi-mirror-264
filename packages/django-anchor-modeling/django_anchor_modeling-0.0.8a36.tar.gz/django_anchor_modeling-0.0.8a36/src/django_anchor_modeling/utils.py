762b0021e169785583801918a1070a74db6032e9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-24 15:03:24 +0000

    Add package django-anchor-modeling/0.0.8a36/django_anchor_modeling-0.0.8a36.tar.gz

---
+++ packages/django-anchor-modeling/django_anchor_modeling-0.0.8a36.tar.gz/django_anchor_modeling-0.0.8a36/src/django_anchor_modeling/utils.py
@@ -0,0 +1,38 @@
+from django_anchor_modeling import constants
+from django_anchor_modeling.exceptions import SentinelUserDoesNotExist
+
+
+def get_knot_choice_value(instance: object):
+    """
+    Given any instance of a model, return the knot choice value
+    format is
+
+    APP__CLASS
+    """
+    app_label = instance._meta.app_label
+    class_name = instance.__class__.__name__
+
+    return f"{app_label}__{class_name}"
+
+
+# utils.py or a similar module
+from django.conf import settings
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
+
+SENTINEL_NULL_USER_ID = getattr(
+    settings, "SENTINEL_NULL_USER_ID", constants.SENTINEL_NULL_USER_ID
+)
+
+
+def get_sentinel_user():
+    sentinel_user, _ = User.objects.get_or_create(pk=SENTINEL_NULL_USER_ID)
+    return sentinel_user
+
+
+def get_sentinel_user_id():
+    if User.objects.filter(pk=SENTINEL_NULL_USER_ID).exists():
+        return SENTINEL_NULL_USER_ID
+    else:
+        raise SentinelUserDoesNotExist()
