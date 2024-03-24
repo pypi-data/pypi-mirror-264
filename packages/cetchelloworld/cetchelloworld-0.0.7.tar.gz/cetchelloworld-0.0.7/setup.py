7d58958a2cb2e23b6ea3603bae569421a6b743e0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-24 14:03:31 +0000

    Add package cetchelloworld/0.0.7/cetchelloworld-0.0.7.tar.gz

---
+++ packages/cetchelloworld/cetchelloworld-0.0.7.tar.gz/cetchelloworld-0.0.7/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name = 'cetchelloworld',
+    version = '0.0.7',
+    package = find_packages(),
+    install_requires = [],
+    include_package_data=True,
+    packages=["cetchelloworld"]
+)
