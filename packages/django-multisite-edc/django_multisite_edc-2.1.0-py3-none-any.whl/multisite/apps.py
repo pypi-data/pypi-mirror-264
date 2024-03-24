62c416aa51733e4fbad0b257357dd97e4d25b4de
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-24 14:02:44 +0000

    Add package django-multisite-edc/2.1.0/django_multisite_edc-2.1.0-py3-none-any.whl

---
+++ packages/django-multisite-edc/django_multisite_edc-2.1.0-py3-none-any.whl/multisite/apps.py
@@ -0,0 +1,28 @@
+from django.apps import AppConfig as DjangoAppConfig
+from django.db.models.signals import post_migrate
+
+from multisite.hacks import use_framework_for_site_cache
+from multisite.utils import create_or_sync_canonical_from_all_sites
+
+
+def post_migrate_sync_alias(apps=None, **kwargs):
+    """Syncs canonical Alias objects for all existing Site objects."""
+    create_or_sync_canonical_from_all_sites(apps)
+
+
+class AppConfig(DjangoAppConfig):
+    name = "multisite"
+    verbose_name = "Multisite"
+    default_auto_field = "django.db.models.BigAutoField"
+
+    def import_models(self):
+        use_framework_for_site_cache()
+        super().import_models()
+
+    def ready(self):
+
+        post_migrate.connect(
+            post_migrate_sync_alias,
+            sender=self,
+            dispatch_uid="django.contrib.auth.management.create_permissions",
+        )
