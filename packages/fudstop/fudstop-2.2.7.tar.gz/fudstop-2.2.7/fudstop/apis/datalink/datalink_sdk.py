ef6f4b640cd986a56da8f124508689d029695ef7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-24 14:03:09 +0000

    Add package fudstop/2.2.7/fudstop-2.2.7.tar.gz

---
+++ packages/fudstop/fudstop-2.2.7.tar.gz/fudstop-2.2.7/fudstop/apis/datalink/datalink_sdk.py
@@ -0,0 +1,65 @@
+import os
+import sys
+from pathlib import Path
+
+import json
+# Add the project directory to the sys.path
+project_dir = str(Path(__file__).resolve().parents[1])
+if project_dir not in sys.path:
+    sys.path.append(project_dir)
+from dotenv import load_dotenv
+load_dotenv()
+import asyncio
+from datetime import datetime, timedelta
+from _asyncpg.asyncpg_sdk import AsyncpgSDK
+import pandas as pd
+import httpx
+class DataLinkSDK:
+    def __init__(self):
+        self.key = os.environ.get('YOUR_NASDAQ_KEY')
+
+        self.today = datetime.now().strftime('%Y-%m-%d')
+        self.yesterday = (datetime.now() - timedelta(days=1)).strftime('%Y-%m-%d')
+        self.tomorrow = (datetime.now() + timedelta(days=1)).strftime('%Y-%m-%d')
+        self.db = AsyncpgSDK()
+
+    async def option_rating(self, ticker:str, date:str=None, insert:bool=False):
+        if date == None:
+            date = self.yesterday
+
+        endpoint = f"https://data.nasdaq.com/api/v3/datatables/QUANTCHA/QOR?date={date}&ticker={ticker}&api_key={self.key}"
+
+
+        async with httpx.AsyncClient() as client:
+            data = await client.get(endpoint)
+            datas = data.json()
+
+            # Assuming 'datas' is your input data structure
+            datatable = datas['datatable'] if 'datatable' in datas else {}
+            data = datatable.get('data', [])
+            columns = datatable.get('columns', [])
+            column_names = [column.get('name') for column in columns]
+
+            # Create the DataFrame
+            df = pd.DataFrame(data, columns=column_names)
+            if insert == True:
+                await self.db.connect()
+                await self.db.batch_insert_dataframe(df, table_name='ivcrush', unique_columns='ticker, date')
+            return df
+        
+        
+            
+
+
+            
+data = DataLinkSDK()
+async def main():
+    x = await data.option_rating('AAPL', insert=True)
+
+    print(x)
+
+    x.to_csv('test.csv', index=False)
+
+
+
+asyncio.run(main())
