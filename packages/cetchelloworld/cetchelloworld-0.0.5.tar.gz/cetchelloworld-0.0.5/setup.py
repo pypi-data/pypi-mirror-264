4f52c629494ec2b9d8cbaf7c9a1fe3e3577660c6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-24 14:03:10 +0000

    Add package cetchelloworld/0.0.5/cetchelloworld-0.0.5.tar.gz

---
+++ packages/cetchelloworld/cetchelloworld-0.0.5.tar.gz/cetchelloworld-0.0.5/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name = 'cetchelloworld',
+    version = '0.0.5',
+    package = find_packages(),
+    install_requires = [],
+    include_package_data=True,
+)
