227d921d51b75d60a6f044d49b7c886273d1195b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-24 15:03:53 +0000

    Add package streamcontroller-plugin-tools/1.0.5/streamcontroller-plugin-tools-1.0.5.tar.gz

---
+++ packages/streamcontroller-plugin-tools/streamcontroller-plugin-tools-1.0.5.tar.gz/streamcontroller-plugin-tools-1.0.5/setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup, find_packages
+
+VERSION = '1.0.5'
+DESCRIPTION = 'Base for StreamController plugins'
+LONG_DESCRIPTION = 'This package contains the base for StreamController plugins.'
+
+# Setting up
+setup(
+       # the name must match the folder name 'verysimplemodule'
+        name="streamcontroller-plugin-tools", 
+        version=VERSION,
+        author="Core447",
+        author_email="core447@proton.me",
+        description=DESCRIPTION,
+        long_description=LONG_DESCRIPTION,
+        packages=find_packages(),
+        install_requires=["Pyro5", "loguru"],
+        keywords=['python', 'StreamController', "plugin"],
+        classifiers= [
+            "Development Status :: 4 - Beta",
+            "Environment :: Plugins",
+            "Intended Audience :: Developers",
+            "Programming Language :: Python :: 3 :: Only",
+            "Operating System :: Unix",
+            "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+            "Natural Language :: English"
+        ],
+        url="https://github.com/StreamController/streamcontroller-plugin-tools",
+)
