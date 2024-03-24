228e36ee73d5f7b46f368dcc24a1a1f251c43593
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-24 03:03:30 +0000

    Add package neogit/0.1/neogit-0.1.tar.gz

---
+++ packages/neogit/neogit-0.1.tar.gz/neogit-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(
+    name="neogit",
+    version="0.1",
+    author="Gokul Raj V",
+    author_email="gokulrajv108@gmail.com",
+    description="Next generation Git",
+)
