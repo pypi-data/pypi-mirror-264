741927e104687f3db1201ca51f5d3c188a8934c3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-24 03:02:40 +0000

    Add package mini-toolbox/0.0.5/mini_toolbox-0.0.5.tar.gz

---
+++ packages/mini-toolbox/mini_toolbox-0.0.5.tar.gz/mini_toolbox-0.0.5/tests/test_install.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+# -*- coding:utf-8 -*-
+# @Mail: haha@haha.com
+# @File: tests/test_install.py
+# @Date: 2024-03-19 10:42:12
+# @Desc:
+
+from mini_toolbox import utils
+
+
+def test_haha():
+    pass
