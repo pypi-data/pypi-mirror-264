323de2d314ce65daf54ac0b721031970a3b7360d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-24 15:03:52 +0000

    Add package busker/0.5.0/busker-0.5.0.tar.gz

---
+++ packages/busker/busker-0.5.0.tar.gz/busker-0.5.0/README.md
@@ -0,0 +1,22 @@
+Busker
+======
+
+Operation
+---------
+
+Python 3.11 or later is required.
+
+This is a work-in-progress.
+
+    usage: main.py [-h] [--config CONFIG] [--url URL] [--number NUMBER]
+
+    options:
+      -h, --help       show this help message and exit
+
+    Settings:
+      --config CONFIG  Set a path to a configuration file [/home/user/busker.toml].
+
+    Plugins:
+      --url URL        Set url path to begin session.
+      --number NUMBER  Set the number of times to run the plugin.
+
---
+++ packages/busker/busker-0.5.0.tar.gz/busker-0.5.0/busker/runner.py
@@ -0,0 +1,79 @@
+#!/usr/bin/env python3
+#   encoding: utf-8
+
+# This is part of the Busker library.
+# Copyright (C) 2024 D E Haynes
+
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as
+# published by the Free Software Foundation, either version 3 of the
+# License, or (at your option) any later version.
+
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+from collections import defaultdict
+from collections import deque
+import concurrent.futures
+import logging
+import pathlib
+import sys
+import time
+import tomllib
+import venv
+
+
+from busker.types import ExecutionEnvironment
+
+
+class Runner:
+
+    @staticmethod
+    def walk_files(path: pathlib.Path, callback=None):
+        callback = callback or pathlib.Path
+        if path.is_dir():
+            try:
+                for p in path.iterdir():
+                    yield from Runner.walk_files(pathlib.Path(p))
+            except Exception:
+                pass
+        yield callback(path)
+
+    @property
+    def jobs(self) -> list:
+        return []
+
+
+class VirtualEnv(Runner):
+
+    def __init__(self, location: pathlib.Path):
+        self.location = location
+
+    def build_virtualenv(self, exenv: ExecutionEnvironment, **kwargs):
+        venv.create(
+            self.location,
+            system_site_packages=True,
+            clear=True,
+            with_pip=True,
+            upgrade_deps=True
+        )
+
+    def check_virtualenv(self, exenv: ExecutionEnvironment, repeat=100, interval=2, **kwargs):
+        values = []
+        while len(values) < repeat:
+            files = list(self.walk_files(self.location))
+            values.append(len(files))
+            exenv.queue.put(values[-1])
+            if len(values) > 3 and values[-3] == values[-1] and values[-1] <= max(values):
+                break
+            else:
+                time.sleep(interval)
+
+    @property
+    def jobs(self) -> list:
+        return [self.build_virtualenv, self.check_virtualenv]
