88ac4607b87661662abe169628cb72a78b8911d7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-24 15:03:27 +0000

    Add package DbUnify/1.0.5/DbUnify-1.0.5-py3-none-any.whl

---
+++ packages/DbUnify/DbUnify-1.0.5-py3-none-any.whl/DbUnify/__init__.py
@@ -0,0 +1,10 @@
+# from .aio.SQLite3.Manager.Manager import Manager
+# from .aio.SQLite3.Raw.Raw import Raw
+# from .aio.SQLite3.Exporter.Exporter import Exporter
+
+# from .sync.SQLite3.Manager.Manager import Manager
+# from .sync.SQLite3.Raw.Raw import Raw
+# from .sync.SQLite3.Exporter.Exporter import Exporter
+
+from .aio import *
+from .sync import *
