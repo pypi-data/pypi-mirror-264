e15ec17e292cde20c6f8fe578cda9764914de5a0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-24 15:03:42 +0000

    Add package swarms/4.5.1/swarms-4.5.1.tar.gz

---
+++ packages/swarms/swarms-4.5.1.tar.gz/swarms-4.5.1/swarms/models/__init__.py
@@ -0,0 +1,106 @@
+from swarms.models.base_embedding_model import BaseEmbeddingModel
+from swarms.models.base_llm import AbstractLLM  # noqa: E402
+from swarms.models.base_multimodal_model import BaseMultiModalModel
+from swarms.models.biogpt import BioGPT  # noqa: E402
+from swarms.models.clipq import CLIPQ  # noqa: E402
+from swarms.models.fire_function import FireFunctionCaller
+from swarms.models.fuyu import Fuyu  # noqa: E402
+from swarms.models.gemini import Gemini  # noqa: E402
+from swarms.models.gpt4_vision_api import GPT4VisionAPI  # noqa: E402
+from swarms.models.huggingface import HuggingfaceLLM  # noqa: E402
+from swarms.models.idefics import Idefics  # noqa: E402
+from swarms.models.kosmos_two import Kosmos  # noqa: E402
+from swarms.models.layoutlm_document_qa import LayoutLMDocumentQA
+from swarms.models.llava import LavaMultiModal  # noqa: E402
+from swarms.models.mistral import Mistral  # noqa: E402
+from swarms.models.mixtral import Mixtral  # noqa: E402
+from swarms.models.mpt import MPT7B  # noqa: E402
+from swarms.models.nougat import Nougat  # noqa: E402
+from swarms.models.openai_tts import OpenAITTS  # noqa: E402
+from swarms.models.petals import Petals  # noqa: E402
+from swarms.models.popular_llms import (
+    AnthropicChat as Anthropic,
+)
+from swarms.models.popular_llms import (
+    AzureOpenAILLM as AzureOpenAI,
+)
+from swarms.models.popular_llms import (
+    CohereChat as Cohere,
+)
+from swarms.models.popular_llms import (
+    MosaicMLChat as MosaicML,
+)
+from swarms.models.popular_llms import (
+    OpenAIChatLLM as OpenAIChat,
+)
+from swarms.models.popular_llms import (
+    OpenAILLM as OpenAI,
+)
+from swarms.models.popular_llms import (
+    ReplicateLLM as Replicate,
+)
+from swarms.models.qwen import QwenVLMultiModal  # noqa: E402
+
+# from swarms.models.roboflow_model import RoboflowMultiModal
+from swarms.models.sam_supervision import SegmentAnythingMarkGenerator
+from swarms.models.sampling_params import SamplingParams, SamplingType
+from swarms.models.timm import TimmModel  # noqa: E402
+from swarms.models.together import TogetherLLM  # noqa: E402
+from swarms.models.types import (  # noqa: E402
+    AudioModality,
+    ImageModality,
+    MultimodalData,
+    TextModality,
+    VideoModality,
+)
+
+# from swarms.models.ultralytics_model import UltralyticsModel
+from swarms.models.vilt import Vilt  # noqa: E402
+from swarms.models.wizard_storytelling import WizardLLMStoryTeller
+from swarms.models.zephyr import Zephyr  # noqa: E402
+from swarms.models.zeroscope import ZeroscopeTTV  # noqa: E402
+
+__all__ = [
+    "AbstractLLM",
+    "Anthropic",
+    "AzureOpenAI",
+    "BaseEmbeddingModel",
+    "BaseMultiModalModel",
+    "BioGPT",
+    "CLIPQ",
+    "Cohere",
+    "FireFunctionCaller",
+    "Fuyu",
+    "GPT4VisionAPI",
+    "Gemini",
+    "HuggingfaceLLM",
+    "Idefics",
+    "Kosmos",
+    "LayoutLMDocumentQA",
+    "LavaMultiModal",
+    "Replicate",
+    "MPT7B",
+    "Mistral",
+    "Mixtral",
+    "MosaicML",
+    "Nougat",
+    "OpenAI",
+    "OpenAIChat",
+    "OpenAITTS",
+    "Petals",
+    "QwenVLMultiModal",
+    "SamplingParams",
+    "SamplingType",
+    "SegmentAnythingMarkGenerator",
+    "TextModality",
+    "TimmModel",
+    "TogetherLLM",
+    "Vilt",
+    "VideoModality",
+    "WizardLLMStoryTeller",
+    "Zephyr",
+    "ZeroscopeTTV",
+    "AudioModality",
+    "ImageModality",
+    "MultimodalData",
+]
