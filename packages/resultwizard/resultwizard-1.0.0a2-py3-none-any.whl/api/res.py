19648badb1cb72b75c9f4efbc522c960475aa8f9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-24 15:03:53 +0000

    Add package resultwizard/1.0.0a2/resultwizard-1.0.0a2-py3-none-any.whl

---
+++ packages/resultwizard/resultwizard-1.0.0a2-py3-none-any.whl/api/res.py
@@ -0,0 +1,136 @@
+from decimal import Decimal
+from typing import Union, List, Tuple
+from plum import dispatch, overload
+
+from api.printable_result import PrintableResult
+from api import parsers
+from application.cache import ResultsCache
+from application.rounder import Rounder
+from application import error_messages
+from domain.result import Result
+
+_res_cache = ResultsCache()
+
+# "Wrong" import position to avoid circular imports
+from api.export import _export  # pylint: disable=wrong-import-position,ungrouped-imports
+import api.config as c  # pylint: disable=wrong-import-position,ungrouped-imports
+
+
+@overload
+def res(
+    name: str,
+    value: Union[float, int, str, Decimal],
+    unit: str = "",
+    sigfigs: Union[int, None] = None,
+    decimal_places: Union[int, None] = None,
+) -> PrintableResult:
+    return res(name, value, [], unit, sigfigs, decimal_places)
+
+
+@overload
+def res(
+    name: str,
+    value: Union[float, int, str, Decimal],
+    uncert: Union[
+        float,
+        str,
+        Decimal,
+        Tuple[Union[float, int, str, Decimal], str],
+        List[Union[float, int, str, Decimal, Tuple[Union[float, int, str, Decimal], str]]],
+        None,
+    ] = None,
+    sigfigs: Union[int, None] = None,
+    decimal_places: Union[int, None] = None,
+) -> PrintableResult:
+    return res(name, value, uncert, "", sigfigs, decimal_places)
+
+
+@overload
+def res(
+    name: str,
+    value: Union[float, int, str, Decimal],
+    sigfigs: Union[int, None] = None,
+    decimal_places: Union[int, None] = None,
+) -> PrintableResult:
+    return res(name, value, [], "", sigfigs, decimal_places)
+
+
+@overload
+# pylint: disable=too-many-arguments
+def res(
+    name: str,
+    value: Union[float, int, str, Decimal],
+    sys: Union[float, Decimal],
+    stat: Union[float, Decimal],
+    unit: str = "",
+    sigfigs: Union[int, None] = None,
+    decimal_places: Union[int, None] = None,
+) -> PrintableResult:
+    return res(name, value, [(sys, "sys"), (stat, "stat")], unit, sigfigs, decimal_places)
+
+
+@overload
+# pylint: disable=too-many-arguments
+def res(
+    name: str,
+    value: Union[float, int, str, Decimal],
+    uncert: Union[
+        float,
+        str,
+        Decimal,
+        Tuple[Union[float, int, str, Decimal], str],
+        List[Union[float, int, str, Decimal, Tuple[Union[float, int, str, Decimal], str]]],
+        None,
+    ] = None,
+    unit: str = "",
+    sigfigs: Union[int, None] = None,
+    decimal_places: Union[int, None] = None,
+) -> PrintableResult:
+    if uncert is None:
+        uncert = []
+
+    if sigfigs is not None and decimal_places is not None:
+        raise ValueError(error_messages.SIGFIGS_AND_DECIMAL_PLACES_AT_SAME_TIME)
+
+    if sigfigs is not None and isinstance(value, str):
+        raise ValueError(error_messages.SIGFIGS_AND_EXACT_VALUE_AT_SAME_TIME)
+
+    if decimal_places is not None and isinstance(value, str):
+        raise ValueError(error_messages.DECIMAL_PLACES_AND_EXACT_VALUE_AT_SAME_TIME)
+
+    # Parse user input
+    name_res = parsers.parse_name(name)
+    value_res = parsers.parse_value(value)
+    uncertainties_res = parsers.parse_uncertainties(uncert)
+    unit_res = parsers.parse_unit(unit)
+    sigfigs_res = parsers.parse_sigfigs(sigfigs)
+    decimal_places_res = parsers.parse_decimal_places(decimal_places)
+
+    # Assemble the result
+    result = Result(
+        name_res, value_res, unit_res, uncertainties_res, sigfigs_res, decimal_places_res
+    )
+    Rounder.round_result(result, c.configuration.to_rounding_config())
+    _res_cache.add(name_res, result)
+
+    # Print automatically
+    printable_result = PrintableResult(result)
+    if c.configuration.print_auto:
+        printable_result.print()
+
+    # Export automatically
+    immediate_export_path = c.configuration.export_auto_to
+    if immediate_export_path != "":
+        _export(immediate_export_path, print_completed=False)
+
+    return printable_result
+
+
+# Hack for method "overloading" in Python
+# see https://beartype.github.io/plum/integration.html
+# This is a good writeup: https://stackoverflow.com/a/29091980/
+@dispatch
+def res(*args, **kwargs) -> object:  # pylint: disable=unused-argument
+    # This method only scans for all `overload`-decorated methods
+    # and properly adds them as Plum methods.
+    pass
---
+++ packages/resultwizard/resultwizard-1.0.0a2-py3-none-any.whl/application/helpers.py
@@ -0,0 +1,83 @@
+import math
+import decimal
+from decimal import Decimal
+
+from application import error_messages
+
+_NUMBER_TO_WORD = {
+    0: "zero",
+    1: "one",
+    2: "two",
+    3: "three",
+    4: "four",
+    5: "five",
+    6: "six",
+    7: "seven",
+    8: "eight",
+    9: "nine",
+    10: "ten",
+    11: "eleven",
+    12: "twelve",
+    13: "thirteen",
+    14: "fourteen",
+    15: "fifteen",
+    16: "sixteen",
+    17: "seventeen",
+    18: "eighteen",
+    19: "nineteen",
+    20: "twenty",
+    30: "thirty",
+    40: "forty",
+    50: "fifty",
+    60: "sixty",
+    70: "seventy",
+    80: "eighty",
+    90: "ninety",
+}
+
+
+class Helpers:
+    @classmethod
+    def get_exponent(cls, value: Decimal) -> int:
+        if value == 0:
+            return 0
+        return math.floor(math.log10(abs(value)))
+
+    @classmethod
+    def get_first_digit(cls, value: Decimal) -> int:
+        n = abs(value) * 10 ** (-cls.get_exponent(value))
+        return math.floor(n)
+
+    @classmethod
+    def round_to_n_decimal_places(cls, value: Decimal, n: int) -> str:
+        if n < 0:
+            raise RuntimeError(error_messages.ROUND_TO_NEGATIVE_DECIMAL_PLACES)
+
+        try:
+            decimal_value = value.quantize(Decimal(f"1.{'0' * n}"))
+            return f"{decimal_value:.{n}f}"
+        except decimal.InvalidOperation as exc:
+            raise ValueError(error_messages.PRECISION_TOO_LOW) from exc
+
+    @classmethod
+    def number_to_word(cls, number: int) -> str:
+        if 0 <= number <= 19:
+            return _NUMBER_TO_WORD[number]
+        if 0 <= number <= 99:
+            tens = number // 10 * 10
+            ones = number % 10
+            if ones == 0:
+                return _NUMBER_TO_WORD[tens]
+            return _NUMBER_TO_WORD[tens] + cls.capitalize(_NUMBER_TO_WORD[ones])
+        if 0 <= number <= 999:
+            hundreds = number // 100
+            tens = number % 100
+            if tens == 0:
+                return _NUMBER_TO_WORD[hundreds] + "Hundred"
+            return _NUMBER_TO_WORD[hundreds] + "Hundred" + cls.capitalize(cls.number_to_word(tens))
+
+        raise ValueError(error_messages.NUMBER_TO_WORD_TOO_HIGH.format(number=number))
+
+    @classmethod
+    def capitalize(cls, s: str) -> str:
+        return s[0].upper() + s[1:]
