3ee2b1fcf002c6ac3dff7592f9f5b27fd5e5b718
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-24 04:03:14 +0000

    Add package vocabsieve/0.12.0/vocabsieve-0.12.0.tar.gz

---
+++ packages/vocabsieve/vocabsieve-0.12.0.tar.gz/vocabsieve-0.12.0/vocabsieve/ui/searchable_boldable_text_edit.py
@@ -0,0 +1,19 @@
+from .searchable_text_edit import SearchableTextEdit
+from ..global_names import logger
+import re
+
+
+class SearchableBoldableTextEdit(SearchableTextEdit):
+    def unbold(self):
+        self.setPlainText(self.toPlainText().replace('__', ''))
+
+    def bold(self, word):
+        logger.debug(f'bolding {word}')
+        self.setPlainText(re.sub(r'\b' + re.escape(word) + r'\b', '__' + word + '__', self.toPlainText()))
+
+    def toAnki(self):
+        # substitute __word__ with <b>word</b>
+        result = re.sub(r'__(.*?)__', r'<b>\1</b>', self.toPlainText())
+        # substitute newlines with <br>
+        result = result.replace('\n', '<br>')
+        return result
