8bdb079ed680a1fe6e14b25858dd3fb90df6799e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-24 15:03:35 +0000

    Add package agency-swarm/0.1.5/agency_swarm-0.1.5-py3-none-any.whl

---
+++ packages/agency-swarm/agency_swarm-0.1.5-py3-none-any.whl/agency_swarm/agency/genesis/AgentCreator/instructions.md
@@ -0,0 +1,12 @@
+# AgentCreator Agent Instructions
+
+You are an agent that creates other agents as instructed by the user. 
+
+The user will communicate to you each agent that needs to be created. Below are your instructions that needs to be followed for each agent communicated by the user.
+
+**Primary Instructions:**
+1. First, read the manifesto using `ReadManifesto` tool if you have not already done so. This file contains the agency manifesto that describes the agency's purpose and goals.
+2. Create a new agent using `CreateAgentTemplate` function. 
+3. Tell the ToolCreator or OpenAPICreator agent to create tools or APIs for this agent. Make sure to also communicate the agent description, name and a summary of the processes that it needs to perform. CEO Agents do not need to utilize any tools, so you can skip this and the following steps.
+4. If there are no issues and tools or APIs have been successfully created, notify the user that the agent has been created. Otherwise, try to resolve any issues with other agents before reporting back.
+5. Repeat this process for each agent that needs to be created.
---
+++ packages/agency-swarm/agency_swarm-0.1.5-py3-none-any.whl/agency_swarm/cli.py
@@ -0,0 +1,46 @@
+import argparse
+import os
+
+
+def main():
+    parser = argparse.ArgumentParser(description='Create agent template.')
+
+    subparsers = parser.add_subparsers(dest='command', help='Create agent template.')
+    subparsers.required = True
+
+    # create-agent-template
+    create_parser = subparsers.add_parser('create-agent-template', help='Create agent template folder locally.')
+    create_parser.add_argument('--path', type=str, default="./", help='Path to create agent folder.')
+    create_parser.add_argument('--use_txt', action='store_true', default=False,
+                               help='Use txt instead of md for instructions and manifesto.')
+    create_parser.add_argument('--name', type=str, help='Name of agent.')
+    create_parser.add_argument('--description', type=str, help='Description of agent.')
+
+    # genesis-agency
+    genesis_parser = subparsers.add_parser('genesis', help='Start genesis agency.')
+    genesis_parser.add_argument('--openai_key', default=None, type=str, help='OpenAI API key.')
+    genesis_parser.add_argument('--with_browsing', default=False, action='store_true',
+                                help='Enable browsing agent.')
+
+    args = parser.parse_args()
+
+    if args.command == "create-agent-template":
+        from agency_swarm.util import create_agent_template
+        create_agent_template(args.name, args.description, args.path, args.use_txt)
+    elif args.command == "genesis":
+        if not os.getenv('OPENAI_API_KEY') and not args.openai_key:
+            print("OpenAI API key not set. "
+                  "Please set it with --openai_key argument or by setting OPENAI_API_KEY environment variable.")
+            return
+
+        if args.openai_key:
+            from agency_swarm import set_openai_key
+            set_openai_key(args.openai_key)
+
+        from agency_swarm.agency.genesis import GenesisAgency
+        agency = GenesisAgency(with_browsing=args.with_browsing)
+        agency.run_demo()
+
+
+if __name__ == "__main__":
+    main()
