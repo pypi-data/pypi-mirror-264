aad0d3e3f33e13bd5038695c38ef185d2f3c1da0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-24 14:02:37 +0000

    Add package metaflow-stubs/2.11.6/metaflow_stubs-2.11.6-py2.py3-none-any.whl

---
+++ packages/metaflow-stubs/metaflow_stubs-2.11.6-py2.py3-none-any.whl/metaflow-stubs/plugins/pypi/__init__.pyi
@@ -0,0 +1,18 @@
+##################################################################################
+#                       Auto-generated Metaflow stub file                        #
+# MF version: 2.11.6                                                             #
+# Generated on 2024-03-23T22:37:23.095974                                        #
+##################################################################################
+
+from __future__ import annotations
+
+
+class MetaflowException(Exception, metaclass=type):
+    def __init__(self, msg = "", lineno = None):
+        ...
+    def __str__(self):
+        ...
+    ...
+
+MAGIC_FILE: str
+
