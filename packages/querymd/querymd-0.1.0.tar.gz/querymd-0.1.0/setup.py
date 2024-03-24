2390a46928bf227ad872aed5179bedc952eb47e2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-24 14:03:38 +0000

    Add package querymd/0.1.0/querymd-0.1.0.tar.gz

---
+++ packages/querymd/querymd-0.1.0.tar.gz/querymd-0.1.0/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='querymd',
+    version='0.1.0',
+    packages=find_packages(),
+    description='A flexible SQL query builder library',
+    author='Your Name',
+    author_email='your.email@example.com',
+    keywords='SQL, query builder, database',
+    zip_safe=False
+)
