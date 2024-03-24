7dc39a3825c2fc1a659f80384e9a5ba821909f90
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-24 15:03:33 +0000

    Add package komodo-sdk/0.0.55/komodo_sdk-0.0.55.tar.gz

---
+++ packages/komodo-sdk/komodo_sdk-0.0.55.tar.gz/komodo_sdk-0.0.55/komodo/core/agents/librarian_agent.py
@@ -0,0 +1,62 @@
+import json
+
+from komodo.core.tools.files.directory_reader import DirectoryReader
+from komodo.core.tools.files.file_reader import FileReader
+from komodo.core.tools.search.vector_search import VectorSearch
+from komodo.core.utils.indexer import Indexer
+from komodo.core.utils.rag_context import RagContext
+from komodo.framework.komodo_agent import KomodoAgent
+from komodo.framework.komodo_context import KomodoContext
+from komodo.models.framework.agent_runner import AgentRunner
+
+
+class LibrarianAgent(KomodoAgent):
+    shortcode = "librarian"
+    name = "Librarian"
+    purpose = "Answers questions based on documents."
+    instructions = "You are a Document QnA Agent. " \
+                   "You will be given vector search tool and a question. " \
+                   "You must answer the question based on the provided data. " \
+                   "Do not use any external sources."
+
+    def __init__(self, rc: RagContext):
+        super().__init__(
+            shortcode=self.shortcode + "_" + rc.shortcode,
+            name=self.name + f" : {rc.get_display_name()}",
+            purpose=self.purpose,
+            instructions=self.instructions)
+
+        self.rag_context = rc
+        self.files = DirectoryReader(rc.path).action({})
+        self.add_tool(FileReader(rc.path, rc.cache_path))
+        self.add_tool(VectorSearch(rc))
+
+    def generate_context(self, prompt=None):
+        context = KomodoContext()
+        context.extend(super().generate_context(prompt))
+        context.add("List of Files for Reference", json.dumps(self.files))
+        return context
+
+    def index(self, reindex=False):
+        indexer = Indexer(self.rag_context)
+        indexer.run(reindex=reindex)
+        if reindex:
+            self.files = DirectoryReader(self.rag_context.path).action({})
+
+
+def run_search():
+    from komodo.testdata.config import TestConfig
+    path = TestConfig().locations().appliance_data('komodo')
+    cache_path = TestConfig().locations().cache_path()
+    agent = LibrarianAgent(RagContext(path=path, cache_path=cache_path))
+    agent.index()
+    runner = AgentRunner(agent)
+    response = runner.run("What did the G20 leaders agreed in 2009?")
+    print(response.text)
+
+    response = runner.run("tell me more about unique swap identifiers (USI) of each clearing swap?")
+    print(response.text)
+
+
+if __name__ == "__main__":
+    run_search()
