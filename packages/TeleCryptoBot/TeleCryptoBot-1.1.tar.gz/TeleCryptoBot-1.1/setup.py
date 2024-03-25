6f1290476a58d7c75daa4273e50a742b31abce47
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-25 03:04:26 +0000

    Add package TeleCryptoBot/1.1/TeleCryptoBot-1.1.tar.gz

---
+++ packages/TeleCryptoBot/TeleCryptoBot-1.1.tar.gz/TeleCryptoBot-1.1/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup
+setup(
+	name='TeleCryptoBot',
+	version='1.1',
+	author='vh1dz',
+	author_email='vh1dz@vh1dz.ru',
+	description='Библиотека для удобного использования API CryptoPay',
+	url='https://github.com/vh1dz/TeleCryptoBot',
+	packages=['TeleCryptoBot'],
+	include_package_data=True,
+	classifiers=[
+		"Programming Language :: Python :: 3",
+		"License :: OSI Approved :: MIT License",
+		"Operating System :: OS Independent",
+	],
+	python_requires='>=3.6',
+)
