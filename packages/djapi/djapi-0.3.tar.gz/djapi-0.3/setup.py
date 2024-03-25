f23b65734b6f5fa6a2a2c798b174f0dd94f46199
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-25 14:03:01 +0000

    Add package djapi/0.3/djapi-0.3.tar.gz

---
+++ packages/djapi/djapi-0.3.tar.gz/djapi-0.3/setup.py
@@ -0,0 +1,54 @@
+from setuptools import setup, find_packages
+
+
+README = open('README.rst').read()
+
+
+setup(
+    name='djapi',
+    version=open('VERSION').read().strip(),
+    author='Alexander Schepanovski',
+    author_email='suor.web@gmail.com',
+
+    description='The library of simple helpers to build API with Django.',
+    long_description=README,
+    long_description_content_type="text/x-rst",
+    url='https://github.com/Suor/djapi',
+    license='BSD',
+
+    packages=find_packages(),
+    install_requires=[
+        'six>=1.10.0',
+        'django>=2.1',
+        'funcy>=1.8,<3.0',
+    ],
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
+        'Programming Language :: Python :: 3.11',
+        'Programming Language :: Python :: 3.12',
+        'Framework :: Django',
+        'Framework :: Django :: 2.1',
+        'Framework :: Django :: 2.2',
+        'Framework :: Django :: 3.0',
+        'Framework :: Django :: 3.1',
+        'Framework :: Django :: 3.2',
+        'Framework :: Django :: 4.2',
+        'Framework :: Django :: 5.0',
+
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'Topic :: Internet :: WWW/HTTP',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ],
+)
