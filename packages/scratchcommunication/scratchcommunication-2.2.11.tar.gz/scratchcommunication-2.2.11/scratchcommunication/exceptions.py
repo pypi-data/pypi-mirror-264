9404c57bb407390144b87c6d8c9ffe6e6d5b7beb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-25 04:03:37 +0000

    Add package scratchcommunication/2.2.11/scratchcommunication-2.2.11.tar.gz

---
+++ packages/scratchcommunication/scratchcommunication-2.2.11.tar.gz/scratchcommunication-2.2.11/scratchcommunication/exceptions.py
@@ -0,0 +1,17 @@
+class InvalidValueError(ValueError):
+    pass
+
+class QuickAccessDisabledError(Exception):
+    pass
+
+class NotSupported(Exception):
+    pass
+
+class ErrorInEventHandler(RuntimeWarning):
+    pass
+
+class ErrorInCloudSocket(RuntimeWarning):
+    pass
+
+class StopException(BaseException):
+    pass
