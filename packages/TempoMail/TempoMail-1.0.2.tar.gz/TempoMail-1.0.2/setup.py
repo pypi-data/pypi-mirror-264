20e65510b61a09a03d583f2fdf6a3ec387b04b2c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-25 03:04:32 +0000

    Add package TempoMail/1.0.2/TempoMail-1.0.2.tar.gz

---
+++ packages/TempoMail/TempoMail-1.0.2.tar.gz/TempoMail-1.0.2/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='TempoMail',
+    version='1.0.2',
+    description='python temp-mail liberary',
+    author='Radin',
+    download_url="https://github.com/radinparhami/TempoMail/releases/latest",
+    url="https://github.com/radinparhami",
+    author_email="radinparhami107@gmail.com",
+    install_requires=['requests'],
+)
