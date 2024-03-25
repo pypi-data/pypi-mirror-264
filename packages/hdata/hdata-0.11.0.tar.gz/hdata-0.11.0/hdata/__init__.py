880c26ab70b31643cdf684a9641f044df414a5d5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-25 14:03:16 +0000

    Add package hdata/0.11.0/hdata-0.11.0.tar.gz

---
+++ packages/hdata/hdata-0.11.0.tar.gz/hdata-0.11.0/hdata/__init__.py
@@ -0,0 +1,8 @@
+"""Top-level package for hdata."""
+
+__author__ = """Judah Cooper"""
+__email__ = 'judah@hyperdata.network'
+__version__ = '0.11.0'
+
+from .models import Source
+from .functions import transform
