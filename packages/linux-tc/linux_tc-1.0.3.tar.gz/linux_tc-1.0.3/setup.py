72dc889a00654b874e9e65f9b2876d5640606b2d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-25 15:03:39 +0000

    Add package linux-tc/1.0.3/linux_tc-1.0.3.tar.gz

---
+++ packages/linux-tc/linux_tc-1.0.3.tar.gz/linux_tc-1.0.3/setup.py
@@ -0,0 +1,36 @@
+﻿# -*- coding: UTF-8 -*-
+from setuptools import setup
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+
+setup(
+    name='linux_tc',
+    version='1.0.03',
+    packages=setuptools.find_packages(),
+    url='',
+    license='MIT',
+    author=' MA JIANLI',
+    author_email='majianli@corp.netease.com',
+    description='A toolchain for simulating weak network conditions',
+    long_description="",
+    #long_description_content_type="text/markdown",
+    install_requires=[
+    'paramiko',
+    'requests',
+    'APScheduler'
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    data_files=[],
+
+    python_requires='>=3.7',
+)
+
+
+
