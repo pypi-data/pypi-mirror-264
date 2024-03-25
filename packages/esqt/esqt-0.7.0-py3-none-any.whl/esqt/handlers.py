657aacee808d990344881134752193a4cad9b399
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-25 14:03:11 +0000

    Add package esqt/0.7.0/esqt-0.7.0-py3-none-any.whl

---
+++ packages/esqt/esqt-0.7.0-py3-none-any.whl/esqt/handlers.py
@@ -0,0 +1,20 @@
+import json
+import typing as t
+
+
+class ActionHandler:
+    def __init__(self, actions: t.Iterable[str]):
+        self._iter = iter(actions)
+
+    def __iter__(self):
+        return self.handle(self._iter)
+
+    def __next__(self):
+        return next(self._iter)
+
+    def handle(self, actions: t.Iterable[str]):
+        for action in actions:
+            yield self.handle_one(action)
+
+    def handle_one(self, action: str):
+        return json.loads(action)
