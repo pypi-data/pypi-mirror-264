0ccad094e4e639c7acd60c698154c34e596336f8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-25 05:02:56 +0000

    Add package tsfontconverter/0.34/tsfontconverter-0.34.tar.gz

---
+++ packages/tsfontconverter/tsfontconverter-0.34.tar.gz/tsfontconverter-0.34/src/tsfontconverter/fontmodel/Font.py
@@ -0,0 +1,26 @@
+from dataclasses import dataclass, field
+
+from .CharacterMap import CharacterMap
+from .FontStandard import FontStandard
+from .Meta import Meta
+
+
+@dataclass
+class Font:
+    name: str = ""
+    meta: Meta = field(default_factory=Meta)
+    characterMap: CharacterMap = field(default_factory=CharacterMap)
+    type: FontStandard = FontStandard.OpenType
+    dataUri: str = ""
+
+    def to_dict(self):
+        meta = Meta().__dict__ if self.meta is None else self.meta.__dict__
+        characterMap = CharacterMap().to_dict() if self.characterMap is None else self.characterMap.to_dict()
+        myType = "" if self.type is None else str(self.type)
+        return {
+            "name": self.name,
+            "meta": meta,
+            "type": myType,
+            "characterMap": characterMap,
+            "dataUri": self.dataUri,
+        }
