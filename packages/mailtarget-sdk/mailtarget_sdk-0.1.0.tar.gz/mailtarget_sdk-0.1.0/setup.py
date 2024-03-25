37a31ccf8cc398265a8870fa71f522f46c97ed48
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-25 15:03:50 +0000

    Add package mailtarget-sdk/0.1.0/mailtarget_sdk-0.1.0.tar.gz

---
+++ packages/mailtarget-sdk/mailtarget_sdk-0.1.0.tar.gz/mailtarget_sdk-0.1.0/setup.py
@@ -0,0 +1,24 @@
+# setup.py
+from setuptools import setup, find_packages
+
+setup(
+    name='mailtarget_sdk',
+    version='0.1.0',
+    author='Wisnu Eka Saputra',
+    author_email='wisnu.eka@mtarget.co',
+    description='SDK Python untuk Mailtarget API',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url='https://gitlab.com/mailtarget/integrations/mailtarget-python-sdk',
+    packages=find_packages(),
+    install_requires=[
+        'requests',
+        'pytest',
+    ],
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',  # Versi minimum Python yang didukung
+)
