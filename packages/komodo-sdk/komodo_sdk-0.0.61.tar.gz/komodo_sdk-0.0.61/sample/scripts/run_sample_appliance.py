a96fb2abcbd2f738a40f94fa74d196dc8aebc57e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-25 14:03:06 +0000

    Add package komodo-sdk/0.0.61/komodo_sdk-0.0.61.tar.gz

---
+++ packages/komodo-sdk/komodo_sdk-0.0.61.tar.gz/komodo_sdk-0.0.61/sample/scripts/run_sample_appliance.py
@@ -0,0 +1,64 @@
+from komodo.loaders.database.appliance_loader import ApplianceLoader
+from komodo.models.framework.agent_runner import AgentRunner
+from komodo.models.framework.appliance_runner import ApplianceRunner
+from komodo.models.framework.appliance_runtime import ApplianceRuntime
+from sample.appliance.appliance import SampleAppliance
+
+
+def build_and_run():
+    appliance = SampleAppliance()
+    prompt = '''
+        Summarize the following text in 5 words and then translate into Spanish, Hindi and German:
+        This is a sample application using the new Komodo SDK.
+    '''
+    runner = ApplianceRunner(appliance)
+    response = runner.run(prompt)
+    print(response.text)
+
+
+def build_and_search():
+    appliance = SampleAppliance()
+    appliance.index()
+    prompt = '''
+        what is revenue breakdown of nvidia?
+    '''
+    runner = ApplianceRunner(appliance)
+    response = runner.run(prompt)
+    print(response.text)
+
+
+def load_and_run():
+    appliance = ApplianceLoader.load('sample')
+    prompt = '''
+        Summarize the following text in 5 words and translate into Spanish, Hindi and German:
+        This is a sample application using the new Komodo 9 SDK.
+    '''
+    runner = ApplianceRunner(appliance)
+    response = runner.run(prompt)
+    print(response.text)
+
+
+def build_and_search_with_agent():
+    appliance = SampleAppliance()
+    appliance.index()
+    prompt = '''
+        Any policy changes for nvidia?
+    '''
+    runtime = ApplianceRuntime(appliance)
+    for a in runtime.get_all_agents():
+        if 'docsearch' in a.shortcode:
+            runner = AgentRunner(a)
+            response = runner.run(prompt)
+            print(response.text)
+
+    agent = runtime.get_agent('librarian')
+    runner = AgentRunner(agent)
+    response = runner.run(prompt)
+    print(response.text)
+
+
+if __name__ == '__main__':
+    appliance = SampleAppliance()
+    email = "test@example.com"
+    profile = next((x for x in appliance.users if x.email == email), None)
+    print(profile.to_dict())
