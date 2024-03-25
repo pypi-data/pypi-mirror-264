95bd855dc55d97d0f80397075e22faecb1161909
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-25 14:03:14 +0000

    Add package czo/0.1.3/czo-0.1.3.tar.gz

---
+++ packages/czo/czo-0.1.3.tar.gz/czo-0.1.3/src/czo/__init__.py
@@ -0,0 +1,9 @@
+"""
+辅助测试的一些常用功能
+"""
+from ._date import FastDate
+from ._faker import Faker
+from ._path import Path
+from ._rand import Random
+
+__all__ = ["FastDate", "Faker", "Path", "Random"]
