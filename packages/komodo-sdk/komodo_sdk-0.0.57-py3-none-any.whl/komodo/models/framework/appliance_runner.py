503eaf1b123edd8e1f74cb824d4b35dec1013276
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-25 04:04:07 +0000

    Add package komodo-sdk/0.0.57/komodo_sdk-0.0.57-py3-none-any.whl

---
+++ packages/komodo-sdk/komodo_sdk-0.0.57-py3-none-any.whl/komodo/models/framework/appliance_runner.py
@@ -0,0 +1,17 @@
+from komodo.models.framework.agent_runner import AgentRunner
+from komodo.models.framework.appliance_runtime import ApplianceRuntime
+from komodo.models.framework.runner import Runner
+
+
+class ApplianceRunner(Runner):
+    def __init__(self, appliance, user=None):
+        agent = ApplianceRuntime(appliance).coordinator_agent()
+        user = agent.user if user is None else user
+        self.runner = AgentRunner(agent, user=user)
+
+    def run(self, prompt, **kwargs):
+        return self.runner.run(prompt, **kwargs)
+
+    def run_streamed(self, prompt, **kwargs):
+        for response in self.runner.run_streamed(prompt, **kwargs):
+            yield response
