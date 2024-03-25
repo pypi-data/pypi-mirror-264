1826c2a8e06210591cdddc6a4a22b71a1ec82c6d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-25 14:03:13 +0000

    Add package tensorgp-tf/0.0.2/tensorgp_tf-0.0.2.tar.gz

---
+++ packages/tensorgp-tf/tensorgp_tf-0.0.2.tar.gz/tensorgp_tf-0.0.2/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages
+
+VERSION = '0.0.2'
+DESCRIPTION = 'A vectorized approach to Genetic Programming - TensorFlow version'
+
+setup(
+    name='tensorgp_tf',
+    version=VERSION,
+    author='Francisco Baeta',
+    author_email='<fjrbaeta@dei.uc.pt>',
+    description=DESCRIPTION,
+    keywords=['Genetic Programming', 'Vectorization', 'GPU', 'Python', 'TensorFlow'],
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+    project_urls = {
+        "Source": "https://github.com/AwardOfSky/TensorGP/tree/dev",
+    },
+    install_requires = ['tensorflow==2.16.1', 'scikit-image', 'matplotlib'],
+    packages=find_packages(),
+    include_package_data=True,
+)
