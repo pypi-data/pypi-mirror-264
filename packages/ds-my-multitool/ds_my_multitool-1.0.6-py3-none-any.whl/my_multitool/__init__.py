f266dab20f2f1f478dda31f6fe85797681b0d752
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-25 03:03:44 +0000

    Add package ds-my-multitool/1.0.6/ds_my_multitool-1.0.6-py3-none-any.whl

---
+++ packages/ds-my-multitool/ds_my_multitool-1.0.6-py3-none-any.whl/my_multitool/__init__.py
@@ -0,0 +1,7 @@
+"""My Multitool.
+
+Multitool for the My Project. Can be used to do a few tasks, like creating the
+database schema and updating the credentials for users.
+"""
+
+__version__ = '1.0.6'
---
+++ packages/ds-my-multitool/ds_my_multitool-1.0.6-py3-none-any.whl/my_multitool/globals.py
@@ -0,0 +1,45 @@
+"""Globals for the package.
+
+Contains the global objects for the package.
+"""
+
+from typing import Any, Optional
+
+from my_data.my_data import MyData
+
+from .config import ConfigManager
+
+config = ConfigManager()
+
+
+def get_my_data_object_for_context(
+    context_name: Optional[str] = None,
+    db_args: Optional[dict[str, Any]] = None,
+) -> MyData:
+    """Get a configured MyData object for a specific context.
+
+    Returns a MyData object with the correct configuration for the given
+    context.
+
+    Args:
+        context_name: the name of the context to use. If not given, the active
+            context will be used.
+        db_args: additional arguments for the database connection.
+
+    Returns:
+        A MyData object.
+    """
+    if not context_name:
+        context_name = config.active_context.name
+
+    data = MyData()
+    context = config.contexts[context_name]
+
+    data.configure(
+        db_connection_str=context.db_string,
+        database_args=db_args,
+        service_username=context.service_user,
+        service_password=context.service_pass,
+    )
+
+    return data
