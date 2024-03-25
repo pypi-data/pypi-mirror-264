f6692e44a9baa06c92beb99718cb502bb8db0286
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-25 03:03:39 +0000

    Add package hanna-calculator/0.0.1/hanna_calculator-0.0.1.tar.gz

---
+++ packages/hanna-calculator/hanna_calculator-0.0.1.tar.gz/hanna_calculator-0.0.1/src/hanna_calculator/main.py
@@ -0,0 +1,15 @@
+def add(a,b):
+    print("sum is", a+b)
+def sub(a,b):
+    print("diff is",a-b)
+def mul(a,b):
+    print("pdt is", a*b)
+def div(a,b):
+    print("quotient is", a/b)
+
+PI=3.14159
+
+G=0.83462 
+
+LN2=0.69314
+
