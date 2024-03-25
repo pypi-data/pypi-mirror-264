eb37436685fa886acdb46480fff3abfdeb776aeb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-25 14:03:01 +0000

    Add package heckit/2/heckit-2.tar.gz

---
+++ packages/heckit/heckit-2.tar.gz/heckit-2/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup, find_packages
+
+setup(name='heckit',
+      version='2',
+      description='none',
+      packages=['heckit'],
+      author_email='antoha123_3@mail.ru',
+      zip_safe=False)
