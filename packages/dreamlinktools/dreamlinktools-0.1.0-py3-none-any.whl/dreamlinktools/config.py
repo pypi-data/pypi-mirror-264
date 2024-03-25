b32707ad58c1316573fceaf71bd673bb4ff8db64
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-25 03:03:32 +0000

    Add package dreamlinktools/0.1.0/dreamlinktools-0.1.0-py3-none-any.whl

---
+++ packages/dreamlinktools/dreamlinktools-0.1.0-py3-none-any.whl/dreamlinktools/config.py
@@ -0,0 +1,3 @@
+from dreamtools.vec3 import Vec3
+
+chunk_dims = Vec3(16, 16, 16)
---
+++ packages/dreamlinktools/dreamlinktools-0.1.0-py3-none-any.whl/dreamlinktools/utils/vec3.py
@@ -0,0 +1,40 @@
+class Vec3:
+    def __init__(self, x, y, z):
+        self.x = x
+        self.y = y
+        self.z = z
+
+    def __add__(self, other):
+        return Vec3(self.x + other.x, self.y + other.y, self.z + other.z)
+
+    def __sub__(self, other):
+        return Vec3(self.x - other.x, self.y - other.y, self.z - other.z)
+
+    def __mul__(self, other):
+        return Vec3(self.x * other.x, self.y * other.y, self.z * other.z)
+
+    def __floordiv__(self, other):
+        return Vec3(self.x // other.x, self.y // other.y, self.z // other.z)
+
+    def __mod__(self, other):
+        return Vec3(self.x % other, self.y % other, self.z % other)
+
+    def volume(self):
+        return self.x * self.y * self.z
+
+    def pack(self, dims):
+        index = 0
+        index += self.y * dims.z * dims.x
+        index += self.z * dims.x
+        index += self.x
+        return index
+        
+    @staticmethod
+    def unpack(value, dims):
+        x = value % dims.x
+        value //= dims.x
+        z = value % dims.z
+        value //= dims.z
+        y = value
+        return Vec3(x, y, z)
+
