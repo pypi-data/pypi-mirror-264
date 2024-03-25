1a2b320c3f45c2541429526f8b0a70b8d14a92f0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-25 15:03:49 +0000

    Add package resumable2/0.1/resumable2-0.1.tar.gz

---
+++ packages/resumable2/resumable2-0.1.tar.gz/resumable2-0.1/setup.py
@@ -0,0 +1,42 @@
+import os
+from setuptools import setup
+
+README = open(os.path.join(os.path.dirname(__file__), 'readme.md')).read()
+
+# allow setup.py to be run from any path
+os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
+
+setup(
+    name='resumable2',
+    version='0.1',
+    packages=['admin_resumable2'],
+    include_package_data=True,
+    package_data={
+        'admin_resumable2': [
+            'templates/admin_resumable2/file_input.html',
+            'static/admin_resumable2/js/resumable.js',
+        ]
+    },
+    license='MIT License',
+    description='A Django app for the uploading of large files from the django admin site.',
+    long_description=README,
+    long_description_content_type="text/markdown",
+    url='https://github.com/icedreamer-praveen/resumable2',
+    author='icedreamer-praveen',
+    author_email='prabinchy1234@gmail.com',
+    classifiers=[
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3.10',
+        'Topic :: Internet :: WWW/HTTP',
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+    ],
+    python_requires=">=3.8",
+    extras_require={
+        "dev": ['twine>=4.0.2',]
+    }
+)
