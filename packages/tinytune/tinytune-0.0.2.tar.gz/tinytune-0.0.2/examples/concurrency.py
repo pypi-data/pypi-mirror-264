36b309ecec301282117bbf96765f8dd72d0113e6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-25 16:04:38 +0000

    Add package tinytune/0.0.2/tinytune-0.0.2.tar.gz

---
+++ packages/tinytune/tinytune-0.0.2.tar.gz/tinytune-0.0.2/examples/concurrency.py
@@ -0,0 +1,88 @@
+from typing import Callable, Any
+from concurrent.futures import ThreadPoolExecutor, Future, as_completed
+from examples.gptcontext import GPTContext
+
+class ParallelRunner:
+    """
+    Represents a parallel runner for executing jobs concurrently.
+    """
+    def __init__(self, gpt: GPTContext) -> None:
+        """
+        Initialize a ParallelRunner object.
+
+        Parameters:
+        - gpt (GPTContext): The GPT context.
+        """
+        self.Jobs: list[Callable[[GPTContext, list[Any]], Any]] = []
+        self.Futures: list[Future]
+        self.Pool: ThreadPoolExecutor = None
+        self.GPT: GPTContext = gpt
+        self.Results: list = [] 
+
+    def GetCompleted(self, onWait: Callable[[Any], Any] = None):
+        """
+        Get completed futures.
+
+        Parameters:
+        - onWait (Callable[[Any], Any], optional): Callback function to execute while waiting.
+
+        Returns:
+        - Iterator[Future]: Iterator of completed futures.
+        """
+        if onWait is not None:
+            onWait(self)
+
+        return as_completed(self.Futures)
+
+    def OnComplete(self, *args) -> Any:
+        """
+        Placeholder method for actions after completion.
+
+        Parameters:
+        - args: Arbitrary arguments.
+
+        Returns:
+        - Any: Result of the completion action.
+        """
+        return
+
+    def AddJob(self, job: Callable[[GPTContext, list[Any]], Any]) -> Any:
+        """
+        Add a job to the runner.
+
+        Parameters:
+        - job (Callable[[GPTContext, list[Any]], Any]): The job to add.
+
+        Returns:
+        - ParallelRunner: The ParallelRunner object.
+        """
+        self.Jobs.append(job)
+        return self 
+
+    def Run(self, onWait: Callable[[Any], Any] = None, onError: Callable[[Exception], Any] = None) -> bool:
+        """
+        Run the parallel runner.
+
+        Parameters:
+        - onWait (Callable[[Any], Any], optional): Callback function to execute while waiting.
+        - onError (Callable[[Exception], Any], optional): Callback function to execute on error.
+
+        Returns:
+        - bool: True if successful, False otherwise.
+        """
+        try:
+            self.Pool = ThreadPoolExecutor(max_workers=len(self.Jobs))
+            self.Futures = [self.Pool.submit(job, self.GPT) for job in self.Jobs] 
+
+            for future in self.GetCompleted(onWait):
+                self.Results.append(future.result())
+
+            self.OnComplete()
+ 
+        except Exception as e:
+            if onError is None:
+                raise e
+
+            onError(e)
+
+        return True
---
+++ packages/tinytune/tinytune-0.0.2.tar.gz/tinytune-0.0.2/examples/main.py
@@ -0,0 +1,47 @@
+import os
+import sys
+
+sys.path.append("../")
+
+from typing import Any
+from tinytune.llmcontext import LLMContext
+from examples.gptcontext import GPTContext, GPTMessage, Model
+from tinytune.pipeline import Pipeline
+from tinytune.prompt import prompt_job, PromptJob
+from PerplexityContext import PerplexityContext, PerplexityMessage
+
+def Main():
+    context = GPTContext("gpt-4-0125-preview", str(os.getenv("OPENAI_KEY")))
+    pContext = PerplexityContext("pplx-70b-online", os.getenv("PERPLEXITY_KEY"))
+
+    def Callback(content):
+        if (content != None):
+            print(content, end="")
+        else:   
+            print()
+    
+    context.OnGenerateCallback = Callback
+    pContext.OnGenerateCallback = Callback
+
+    @prompt_job(id="search", context=pContext)
+    def Job(id: str, context: PerplexityContext, prevResult: Any):
+        (context.Prompt(PerplexityMessage("user", "Find me Japanese restaurants in Vancouver"))
+                .Run(stream=True))
+
+        return context.Messages[-1]
+
+    @prompt_job("extract json", context)
+    def Job1(id: str, context: GPTContext, prevResult: Any):
+        print("prevResult: ", prevResult.ToDict())
+        (context.Prompt(GPTMessage("user", f"""{prevResult.Content} extract this data into JSON, and only return the JSON, no formatting, backticks, or explanation"""))
+                .Run(stream=True)) 
+
+    pipeline: Pipeline = Pipeline(context)
+
+    (pipeline.AddJob(Job)
+            .AddJob(Job1))
+
+    pipeline.Run()
+
+
+Main()
