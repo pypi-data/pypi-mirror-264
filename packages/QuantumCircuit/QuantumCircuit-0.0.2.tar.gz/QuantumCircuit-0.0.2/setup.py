3f48d82c445bee7cbf7bc6c7867c660b1ccd36d5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-25 14:03:22 +0000

    Add package QuantumCircuit/0.0.2/QuantumCircuit-0.0.2.tar.gz

---
+++ packages/QuantumCircuit/QuantumCircuit-0.0.2.tar.gz/QuantumCircuit-0.0.2/setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
+    name='QuantumCircuit',
+    version='0.0.2',
+    description='TGQ量子模拟器',  # 包描述
+    long_description=long_description,  # 详细描述
+    long_description_content_type='text/markdown',
+    author='CETC',  # 作者姓名
+    author_email='wangzhiqiang@tgqs.net',  # 作者邮箱
+    license='MIT',  # 许可证
+    package=find_packages(),
+    include_package_data=True,
+    packages=["QuantumCircuit", "QuantumCircuit.GateSimulation"],
+    install_requires=[
+        'numpy==1.24.3',
+        'numba==0.58.1',
+        'matplotlib==3.7.1',
+    ],
+    classifiers=[
+    'Operating System :: OS Independent',
+    'License :: OSI Approved :: MIT License',
+    'Programming Language :: Python :: 3',
+    ],
+)
