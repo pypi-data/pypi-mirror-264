de733ce8b5cbc5bada99d56ce826b19b0fa1f474
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-25 15:03:11 +0000

    Add package findpython/0.5.0/findpython-0.5.0-py3-none-any.whl

---
+++ packages/findpython/findpython-0.5.0-py3-none-any.whl/findpython/providers/rye.py
@@ -0,0 +1,35 @@
+from __future__ import annotations
+
+import shutil
+import typing as t
+from pathlib import Path
+
+from findpython.providers.base import BaseProvider
+from findpython.python import PythonVersion
+from findpython.utils import WINDOWS, safe_iter_dir
+
+
+class RyeProvider(BaseProvider):
+    def __init__(self) -> None:
+        self.root = Path.home() / ".rye"
+        self.rye_bin = shutil.which("rye")
+
+    @classmethod
+    def create(cls) -> t.Self | None:
+        return cls()
+
+    def find_pythons(self) -> t.Iterable[PythonVersion]:
+        py_root = self.root / "py"
+        if not py_root.exists():
+            return
+        for child in safe_iter_dir(py_root):
+            if child.is_symlink():  # registered an existing python
+                continue
+            for intermediate in ("", "install/"):
+                if WINDOWS:
+                    python_bin = child / (intermediate + "python.exe")
+                else:
+                    python_bin = child / (intermediate + "bin/python3")
+                if python_bin.exists():
+                    yield self.version_maker(python_bin, _interpreter=python_bin)
+                    break
