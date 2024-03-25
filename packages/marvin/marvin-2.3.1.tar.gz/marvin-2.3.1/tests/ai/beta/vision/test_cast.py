88f0a1cfdf4b4179ca6903d4b25c61df43684fdc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-25 14:03:20 +0000

    Add package marvin/2.3.1/marvin-2.3.1.tar.gz

---
+++ packages/marvin/marvin-2.3.1.tar.gz/marvin-2.3.1/tests/ai/beta/vision/test_cast.py
@@ -0,0 +1,131 @@
+import marvin
+import pytest
+from pydantic import BaseModel, Field
+
+
+class Location(BaseModel):
+    city: str
+    state: str = Field(description="The two letter abbreviation")
+
+
+@pytest.mark.flaky(max_runs=3)
+class TestVisionCast:
+    def test_cast_ny(self):
+        img = marvin.beta.Image(
+            "https://images.unsplash.com/photo-1568515387631-8b650bbcdb90"
+        )
+        result = marvin.beta.cast(img, target=Location)
+        assert result in (
+            Location(city="New York", state="NY"),
+            Location(city="New York City", state="NY"),
+        )
+
+    def test_cast_dc(self):
+        img = marvin.beta.Image(
+            "https://images.unsplash.com/photo-1617581629397-a72507c3de9e"
+        )
+        result = marvin.beta.cast(img, target=Location)
+        assert isinstance(result, Location)
+        assert "Washington" in result.city
+        assert result.state.index("D") < result.state.index("C")
+
+    def test_cast_ny_images_input(self):
+        img = marvin.beta.Image(
+            "https://images.unsplash.com/photo-1568515387631-8b650bbcdb90"
+        )
+        result = marvin.beta.cast(data=None, images=[img], target=Location)
+        assert result in (
+            Location(city="New York", state="NY"),
+            Location(city="New York City", state="NY"),
+        )
+
+    def test_cast_ny_image_input(self):
+        img = marvin.beta.Image(
+            "https://images.unsplash.com/photo-1568515387631-8b650bbcdb90"
+        )
+        result = marvin.beta.cast(data=img, target=Location)
+        assert result in (
+            Location(city="New York", state="NY"),
+            Location(city="New York City", state="NY"),
+        )
+
+    def test_cast_ny_image_and_text(self):
+        img = marvin.beta.Image(
+            "https://images.unsplash.com/photo-1568515387631-8b650bbcdb90"
+        )
+        result = marvin.beta.cast(
+            data="I see the empire state building",
+            images=[img],
+            target=Location,
+        )
+        assert result in (
+            Location(city="New York", state="NY"),
+            Location(city="New York City", state="NY"),
+        )
+
+    def test_cast_book(self):
+        class Book(BaseModel):
+            title: str
+            subtitle: str
+            authors: list[str]
+
+        img = marvin.beta.Image(
+            "https://hastie.su.domains/ElemStatLearn/CoverII_small.jpg"
+        )
+        result = marvin.beta.cast(img, target=Book)
+        assert result == Book(
+            title="The Elements of Statistical Learning",
+            subtitle="Data Mining, Inference, and Prediction",
+            authors=["Trevor Hastie", "Robert Tibshirani", "Jerome Friedman"],
+        )
+
+
+class TestAsync:
+    async def test_cast_ny(self):
+        img = marvin.beta.Image(
+            "https://images.unsplash.com/photo-1568515387631-8b650bbcdb90"
+        )
+        result = await marvin.beta.cast_async(img, target=Location)
+        assert result in (
+            Location(city="New York", state="NY"),
+            Location(city="New York City", state="NY"),
+        )
+
+
+class TestMapping:
+    def test_map(self):
+        ny = marvin.beta.Image(
+            "https://images.unsplash.com/photo-1568515387631-8b650bbcdb90"
+        )
+        dc = marvin.beta.Image(
+            "https://images.unsplash.com/photo-1617581629397-a72507c3de9e"
+        )
+        result = marvin.beta.cast.map([ny, dc], target=Location)
+        assert isinstance(result, list)
+        assert result[0] in (
+            Location(city="New York", state="NY"),
+            Location(city="New York City", state="NY"),
+        )
+        assert result[1] in (
+            Location(city="Washington", state="DC"),
+            Location(city="Washington", state="D.C."),
+        )
+
+    @pytest.mark.flaky(reruns=3)
+    async def test_async_map(self):
+        ny = marvin.beta.Image(
+            "https://images.unsplash.com/photo-1568515387631-8b650bbcdb90"
+        )
+        dc = marvin.beta.Image(
+            "https://images.unsplash.com/photo-1617581629397-a72507c3de9e"
+        )
+        result = await marvin.beta.cast_async.map([ny, dc], target=Location)
+        assert isinstance(result, list)
+        assert result[0] in (
+            Location(city="New York", state="NY"),
+            Location(city="New York City", state="NY"),
+        )
+        assert result[1] in (
+            Location(city="Washington", state="DC"),
+            Location(city="Washington", state="D.C."),
+        )
---
+++ packages/marvin/marvin-2.3.1.tar.gz/marvin-2.3.1/tests/ai/test_classify.py
@@ -0,0 +1,140 @@
+from enum import Enum
+from typing import Literal
+
+import marvin
+import pytest
+
+Sentiment = Literal["Positive", "Negative"]
+
+
+class GitHubIssueTag(Enum):
+    BUG = "bug"
+    FEATURE = "feature"
+    ENHANCEMENT = "enhancement"
+    DOCS = "docs"
+
+
+class TestClassify:
+    class TestLiteral:
+        def test_classify_sentiment(self):
+            result = marvin.classify("This is a great feature!", Sentiment)
+            assert result == "Positive"
+
+        def test_classify_negative_sentiment(self):
+            result = marvin.classify(
+                "This feature is absolutely terrible!",
+                Sentiment,
+            )
+            assert result == "Negative"
+
+    class TestEnum:
+        def test_classify_bug_tag(self):
+            result = marvin.classify("This is a bug", GitHubIssueTag)
+            assert result == GitHubIssueTag.BUG
+
+        def test_classify_feature_tag(self):
+            result = marvin.classify("This is a great feature!", GitHubIssueTag)
+            assert result == GitHubIssueTag.FEATURE
+
+        def test_classify_enhancement_tag(self):
+            result = marvin.classify("This is an enhancement", GitHubIssueTag)
+            assert result == GitHubIssueTag.ENHANCEMENT
+
+        def test_classify_docs_tag(self):
+            result = marvin.classify("This is a documentation update", GitHubIssueTag)
+            assert result == GitHubIssueTag.DOCS
+
+    class TestList:
+        def classify_bug_tag(self):
+            result = marvin.classify(
+                "This is a bug", ["bug", "feature", "enhancement", "docs"]
+            )
+            assert result == "bug"
+
+        def test_classify_number(self):
+            # a version of the prompt would choose the label *number* that
+            # matched the data, rather than the label *description*
+            result = marvin.classify(0, ["letter", "number"])
+            assert result == "number"
+
+    class TestBool:
+        def test_classify_positive_sentiment(self):
+            result = marvin.classify("This is a great feature!", bool)
+            assert result is True
+
+        def test_classify_negative_sentiment(self):
+            result = marvin.classify("This feature is terrible!", bool)
+            assert result is False
+
+        def test_classify_falseish(self):
+            result = marvin.classify("nope", bool)
+            assert result is False
+
+    class TestInstructions:
+        def test_classify_positive_sentiment_with_instructions(self):
+            result = marvin.classify(
+                "This is a great feature!", Sentiment, instructions="It's opposite day."
+            )
+            assert result == "Negative"
+
+    class TestAsync:
+        async def test_classify_positive_sentiment(self):
+            result = await marvin.classify_async("This is a great feature!", bool)
+            assert result is True
+
+    class TestExamples:
+        async def test_hogwarts_sorting_hat(self):
+            description = "Brave, daring, chivalrous, and sometimes a bit reckless."
+
+            house = marvin.classify(
+                description,
+                labels=["Gryffindor", "Hufflepuff", "Ravenclaw", "Slytherin"],
+            )
+
+            assert house == "Gryffindor"
+
+        @pytest.mark.parametrize(
+            "user_input, expected_selection",
+            [
+                ("I want to do an event with marvin!", "events and relations"),
+                ("Well FooCo offered me a better deal", "sales"),
+                ("*angry noises*", "support"),
+            ],
+        )
+        async def test_call_routing(self, user_input, expected_selection):
+            class Department(Enum):
+                SALES = "sales"
+                SUPPORT = "support"
+                EVENTS = "events and relations"
+
+            def router(transcript: str) -> Department:
+                return marvin.classify(
+                    transcript,
+                    labels=Department,
+                    instructions="Select the best department for the customer request",
+                )
+
+            assert router(user_input).value == expected_selection
+
+
+class TestMapping:
+    def test_classify_map(self):
+        result = marvin.classify.map(["This is great!", "This is terrible!"], Sentiment)
+        assert isinstance(result, list)
+        assert result == ["Positive", "Negative"]
+
+    def test_classify_map_with_instructions(self, gpt_4):
+        result = marvin.classify.map(
+            ["o", "0"],
+            ["letter", "number"],
+            instructions="'o' means zero",
+        )
+        assert isinstance(result, list)
+        assert result == ["number", "number"]
+
+    async def test_async_classify_map(self):
+        result = await marvin.classify_async.map(
+            ["This is great!", "This is terrible!"], Sentiment
+        )
+        assert isinstance(result, list)
+        assert result == ["Positive", "Negative"]
