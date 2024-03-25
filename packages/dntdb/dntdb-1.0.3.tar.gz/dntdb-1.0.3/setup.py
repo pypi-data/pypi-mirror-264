ca2ce75f536792c321ba719e4f90817ae75d3780
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-25 04:04:04 +0000

    Add package dntdb/1.0.3/dntdb-1.0.3.tar.gz

---
+++ packages/dntdb/dntdb-1.0.3.tar.gz/dntdb-1.0.3/setup.py
@@ -0,0 +1,33 @@
+from setuptools import setup, find_packages
+import os
+
+with open("dntdb/requirements.txt") as f:
+    required = f.read().splitlines()
+
+
+def find_so_files(root_dir):
+    so_files = []
+    for dirpath, dirnames, filenames in os.walk(root_dir):
+        for filename in filenames:
+            if filename.endswith(".so"):
+                so_files.append(os.path.join(dirpath, filename))
+    return so_files
+
+
+setup(
+    name="dntdb",
+    version="1.0.3",
+    author="DNT",
+    description="Search Engine",
+    long_description=open("dntdb/README.md").read(),
+    long_description_content_type="text/markdown",
+    packages=find_packages(),
+    install_requires=required,
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    include_package_data=True,
+    data_files=[("dntdb", find_so_files("dntdb"))],
+)
