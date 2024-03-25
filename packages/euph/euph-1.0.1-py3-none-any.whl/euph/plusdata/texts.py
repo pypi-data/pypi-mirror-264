f143a9adef6b0a43075a66ce6126341514bb7553
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-25 03:03:32 +0000

    Add package euph/1.0.1/euph-1.0.1-py3-none-any.whl

---
+++ packages/euph/euph-1.0.1-py3-none-any.whl/euph/plusdata/texts.py
@@ -0,0 +1,27 @@
+PYPROJECT_TEXT = '''[project]
+name = ""
+version = "0.0.0"
+authors = [
+  { name="", email="" },
+]
+description = ""
+readme = "README.md"
+requires-python = ">=3.11"
+classifiers = [
+    "Programming Language :: Python :: 3",
+    "License :: OSI Approved :: MIT License",
+    "Operating System :: OS Independent",
+]
+
+[project.urls]
+Homepage = ""
+Issues = ""
+'''
+
+MAIN__TEXT = '''if __name__ == '__main__':
+    print('Hello world!')
+'''
+__all__ = (
+    'PYPROJECT_TEXT',
+    'MAIN__TEXT',
+)
