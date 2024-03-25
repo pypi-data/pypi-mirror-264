1d625e88930b0abc6fd6a4c06ea0ce517e6832d7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-25 15:03:38 +0000

    Add package request2-new/0.2.2/request2_new-0.2.2-py3-none-any.whl

---
+++ packages/request2-new/request2_new-0.2.2-py3-none-any.whl/request2/spam/main.py
@@ -0,0 +1,8 @@
+class EnviadorDeSpam:
+    def __init__(self, sessao, enviador):
+        self.sessao = sessao
+        self.enviador = enviador
+
+    def enviar_emails(self, remetente, assunto, corpo):
+        for usuario in self.sessao.listar():
+            self.enviador.enviar(remetente, usuario.email, assunto, corpo)
