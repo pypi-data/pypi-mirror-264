8cbd3eb3be51091b757f05538a08f7d2e4264c59
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-25 04:03:28 +0000

    Add package katuyou/0.1/katuyou-0.1.tar.gz

---
+++ packages/katuyou/katuyou-0.1.tar.gz/katuyou-0.1/katuyou/__init__.py
@@ -0,0 +1,3 @@
+from .conjugations import conjugate_multiple
+from .conjugations import add_da
+from .conjugations import add_to_i_adj
---
+++ packages/katuyou/katuyou-0.1.tar.gz/katuyou-0.1/setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(
+    name='katuyou',
+    version='0.1',
+    packages=['katuyou'],
+    author='Lachlan M.',
+    author_email='azure.fjord.ao@gmail.com',
+    description='Conjugates verbs and i-adjectives according to 8 helping verbs (た、せる、たい、ない、う、て、ます、だ）'
+)
