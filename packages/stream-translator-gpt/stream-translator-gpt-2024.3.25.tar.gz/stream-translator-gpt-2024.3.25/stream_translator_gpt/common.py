9c22e01caa366c3086598497171ef04a19873319
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-25 04:04:12 +0000

    Add package stream-translator-gpt/2024.3.25/stream-translator-gpt-2024.3.25.tar.gz

---
+++ packages/stream-translator-gpt/stream-translator-gpt-2024.3.25.tar.gz/stream-translator-gpt-2024.3.25/stream_translator_gpt/common.py
@@ -0,0 +1,30 @@
+import numpy as np
+from abc import ABC, abstractmethod
+from whisper.audio import SAMPLE_RATE
+
+
+class TranslationTask:
+
+    def __init__(self, audio: np.array, time_range: tuple[float, float]):
+        self.audio = audio
+        self.transcribed_text = None
+        self.translated_text = None
+        self.time_range = time_range
+        self.start_time = None
+
+
+def _auto_args(func, kwargs):
+    names = func.__code__.co_varnames
+    return {k: v for k, v in kwargs.items() if k in names}
+
+
+class LoopWorkerBase(ABC):
+
+    @abstractmethod
+    def loop(self):
+        pass
+
+    @classmethod
+    def work(cls, **kwargs):
+        obj = cls(**_auto_args(cls.__init__, kwargs))
+        obj.loop(**_auto_args(obj.loop, kwargs))
