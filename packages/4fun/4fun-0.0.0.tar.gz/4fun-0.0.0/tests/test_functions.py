687e63bc7d386e733f8cd863170b17b9adaf9c68
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-25 05:02:49 +0000

    Add package 4fun/0.0.0/4fun-0.0.0.tar.gz

---
+++ packages/4fun/4fun-0.0.0.tar.gz/4fun-0.0.0/tests/test_functions.py
@@ -0,0 +1,11 @@
+import pytest
+from 4fun import magic_8_ball
+
+class Tests:
+    def test_roll(self):
+        '''
+        Verify roll returns a non-empty string.
+        '''
+        actual = magic_8_ball.roll() # get the actual return value of the function
+        assert isinstance(actual, str), f"Expected some_function() to return a string. Instead, it returned {actual}"
+        assert len(actual) > 0, f"Expected some_function() not to be empty. Instead, it returned a string with {len(actual)} characters"
