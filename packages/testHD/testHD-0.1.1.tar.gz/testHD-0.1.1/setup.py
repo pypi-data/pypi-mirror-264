83469dc69d8c9ad1d3923822208a500fa814bcfc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-25 15:03:19 +0000

    Add package testHD/0.1.1/testHD-0.1.1.tar.gz

---
+++ packages/testHD/testHD-0.1.1.tar.gz/testHD-0.1.1/setup.py
@@ -0,0 +1,23 @@
+
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="testHD", ## project name
+    version="0.1.1", ## __version__.py 파일과 맞춰주기
+    author="HaeDream", ## 
+    author_email="jhj990203@gmail.com", ##
+    description="This is a package for test HD", ##
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/jhj0203/Portfolio.git", ##
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
