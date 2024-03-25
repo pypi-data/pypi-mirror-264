171cc71783e7f10a170ad3f0dbd940bf6ee7282d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-25 15:03:12 +0000

    Add package aligned/0.0.86/aligned-0.0.86.tar.gz

---
+++ packages/aligned/aligned-0.0.86.tar.gz/aligned-0.0.86/aligned/__init__.py
@@ -0,0 +1,73 @@
+from aligned.compiler.feature_factory import (
+    UUID,
+    Bool,
+    Entity,
+    EventTimestamp,
+    Float,
+    Int32,
+    Int64,
+    Json,
+    String,
+    Timestamp,
+    CustomAggregation,
+    List,
+    Embedding,
+)
+from aligned.compiler.model import model_contract, FeatureInputVersions
+from aligned.data_source.stream_data_source import HttpStreamSource
+from aligned.data_source.batch_data_source import CustomMethodDataSource
+from aligned.feature_store import FeatureStore
+from aligned.feature_view import feature_view, combined_feature_view, check_schema
+from aligned.schemas.text_vectoriser import EmbeddingModel
+from aligned.sources.kafka import KafkaConfig
+from aligned.sources.local import FileSource, Directory, ParquetConfig, CsvConfig
+from aligned.sources.psql import PostgreSQLConfig
+from aligned.sources.redis import RedisConfig
+from aligned.sources.redshift import RedshiftSQLConfig
+from aligned.sources.s3 import AwsS3Config
+from aligned.schemas.feature import FeatureLocation
+
+__all__ = [
+    'FeatureStore',
+    'feature_view',
+    # Batch Data sources
+    'PostgreSQLConfig',
+    'FileSource',
+    'AwsS3Config',
+    'RedshiftSQLConfig',
+    'CustomMethodDataSource',
+    # Stream Data Source
+    'HttpStreamSource',
+    # Online Source
+    'RedisConfig',
+    # Streaming Sources
+    'KafkaConfig',
+    # Types
+    'Entity',
+    'String',
+    'Bool',
+    'Entity',
+    'UUID',
+    'Int32',
+    'Int64',
+    'Float',
+    'EventTimestamp',
+    'Timestamp',
+    'List',
+    'Embedding',
+    'Json',
+    'EmbeddingModel',
+    'feature_view',
+    'combined_feature_view',
+    'model_contract',
+    # Aggregation
+    'CustomAggregation',
+    # Schemas
+    'FeatureLocation',
+    'FeatureInputVersions',
+    'check_schema',
+    'Directory',
+    # File Configs
+    'CsvConfig',
+    'ParquetConfig',
+]
