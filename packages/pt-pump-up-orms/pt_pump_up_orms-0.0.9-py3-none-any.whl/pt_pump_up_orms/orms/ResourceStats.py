b0d5e8328dfb6b7289d352e933da14c5f9faa876
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-25 03:04:33 +0000

    Add package pt-pump-up-orms/0.0.9/pt_pump_up_orms-0.0.9-py3-none-any.whl

---
+++ packages/pt-pump-up-orms/pt_pump_up_orms-0.0.9-py3-none-any.whl/pt_pump_up_orms/orms/ResourceStats.py
@@ -0,0 +1,58 @@
+from pt_pump_up_orms import ORM
+
+
+class ResourceStats(ORM):
+    def __init__(self,
+                 id: int = None,
+                 standard_format: bool = None,
+                 off_the_shelf: bool = None,
+                 preservation_rating: str = None) -> None:
+
+        super().__init__(id, "resource-stats")
+
+        self._standard_format = standard_format
+        self._off_the_shelf = off_the_shelf
+        self._preservation_rating = preservation_rating.lower(
+        ) if preservation_rating is not None else None
+
+    def serialize(self) -> dict:
+        return {
+            "id": self.id,
+            "standard_format": self.standard_format,
+            "off_the_shelf": self.off_the_shelf,
+            "preservation_rating": self.preservation_rating
+        }
+
+    @property
+    def standard_format(self) -> bool:
+        if self._standard_format is None:
+            raise ValueError("Standard Format is not set")
+
+        return self._standard_format
+
+    @standard_format.setter
+    def standard_format(self, standard_format: bool) -> None:
+        self._standard_format = standard_format
+
+    @property
+    def off_the_shelf(self) -> bool:
+        if self._off_the_shelf is None:
+            raise ValueError("Off The Shelf is not set")
+
+        return self._off_the_shelf
+
+    @off_the_shelf.setter
+    def off_the_shelf(self, off_the_shelf: bool) -> None:
+        self._off_the_shelf = off_the_shelf
+
+    @property
+    def preservation_rating(self) -> str:
+        if self._preservation_rating is None:
+            raise ValueError("Preservation Rating is not set")
+
+        return self._preservation_rating
+
+    @preservation_rating.setter
+    def preservation_rating(self, preservation_rating: str) -> None:
+        self._preservation_rating = preservation_rating.lower(
+        ) if preservation_rating is not None else None
---
+++ packages/pt-pump-up-orms/pt_pump_up_orms-0.0.9-py3-none-any.whl/pt_pump_up_orms/orms/Result.py
@@ -0,0 +1,86 @@
+from pt_pump_up_orms.ORM import ORM
+from pt_pump_up_orms.orms import Dataset
+
+
+class Result(ORM):
+    def __init__(self,
+                 id: int = None,
+                 metric: str = None,
+                 value: float = None,
+                 train_dataset: Dataset = None,
+                 validation_dataset: Dataset = None,
+                 test_dataset: Dataset = None,
+                 ) -> None:
+
+        super().__init__(id, "result")
+
+        self._metric = metric
+        self._value = value
+        self._train_dataset = train_dataset
+        self._validation_dataset = validation_dataset
+        self._test_dataset = test_dataset
+
+    def serialize(self) -> dict:
+        return {
+            "id": self.id,
+            "metric": self.metric,
+            "value": self.value,
+            "train_dataset": self.train_dataset.serialize(),
+            "validation_dataset": self.validation_dataset.serialize(),
+            "test_dataset": self.test_dataset.serialize()
+        }
+
+    @property
+    def metric(self) -> str:
+        if self._metric is None:
+            raise ValueError("Metric is not set")
+
+        return self._metric
+
+    @metric.setter
+    def metric(self, metric: str) -> None:
+        self._metric = metric
+
+    @property
+    def value(self) -> float:
+        if self._value is None:
+            raise ValueError("Value is not set")
+
+        return self._value
+
+    @value.setter
+    def value(self, value: float) -> None:
+        self._value = value
+
+    @property
+    def train_dataset(self) -> Dataset:
+        if self._train_dataset is None:
+            raise ValueError("Train Dataset is not set")
+
+        return self._train_dataset
+
+    @train_dataset.setter
+    def train_dataset(self, train_dataset: Dataset) -> None:
+        self._train_dataset = train_dataset
+
+    @property
+    def validation_dataset(self) -> Dataset:
+        if self._validation_dataset is None:
+            raise ValueError("Validation Dataset is not set")
+
+        return self._validation_dataset
+
+    @validation_dataset.setter
+    def validation_dataset(self, validation_dataset: Dataset) -> None:
+        self._validation_dataset = validation_dataset
+
+    @property
+    def test_dataset(self) -> Dataset:
+        if self._test_dataset is None:
+            raise ValueError("Test Dataset is not set")
+
+        return self._test_dataset
+
+    @test_dataset.setter
+    def test_dataset(self, test_dataset: Dataset) -> None:
+        self._test_dataset = test_dataset
