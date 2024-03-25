47512d80959b90724490300a89439742854ddceb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-25 14:03:29 +0000

    Add package LatexGenerator-AP2024/1.0/LatexGenerator_AP2024-1.0.tar.gz

---
+++ packages/LatexGenerator-AP2024/LatexGenerator_AP2024-1.0.tar.gz/LatexGenerator_AP2024-1.0/setup.py
@@ -0,0 +1,16 @@
+import setuptools
+
+setuptools.setup(
+    name='LatexGenerator_AP2024',
+    version='1.0',
+    author='Daniil_B',
+    # py_modules=['LatexGeneratorAP2024/LatexGeneratorAP2024'],
+    description='latex python LatexGeneratorAP2024',
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6'
+)
