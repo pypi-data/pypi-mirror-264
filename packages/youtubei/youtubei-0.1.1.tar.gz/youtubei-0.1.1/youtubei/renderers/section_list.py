7b2c1c1204261dfc6fcc610e4210a81eea695b4c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-25 03:04:13 +0000

    Add package youtubei/0.1.1/youtubei-0.1.1.tar.gz

---
+++ packages/youtubei/youtubei-0.1.1.tar.gz/youtubei-0.1.1/youtubei/renderers/section_list.py
@@ -0,0 +1,30 @@
+from typing import Optional, Sequence, Union
+
+from youtubei._registries import ANDROID_REGISTRY, WEB_REGISTRY, WEB_REMIX_REGISTRY
+from youtubei.models.data import NextContinuationData
+from youtubei.parse import Dynamic
+from youtubei.renderers.item_section import ItemSectionRenderer
+from youtubei.renderers.music_carousel_shelf import MusicCarouselShelfRenderer
+from youtubei.renderers.music_playlist_shelf import MusicPlaylistShelfRenderer
+from youtubei.renderers.music_shelf import MusicShelfRenderer
+
+from ._base import BaseRenderer
+
+__all__ = ("SectionListRenderer",)
+
+
+@WEB_REMIX_REGISTRY
+@WEB_REGISTRY
+@ANDROID_REGISTRY
+class SectionListRenderer(BaseRenderer):
+    contents: Sequence[
+        Dynamic[
+            Union[
+                ItemSectionRenderer,
+                MusicPlaylistShelfRenderer,
+                MusicCarouselShelfRenderer,
+                MusicShelfRenderer,
+            ]
+        ]
+    ]
+    continuations: Optional[Sequence[Dynamic[NextContinuationData]]] = None
