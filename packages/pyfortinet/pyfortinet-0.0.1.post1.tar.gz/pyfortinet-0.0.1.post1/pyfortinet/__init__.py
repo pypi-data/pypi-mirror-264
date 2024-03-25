f331dcae9c7d2319b24c986577ad3e3de44a8947
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-25 14:03:29 +0000

    Add package pyfortinet/0.0.1.post1/pyfortinet-0.0.1.post1.tar.gz

---
+++ packages/pyfortinet/pyfortinet-0.0.1.post1.tar.gz/pyfortinet-0.0.1.post1/pyfortinet/__init__.py
@@ -0,0 +1,10 @@
+"""Fortinet Python library"""
+__version__ = "0.0.1.post1"
+
+from pyfortinet.fmg_api.async_fmgbase import AsyncFMGBase, AsyncFMGResponse
+from pyfortinet.fmg_api.fmg import FMG
+from pyfortinet.fmg_api.async_fmg import AsyncFMG
+from pyfortinet.fmg_api.fmgbase import FMGBase, FMGResponse
+from pyfortinet.settings import FMGSettings
+
+__all__ = ("FMGBase", "FMG", "FMGResponse", "AsyncFMGBase", "AsyncFMG", "AsyncFMGResponse", "FMGSettings")
---
+++ packages/pyfortinet/pyfortinet-0.0.1.post1.tar.gz/pyfortinet-0.0.1.post1/pyfortinet/exceptions.py
@@ -0,0 +1,57 @@
+"""FMG API exceptions"""
+
+
+class FMGException(Exception):
+    """General FMG error"""
+
+
+class FMGConfigurationException(FMGException):
+    """FMG configuration problem"""
+
+
+class FMGTokenException(FMGException):
+    """No Token error"""
+
+
+class FMGAuthenticationException(FMGException):
+    """Authentication error"""
+
+
+class FMGLockNeededException(FMGException):
+    """Lock needed error"""
+
+
+class FMGLockException(FMGException):
+    """Locking error"""
+
+
+class FMGWrongRequestException(FMGException):
+    """Locking error"""
+
+
+class FMGUnhandledException(FMGException):
+    """Unhandled error"""
+
+
+class FMGEmptyResultException(FMGException):
+    """No result for a request"""
+
+
+class FMGInvalidDataException(FMGException):
+    """The data is invalid for selected url"""
+
+
+class FMGObjectAlreadyExistsException(FMGException):
+    """The object is already in the database"""
+
+
+class FMGMissingScopeException(FMGException):
+    """Scope must be set before referencing URL"""
+
+
+class FMGInvalidURL(FMGException):
+    """Invalid URL"""
+
+
+class FMGNotAssignedException(FMGException):
+    """FMG not assigned to object"""
