041095929b05fc25d1946809fa31fc277faf9c87
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-25 03:04:35 +0000

    Add package flect/0.2.8/flect-0.2.8.tar.gz

---
+++ packages/flect/flect-0.2.8.tar.gz/flect-0.2.8/src/python-flect/src/flect/application.py
@@ -0,0 +1,34 @@
+import asyncio
+import pathlib
+from types import ModuleType
+from typing import Any, Optional
+
+from fastapi import FastAPI
+from fastapi.staticfiles import StaticFiles
+
+from flect.routing import configure_app_router
+
+
+class flect(FastAPI):
+    def __init__(
+        self,
+        app: ModuleType,
+        prebuilt_uri: Optional[str] = None,
+        **kwargs: Any,
+    ) -> None:
+        super().__init__(**kwargs)
+        self.app_module = app
+        self.prebuilt_uri = self.validate_prebuilt_uri(prebuilt_uri)
+        self.reload_event = asyncio.Event()
+        self.setup_flect()
+
+    def setup_flect(self) -> None:
+        app_router = configure_app_router(self.app_module, self.prebuilt_uri)
+        self.include_router(app_router, tags=["flect"])
+
+    def validate_prebuilt_uri(self, prebuilt_uri: Optional[str]) -> Optional[str]:
+        if prebuilt_uri is not None:
+            if not prebuilt_uri.startswith("http"):
+                self.mount("/static", StaticFiles(directory=pathlib.Path(prebuilt_uri)), name="static")
+                prebuilt_uri = "/static"
+        return prebuilt_uri
