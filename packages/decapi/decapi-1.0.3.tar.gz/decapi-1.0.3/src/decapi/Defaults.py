22724b805d6c84f1c926d2243c300995054a74ca
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-25 15:02:58 +0000

    Add package decapi/1.0.3/decapi-1.0.3.tar.gz

---
+++ packages/decapi/decapi-1.0.3.tar.gz/decapi-1.0.3/src/decapi/Defaults.py
@@ -0,0 +1,11 @@
+class Defaults:
+    """
+    Class for DECTalk default voices.
+    Available voices:
+        Paul
+    """
+    def __init__(self):
+        self.Paul=Voice(112,100,280,330,18,0,3300,3650,18,100,0,0,10,100,40,80,30,25,1)
+        """
+        Perfect Paul.
+        """
