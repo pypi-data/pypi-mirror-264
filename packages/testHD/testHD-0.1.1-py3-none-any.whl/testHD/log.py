b8269aedbda56369f71a443fc5c10df65b9627c9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-25 15:03:19 +0000

    Add package testHD/0.1.1/testHD-0.1.1-py3-none-any.whl

---
+++ packages/testHD/testHD-0.1.1-py3-none-any.whl/testHD/log.py
@@ -0,0 +1,38 @@
+import requests
+
+class ModelRunner:
+
+    server_url = "http://localhost:8088/save_data"
+
+    def run_model(self, model_name, project_name, input_data, api_key, output_data=None):
+        # 모델 실행 및 결과 반환
+        # output_data = self._execute_model(model_name, project_name, input_data, api_key)
+        
+        # 결과를 JSON 형식으로 변환
+        json_output = self._format_output(model_name, project_name, input_data, output_data, api_key)
+        
+        # 결과를 서버로 전송하여 저장
+        self._save_to_server(json_output)
+        
+        # 결과 반환
+        return json_output
+
+    def _format_output(self, model_name, project_name, input_data, output_data, api_key):
+        # 결과를 JSON 형식으로 포맷팅합니다.
+        result = {
+            "model_name": model_name,
+            "project_name": project_name,
+            "input_data": input_data,
+            "output_data": output_data,
+            "api_key": api_key
+        }
+        return result
+
+    def _save_to_server(self, data):
+        # 서버로 데이터를 전송하여 저장합니다.
+        response = requests.post(self.server_url, json=data)
+        if response.status_code == 200:
+            print("Data successfully saved to server.")
+        else:
+            print("Failed to save data to server.")
+            print(response.status_code)
