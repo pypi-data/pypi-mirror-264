b247043116332dd84fef1df2c7b130e91f737e5e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-25 03:04:10 +0000

    Add package optivision/0.1.0/optivision-0.1.0.tar.gz

---
+++ packages/optivision/optivision-0.1.0.tar.gz/optivision-0.1.0/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="optivision",
+    version="0.1.0",
+    author="Preet Sojitra",
+    author_email="preet.dev373@gmail.com",
+    description="A simple image processing library",
+    packages=find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.6",
+)
