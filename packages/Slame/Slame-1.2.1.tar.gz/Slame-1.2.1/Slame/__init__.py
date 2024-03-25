f59ff0e8c22b0859c2085aa8778cd26f6642848b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-25 03:03:52 +0000

    Add package Slame/1.2.1/Slame-1.2.1.tar.gz

---
+++ packages/Slame/Slame-1.2.1.tar.gz/Slame-1.2.1/Slame/__init__.py
@@ -0,0 +1,12 @@
+"""
+:authors: ZHRXXgroup
+:license: MIT License
+
+:copyright: (c) ZHRXXgroup
+"""
+
+from slame import (ZHRXX, Work_with_Database,Sessions, Plugins)
+
+__author__ = 'ZHRXXgroup'
+__version__ = '1.2'
+__email__ = 'info@zhrxxgroup.com'
