fdcc77edcf2b5a48afeca3c15d74415366d34832
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-25 16:04:34 +0000

    Add package dallCrawler/0.0.6/dallCrawler-0.0.6-py3-none-any.whl

---
+++ packages/dallCrawler/dallCrawler-0.0.6-py3-none-any.whl/dallCrawler/crawler.py
@@ -0,0 +1,30 @@
+import requests
+from typing_extensions import Self
+import json
+from bs4 import BeautifulSoup
+
+class Crawler():
+    def __init__(self, **kwargs):
+        self.config = dict()
+        for key, value in kwargs.items():
+            self.config[key] = value
+    
+    def crawl(self, *params, **kwargs) -> str:
+        if('url' in self.params.keys()):
+            try:
+                response = requests.get(self.config['url'].format(*params))
+                if 'type' in kwargs.keys():
+                    if kwargs['type'] == 'json':
+                        return json.loads(response.text)
+                    elif kwargs['type'] == 'html':
+                        soup = BeautifulSoup(response.text, 'html.parser')
+                        return soup
+                return response.text
+            except:
+                raise Exception("Number of paremeters {}, not match number of parameters in url {}".format(len(params) , self.config['url'].count("{}")))
+        else:
+            raise Exception("url parameter not set in crawler object. Use setUrl(url) method for setting it.")
+    
+    def setUrl(self, url: str) -> Self:
+        self.params['url'] = url
+        return self
