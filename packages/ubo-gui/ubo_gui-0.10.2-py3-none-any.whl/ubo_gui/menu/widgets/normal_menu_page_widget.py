36cbd64b6e82b47f665a16bb68113511c97dacd1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-25 03:04:25 +0000

    Add package ubo-gui/0.10.2/ubo_gui-0.10.2-py3-none-any.whl

---
+++ packages/ubo-gui/ubo_gui-0.10.2-py3-none-any.whl/ubo_gui/menu/widgets/normal_menu_page_widget.py
@@ -0,0 +1,19 @@
+"""Module for the `NormalMenuPageWidget` class."""
+
+import pathlib
+
+from kivy.lang.builder import Builder
+
+from ubo_gui.page import PageWidget
+
+
+class NormalMenuPageWidget(PageWidget):
+    """renders a normal page of a `Menu`."""
+
+
+Builder.load_file(
+    pathlib.Path(__file__)
+    .parent.joinpath('normal_menu_page_widget.kv')
+    .resolve()
+    .as_posix(),
+)
