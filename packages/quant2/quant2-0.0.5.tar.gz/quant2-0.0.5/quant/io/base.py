9107b8fc86eb40aa3ba5a1d7fdd8a0c732003b65
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-25 14:03:12 +0000

    Add package quant2/0.0.5/quant2-0.0.5.tar.gz

---
+++ packages/quant2/quant2-0.0.5.tar.gz/quant2-0.0.5/quant/io/base.py
@@ -0,0 +1,13 @@
+import json
+
+
+def load_json(file):
+    with open(file, "r", encoding="utf8") as f:
+        data = json.load(f)
+    return data
+
+
+def save_json(file, data):
+    with open(file, "w", encoding="utf8") as f:
+        json.dump(data, f, ensure_ascii=False)
+    return file
