0bf01ed8af378d0466413c82eb68c7d357a98b08
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-25 04:04:10 +0000

    Add package kagtool/3.0.0/kagtool-3.0.0.tar.gz

---
+++ packages/kagtool/kagtool-3.0.0.tar.gz/kagtool-3.0.0/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='kagtool',
+    version='3.0.0',
+    description='Helpers For Kaggle Competition',
+    author='Your Name',
+    author_email='your@email.com',
+    packages=['kagtool'],
+    install_requires=[
+        'kaggle',
+    ],
+)
