99c6e87f0721de19b43feecee847607244ad9027
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-25 03:03:45 +0000

    Add package threed-optix/4.2.3/threed_optix-4.2.3.tar.gz

---
+++ packages/threed-optix/threed_optix-4.2.3.tar.gz/threed_optix-4.2.3/setup.py
@@ -0,0 +1,40 @@
+from setuptools import setup, find_packages
+from setuptools.command.develop import develop
+from setuptools.command.install import install
+import subprocess
+import os
+
+
+with open('README.md') as f:
+    long_description = f.read()
+
+setup(
+    name='threed_optix',
+    version='4.2.3',
+    license='MIT',
+    author="3DOptix",
+    author_email='ereztep@3doptix.com',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    packages=find_packages('src'),
+    package_dir={'': 'src'},
+    url='https://3doptix.com',
+    keywords=["Optics", "Optical Design", "Optical Simulation", "Optical Design Software", "3DOptix"],
+    install_requires=[
+        'requests',
+        'pandas',
+        'matplotlib',
+        'plotly',
+        'colorama',
+        'nbformat',
+        'numpy',
+        'scikit-image',
+        'scipy',
+        'opencv-python',
+        'dill',
+        "ply",
+        "networkx",
+        "pytest",
+        #'matlab2python @ git+https://github.com/ebranlard/matlab2python.git#egg=matlab2python',
+    ],
+)
