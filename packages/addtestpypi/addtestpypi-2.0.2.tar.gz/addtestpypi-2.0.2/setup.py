63d0ba75bae7b3833fb126b202479eb2d21437c4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-25 03:03:32 +0000

    Add package addtestpypi/2.0.2/addtestpypi-2.0.2.tar.gz

---
+++ packages/addtestpypi/addtestpypi-2.0.2.tar.gz/addtestpypi-2.0.2/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='addtestpypi',
+    version='2.0.2',
+    packages=find_packages(),
+    install_requires=[],  # We don't have any dependencies for this example
+    author='Yamini',
+    author_email='kyamini21@gmail.com',
+    description='A simple library for adding numbers',
+    url='https://github.com/yamini-2195/addtestpypi',
+)
