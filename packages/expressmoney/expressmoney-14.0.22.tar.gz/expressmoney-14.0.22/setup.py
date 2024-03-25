7746b65d457118914a37c41ce3aa738d6921e374
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-25 03:04:15 +0000

    Add package expressmoney/14.0.22/expressmoney-14.0.22.tar.gz

---
+++ packages/expressmoney/expressmoney-14.0.22.tar.gz/expressmoney-14.0.22/setup.py
@@ -0,0 +1,16 @@
+"""
+py setup.py sdist
+twine upload dist/expressmoney-14.0.22.tar.gz
+"""
+import setuptools
+
+setuptools.setup(
+    name='expressmoney',
+    packages=setuptools.find_packages(),
+    version='14.0.22',
+    description='SDK ExpressMoney',
+    author='Development team',
+    author_email='dev@expressmoney.com',
+    install_requires=('python-json-logger==2.0.4', 'django-phonenumber-field[phonenumberslite]==5.2.0'),
+    python_requires='>=3.7',
+)
