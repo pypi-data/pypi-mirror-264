0771554eeaa3d32c74c77dd90627dd0f3fb7734c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-25 04:03:20 +0000

    Add package signaturizer3D/0.1.8/signaturizer3d-0.1.8-py3-none-any.whl

---
+++ packages/signaturizer3D/signaturizer3d-0.1.8-py3-none-any.whl/signaturizer3d/inference.py
@@ -0,0 +1,47 @@
+import logging
+from typing import TYPE_CHECKING
+
+import numpy as np
+import torch
+from torch.utils.data import DataLoader
+from tqdm import tqdm
+
+from signaturizer3d.normalize.normalize import normalize_for_space
+
+if TYPE_CHECKING:
+    from signaturizer3d.model import MolDataset, UniMolModel
+    from signaturizer3d.space import CCSpace
+
+logger = logging.getLogger(__name__)
+
+
+def run_inference(
+    model: "UniMolModel",
+    space: "CCSpace",
+    dataset: "MolDataset",
+    device,
+    batch_size: int = 32,
+):
+    model = model.to(device)
+
+    dataloader = DataLoader(
+        dataset=dataset,
+        batch_size=batch_size,
+        collate_fn=model.batch_collate_fn,
+        shuffle=False,
+        drop_last=False,
+    )
+    model = model.eval()
+    raw_outputs_list = []
+    logging.info(f"{space.name} starting inference")
+    for batch in tqdm(dataloader):
+        input_data, _ = batch
+        input_data = {k: v.to(device) for k, v in input_data.items()}
+        with torch.no_grad():
+            raw_outputs = model(**input_data, features_only=True)
+            normalized_output = normalize_for_space(raw_outputs, space=space)
+            raw_outputs_list.append(normalized_output.cpu().numpy())
+
+    logging.info(f"{space.name} finished inference")
+    raw_outputs_array = np.concatenate(raw_outputs_list, axis=0)
+    return raw_outputs_array
---
+++ packages/signaturizer3D/signaturizer3d-0.1.8-py3-none-any.whl/signaturizer3d/signaturizer.py
@@ -0,0 +1,46 @@
+from typing import List
+
+import numpy as np
+import numpy.typing as npt
+
+from signaturizer3d.input.sdf import gather_sdf_data
+from signaturizer3d.model import FineTunedUniMol
+from signaturizer3d.space import CCSpace
+
+
+class Signaturizer:
+    def __init__(self, space: CCSpace, local_weights_path: str = None):
+        if not isinstance(space, CCSpace):
+            try:
+                space = CCSpace[space]
+            except KeyError:
+                raise ValueError(
+                    f"{space} is not a valid CCSpace. Valid spaces are A1 ... E5, see the CCSpace enum."
+                )
+        self.space = space
+
+        # Hydrogens are always removed as the signaturizers models are fine
+        # tuned from the pre-trained UniMol model trained without hydrogens
+        self.model = FineTunedUniMol(
+            space=self.space, remove_hs=True, local_weights_path=local_weights_path
+        )
+
+    def infer_from_coordinates(
+        self, atoms: List[str], coordinates: List[List[float]]
+    ) -> npt.NDArray[np.float32]:  # (n, 128) dim array for n signatures
+        sig_output = self.model.get_sig4_coordinates(atoms, coordinates)
+        return sig_output
+
+    def infer_from_smiles(self, smiles_list: List[str]) -> npt.NDArray[np.float32]:
+        sig_output = self.model.get_sig4_smiles(smiles_list)
+        return sig_output
+
+    def infer_from_sdf(self, path: str) -> npt.NDArray[np.float32]:
+        """
+        Perform inference on molecules in an SDF file or all SDF files within a directory.
+
+        Parameters:
+        path (str): Path to an SDF file or a directory containing SDF files.
+        """
+        atoms_list, coords_list = gather_sdf_data(path)
+        return self.infer_from_coordinates(atoms_list, coords_list)
