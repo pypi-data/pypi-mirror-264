c8c52a8a26bf7b310cf766877418a92fc720f973
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-25 15:03:14 +0000

    Add package zimscraperlib/3.3.2/zimscraperlib-3.3.2.tar.gz

---
+++ packages/zimscraperlib/zimscraperlib-3.3.2.tar.gz/zimscraperlib-3.3.2/src/zimscraperlib/image/transformation.py
@@ -0,0 +1,66 @@
+#!/usr/bin/env python3
+# vim: ai ts=4 sts=4 et sw=4 nu
+
+import io
+import pathlib
+from typing import Optional, Union
+
+import PIL
+from resizeimage import resizeimage
+
+from zimscraperlib.constants import ALPHA_NOT_SUPPORTED
+from zimscraperlib.image.utils import save_image
+
+
+def resize_image(
+    src: Union[pathlib.Path, io.BytesIO],
+    width: int,
+    height: Optional[int] = None,
+    dst: Optional[Union[pathlib.Path, io.BytesIO]] = None,
+    method: Optional[str] = "width",
+    allow_upscaling: Optional[bool] = True,  # noqa: FBT002
+    **params: str,
+) -> None:
+    """resize an image to requested dimensions
+
+    methods: width, height, cover, thumbnail
+    allow upscaling: upscale image first, preserving aspect ratio if required"""
+    with PIL.Image.open(src) as image:  # pyright: ignore
+        # preserve image format as resize() does not transmit it into new object
+        image_format = image.format
+        image_mode = image.mode
+
+        # upscale if required preserving the aspect ratio
+        if allow_upscaling:
+            height_width_ratio = float(image.size[1]) / float(image.size[0])
+            if image.size[0] < width:
+                image = image.resize(  # noqa: PLW2901
+                    (width, int(width * height_width_ratio))
+                )
+            if height and image.size[1] < height:
+                image = image.resize(  # noqa: PLW2901
+                    (int(height / height_width_ratio), height)
+                )
+
+        # resize using the requested method
+        if method == "width":
+            resized = resizeimage.resize(method, image, width)
+        elif method == "height":
+            resized = resizeimage.resize(method, image, height)
+        else:
+            resized = resizeimage.resize(method, image, [width, height])
+
+    # remove alpha layer if not supported and added during resizing
+    if resized.mode == "RGBA" and image_format in ALPHA_NOT_SUPPORTED:
+        resized = resized.convert(image_mode)
+
+    # reset src if it's a byte stream and should be resized in-place
+    if dst is None and isinstance(src, io.BytesIO):
+        src.seek(0)
+
+    save_image(
+        resized,
+        dst if dst is not None else src,  # pyright: ignore
+        image_format,
+        **params,
+    )
---
+++ packages/zimscraperlib/zimscraperlib-3.3.2.tar.gz/zimscraperlib-3.3.2/src/zimscraperlib/video/presets.py
@@ -0,0 +1,118 @@
+#!/usr/bin/env python3
+# vim: ai ts=4 sts=4 et sw=4 nu
+
+from __future__ import annotations
+
+from typing import ClassVar, Optional, Union
+
+from zimscraperlib.video.config import Config
+
+preset_type = "video"
+
+
+class VoiceMp3Low(Config):
+    """Low quality mp3 audio
+
+    44Khz audio sampling
+    48k audio bitrate
+    audio-only"""
+
+    VERSION = 1
+
+    ext = "mp3"
+    mimetype = "audio/mp3"
+
+    options: ClassVar[dict[str, Optional[Union[str, bool, int]]]] = {
+        "-vn": "",  # remove video stream
+        "-codec:a": "mp3",  # audio codec
+        "-ar": "44100",  # audio sampling rate
+        "-b:a": "48k",  # target audio bitrate
+    }
+
+
+class VideoWebmLow(Config):
+    """Low Quality webm video
+
+    480:h format with height adjusted to keep aspect ratio
+    128k target video bitrate but stay within quality boundaries.
+    48k audio bitrate"""
+
+    VERSION = 2
+
+    ext = "webm"
+    mimetype = f"{preset_type}/webm"
+
+    options: ClassVar[dict[str, Optional[Union[str, bool, int]]]] = {
+        "-codec:v": "libvpx",  # video codec
+        "-quality": "best",  # codec preset
+        "-b:v": "128k",  # Adjust quantizer within min/max to target this bitrate
+        "-qmin": "18",  # Reduce the bitrate on very still videos
+        "-qmax": "40",  # Increase the bitrate on very busy videos
+        "-vf": "scale='480:trunc(ow/a/2)*2'",  # frame size
+        "-codec:a": "libvorbis",  # audio codec
+        "-ar": "44100",  # audio sampling rate
+        "-b:a": "48k",  # target audio bitrate
+    }
+
+
+class VideoMp4Low(Config):
+    """Low Quality mp4 video
+
+    480:h format with height adjusted to keep aspect ratio
+    300k video bitrate
+    48k audio bitrate
+    highly degraded quality (30, 42)"""
+
+    VERSION = 1
+
+    ext = "mp4"
+    mimetype = f"{preset_type}/mp4"
+
+    options: ClassVar[dict[str, Optional[Union[str, bool, int]]]] = {
+        "-codec:v": "h264",  # video codec
+        "-b:v": "300k",  # target video bitrate
+        "-maxrate": "300k",  # max video bitrate
+        "-minrate": "300k",  # min video bitrate
+        "-qmin": "30",  # min quantizer scale
+        "-qmax": "42",  # max quantizer scale
+        "-vf": "scale='480:trunc(ow/a/2)*2'",  # frame size
+        "-codec:a": "aac",  # audio codec
+        "-ar": "44100",  # audio sampling rate
+        "-b:a": "48k",  # target audio bitrate
+        "-movflags": "+faststart",  # extra flag
+    }
+
+
+class VideoWebmHigh(Config):
+    """High Quality webm video
+
+    25 constant quality"""
+
+    VERSION = 1
+
+    ext = "webm"
+    mimetype = f"{preset_type}/webm"
+
+    options: ClassVar[dict[str, Optional[Union[str, bool, int]]]] = {
+        "-codec:v": "libvpx",  # video codec
+        "-codec:a": "libvorbis",  # audio codec
+        "-crf": "25",  # constant quality, lower value gives better qual and larger size
+        "-b:v": "0",  # must be passed if using constant quality mode via -cbr for codec
+    }
+
+
+class VideoMp4High(Config):
+    """Low Quality mp4 video
+
+    20 constant quality"""
+
+    VERSION = 1
+
+    ext = "mp4"
+    mimetype = f"{preset_type}/mp4"
+
+    options: ClassVar[dict[str, Optional[Union[str, bool, int]]]] = {
+        "-codec:v": "h264",  # video codec
+        "-codec:a": "aac",  # audio codec
+        "-crf": "20",  # constant quality, lower value gives better qual and larger size
+    }
