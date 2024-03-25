108366bfa210456bfbf59b73bd2c9a0339c05906
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-25 05:02:46 +0000

    Add package zuspec-arl-dm/0.0.1.8412024765/zuspec_arl_dm-0.0.1.8412024765-cp312-cp312-manylinux_2_17_x86_64.manylinux2014_x86_64.whl

---
+++ packages/zuspec-arl-dm/zuspec_arl_dm-0.0.1.8412024765-cp312-cp312-manylinux_2_17_x86_64.manylinux2014_x86_64.whl/zsp_arl_dm/share/include/zsp/arl/dm/IDataTypeActivityTraverseType.h
@@ -0,0 +1,25 @@
+/*
+ * IDataTypeActivityTraverseType.h
+ *
+ *  Created on: Jun 18, 2022
+ *      Author: mballance
+ */
+
+#pragma once
+#include "zsp/arl/dm/IDataTypeActivity.h"
+
+namespace zsp {
+namespace arl {
+namespace dm {
+
+
+class IDataTypeActivityTraverseType : public virtual IDataTypeActivity {
+public:
+
+	virtual ~IDataTypeActivityTraverseType() { }
+
+};
+
+}
+}
+}
