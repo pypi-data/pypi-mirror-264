7bb050073231ac05368828411f83c8c4706913db
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-25 03:04:20 +0000

    Add package cira/3.0.0/cira-3.0.0-py3-none-any.whl

---
+++ packages/cira/cira-3.0.0-py3-none-any.whl/cira/__init__.py
@@ -0,0 +1,20 @@
+"""
+Cira
+
+A simpler libray for alpaca-trade-api from Alpaca Markets.
+"""
+
+from . import auth
+from . import config
+from . import util
+from . import log
+
+from .exchange import Exchange, DemoExchange
+from .asset import Asset, Stock, Cryptocurrency
+from .portfolio import Portfolio, Position
+
+import alpaca
+
+__version__ = "3.0.0"
+__author__ = "Axel Gard"
+__credits__ = "alpaca.markets"
