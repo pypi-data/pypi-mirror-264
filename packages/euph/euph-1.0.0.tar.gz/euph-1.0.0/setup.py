7bbd77cde2918b9d6c1fe6ddeccf7f7347b570d5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-25 03:04:00 +0000

    Add package euph/1.0.0/euph-1.0.0.tar.gz

---
+++ packages/euph/euph-1.0.0.tar.gz/euph-1.0.0/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+
+setup(
+    long_description="Module for fast initialization of packages",
+    long_description_content_type="text/markdown",
+)
+
+'''
+python -m build
+twine upload dist/*
+'''
