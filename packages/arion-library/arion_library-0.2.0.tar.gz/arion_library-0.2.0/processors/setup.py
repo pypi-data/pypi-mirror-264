9dff9b8be0532a9131384a0a7878339e9b2f3701
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-25 14:03:02 +0000

    Add package arion-library/0.2.0/arion_library-0.2.0.tar.gz

---
+++ packages/arion-library/arion_library-0.2.0.tar.gz/arion_library-0.2.0/processors/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='arion_library',  # Replace with your package name
+    version='0.2.0',  # Replace with your package version
+    author='Heni Nechi',  # Replace with your name
+    author_email='h.nechi@arion-tech.com',  # Replace with your email
+    url='https://github.com/HaniNechi/arion_library',  # Replace with your repository URL
+    packages=find_packages(),  # Automatically find all packages
+    python_requires='>=3.8',  # Specify Python version requirements
+    install_requires=[
+        'pyodbc',
+        'pytest',
+        'azure-core'
+        'azure-data-tables'
+        'azure-storage-blob'
+        'python-dotenv'
+        'pytest'
+    ],
+)
