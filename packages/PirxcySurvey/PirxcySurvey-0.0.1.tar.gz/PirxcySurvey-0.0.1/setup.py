e6e67e0b5d59dc048f35a10acd9f377284178240
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-25 04:03:33 +0000

    Add package PirxcySurvey/0.0.1/PirxcySurvey-0.0.1.tar.gz

---
+++ packages/PirxcySurvey/PirxcySurvey-0.0.1.tar.gz/PirxcySurvey-0.0.1/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+setup(
+name='PirxcySurvey',
+version='0.0.1',
+author='pirxcy',
+description='Survey package fixed for exe!',
+packages=find_packages(),
+classifiers=[
+'Programming Language :: Python :: 3',
+'License :: OSI Approved :: MIT License',
+'Operating System :: OS Independent',
+],
+python_requires='>=3.10',
+)
