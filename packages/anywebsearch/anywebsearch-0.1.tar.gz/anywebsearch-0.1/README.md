5bfb31036ae1f17510aede4b0c470b3a580c8969
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-25 04:03:21 +0000

    Add package anywebsearch/0.1/anywebsearch-0.1.tar.gz

---
+++ packages/anywebsearch/anywebsearch-0.1.tar.gz/anywebsearch-0.1/README.md
@@ -0,0 +1,3 @@
+# Any Web Search
+
+Unified internet search across different search engines: Google, Bing(ddg), Brave, Qwant, Yandex
---
+++ packages/anywebsearch/anywebsearch-0.1.tar.gz/anywebsearch-0.1/anywebsearch/engines/__init__.py
@@ -0,0 +1,5 @@
+from .google import google_search
+from .qwant import qwant_search
+from .duck import duck_search
+from .brave import brave_search
+from .yandex import yandex_search
