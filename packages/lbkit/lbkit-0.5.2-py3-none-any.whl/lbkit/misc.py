97de983e4d6f9ed06d9beda8929eab9052b87491
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-25 03:04:19 +0000

    Add package lbkit/0.5.2/lbkit-0.5.2-py3-none-any.whl

---
+++ packages/lbkit/lbkit-0.5.2-py3-none-any.whl/lbkit/misc.py
@@ -0,0 +1,45 @@
+import os
+import argparse
+import textwrap
+from colorama import Fore, Style
+
+
+LOG_DIR = "/tmp/lbkit/log"
+
+class Color(object):
+    """ Wrapper around colorama colors that are undefined in importing
+    """
+    RED = Fore.RED  # @UndefinedVariable
+    WHITE = Fore.WHITE  # @UndefinedVariable
+    CYAN = Fore.CYAN  # @UndefinedVariable
+    GREEN = Fore.GREEN  # @UndefinedVariable
+    MAGENTA = Fore.MAGENTA  # @UndefinedVariable
+    BLUE = Fore.BLUE  # @UndefinedVariable
+    YELLOW = Fore.YELLOW  # @UndefinedVariable
+    BLACK = Fore.BLACK  # @UndefinedVariable
+    RESET_ALL = Style.RESET_ALL
+
+    BRIGHT_RED = Style.BRIGHT + Fore.RED  # @UndefinedVariable
+    BRIGHT_BLUE = Style.BRIGHT + Fore.BLUE  # @UndefinedVariable
+    BRIGHT_YELLOW = Style.BRIGHT + Fore.YELLOW  # @UndefinedVariable
+    BRIGHT_GREEN = Style.BRIGHT + Fore.GREEN  # @UndefinedVariable
+    BRIGHT_CYAN = Style.BRIGHT + Fore.CYAN   # @UndefinedVariable
+    BRIGHT_WHITE = Style.BRIGHT + Fore.WHITE   # @UndefinedVariable
+    BRIGHT_MAGENTA = Style.BRIGHT + Fore.MAGENTA   # @UndefinedVariable
+
+
+if os.environ.get("COLOR_DARK", 0):
+    Color.WHITE = Fore.BLACK
+    Color.CYAN = Fore.BLUE
+    Color.YELLOW = Fore.MAGENTA
+    Color.BRIGHT_WHITE = Fore.BLACK
+    Color.BRIGHT_CYAN = Fore.BLUE
+    Color.BRIGHT_YELLOW = Fore.MAGENTA
+    Color.BRIGHT_GREEN = Fore.GREEN
+
+class SmartFormatter(argparse.HelpFormatter):
+    """重写HelpFormatter"""
+    def _fill_text(self, text, width, indent):
+        """重写HelpFormatter"""
+        text = textwrap.dedent(text)
+        return ''.join(indent + line for line in text.splitlines(True))
