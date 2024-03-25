b15659c0c64265c3a3c2e22397e535f22b188d1b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-25 05:02:56 +0000

    Add package wagtail-editorjs/1.2.2/wagtail_editorjs-1.2.2.tar.gz

---
+++ packages/wagtail-editorjs/wagtail_editorjs-1.2.2.tar.gz/wagtail_editorjs-1.2.2/wagtail_editorjs/urls.py
@@ -0,0 +1,28 @@
+from django.urls import path, include
+from .views import (
+    images,
+    attaches,
+)
+
+app_name = 'wagtail_editorjs'
+
+
+urlpatterns = [
+    # URL to format in javascript side.
+    path('images/', images.image_for_id, name='image_for_id_fmt'),
+    path('images/<int:image_id>/', images.image_for_id, name='image_for_id'),
+    path('attaches/', attaches.attaches_upload, name='attaches_upload'),
+]
+
+
+# URL configuration to be included in wagtail admin urls.
+wagtail_urlpatterns = [
+    path(
+        'wagtail-editorjs/',
+        name='wagtail_editorjs',
+        view=include(
+            (urlpatterns, 'wagtail_editorjs'),
+            namespace='wagtail_editorjs'
+        ),
+    ),
+]
