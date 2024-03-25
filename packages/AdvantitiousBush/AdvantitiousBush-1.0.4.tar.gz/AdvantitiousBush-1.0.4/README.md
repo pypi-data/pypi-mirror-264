5b5fb4796d698f3dc52fdbcb26c317946e8c942e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-25 15:03:35 +0000

    Add package AdvantitiousBush/1.0.4/AdvantitiousBush-1.0.4.tar.gz

---
+++ packages/AdvantitiousBush/AdvantitiousBush-1.0.4.tar.gz/AdvantitiousBush-1.0.4/README.md
@@ -0,0 +1,14 @@
+# Advantitious Bush
+
+Advantitious Bush is a Python library for implementing a machine learning algorithm based on the Advantitious Bush method.
+
+## Contact Me!
+
+Email me at senseiwhales@gmail.com
+
+## Installation
+
+You can install Advantitious Bush via pip:
+
+```bash
+pip install AdvantitiousBush
---
+++ packages/AdvantitiousBush/AdvantitiousBush-1.0.4.tar.gz/AdvantitiousBush-1.0.4/setup.py
@@ -0,0 +1,32 @@
+import os
+from setuptools import setup, find_packages
+
+# Get the directory where setup.py is located
+base_dir = os.path.dirname(os.path.abspath(__file__))
+
+# Read the contents of README.md
+with open(os.path.join(base_dir, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(
+    name='AdvantitiousBush',
+    version='1.0.4',
+    packages=find_packages(),
+    install_requires=[
+        'numpy',
+        'pandas',
+        # Add other dependencies here
+    ],
+    author='senseiwhales',
+    author_email='senseiwhales@gmail.com',
+    description='A Python library for implementing the Advantitious Bush algorithm',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url='https://github.com/senseiwhales/advantitious-bush',
+    license='Apache License 2.0',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: Apache Software License',
+        'Operating System :: OS Independent',
+    ],
+)
