fcf1bd6f88e1238cd442216835874827cac18e8b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-25 03:04:21 +0000

    Add package markarth/0.0.4/markarth-0.0.4.tar.gz

---
+++ packages/markarth/markarth-0.0.4.tar.gz/markarth-0.0.4/setup.py
@@ -0,0 +1,19 @@
+from setuptools import find_packages, setup
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
+    name='markarth',
+    version='0.0.4',
+    author='Domenico Nucera',
+    description='Automatic code conversion from python to cython',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    packages=find_packages(),
+    url='https://github.com/nucccc/markarth',
+    license='MIT License',
+    install_requires=[
+        'pydantic >= 2.4.2',
+        'coverage >= 7.3.2'
+    ])
