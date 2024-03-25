010edf5fef2323aa43d8afcc2a3d66194c0b3b87
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-25 14:03:12 +0000

    Add package pycycles/1.0.5/pycycles-1.0.5-cp311-cp311-macosx_10_9_universal2.whl

---
+++ packages/pycycles/pycycles-1.0.5-cp311-cp311-macosx_10_9_universal2.whl/cycles/__init__.py
@@ -0,0 +1,6 @@
+from .clifford import CliffordGroup
+from .cycles import Cycles, find_permutation, permute, random_permutation
+from .group import Coset, PermutationGroup
+from .named_group import (AbelianGroup, AlternatingGroup, CyclicGroup,
+                          DihedralGroup, SymmetricGroup)
+from .special_unitary_group import SU
