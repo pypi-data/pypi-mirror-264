bca536765ccbca7540e7d57678ac8f100b17b313
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-26 06:04:00 +0000

    Add package betby/0.3.59/betby-0.3.59.tar.gz

---
+++ packages/betby/betby-0.3.59.tar.gz/betby-0.3.59/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup
+
+setup(
+    name="betby",
+    version="0.3.59",
+    author="Ayrat Badrutdinov",
+    author_email="a.badrutdinov@betby.com",
+    description="Betby libraries",
+    packages=["betby"],
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+    ],
+)
