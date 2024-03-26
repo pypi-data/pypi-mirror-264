970614804b21a79ecbd59e217f5a0abba67462b9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-26 06:04:11 +0000

    Add package latex-gen-pro-py/0.0.1/latex_gen_pro_py-0.0.1.tar.gz

---
+++ packages/latex-gen-pro-py/latex_gen_pro_py-0.0.1.tar.gz/latex_gen_pro_py-0.0.1/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+
+
+setup(
+  name='latex_gen_pro_py',
+  version='0.0.1',
+  author='DedAzaMarks',
+  author_email='margaretballard1986@autisdmail.com',
+  description='embed tables and png im latex code',
+  packages=find_packages(),
+  classifiers=[
+    'Programming Language :: Python :: 3.11',
+    'License :: OSI Approved :: MIT License',
+    'Operating System :: OS Independent'
+  ],
+  python_requires='>=3.6'
+)
