4435cac361a96391fbb9535b79432f5a7451d2dd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-26 05:04:58 +0000

    Add package cbc-syslog/2.0.3/cbc_syslog-2.0.3.tar.gz

---
+++ packages/cbc-syslog/cbc_syslog-2.0.3.tar.gz/cbc_syslog-2.0.3/src/cbc_syslog/__init__.py
@@ -0,0 +1,11 @@
+from __future__ import absolute_import
+
+__title__ = 'cbc_syslog'
+__author__ = 'Carbon Black Developer Network'
+__license__ = 'MIT'
+__copyright__ = 'Copyright 2018-2023 VMware Carbon Black'
+__version__ = '2.0.3'
+
+from .core import poll, check, history, wizard, convert
+
+from .cli import main
---
+++ packages/cbc-syslog/cbc_syslog-2.0.3.tar.gz/cbc_syslog-2.0.3/src/cbc_syslog/util/__init__.py
@@ -0,0 +1,6 @@
+from __future__ import absolute_import
+
+from cbc_syslog.util.config import Config
+from cbc_syslog.util.carbon_black_cloud import CarbonBlackCloud
+from cbc_syslog.util.output import Output
+from cbc_syslog.util.transform import Transform
