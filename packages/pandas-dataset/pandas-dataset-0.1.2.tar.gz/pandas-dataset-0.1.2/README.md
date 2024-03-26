63f66cf807aa43876238e18e80637756cf52b9df
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-26 16:03:40 +0000

    Add package pandas-dataset/0.1.2/pandas-dataset-0.1.2.tar.gz

---
+++ packages/pandas-dataset/pandas-dataset-0.1.2.tar.gz/pandas-dataset-0.1.2/README.md
@@ -0,0 +1,7 @@
+# Pandas Dataset library
+
+Wrapper on top of pandas to support nested datasets from pandas dataframes provided as dicts.
+
+Readers for CSV and parquet.
+
+Minimal support for adding new features.
---
+++ packages/pandas-dataset/pandas-dataset-0.1.2.tar.gz/pandas-dataset-0.1.2/setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup, find_packages
+from os import path
+
+name = "pandas-dataset"
+version = "0.1.2"
+description = "Python Datasets on top of Pandas"
+url = "https://gitlab.com/meehai/pandas-dataset"
+
+loc = path.abspath(path.dirname(__file__))
+with open(f"{loc}/README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+required = ["pandas", "numpy", "loguru", "pyarrow", "natsort"]
+
+setup(
+    name=name,
+    version=version,
+    description=description,
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url=url,
+    packages=find_packages(),
+    install_requires=required,
+    dependency_links=[],
+    license="WTFPL",
+    python_requires=">=3.8"
+)
