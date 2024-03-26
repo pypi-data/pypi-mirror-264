65fe61051d500329a60504eb9f165421c2a01b80
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-26 16:03:54 +0000

    Add package gocodeo/0.0.8/gocodeo-0.0.8.tar.gz

---
+++ packages/gocodeo/gocodeo-0.0.8.tar.gz/gocodeo-0.0.8/setup.py
@@ -0,0 +1,33 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='gocodeo',
+    version='0.0.8',
+    packages=find_packages(),
+    entry_points={
+        'console_scripts': [
+            'gocodeo = gocodeo.generator:generate_tests_cli'
+        ]
+    },
+    install_requires= ['vertexai',
+                       'requests',
+                       'google-cloud-aiplatform',
+                       'google-auth',
+                       'python-dotenv'],
+
+    author='GoCodeo AI',
+    description='A package to generate unit tests for a file',
+    long_description='''\
+        gocodeo is a  package that provides a command-line interface (CLI) to generate unit tests for files. 
+        To use gocodeo , simply run the command:
+        
+            gocodeo generate <file_path>
+            
+        For example:
+        
+            gocodeo generate C:\\Users\\Sky\\Desktop\\Demo.ts
+        
+        This will analyze the file specified and generate unit tests based on its contents.
+    ''',
+    long_description_content_type='text/markdown',
+)
