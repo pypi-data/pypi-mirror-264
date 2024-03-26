ec047d69c1092f5c4634c797a798e67fbd6e649a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-26 14:04:19 +0000

    Add package gpt-all-star/0.0.18/gpt_all_star-0.0.18.tar.gz

---
+++ packages/gpt-all-star/gpt_all_star-0.0.18.tar.gz/gpt_all_star-0.0.18/gpt_all_star/core/steps/steps.py
@@ -0,0 +1,46 @@
+from enum import Enum
+
+from gpt_all_star.core.steps.development.development import Development
+from gpt_all_star.core.steps.entrypoint.entrypoint import Entrypoint
+from gpt_all_star.core.steps.healing.healing import Healing
+from gpt_all_star.core.steps.improvement.improvement import Improvement
+from gpt_all_star.core.steps.specification.specification import Specification
+from gpt_all_star.core.steps.system_design.system_design import SystemDesign
+from gpt_all_star.core.steps.ui_design.ui_design import UIDesign
+
+
+class StepType(str, Enum):
+    NONE = "none"
+    DEFAULT = "default"
+    BUILD = "build"
+    SPECIFICATION = "specification"
+    SYSTEM_DESIGN = "system_design"
+    DEVELOPMENT = "development"
+    ENTRYPOINT = "entrypoint"
+    UI_DESIGN = "ui_design"
+    IMPROVEMENT = "improvement"
+    HEALING = "healing"
+
+
+STEPS = {
+    StepType.NONE: [],
+    StepType.DEFAULT: [
+        Specification,
+        SystemDesign,
+        Development,
+        UIDesign,
+        Entrypoint,
+    ],
+    StepType.BUILD: [
+        Development,
+        UIDesign,
+        Entrypoint,
+    ],
+    StepType.SPECIFICATION: [Specification],
+    StepType.SYSTEM_DESIGN: [SystemDesign],
+    StepType.DEVELOPMENT: [Development],
+    StepType.ENTRYPOINT: [Entrypoint],
+    StepType.UI_DESIGN: [UIDesign],
+    StepType.IMPROVEMENT: [Improvement],
+    StepType.HEALING: [Healing],
+}
