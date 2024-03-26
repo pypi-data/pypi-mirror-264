511c69611e83f29e518d915e978c044e92392aea
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-26 17:05:08 +0000

    Add package OCFit/0.2.2/OCFit-0.2.2-py3-none-any.whl

---
+++ packages/OCFit/OCFit-0.2.2-py3-none-any.whl/OCFit/__init__.py
@@ -0,0 +1,11 @@
+from .OC_class import FitLinear
+from .OC_class import FitQuad
+from .OC_class import OCFit
+from .OC_class import OCFitLoad
+from .OC_class import DeltaEpoch,Epoch
+
+__version__='0.2.2'
+
+del ga
+del OC_class
+#del info_ga
