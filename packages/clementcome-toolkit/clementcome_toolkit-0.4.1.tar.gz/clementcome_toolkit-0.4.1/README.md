2579635f8450ed648974e4f1ca2337dbd875a72e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-26 04:04:23 +0000

    Add package clementcome-toolkit/0.4.1/clementcome_toolkit-0.4.1.tar.gz

---
+++ packages/clementcome-toolkit/clementcome_toolkit-0.4.1.tar.gz/clementcome_toolkit-0.4.1/README.md
@@ -0,0 +1,12 @@
+# toolkit
+My data science tool kit
+
+## Documentation
+
+Documentation will be available at [https://clementcome-toolkit.readthedocs.io/en/latest/](https://clementcome-toolkit.readthedocs.io/en/latest/).
+
+## Installation
+
+```bash
+pip install clementcome-toolkit
+```
---
+++ packages/clementcome-toolkit/clementcome_toolkit-0.4.1.tar.gz/clementcome_toolkit-0.4.1/cc_tk/relationship/__init__.py
@@ -0,0 +1,7 @@
+"""`relationship` module is dedicated to testing statistical relationship."""
+from cc_tk.relationship.significance import (
+    significance_categorical_categorical,  # noqa: F401
+    significance_numeric_categorical,  # noqa: F401
+    significance_numeric_numeric,  # noqa: F401
+)
+from cc_tk.relationship.summary import RelationshipSummary  # noqa: F401
