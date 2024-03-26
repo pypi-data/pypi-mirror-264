4eaf28d0adb8d1d2f3659354759046354cefde9d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-26 17:03:43 +0000

    Add package lago-python-client/1.1.0/lago-python-client-1.1.0.tar.gz

---
+++ packages/lago-python-client/lago-python-client-1.1.0.tar.gz/lago-python-client-1.1.0/lago_python_client/models/billable_metric.py
@@ -0,0 +1,37 @@
+from typing import Any, Dict, List, Optional, Union
+
+from lago_python_client.base_model import BaseModel
+
+from ..base_model import BaseResponseModel
+
+
+class BillableMetricGroup(BaseModel):
+    key: Optional[str]
+    values: Optional[List[Union[str, Dict[str, Any]]]]
+
+
+class BillableMetric(BaseModel):
+    name: Optional[str]
+    code: Optional[str]
+    description: Optional[str]
+    recurring: Optional[bool]
+    aggregation_type: Optional[str]
+    weighted_interval: Optional[str]
+    field_name: Optional[str]
+    group: Optional[BillableMetricGroup]
+
+
+class BillableMetricResponse(BaseResponseModel):
+    lago_id: str
+    name: str
+    code: str
+    description: Optional[str]
+    recurring: Optional[bool]
+    aggregation_type: Optional[str]
+    weighted_interval: Optional[str]
+    field_name: Optional[str]
+    created_at: str
+    group: BillableMetricGroup
+    active_subscriptions_count: int
+    draft_invoices_count: int
+    plans_count: int
---
+++ packages/lago-python-client/lago-python-client-1.1.0.tar.gz/lago-python-client-1.1.0/lago_python_client/models/invoice_item.py
@@ -0,0 +1,15 @@
+from typing import Dict, Optional
+
+from ..base_model import BaseResponseModel
+
+
+class InvoiceItemResponse(BaseResponseModel):
+    type: Optional[str]
+    code: Optional[str]
+    name: Optional[str]
+    lago_id: Optional[str]
+    invoice_display_name: Optional[str]
+    group_invoice_display_name: Optional[str]
+    lago_item_id: Optional[str]
+    item_type: Optional[str]
+    grouped_by: Optional[Dict[str, str]]
