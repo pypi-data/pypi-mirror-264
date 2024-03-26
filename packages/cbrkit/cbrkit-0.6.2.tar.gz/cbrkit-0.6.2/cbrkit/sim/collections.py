ea3e13038a4f77c201260d5752791cfb0d499ea7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-26 17:03:46 +0000

    Add package cbrkit/0.6.2/cbrkit-0.6.2.tar.gz

---
+++ packages/cbrkit/cbrkit-0.6.2.tar.gz/cbrkit-0.6.2/cbrkit/sim/collections.py
@@ -0,0 +1,28 @@
+from collections.abc import Collection, Set
+from typing import Any
+
+from cbrkit.helpers import dist2sim
+from cbrkit.typing import SimPairFunc
+
+__all__ = ["jaccard"]
+
+
+def jaccard() -> SimPairFunc[Collection[Any], float]:
+    """Jaccard similarity function.
+
+    Examples:
+        >>> sim = jaccard()
+        >>> sim(["a", "b", "c", "d"], ["a", "b", "c"])
+        0.8
+    """
+    from nltk.metrics import jaccard_distance
+
+    def wrapped_func(x: Collection[Any], y: Collection[Any]) -> float:
+        if not isinstance(x, Set):
+            x = set(x)
+        if not isinstance(y, Set):
+            y = set(y)
+
+        return dist2sim(jaccard_distance(x, y))
+
+    return wrapped_func
