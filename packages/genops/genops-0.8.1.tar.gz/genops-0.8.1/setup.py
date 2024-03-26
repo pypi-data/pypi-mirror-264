e5a99ee6b5a28c295ac3bd6f27e71fe04dcaeb42
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-26 05:04:46 +0000

    Add package genops/0.8.1/genops-0.8.1.tar.gz

---
+++ packages/genops/genops-0.8.1.tar.gz/genops-0.8.1/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="genops",
+    version="0.8.1",
+    packages=find_packages(),
+    install_requires=[
+        "requests",
+    ],
+    author="Ramsay Brown",
+    author_email="ramsay@usemissioncontrol.com",
+    description="Connect your python application to Mission Control's GenOps platform",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/airesponsibility/genops-python",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
