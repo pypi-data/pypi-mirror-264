0748fe527f5e9994db5e16d74a47c8f377635596
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-26 03:03:44 +0000

    Add package polars-ols/0.1.0/polars_ols-0.1.0.tar.gz

---
+++ packages/polars-ols/polars_ols-0.1.0.tar.gz/polars_ols-0.1.0/src/lib.rs
@@ -0,0 +1,121 @@
+mod expressions;
+pub mod least_squares;
+
+use pyo3::types::PyModule;
+use pyo3::{pymodule, PyResult, Python};
+
+#[cfg(test)]
+mod tests {
+    use crate::expressions::convert_polars_to_ndarray;
+    use crate::least_squares::{
+        solve_elastic_net, solve_ols_qr, solve_recursive_least_squares, solve_ridge,
+        solve_rolling_ols, woodbury_update,
+    };
+    use ndarray::prelude::*;
+    use ndarray_linalg::assert_close_l2;
+    use ndarray_rand::rand_distr::Normal;
+    use ndarray_rand::RandomExt;
+    use polars::datatypes::DataType::Float32;
+    use polars::prelude::*;
+
+    fn make_data() -> (Series, Series, Series) {
+        let x1 = Series::from_vec(
+            "x1",
+            Array::random(10_000, Normal::new(0., 1.).unwrap()).to_vec(),
+        )
+        .cast(&Float32)
+        .unwrap();
+        let x2 = Series::from_vec(
+            "x2",
+            Array::random(10_000, Normal::new(0., 1.).unwrap()).to_vec(),
+        )
+        .cast(&Float32)
+        .unwrap();
+        let y = (&x1 + &x2).with_name("y");
+        (y, x1, x2)
+    }
+
+    #[test]
+    fn test_ols_qr() {
+        let (y, x1, x2) = make_data();
+        let (targets, features) = convert_polars_to_ndarray(&[y.clone(), x1, x2]);
+        let coefficients = solve_ols_qr(&targets, &features);
+        let expected = array![1., 1.];
+        assert_close_l2!(&coefficients, &expected, 0.001);
+    }
+
+    #[test]
+    fn test_ridge() {
+        let (y, x1, x2) = make_data();
+        let (targets, features) = convert_polars_to_ndarray(&[y.clone(), x1, x2]);
+        let coefficients = solve_ridge(&targets, &features, 10.0);
+        let expected = array![0.999, 0.999];
+        assert_close_l2!(&coefficients, &expected, 0.001);
+    }
+
+    #[test]
+    fn test_elastic_net() {
+        let (y, x1, x2) = make_data();
+        let (targets, features) = convert_polars_to_ndarray(&[y.clone(), x1, x2]);
+        let coefficients =
+            solve_elastic_net(&targets, &features, 0.001, Some(0.5), None, None, None);
+        let expected = array![0.999, 0.999];
+        assert_close_l2!(&coefficients, &expected, 0.001);
+    }
+
+    #[test]
+    fn test_recursive_least_squares() {
+        let (y, x1, x2) = make_data();
+        let (targets, features) = convert_polars_to_ndarray(&[y.clone(), x1, x2]);
+        let coefficients =
+            solve_recursive_least_squares(&targets, &features, Some(252.0), Some(0.01), None);
+        let expected = array![1.0, 1.0];
+        println!("{:?}", coefficients.slice(s![0, ..]));
+        println!("{:?}", coefficients.slice(s![-1, ..]));
+        assert_close_l2!(&coefficients.slice(s![-1, ..]), &expected, 0.0001);
+    }
+
+    #[test]
+    fn test_rolling_least_squares() {
+        let (y, x1, x2) = make_data();
+        let (targets, features) = convert_polars_to_ndarray(&[y.clone(), x1, x2]);
+        let coefficients =
+            solve_rolling_ols(&targets, &features, 1_000usize, Some(100usize), Some(false));
+        let expected: Array1<f32> = array![1.0, 1.0];
+        println!("{:?}", coefficients.slice(s![0, ..]));
+        println!("{:?}", coefficients.slice(s![-1, ..]));
+        assert_close_l2!(&coefficients.slice(s![-1, ..]), &expected, 0.0001);
+    }
+
+    #[test]
+    fn test_woodbury_update() {
+        // Test matrices
+        let a_inv = array![[0.5, 0.0], [0.0, 0.5]]; // A^{-1}
+        let u = array![[1.0, 2.0], [3.0, 4.0]]; // U
+        let c = array![[2.0, 0.0], [0.0, 2.0]]; // C
+        let v = array![[1.0, 0.0], [0.0, 1.0]]; // V
+
+        // Expected result
+        let expected_result = array![[0.625, -0.25], [-0.375, 0.25]]; // Expected result
+
+        // Compute the Woodbury update
+        let result = woodbury_update(&a_inv, &u, &c, &v, Some(true));
+
+        // Compare with expected result
+        assert_close_l2!(&result, &expected_result, 0.00001);
+    }
+}
+
+#[cfg(target_os = "linux")]
+use jemallocator::Jemalloc;
+
+#[global_allocator]
+#[cfg(target_os = "linux")]
+static ALLOC: Jemalloc = Jemalloc;
+
+#[pymodule]
+#[pyo3(name = "_polars_ols")]
+fn _internal(_py: Python, m: &PyModule) -> PyResult<()> {
+    m.add("__version__", env!("CARGO_PKG_VERSION"))?;
+    Ok(())
+}
---
+++ packages/polars-ols/polars_ols-0.1.0.tar.gz/polars_ols-0.1.0/tests/benchmark.py
@@ -0,0 +1,135 @@
+import numpy as np
+import polars as pl
+import pyperf
+import statsmodels.formula.api as smf
+from sklearn.linear_model import ElasticNet
+
+import polars_ols as pls  # import package to register the .least_squares namespace
+
+
+def _make_data() -> pl.DataFrame:
+    x = np.random.normal(size=(2_000, 5)).astype("float32")
+    eps = np.random.normal(size=2_000, scale=0.1).astype("float32")
+    return pl.DataFrame(data=x, schema=[f"x{i + 1}" for i in range(5)]).with_columns(
+        y=pl.lit(x.sum(1) + eps)
+    )
+
+
+def benchmark_least_squares(data: pl.DataFrame):
+    return (
+        data.lazy()
+        .with_columns(
+            pl.col("y")
+            .least_squares.ols(*[pl.col(c) for c in data.columns if c != "y"])
+            .alias("predictions")
+        )
+        .collect()
+    )
+
+
+def benchmark_least_squares_numpy(data: pl.DataFrame):
+    y, x = data.select("y").to_numpy().flatten(), data.select(pl.all().exclude("y")).to_numpy()
+    coef = np.linalg.lstsq(x, y, rcond=None)[0]
+    return data.lazy().with_columns(predictions=pl.lit(x @ coef).alias("predictions")).collect()
+
+
+def benchmark_ridge(data: pl.DataFrame):
+    return (
+        data.lazy()
+        .with_columns(
+            pl.col("y")
+            .least_squares.ridge(*[pl.col(c) for c in data.columns if c != "y"], alpha=0.0001)
+            .alias("predictions")
+        )
+        .collect()
+    )
+
+
+def benchmark_ridge_numpy(data: pl.DataFrame):
+    alpha: float = 0.0001
+    y, x = data.select("y").to_numpy().flatten(), data.select(pl.all().exclude("y")).to_numpy()
+    xtx = x.T @ x + np.eye(x.shape[1]) * alpha
+    xty = x.T @ y
+    coef = np.linalg.solve(xtx, xty)
+    return data.lazy().with_columns(predictions=pl.lit(x @ coef).alias("predictions")).collect()
+
+
+def benchmark_wls_from_formula(data: pl.DataFrame):
+    return (
+        data.lazy()
+        .with_columns(pl.lit(1.0).alias("sample_weights"))
+        .with_columns(
+            pl.col("y")
+            .least_squares.from_formula(
+                "x1 + x2 + x3 + x4 + x5", sample_weights=pl.col("sample_weights")
+            )
+            .alias("predictions")
+        )
+    ).collect()
+
+
+def benchmark_wls_from_formula_statsmodels(data: pl.DataFrame):
+    predictions = (
+        smf.wls(
+            data=data,
+            formula="y ~ x1 + x2 + x3 + x4 + x5",
+            weights=np.ones(len(data), dtype="float32"),
+        )
+        .fit()
+        .predict()
+    )
+    return data.lazy().with_columns(predictions=pl.lit(predictions)).collect()
+
+
+def benchmark_elastic_net(data: pl.DataFrame):
+    return (
+        data.lazy()
+        .with_columns(
+            pl.col("y").least_squares.elastic_net(
+                *[pl.col(c) for c in data.columns if c != "y"],
+                alpha=0.1,
+                l1_ratio=0.5,
+                max_iter=1_000,
+            )
+        )
+        .collect()
+    )
+
+
+def benchmark_elastic_net_sklearn(data: pl.DataFrame):
+    mdl = ElasticNet(fit_intercept=False, alpha=0.1, l1_ratio=0.5, max_iter=1_000)
+    y, x = data.select("y").to_numpy().flatten(), data.select(pl.all().exclude("y")).to_numpy()
+    mdl.fit(x, y)
+    return data.lazy().with_columns(predictions=pl.lit(mdl.predict(x))).collect()
+
+
+def benchmark_recursive_least_squares(data: pl.DataFrame):
+    return (
+        data.lazy()
+        .with_columns(
+            pl.col("y").least_squares.rls(
+                *[pl.col(c) for c in data.columns if c != "y"],
+                half_life=252,
+            )
+        )
+        .collect()
+    )
+
+
+if __name__ == "__main__":
+    # example: python polars_ols/tests/benchmark.py --quiet
+    # we run the benchmarks in python (as opposed to rust) so that overhead of pyO3 is included
+    df = _make_data()
+    runner = pyperf.Runner()
+    runner.bench_func("benchmark_least_squares", benchmark_least_squares, df)
+    runner.bench_func("benchmark_ridge", benchmark_ridge, df)
+    runner.bench_func("benchmark_wls_from_formula", benchmark_wls_from_formula, df)
+    runner.bench_func("benchmark_elastic_net", benchmark_elastic_net, df)
+    runner.bench_func("benchmark_recursive_least_squares", benchmark_recursive_least_squares, df)
+
+    # runner.bench_func("benchmark_least_squares_numpy", benchmark_least_squares_numpy, df)
+    # runner.bench_func("benchmark_ridge_numpy", benchmark_ridge_numpy, df)
+    # runner.bench_func(
+    #     "benchmark_wls_from_formula_statsmodels", benchmark_wls_from_formula_statsmodels, df
+    # )
+    # runner.bench_func("benchmark_elastic_net_sklearn", benchmark_elastic_net_sklearn, df)
