1412fc13a8e03eb87110eca760e2d2beb54dc9aa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-26 17:04:25 +0000

    Add package thanosql/0.0.11/thanosql-0.0.11-py3-none-any.whl

---
+++ packages/thanosql/thanosql-0.0.11-py3-none-any.whl/thanosql/resources/__init__.py
@@ -0,0 +1,19 @@
+from __future__ import annotations
+
+from ._file import FileService
+from ._query import QueryLog, QueryService, QueryTemplate
+from ._schema import SchemaService
+from ._table import (
+    BaseColumn,
+    BaseTable,
+    Column,
+    Constraints,
+    ForeignKey,
+    PrimaryKey,
+    Table,
+    TableObject,
+    TableService,
+    TableTemplate,
+    Unique,
+)
+from ._view import View, ViewService
