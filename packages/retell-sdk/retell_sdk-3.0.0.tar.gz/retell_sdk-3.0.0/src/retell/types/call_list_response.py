ea9d03a8caa4058503511143a384f6f841611c3a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-26 15:04:12 +0000

    Add package retell-sdk/3.0.0/retell_sdk-3.0.0.tar.gz

---
+++ packages/retell-sdk/retell_sdk-3.0.0.tar.gz/retell_sdk-3.0.0/src/retell/types/call_list_response.py
@@ -0,0 +1,9 @@
+# File generated from our OpenAPI spec by Stainless. See CONTRIBUTING.md for details.
+
+from typing import List
+
+from .call_detail_response import CallDetailResponse
+
+__all__ = ["CallListResponse"]
+
+CallListResponse = List[CallDetailResponse]
