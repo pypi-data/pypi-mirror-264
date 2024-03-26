19e8db23089e7f973d30b7e5c1e094fd00a21a96
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-26 04:04:32 +0000

    Add package clear-skies/1.19.18/clear_skies-1.19.18-py3-none-any.whl

---
+++ packages/clear-skies/clear_skies-1.19.18-py3-none-any.whl/clearskies/authentication/__init__.py
@@ -0,0 +1,37 @@
+from ..binding_config import BindingConfig
+from .secret_bearer import SecretBearer
+from .public import Public
+from .auth0_jwks import Auth0JWKS
+from .authorization import Authorization
+from .jwks import JWKS
+
+
+def public():
+    return BindingConfig(Public)
+
+
+def secret_bearer(**kwargs):
+    return BindingConfig(SecretBearer, **kwargs)
+
+
+def auth0_jwks(**kwargs):
+    return BindingConfig(Auth0JWKS, **kwargs)
+
+
+def jwks(jwks_url, **kwargs):
+    return BindingConfig(JWKS, jwks_url=jwks_url, **kwargs)
+
+
+__all__ = [
+    "Authorization",
+    "BindingConfig",
+    "public",
+    "Public",
+    "secret_bearer",
+    "SecretBearer",
+    "auth0_jwks",
+    "Auth0JWKS",
+    "authorization",
+    "jwks",
+    "JWKS",
+]
