532a4b94ec358e44a066dcdb90c0dc5d9ebeaa4f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-26 05:04:29 +0000

    Add package bigframes/1.0.0/bigframes-1.0.0-py2.py3-none-any.whl

---
+++ packages/bigframes/bigframes-1.0.0-py2.py3-none-any.whl/bigframes/ml/compose.py
@@ -0,0 +1,151 @@
+# Copyright 2023 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""Build composite transformers on heterogeneous data. This module is styled
+after Scikit-Learn's compose module:
+https://scikit-learn.org/stable/modules/classes.html#module-sklearn.compose."""
+
+from __future__ import annotations
+
+import typing
+from typing import List, Optional, Tuple, Union
+
+import bigframes_vendored.sklearn.compose._column_transformer
+
+from bigframes import constants
+from bigframes.core import log_adapter
+from bigframes.ml import base, core, globals, preprocessing, utils
+import bigframes.pandas as bpd
+
+CompilablePreprocessorType = Union[
+    preprocessing.OneHotEncoder,
+    preprocessing.StandardScaler,
+    preprocessing.MaxAbsScaler,
+    preprocessing.MinMaxScaler,
+    preprocessing.KBinsDiscretizer,
+    preprocessing.LabelEncoder,
+]
+
+
+@log_adapter.class_logger
+class ColumnTransformer(
+    base.Transformer,
+    bigframes_vendored.sklearn.compose._column_transformer.ColumnTransformer,
+):
+    __doc__ = (
+        bigframes_vendored.sklearn.compose._column_transformer.ColumnTransformer.__doc__
+    )
+
+    def __init__(
+        self,
+        transformers: List[
+            Tuple[
+                str,
+                CompilablePreprocessorType,
+                Union[str, List[str]],
+            ]
+        ],
+    ):
+        # TODO: if any(transformers) has fitted raise warning
+        self.transformers = transformers
+        self._bqml_model: Optional[core.BqmlModel] = None
+        self._bqml_model_factory = globals.bqml_model_factory()
+        # call self.transformers_ to check chained transformers
+        self.transformers_
+
+    @property
+    def transformers_(
+        self,
+    ) -> List[Tuple[str, CompilablePreprocessorType, str,]]:
+        """The collection of transformers as tuples of (name, transformer, column)."""
+        result: List[
+            Tuple[
+                str,
+                CompilablePreprocessorType,
+                str,
+            ]
+        ] = []
+
+        column_set: set[str] = set()
+        for entry in self.transformers:
+            name, transformer, column_or_columns = entry
+            columns = (
+                column_or_columns
+                if isinstance(column_or_columns, List)
+                else [column_or_columns]
+            )
+
+            for column in columns:
+                if column in column_set:
+                    raise NotImplementedError(
+                        f"Chained transformers on the same column isn't supported. {constants.FEEDBACK_LINK}"
+                    )
+                result.append((name, transformer, column))
+
+        return result
+
+    def _compile_to_sql(
+        self,
+        columns: List[str],
+        X: bpd.DataFrame,
+    ) -> List[Tuple[str, str]]:
+        """Compile this transformer to a list of SQL expressions that can be included in
+        a BQML TRANSFORM clause
+
+        Args:
+            columns (List[str]):
+                a list of column names to transform
+            X (bpd.DataFrame):
+                The Dataframe with training data.
+
+        Returns:
+            a list of tuples of (sql_expression, output_name)"""
+        return [
+            transformer._compile_to_sql([column], X=X)[0]
+            for column in columns
+            for _, transformer, target_column in self.transformers_
+            if column == target_column
+        ]
+
+    def fit(
+        self,
+        X: Union[bpd.DataFrame, bpd.Series],
+        y=None,  # ignored
+    ) -> ColumnTransformer:
+        (X,) = utils.convert_to_dataframe(X)
+
+        compiled_transforms = self._compile_to_sql(X.columns.tolist(), X)
+        transform_sqls = [transform_sql for transform_sql, _ in compiled_transforms]
+
+        self._bqml_model = self._bqml_model_factory.create_model(
+            X,
+            options={"model_type": "transform_only"},
+            transforms=transform_sqls,
+        )
+
+        # The schema of TRANSFORM output is not available in the model API, so save it during fitting
+        self._output_names = [name for _, name in compiled_transforms]
+        return self
+
+    def transform(self, X: Union[bpd.DataFrame, bpd.Series]) -> bpd.DataFrame:
+        if not self._bqml_model:
+            raise RuntimeError("Must be fitted before transform")
+
+        (X,) = utils.convert_to_dataframe(X)
+
+        df = self._bqml_model.transform(X)
+        return typing.cast(
+            bpd.DataFrame,
+            df[self._output_names],
+        )
---
+++ packages/bigframes/bigframes-1.0.0-py2.py3-none-any.whl/bigframes_vendored/pandas/core/config_init.py
@@ -0,0 +1,90 @@
+# Contains code from https://github.com/pandas-dev/pandas/blob/main/pandas/core/config_init.py
+"""
+This module is imported from the pandas package __init__.py file
+in order to ensure that the core.config options registered here will
+be available as soon as the user loads the package. if register_option
+is invoked inside specific modules, they will not be registered until that
+module is imported, which may or may not be a problem.
+
+If you need to make sure options are available even before a certain
+module is imported, register them here rather than in the module.
+
+"""
+from __future__ import annotations
+
+display_options_doc = """
+Encapsulates configuration for displaying objects.
+
+**Examples:**
+
+Define Repr mode to "deferred" will prevent job execution in repr.
+    >>> import bigframes.pandas as bpd
+    >>> df = bpd.read_gbq("bigquery-public-data.ml_datasets.penguins")
+
+    >>> bpd.options.display.repr_mode = "deferred"
+    >>> df.head(20) # will no longer run the job
+    Computation deferred. Computation will process 28.9 kB
+
+Users can also get a dry run of the job by accessing the query_job property before they've run the job. This will return a dry run instance of the job they can inspect.
+    >>> df.query_job.total_bytes_processed
+    28947
+
+User can execute the job by calling .to_pandas()
+    >>> # df.to_pandas()
+
+Reset option
+    >>> bpd.options.display.repr_mode = "head"
+
+Attributes:
+    max_columns (int, default 20):
+        If `max_columns` is exceeded, switch to truncate view.
+    max_rows (int, default 25):
+        If `max_rows` is exceeded, switch to truncate view.
+    progress_bar (Optional(str), default "auto"):
+        Determines if progress bars are shown during job runs.
+        Valid values are `auto`, `notebook`, and `terminal`. Set
+        to `None` to remove progress bars.
+    repr_mode (Literal[`head`, `deferred`]):
+        `head`:
+            Execute, download, and display results (limited to head) from
+            dataframe and series objects during repr.
+        `deferred`:
+            Prevent executions from repr statements in dataframe and series objects.
+            Instead estimated bytes processed will be shown. Dataframe and Series
+            objects can still be computed with methods that explicitly execute and
+            download results.
+    max_info_columns (int):
+        max_info_columns is used in DataFrame.info method to decide if
+        per column information will be printed.
+    max_info_rows (int or None):
+        df.info() will usually show null-counts for each column.
+        For large frames this can be quite slow. max_info_rows and max_info_cols
+        limit this null check only to frames with smaller dimensions than
+        specified.
+    memory_usage (bool):
+        This specifies if the memory usage of a DataFrame should be displayed when
+        df.info() is called. Valid values True,False,
+"""
+
+sampling_options_doc = """
+Encapsulates configuration for data sampling.
+
+Attributes:
+    max_download_size (int, default 500):
+        Download size threshold in MB. If value set to None, the download size
+        won't be checked.
+    enable_downsampling (bool, default False):
+        Whether to enable downsampling, If max_download_size is exceeded when
+        downloading data (e.g., to_pandas()), the data will be downsampled
+        if enable_downsampling is True, otherwise, an error will be raised.
+    sampling_method (str, default "uniform"):
+        Downsampling algorithms to be chosen from, the choices are:
+        "head": This algorithm returns a portion of the data from
+        the beginning. It is fast and requires minimal computations
+        to perform the downsampling.; "uniform": This algorithm returns
+        uniform random samples of the data.
+    random_state (int, default None):
+        The seed for the uniform downsampling algorithm. If provided,
+        the uniform method may take longer to execute and require more
+        computation.
+"""
