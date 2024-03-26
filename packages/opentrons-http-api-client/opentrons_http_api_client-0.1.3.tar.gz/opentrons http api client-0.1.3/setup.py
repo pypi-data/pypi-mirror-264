8d2dc03201aa305421918b43a70b522029a44d73
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-26 05:03:30 +0000

    Add package opentrons-http-api-client/0.1.3/opentrons_http_api_client-0.1.3.tar.gz

---
+++ packages/opentrons-http-api-client/opentrons_http_api_client-0.1.3.tar.gz/opentrons http api client-0.1.3/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+with open('README.md', encoding='utf-8') as f:
+  long_description = f.read()
+
+setup(
+    name='opentrons http api client',
+    version="0.1.3",
+    python_requires=">=3.7",
+    packages=find_packages(exclude="testing"),
+    description='A Python library for interacting with the Opentrons HTTP API',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    install_requires=[
+      "opentrons",
+      "requests",
+    ],
+    url='https://github.com/rickwierenga/opentrons-python-api.git',
+)
