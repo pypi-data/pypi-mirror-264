99ed41d5863b0a7cf945774142b5ece4742b090a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-26 04:04:19 +0000

    Add package hellomaaya/0.1/hellomaaya-0.1.tar.gz

---
+++ packages/hellomaaya/hellomaaya-0.1.tar.gz/hellomaaya-0.1/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='hellomaaya',
+    version='0.1',
+    packages=find_packages(),
+    description='A simple example package',
+    long_description='A longer description of your package',
+    author='Your Name',
+    author_email='iammayakunnath@gmail.com',
+    url='https://github.com/vimal1986/greetpackage',
+    license='MIT',
+    install_requires=[
+        # Any dependencies here, e.g., 'requests >= 2.22.0'
+],
+)
