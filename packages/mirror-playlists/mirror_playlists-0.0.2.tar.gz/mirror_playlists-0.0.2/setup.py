afb90b553797b36c57e00f4913dcdef479eed553
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-26 17:04:49 +0000

    Add package mirror-playlists/0.0.2/mirror_playlists-0.0.2.tar.gz

---
+++ packages/mirror-playlists/mirror_playlists-0.0.2.tar.gz/mirror_playlists-0.0.2/setup.py
@@ -0,0 +1,21 @@
+"""Setup of mirror_playlists."""
+
+from setuptools import find_packages, setup
+
+setup(
+    name="mirror_playlists",
+    version="0.0.2",
+    packages=find_packages(),
+    entry_points={"console_scripts": ["mirror_playlists = mirror_playlists.main:main"]},
+    author="Maxime Haselbauer",
+    author_email="maxime.haselbauer@googlemail.com",
+    description="A tool to mirror playlists on devices",
+    license="MIT",
+    keywords="keywords ",
+    url="https://github.com/renn0xtek9/mirror-playlists",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
