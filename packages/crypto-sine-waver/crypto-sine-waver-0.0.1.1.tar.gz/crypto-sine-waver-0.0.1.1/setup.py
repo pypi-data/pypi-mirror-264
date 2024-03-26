4c79f12a3c904d6fe60c46616d8dadd811780f6c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-26 06:04:14 +0000

    Add package crypto-sine-waver/0.0.1.1/crypto-sine-waver-0.0.1.1.tar.gz

---
+++ packages/crypto-sine-waver/crypto-sine-waver-0.0.1.1.tar.gz/crypto-sine-waver-0.0.1.1/setup.py
@@ -0,0 +1,17 @@
+import setuptools 
+
+setuptools.setup(
+    name='crypto-sine-waver',
+    version='0.0.1.1',
+    author='Tahmi ogosi',
+    packages=setuptools.find_packages(),
+    url="https://github.com/tahmi123/crypto-sine-waver",
+    
+    description="A price transform for cryptocurrency",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    install_requires=['numpy', 'matplotlib'],
+)
