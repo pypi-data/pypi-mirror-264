3803f5618f13fecf6d35406c03a5de28a82ad83f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-26 03:03:49 +0000

    Add package increase/0.45.0/increase-0.45.0-py3-none-any.whl

---
+++ packages/increase/increase-0.45.0-py3-none-any.whl/increase/types/external_account_update_params.py
@@ -0,0 +1,27 @@
+# File generated from our OpenAPI spec by Stainless. See CONTRIBUTING.md for details.
+
+from __future__ import annotations
+
+from typing_extensions import Literal, TypedDict
+
+__all__ = ["ExternalAccountUpdateParams"]
+
+
+class ExternalAccountUpdateParams(TypedDict, total=False):
+    account_holder: Literal["business", "individual"]
+    """The type of entity that owns the External Account.
+
+    - `business` - The External Account is owned by a business.
+    - `individual` - The External Account is owned by an individual.
+    """
+
+    description: str
+    """The description you choose to give the external account."""
+
+    status: Literal["active", "archived"]
+    """The status of the External Account.
+
+    - `active` - The External Account is active.
+    - `archived` - The External Account is archived and won't appear in the
+      dashboard.
+    """
