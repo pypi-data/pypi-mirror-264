d67af5c20d4fe246dd855e7844bbfde71b89642b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-26 06:04:24 +0000

    Add package rolabesti/0.5.3/rolabesti-0.5.3.tar.gz

---
+++ packages/rolabesti/rolabesti-0.5.3.tar.gz/rolabesti-0.5.3/rolabesti/__init__.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+rolabesti
+~~~~~~~~~
+
+rolabesti is a CLI application to manage a music collection,
+performing the following actions on mp3 files: loading to database, parsing,
+searching, playing, enqueueing, copying, and tagging.
+"""
+
+__version__ = '0.5.3'
+__description__ = 'CLI application to manage a music collection of mp3 tracks'
