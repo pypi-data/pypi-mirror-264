9c4a61816ae65dfd9381783326d3983227ce5d54
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-26 14:04:22 +0000

    Add package AutoFL/1.3/AutoFL-1.3.tar.gz

---
+++ packages/AutoFL/AutoFL-1.3.tar.gz/AutoFL-1.3/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='AutoFL',
+    version='1.3',
+    packages=find_packages(),
+    author='Owen Orcan',
+    author_email='owenorcan@gmail.com',
+    url='https://github.com/OwenOrcan/AutoFlask',
+    license='MIT License',
+    description="AutoFlask simplifies Flask project setup, automating the creation of essential files and directories with just one command.",
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+
+    entry_points={
+        'console_scripts': [
+            'autoflask = AutoFlask.AutoFlask:main',
+        ],
+    },
+)
