c8c0f7023017e29624390bb76c78a764d5690bb3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-26 16:03:44 +0000

    Add package simba-torch/0.0.5/simba_torch-0.0.5.tar.gz

---
+++ packages/simba-torch/simba_torch-0.0.5.tar.gz/simba_torch-0.0.5/setup.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['simba_torch']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['einops', 'torch', 'zetascale']
+
+setup_kwargs = {
+    'name': 'simba-torch',
+    'version': '0.0.5',
+    'description': 'Paper - Pytorch',
+    'long_description': '\n# Simba\n![graph](graph.png)\nA simpler Pytorch + Zeta Implementation of the paper: "SiMBA: Simplified Mamba-based Architecture for Vision and Multivariate Time series"\n\n\n## install\n`$ pip install simba-torch`\n\n## usage\n```python\n\nimport torch \nfrom simba_torch.main import Simba\n\n# Forward pass with images\nimg = torch.randn(1, 3, 224, 224)\n\n# Create model\nmodel = Simba(\n    dim = 4,                # Dimension of the transformer\n    dropout = 0.1,          # Dropout rate for regularization\n    d_state=64,             # Dimension of the transformer state\n    d_conv=64,              # Dimension of the convolutional layers\n    num_classes=64,         # Number of output classes\n    depth=8,                # Number of transformer layers\n    patch_size=16,          # Size of the image patches\n    image_size=224,         # Size of the input image\n    channels=3,             # Number of input channels\n    # use_pos_emb=True # If you want\n)\n\n# Forward pass\nout = model(img)\nprint(out.shape)\n\n```\n\n\n# License\nMIT\n\n# Todo\n- [ ] Add paper link\n- [ ] Add citation bibtex\n- [ ] cleanup',
+    'author': 'Kye Gomez',
+    'author_email': 'kye@apac.ai',
+    'maintainer': 'None',
+    'maintainer_email': 'None',
+    'url': 'https://github.com/kyegomez/Simba',
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.6,<4.0',
+}
+
+
+setup(**setup_kwargs)
