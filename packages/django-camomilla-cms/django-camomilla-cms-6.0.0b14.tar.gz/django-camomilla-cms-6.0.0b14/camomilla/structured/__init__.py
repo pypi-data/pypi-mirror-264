c2b830a52fb16405a9b5e1e3e8ea243fd924c0f8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-26 03:03:51 +0000

    Add package django-camomilla-cms/6.0.0b14/django-camomilla-cms-6.0.0b14.tar.gz

---
+++ packages/django-camomilla-cms/django-camomilla-cms-6.0.0b14.tar.gz/django-camomilla-cms-6.0.0b14/camomilla/structured/__init__.py
@@ -0,0 +1,125 @@
+import json
+from typing import Any, Union, Type, TYPE_CHECKING, List
+
+from django.db.models import JSONField
+from django.db.models.query_utils import DeferredAttribute
+from django_jsonform.models.fields import JSONFormField
+from pydantic import (
+    TypeAdapter,
+    ValidationInfo,
+    ValidatorFunctionWrapHandler,
+    WrapValidator,
+)
+
+from .fields import *
+from .models import *
+
+if TYPE_CHECKING:
+    from pydantic import BaseModel
+    from pydantic.fields import Field
+    from typing_extensions import Annotated
+
+
+class StructuredJSONFormField(JSONFormField):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.encoder = kwargs.get("encoder", None)
+        self.decoder = kwargs.get("decoder", None)
+
+    def prepare_value(self, value):
+        if isinstance(value, list):
+            return json.dumps([v.model_dump() for v in value])
+        return value.model_dump_json()
+
+
+class StructuredDescriptior(DeferredAttribute):
+    field: "StructuredJSONField"
+
+    def __set__(self, instance, value):
+        # TODO: check if it's better to validate here or in __get__ function (performance reasons)
+        # if not self.field.check_type(value):
+        #     value = self.field.schema.validate_python(value)
+        instance.__dict__[self.field.attname] = value
+
+    def __get__(self, instance, cls=None):
+        value = super().__get__(instance, cls)
+        if not self.field.check_type(value):
+            return self.field.schema.validate_python(value)
+        return value
+
+
+class StructuredJSONField(JSONField):
+    # TODO: share cache in querysets of models having this same field
+    # TODO: write queries for prefetch related for models inside the field
+
+    descriptor_class = StructuredDescriptior
+
+    @property
+    def list_data_validator(self):
+        def list_data_validator(
+            value: Any, handler: ValidatorFunctionWrapHandler, info: ValidationInfo
+        ) -> Any:
+            from camomilla.structured.cache import CacheBuilder
+
+            cache = CacheBuilder.from_model(self.orig_schema)
+            if info.mode == "json" and isinstance(value, str):
+                return self.schema.validate_python(
+                    cache.inject_cache(json.loads(value))
+                )
+            return handler(cache.inject_cache(value))
+
+        return list_data_validator
+
+    def __init__(self, schema: Type[BaseModel], *args: Any, **kwargs: Any) -> None:
+        self.orig_schema = schema
+        self.schema = schema
+        default = kwargs.get("default", dict)
+        self.file_handler = kwargs.pop("file_handler", "")
+        self.many = kwargs.pop(
+            "many", isinstance(default() if callable(default) else default, list)
+        )
+        if self.many:
+            self.schema = TypeAdapter(
+                Annotated[
+                    List[self.schema],
+                    Field(default_factory=list),
+                    WrapValidator(self.list_data_validator),
+                ]
+            )
+        return super().__init__(*args, **kwargs)
+
+    def check_type(self, value: Any):
+        if self.many:
+            return isinstance(value, list) and all(
+                isinstance(v, self.orig_schema) for v in value
+            )
+        return isinstance(value, self.orig_schema)
+
+    def get_prep_value(
+        self, value: Union[List[Type[BaseModel]], Type[BaseModel]]
+    ) -> str:
+        if isinstance(value, list) and self.many:
+            return self.schema.dump_json(value, exclude_unset=True).decode()
+        return value.model_dump_json(exclude_unset=True)
+
+    def from_db_value(self, value: Any, expression: Any, connection: Any) -> Any:
+        data = super().from_db_value(value, expression, connection)
+        if isinstance(data, str):
+            return json.loads(data)
+        return data
+
+    def deconstruct(self):
+        name, path, args, kwargs = super().deconstruct()
+        kwargs["schema"] = self.orig_schema
+        return name, path, args, kwargs
+
+    def formfield(self, **kwargs):
+        return super().formfield(
+            **{
+                "form_class": StructuredJSONFormField,
+                "schema": self.schema.json_schema(),
+                "model_name": self.model.__name__,
+                "file_handler": self.file_handler,
+                **kwargs,
+            }
+        )
---
+++ packages/django-camomilla-cms/django-camomilla-cms-6.0.0b14.tar.gz/django-camomilla-cms-6.0.0b14/camomilla/structured/cache.py
@@ -0,0 +1,202 @@
+from collections import defaultdict
+from inspect import isclass
+from typing import Any, Dict, Sequence, Type, Iterable, Union
+from typing_extensions import get_args, get_origin
+from camomilla.settings import STRUCTURED_FIELD_CACHE_ENABLED
+from camomilla.structured.fields import ForeignKey, QuerySet
+from camomilla.structured.models import BaseModel
+from camomilla.structured.utils import _LazyType, get_type, pointed_setter
+from django.db.models import Model as DjangoModel
+from camomilla.utils.getters import pointed_getter
+
+
+# TODO:
+# ::: Actually this is a first draft.
+# The system should be more type safe.
+# It should handle initialization made with model instance not only dicts
+# Neet to check if there are problems with different formats for pks (model instaces or string or dicts)
+
+
+class Cache(dict):
+    pass
+
+
+class ValueWithCache:
+    def __init__(self, cache, model, value) -> None:
+        self.cache: Cache = cache
+        self.value: Union[Iterable[Union[str, int]], str, int] = value
+        self.model: Type[DjangoModel] = model
+
+    def retrieve(self):
+        cache = self.cache.get(self.model)
+        if hasattr(self.value, '__iter__'):
+            qs = self.model.objects.filter(pk__in=self.value)
+            setattr(
+                qs,
+                "_result_cache",
+                [cache[i] for i in self.value if i in cache] if cache else [],
+            )
+            return qs
+        else:
+            val = cache.get(self.value, None)
+            if val is None:
+                return self.model._default_manager.get(pk=self.value)
+            else:
+                return val
+
+
+class RelInfo:
+    FKField: str = "fk"
+    QSField: str = "qs"
+    RIField: str = "rel"
+    RLField: str = "rel_l"
+
+    def __init__(self, model, type, field) -> None:
+        self.model = model
+        self.type = type
+        self.field = field
+
+
+class CacheBuilder:
+    def __init__(self, related_fields: Dict[str, RelInfo]) -> None:
+        self.__related_fields__ = related_fields
+
+    @classmethod
+    def from_model(cls, model: BaseModel) -> "CacheBuilder":
+        return cls(related_fields=cls.inspect_related_fields(model))
+
+    @classmethod
+    def inspect_related_fields(cls, model: Type[BaseModel]) -> Dict[str, RelInfo]:
+        related = {}
+        for field_name, field in model.model_fields.items():
+            annotation = field.annotation
+            origin = get_origin(annotation)
+            args = get_args(annotation)
+
+            if isclass(origin) and issubclass(origin, ForeignKey):
+                related[field_name] = RelInfo(
+                    get_type(annotation), RelInfo.FKField, field
+                )
+            elif isclass(origin) and issubclass(origin, QuerySet):
+                related[field_name] = RelInfo(
+                    get_type(annotation), RelInfo.QSField, field
+                )
+
+            elif isclass(origin) and issubclass(origin, Sequence):
+                lazy_types = [
+                    _LazyType(arg).evaluate(model)
+                    for arg in args
+                    if isinstance(arg, str)
+                ]
+                subclass = next(
+                    (
+                        c
+                        for c in list(args) + lazy_types
+                        if isclass(c) and issubclass(c, BaseModel)
+                    ),
+                    None,
+                )
+                if isclass(subclass) and issubclass(subclass, BaseModel):
+                    related[field_name] = RelInfo(subclass, RelInfo.RLField, field)
+
+            elif isclass(annotation) and issubclass(annotation, BaseModel):
+                related[field_name] = RelInfo(annotation, RelInfo.RIField, field)
+        return related
+
+    def get_all_fk_data(self, data):
+        if isinstance(data, Sequence):
+            fk_data = defaultdict(list)
+            for index in range(len(data)):
+                child_fk_data = self.get_all_fk_data(data[index])
+                for model, touples in child_fk_data.items():
+                    fk_data[model] += [(f"{index}.{t[0]}", t[1]) for t in touples]
+            return fk_data
+        return self.get_fk_data(data)
+
+    def get_fk_data(self, data):
+        fk_data = defaultdict(list)
+        if not data:
+            return fk_data
+        for field_name, info in self.__related_fields__.items():
+            if info.type == RelInfo.FKField:
+                value = pointed_getter(data, field_name, None)
+                if value:
+                    if isinstance(
+                        value, ValueWithCache
+                    ):  # needed to break recursive cache builds
+                        fk_data = {}
+                        break
+                    fk_data[info.model].append(
+                        (
+                            field_name,
+                            pointed_getter(value, info.model._meta.pk.attname, value),
+                        )
+                    )
+            elif info.type == RelInfo.QSField:
+                value = pointed_getter(data, field_name, [])
+                if isinstance(value, list):
+                    if any(
+                        True for v in value if isinstance(v, ValueWithCache)
+                    ):  # needed to break recursive cache builds
+                        fk_data = {}
+                        break
+                    fk_data[info.model].append(
+                        (
+                            field_name,
+                            [
+                                pointed_getter(i, info.model._meta.pk.attname, i)
+                                for i in value
+                                if i
+                            ],
+                        )
+                    )
+
+            elif info.type == RelInfo.RLField:
+                values = pointed_getter(data, field_name, [])
+                if isinstance(values, list):
+                    for index in range(len(values)):
+                        for model, touples in (
+                            self.from_model(info.model)
+                            .get_all_fk_data(values[index])
+                            .items()
+                        ):
+                            fk_data[model] += [
+                                (f"{field_name}.{index}.{t[0]}", t[1]) for t in touples
+                            ]
+            elif info.type == RelInfo.RIField:
+                value = pointed_getter(data, field_name, None)
+                child_fk_data = self.from_model(info.model).get_all_fk_data(value)
+                for model, touples in child_fk_data.items():
+                    fk_data[model] += [(f"{field_name}.{t[0]}", t[1]) for t in touples]
+        return fk_data
+
+    def inject_cache(self, data: Any) -> Any:
+        if not STRUCTURED_FIELD_CACHE_ENABLED:
+            return data
+        fk_data = self.get_all_fk_data(data)
+        plainset = defaultdict(set)
+        for model, touples in fk_data.items():
+            for t in touples:
+                if isinstance(t[1], Sequence):
+                    plainset[model].update(t[1])
+                else:
+                    plainset[model].add(t[1])
+        cache = Cache()
+        for model, values in plainset.items():
+            models = []
+            pks = []
+            for value in values:
+                if isinstance(value, model):
+                    models.append(value)
+                else:
+                    pks.append(value)
+            models_pks = [m.pk for m in models]
+            pks = [pk for pk in pks if pk not in models_pks]
+            if len(pks):
+                models += list(model.objects.filter(pk__in=pks))
+            cache[model] = {obj.pk: obj for obj in models}
+
+        for model, touples in fk_data.items():
+            for t in touples:
+                pointed_setter(data, t[0], ValueWithCache(cache, model, t[1]))
+        return data
