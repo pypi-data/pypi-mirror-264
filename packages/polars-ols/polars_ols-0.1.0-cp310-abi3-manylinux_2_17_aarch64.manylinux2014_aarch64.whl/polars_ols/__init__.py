7d9ab250040b263f265b17bb6c00dd0b50b7c980
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-26 03:03:43 +0000

    Add package polars-ols/0.1.0/polars_ols-0.1.0-cp310-abi3-manylinux_2_17_aarch64.manylinux2014_aarch64.whl

---
+++ packages/polars-ols/polars_ols-0.1.0-cp310-abi3-manylinux_2_17_aarch64.manylinux2014_aarch64.whl/polars_ols/__init__.py
@@ -0,0 +1,56 @@
+# ruff: noqa: E402
+# flake8: noqa: E402
+import os
+
+os.environ["POLARS_SKIP_CPU_CHECK"] = "1"
+
+import polars as pl
+
+from polars_ols.least_squares import (
+    least_squares,
+    least_squares_from_formula,
+    recursive_least_squares,
+)
+from polars_ols.utils import build_expressions_from_patsy_formula
+
+__all__ = [
+    "least_squares",
+    "least_squares_from_formula",
+    "LeastSquares",
+]
+
+
+@pl.api.register_expr_namespace("least_squares")
+class LeastSquares:
+    def __init__(self, expr: pl.Expr):
+        self._expr = expr
+
+    def least_squares(self, *features: pl.Expr, **kwargs) -> pl.Expr:
+        return least_squares(self._expr, *features, **kwargs)
+
+    def ols(self, *features: pl.Expr, **kwargs) -> pl.Expr:
+        return self.least_squares(*features, **kwargs)
+
+    def wls(self, *features: pl.Expr, sample_weights: pl.Expr, **kwargs) -> pl.Expr:
+        return self.least_squares(*features, sample_weights=sample_weights, **kwargs)
+
+    def ridge(self, *features: pl.Expr, alpha: float, **kwargs) -> pl.Expr:
+        return self.least_squares(*features, alpha=alpha, l1_ratio=0.0, **kwargs)
+
+    def lasso(self, *features: pl.Expr, alpha: float, **kwargs) -> pl.Expr:
+        return self.least_squares(*features, alpha=alpha, l1_ratio=1.0, **kwargs)
+
+    def elastic_net(self, *features: pl.Expr, alpha: float, l1_ratio: float = 0.5, **kwargs):
+        return self.least_squares(*features, alpha=alpha, l1_ratio=l1_ratio, **kwargs)
+
+    def rls(self, *features: pl.Expr, half_life: float | None, **kwargs):
+        return recursive_least_squares(self._expr, *features, half_life=half_life, **kwargs)
+
+    def expanding_ols(self, *features: pl.Expr, **kwargs):
+        return self.rls(*features, half_life=None, **kwargs)
+
+    def from_formula(self, formula: str, **kwargs) -> pl.Expr:
+        features, add_intercept = build_expressions_from_patsy_formula(
+            formula, include_dependent_variable=False
+        )
+        return self.least_squares(*features, add_intercept=add_intercept, **kwargs)
