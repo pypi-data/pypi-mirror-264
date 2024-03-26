52d2df8f4247512e21b274bf459728732431f11a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-26 03:04:08 +0000

    Add package ChemMod/0.0.4/ChemMod-0.0.4.tar.gz

---
+++ packages/ChemMod/ChemMod-0.0.4.tar.gz/ChemMod-0.0.4/ChemMod/__init__.py
@@ -0,0 +1,6 @@
+
+from ChemMod import equation
+from ChemMod import plot
+from ChemMod import ChemMod
+
+__all__ = ["element_data","equil","M","arrhenius_plot","bjerrum_plot","element_data","gibbs_plot","order_plot"]
