5c8ae0effecfabb3fdc4ff538100be78242b25fc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-26 16:04:35 +0000

    Add package yanga/1.5.9/yanga-1.5.9.tar.gz

---
+++ packages/yanga/yanga-1.5.9.tar.gz/yanga-1.5.9/src/yanga/ymain.py
@@ -0,0 +1,36 @@
+import sys
+from argparse import ArgumentParser
+from sys import argv
+
+from py_app_dev.core.cmd_line import CommandLineHandlerBuilder
+from py_app_dev.core.exceptions import UserNotificationException
+from py_app_dev.core.logging import logger, setup_logger
+
+from yanga import __version__
+from yanga.commands.gui import GuiCommand
+from yanga.commands.init import InitCommand
+from yanga.commands.install import InstallCommand
+from yanga.commands.run import RunCommand
+
+
+def do_run() -> None:
+    parser = ArgumentParser(prog="yanga", description="Yanga CLI", exit_on_error=False)
+    parser.add_argument("-v", "--version", action="version", version=f"%(prog)s {__version__}")
+    builder = CommandLineHandlerBuilder(parser)
+    builder.add_commands([InitCommand(), InstallCommand(), GuiCommand(), RunCommand()])
+    handler = builder.create()
+    handler.run(argv[1:])
+
+
+def main() -> int:
+    try:
+        setup_logger()
+        do_run()
+    except UserNotificationException as e:
+        logger.error(f"{e}")
+        return 1
+    return 0
+
+
+if __name__ == "__main__":
+    sys.exit(main())
