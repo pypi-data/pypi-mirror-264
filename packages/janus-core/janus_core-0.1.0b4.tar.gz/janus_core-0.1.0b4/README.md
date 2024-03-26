095de020720e8f956eab924c615cf543cf21fa37
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-26 17:03:51 +0000

    Add package janus-core/0.1.0b4/janus_core-0.1.0b4.tar.gz

---
+++ packages/janus-core/janus_core-0.1.0b4.tar.gz/janus_core-0.1.0b4/README.md
@@ -0,0 +1,131 @@
+[![Build Status][ci-badge]][ci-link]
+[![Coverage Status][cov-badge]][cov-link]
+[![Docs status][docs-badge]][docs-link]
+[![PyPI version][pypi-badge]][pypi-link]
+[![License][license-badge]][license-link]
+[![DOI][doi-badge]][doi-link]
+
+# janus-core
+
+Tools for machine learnt interatomic potentials
+
+## Features in development
+
+- [x] Support for multiple MLIPs
+  - MACE
+  - M3GNET
+  - CHGNET
+- [x] Single point calculations
+- [x] Geometry optimisation
+- [ ] Molecular Dynamics
+  - NVE
+  - NVT (Langevin(Eijnden/Ciccotti flavour) and Nosé-Hoover (Melchionna flavour))
+  - NPT (Nosé-Hoover (Melchiona flavour))
+- [ ] Nudge Elastic Band
+- [ ] Phonons
+  - vibroscopy
+- [ ] Training ML potentials
+  - MACE
+- [ ] Fine tunning MLIPs
+  - MACE
+- [ ] Rare events simulations
+  - PLUMED
+
+The code relies heavily on ASE, unless something else is mentioned.
+
+## Development
+
+1. Install [poetry](https://python-poetry.org/docs/#installation)
+2. (Optional) Create a virtual environment
+3. Install `janus-core` with dependencies:
+
+```shell
+git clone https://github.com/stfc/janus-core
+cd janus-core
+pip install --upgrade pip
+poetry install --with pre-commit,dev,docs  # install extra dependencies
+pre-commit install  # install pre-commit hooks
+pytest -v  # discover and run all tests
+```
+
+Manually updating ASE via https://gitlab.com/ase/ase is strongly recommended, as tags are no longer regularly published. For example:
+
+```shell
+pip install git+https://gitlab.com/ase/ase.git@b31569210d739bd12c8ad2b6ec0290108e049eea
+```
+
+To prevent poetry downgrading ASE when installing in future, add the commit to pyproject.toml:
+
+```shell
+poetry add git+https://gitlab.com:ase/ase.git#b31569210d739bd12c8ad2b6ec0290108e049eea
+```
+
+## Examples
+
+### Single Point Calculations
+
+Perform a single point calcuation (using the [MACE-MP](https://github.com/ACEsuit/mace-mp) "small" force-field):
+```shell
+janus singlepoint --struct tests/data/NaCl.cif --arch mace_mp --calc-kwargs "{'model' : 'small'}"
+```
+
+This will calculate the energy, stress and forces and save this in `NaCl-results.xyz`, in addition to generating a log file, `singlepoint.log`.
+
+Additional options may be specified. For example:
+
+```shell
+janus singlepoint --struct tests/data/NaCl.cif --arch mace --calc-kwargs "{'model' : '/path/to/your/ml.model'}" --property energy --log './example.log' --write-kwargs "{'filename': './example.xyz'}"
+```
+
+This defines the MLIP architecture and path to your locally saved model, as well as changing where the log and results files are saved.
+
+Note: the MACE calculator currently returns energy, forces and stress together, so in this case the choice of property will not change the output.
+
+For all options, run `janus singlepoint --help`.
+
+
+### Geometry optimization
+
+Perform geometry optimization (using the [MACE-MP](https://github.com/ACEsuit/mace-mp) "small" force-field):
+
+```shell
+janus geomopt --struct tests/data/H2O.cif --arch mace_mp --calc-kwargs "{'model' : 'small'}"
+```
+
+This will calculate optimize the atomic positions and save the resulting structure in `H2O-opt.xyz`, in addition to generating a log file, `geomopt.log`.
+
+Additional options may be specified. This shares most options with `singlepoint`, as well as a few additional options, such as:
+
+```shell
+janus geomopt --struct tests/data/NaCl.cif --arch mace_mp --calc-kwargs "{'model' : 'small'}" --fully-opt --vectors-only --traj 'NaCl-traj.xyz'
+```
+
+This allows the cell to be optimised, allowing only hydrostatic deformation, and saves the optimization trajector in addition to the final structure and log.
+
+For all options, run `janus geomopt --help`.
+
+## License
+
+[BSD 3-Clause License](LICENSE)
+
+## Funding
+
+Contributors to this project were funded by
+
+[![PSDI](https://raw.githubusercontent.com/stfc/janus-core/main/docs/source/images/psdi-100.webp)](https://www.psdi.ac.uk/)
+[![ALC](https://raw.githubusercontent.com/stfc/janus-core/main/docs/source/images/alc-100.webp)](https://adalovelacecentre.ac.uk/)
+[![CoSeC](https://raw.githubusercontent.com/stfc/janus-core/main/docs/source/images/cosec-100.webp)](https://www.scd.stfc.ac.uk/Pages/CoSeC.aspx)
+
+
+[ci-badge]: https://github.com/stfc/janus-core/workflows/ci/badge.svg
+[ci-link]: https://github.com/stfc/janus-core/actions
+[cov-badge]: https://coveralls.io/repos/github/stfc/janus-core/badge.svg?branch=main
+[cov-link]: https://coveralls.io/github/stfc/janus-core?branch=main
+[docs-badge]: https://github.com/stfc/janus-core/actions/workflows/docs.yml/badge.svg
+[docs-link]: https://stfc.github.io/janus-core/
+[pypi-badge]: https://badge.fury.io/py/janus-core.svg
+[pypi-link]: https://badge.fury.io/py/janus-core
+[license-badge]: https://img.shields.io/badge/License-BSD_3--Clause-blue.svg
+[license-link]: https://opensource.org/licenses/BSD-3-Clause
+[doi-link]: https://zenodo.org/badge/latestdoi/754081470
+[doi-badge]: https://zenodo.org/badge/754081470.svg
---
+++ packages/janus-core/janus_core-0.1.0b4.tar.gz/janus_core-0.1.0b4/janus_core/cli.py
@@ -0,0 +1,375 @@
+"""Set up commandline interface."""
+
+import ast
+from pathlib import Path
+from typing import Annotated
+
+import typer
+
+from janus_core.geom_opt import optimize
+from janus_core.single_point import SinglePoint
+
+app = typer.Typer()
+
+
+class TyperDict:  #  pylint: disable=too-few-public-methods
+    """
+    Custom dictionary for typer.
+
+    Parameters
+    ----------
+    value : str
+        Value of string representing a dictionary.
+    """
+
+    def __init__(self, value: str):
+        """
+        Initialise class.
+
+        Parameters
+        ----------
+        value : str
+            Value of string representing a dictionary.
+        """
+        self.value = value
+
+    def __str__(self):
+        """
+        String representation of class.
+
+        Returns
+        -------
+        str
+            Class name and value of string representing a dictionary.
+        """
+        return f"<TyperDict: value={self.value}>"
+
+
+def parse_dict_class(value: str):
+    """
+    Convert string input into a dictionary.
+
+    Parameters
+    ----------
+    value : str
+        String representing dictionary to be parsed.
+
+    Returns
+    -------
+    TyperDict
+        Parsed string as a dictionary.
+    """
+    return TyperDict(ast.literal_eval(value))
+
+
+def parse_typer_dicts(typer_dicts: list[TyperDict]) -> list[dict]:
+    """
+    Convert list of TyperDict objects to list of dictionaries.
+
+    Parameters
+    ----------
+    typer_dicts : list[TyperDict]
+        List of TyperDict objects to convert.
+
+    Returns
+    -------
+    list[dict]
+        List of converted dictionaries.
+
+    Raises
+    ------
+    ValueError
+        If items in list are not converted to dicts.
+    """
+    for i, typer_dict in enumerate(typer_dicts):
+        typer_dicts[i] = typer_dict.value if typer_dict else {}
+        if not isinstance(typer_dicts[i], dict):
+            raise ValueError(f"{typer_dicts[i]} must be passed as a dictionary")
+    return typer_dicts
+
+
+# Shared type aliases
+StructPath = Annotated[
+    Path, typer.Option("--struct", help="Path of structure to simulate.")
+]
+Architecture = Annotated[
+    str, typer.Option("--arch", help="MLIP architecture to use for calculations.")
+]
+Device = Annotated[str, typer.Option(help="Device to run calculations on.")]
+ReadKwargs = Annotated[
+    TyperDict,
+    typer.Option(
+        parser=parse_dict_class,
+        help=(
+            """
+            Keyword arguments to pass to ase.io.read. Must be passed as a dictionary
+            wrapped in quotes, e.g. "{'key' : value}".  [default: "{}"]
+            """
+        ),
+        metavar="DICT",
+    ),
+]
+CalcKwargs = Annotated[
+    TyperDict,
+    typer.Option(
+        parser=parse_dict_class,
+        help=(
+            """
+            Keyword arguments to pass to selected calculator. Must be passed as a
+            dictionary wrapped in quotes, e.g. "{'key' : value}". For the default
+            architecture ('mace_mp'), "{'model':'small'}" is set unless overwritten.
+            """
+        ),
+        metavar="DICT",
+    ),
+]
+WriteKwargs = Annotated[
+    TyperDict,
+    typer.Option(
+        parser=parse_dict_class,
+        help=(
+            """
+            Keyword arguments to pass to ase.io.write when saving results. Must be
+            passed as a dictionary wrapped in quotes, e.g. "{'key' : value}".
+             [default: "{}"]
+            """
+        ),
+        metavar="DICT",
+    ),
+]
+LogFile = Annotated[Path, typer.Option("--log", help="Path to save logs to.")]
+
+
+@app.command(help="Perform single point calculations and save to file.")
+def singlepoint(
+    struct_path: StructPath,
+    architecture: Architecture = "mace_mp",
+    device: Device = "cpu",
+    properties: Annotated[
+        list[str],
+        typer.Option(
+            "--property",
+            help=(
+                "Properties to calculate. If not specified, 'energy', 'forces' "
+                "and 'stress' will be returned."
+            ),
+        ),
+    ] = None,
+    out_file: Annotated[
+        Path,
+        typer.Option(
+            "--out",
+            help=(
+                "Path to save structure with calculated results. Default is inferred "
+                "from name of structure file."
+            ),
+        ),
+    ] = None,
+    read_kwargs: ReadKwargs = None,
+    calc_kwargs: CalcKwargs = None,
+    write_kwargs: WriteKwargs = None,
+    log_file: LogFile = "singlepoint.log",
+):
+    """
+    Perform single point calculations and save to file.
+
+    Parameters
+    ----------
+    struct_path : Path
+        Path of structure to simulate.
+    architecture : Optional[str]
+        MLIP architecture to use for single point calculations.
+        Default is "mace_mp".
+    device : Optional[str]
+        Device to run model on. Default is "cpu".
+    properties : Optional[str]
+        Physical properties to calculate. Default is "energy".
+    out_file : Optional[Path]
+        Path to save structure with calculated results. Default is inferred from name
+        of the structure file.
+    read_kwargs : Optional[dict[str, Any]]
+        Keyword arguments to pass to ase.io.read. Default is {}.
+    calc_kwargs : Optional[dict[str, Any]]
+        Keyword arguments to pass to the selected calculator. Default is {}.
+    write_kwargs : Optional[dict[str, Any]]
+        Keyword arguments to pass to ase.io.write when saving results. Default is {}.
+    log_file : Optional[Path]
+        Path to write logs to. Default is "singlepoint.log".
+    """
+    [read_kwargs, calc_kwargs, write_kwargs] = parse_typer_dicts(
+        [read_kwargs, calc_kwargs, write_kwargs]
+    )
+
+    # Check filename for results not duplicated
+    if "filename" in write_kwargs:
+        raise ValueError("'filename' must be passed through the --out option")
+
+    # Default filname for saving results determined in SinglePoint if not specified
+    if out_file:
+        write_kwargs["filename"] = out_file
+
+    s_point = SinglePoint(
+        struct_path=struct_path,
+        architecture=architecture,
+        device=device,
+        read_kwargs=read_kwargs,
+        calc_kwargs=calc_kwargs,
+        log_kwargs={"filename": log_file, "filemode": "w"},
+    )
+    s_point.run(properties=properties, write_results=True, write_kwargs=write_kwargs)
+
+
+@app.command(
+    help="Perform geometry optimization and save optimized structure to file.",
+)
+def geomopt(  # pylint: disable=too-many-arguments,too-many-locals
+    struct_path: StructPath,
+    fmax: Annotated[
+        float, typer.Option("--max-force", help="Maximum force for convergence.")
+    ] = 0.1,
+    steps: Annotated[
+        int, typer.Option("--steps", help="Maximum number of optimization steps.")
+    ] = 1000,
+    architecture: Architecture = "mace_mp",
+    device: Device = "cpu",
+    vectors_only: Annotated[
+        bool,
+        typer.Option(
+            "--vectors-only",
+            help=("Optimize cell vectors, as well as atomic positions."),
+        ),
+    ] = False,
+    fully_opt: Annotated[
+        bool,
+        typer.Option(
+            "--fully-opt",
+            help="Fully optimize the cell vectors, angles, and atomic positions.",
+        ),
+    ] = False,
+    out_file: Annotated[
+        Path,
+        typer.Option(
+            "--out",
+            help=(
+                "Path to save optimized structure. Default is inferred from name "
+                "of structure file."
+            ),
+        ),
+    ] = None,
+    traj_file: Annotated[
+        str,
+        typer.Option(
+            "--traj", help="Path if saving optimization frames.  [default: None]"
+        ),
+    ] = None,
+    read_kwargs: ReadKwargs = None,
+    calc_kwargs: CalcKwargs = None,
+    opt_kwargs: Annotated[
+        TyperDict,
+        typer.Option(
+            parser=parse_dict_class,
+            help=(
+                """
+                Keyword arguments to pass to optimizer. Must be passed as a dictionary
+                wrapped in quotes, e.g. "{'key' : value}".  [default: "{}"]
+                """
+            ),
+            metavar="DICT",
+        ),
+    ] = None,
+    write_kwargs: WriteKwargs = None,
+    log_file: LogFile = "geomopt.log",
+):
+    """
+    Perform geometry optimization and save optimized structure to file.
+
+    Parameters
+    ----------
+    struct_path : Path
+        Path of structure to simulate.
+    fmax : float
+        Set force convergence criteria for optimizer in units eV/Å.
+        Default is 0.1.
+    steps : int
+        Set maximum number of optimization steps to run. Default is 1000.
+    architecture : Optional[str]
+        MLIP architecture to use for geometry optimization.
+        Default is "mace_mp".
+    device : Optional[str]
+        Device to run model on. Default is "cpu".
+    vectors_only : bool
+        Whether to optimize cell vectors, as well as atomic positions, by setting
+        `hydrostatic_strain` in the filter function. Default is False.
+    fully_opt : bool
+        Whether to fully optimize the cell vectors, angles, and atomic positions.
+        Default is False.
+    out_file : Optional[Path]
+        Path to save optimized structure, or last structure if optimization did not
+        converge. Default is inferred from name of structure file.
+    traj_file : Optional[str]
+        Path if saving optimization frames. Default is None.
+    read_kwargs : Optional[dict[str, Any]]
+        Keyword arguments to pass to ase.io.read. Default is {}.
+    calc_kwargs : Optional[dict[str, Any]]
+        Keyword arguments to pass to the selected calculator. Default is {}.
+    opt_kwargs : Optional[ASEOptArgs]
+        Keyword arguments to pass to optimizer. Default is {}.
+    write_kwargs : Optional[ASEWriteArgs]
+        Keyword arguments to pass to ase.io.write when saving optimized structure.
+        Default is {}.
+    log_file : Optional[Path]
+        Path to write logs to. Default is "geomopt.log".
+    """
+    [read_kwargs, calc_kwargs, opt_kwargs, write_kwargs] = parse_typer_dicts(
+        [read_kwargs, calc_kwargs, opt_kwargs, write_kwargs]
+    )
+
+    # Set up single point calculator
+    s_point = SinglePoint(
+        struct_path=struct_path,
+        architecture=architecture,
+        device=device,
+        read_kwargs=read_kwargs,
+        calc_kwargs=calc_kwargs,
+        log_kwargs={"filename": log_file, "filemode": "w"},
+    )
+
+    # Check optimized structure path not duplicated
+    if "filename" in write_kwargs:
+        raise ValueError("'filename' must be passed through the --out option")
+
+    # Check trajectory path not duplicated
+    if "trajectory" in opt_kwargs:
+        raise ValueError("'trajectory' must be passed through the --traj option")
+
+    # Set default filname for writing optimized structure if not specified
+    if out_file:
+        write_kwargs["filename"] = out_file
+    else:
+        write_kwargs["filename"] = f"{s_point.struct_name}-opt.xyz"
+
+    # Set same trajectory filenames to overwrite saved binary with xyz
+    opt_kwargs["trajectory"] = traj_file if traj_file else None
+    traj_kwargs = {"filename": traj_file} if traj_file else None
+
+    # Set hydrostatic_strain
+    # If not passed --fully-opt or --vectors-only, will be unused
+    filter_kwargs = {"hydrostatic_strain": vectors_only}
+
+    # Use default filter if passed --fully-opt or --vectors-only
+    # Otherwise override with None
+    fully_opt_dict = {} if (fully_opt or vectors_only) else {"filter_func": None}
+
+    # Run geometry optimization and save output structure
+    optimize(
+        s_point.struct,
+        fmax=fmax,
+        steps=steps,
+        filter_kwargs=filter_kwargs,
+        **fully_opt_dict,
+        opt_kwargs=opt_kwargs,
+        write_results=True,
+        write_kwargs=write_kwargs,
+        traj_kwargs=traj_kwargs,
+        log_kwargs={"filename": log_file, "filemode": "a"},
+    )
