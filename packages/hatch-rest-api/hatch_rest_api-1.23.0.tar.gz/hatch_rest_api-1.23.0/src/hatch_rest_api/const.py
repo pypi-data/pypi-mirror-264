75eccbba65e7d37db8adbd0f96f4ca4de3dfef63
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-26 03:04:10 +0000

    Add package hatch-rest-api/1.23.0/hatch_rest_api-1.23.0.tar.gz

---
+++ packages/hatch-rest-api/hatch_rest_api-1.23.0.tar.gz/hatch_rest_api-1.23.0/src/hatch_rest_api/const.py
@@ -0,0 +1,67 @@
+from enum import Enum
+
+SENSITIVE_FIELD_NAMES = [
+    "username",
+    "password",
+]
+
+MAX_IOT_VALUE = 65535
+
+
+class RestMiniAudioTrack(Enum):
+    NONE = 0
+    Heartbeat = 10124
+    Water = 10125
+    WhiteNoise = 10126
+    Dryer = 10127
+    Ocean = 10128
+    Wind = 10129
+    Rain = 10130
+    Birds = 10131
+
+
+class RestPlusAudioTrack(Enum):
+    NONE = 0
+    Stream = 2
+    PinkNoise = 3
+    Dryer = 4
+    Ocean = 5
+    Wind = 6
+    Rain = 7
+    Bird = 9
+    Crickets = 10
+    Brahms = 11
+    Twinkle = 13
+    RockABye = 14
+
+
+REST_MINI_AUDIO_TRACKS = [
+    RestMiniAudioTrack.NONE,
+    RestMiniAudioTrack.WhiteNoise,
+    RestMiniAudioTrack.Ocean,
+    RestMiniAudioTrack.Rain,
+    RestMiniAudioTrack.Water,
+    RestMiniAudioTrack.Wind,
+    RestMiniAudioTrack.Birds,
+    RestMiniAudioTrack.Dryer,
+    RestMiniAudioTrack.Heartbeat,
+]
+
+
+REST_PLUS_AUDIO_TRACKS = [
+    RestPlusAudioTrack.NONE,
+    RestPlusAudioTrack.Stream,
+    RestPlusAudioTrack.PinkNoise,
+    RestPlusAudioTrack.Dryer,
+    RestPlusAudioTrack.Ocean,
+    RestPlusAudioTrack.Wind,
+    RestPlusAudioTrack.Rain,
+    RestPlusAudioTrack.Bird,
+    RestPlusAudioTrack.Crickets,
+    RestPlusAudioTrack.Brahms,
+    RestPlusAudioTrack.Twinkle,
+    RestPlusAudioTrack.RockABye,
+]
+
+RIOT_FLAGS_CLOCK_24_HOUR = 2048
+RIOT_FLAGS_CLOCK_ON = 32768
