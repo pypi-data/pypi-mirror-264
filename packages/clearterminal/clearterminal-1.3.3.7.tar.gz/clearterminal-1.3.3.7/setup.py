3cf862e49c80e21ad156a16326f820b8757a36a9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-26 17:05:08 +0000

    Add package clearterminal/1.3.3.7/clearterminal-1.3.3.7.tar.gz

---
+++ packages/clearterminal/clearterminal-1.3.3.7.tar.gz/clearterminal-1.3.3.7/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='clearterminal',
+      version='1.3.3.7',
+      description='Clear your terminal with ONE COMMAND',
+      packages=['clearterminal'],
+      author_email='holinim@duck.com',
+      zip_safe=True)
