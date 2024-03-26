7987877ff6bd17880292b6f86384308bd3c0b6f0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-26 14:04:34 +0000

    Add package pyrandommedia/0.0.2/pyRandomMedia-0.0.2.tar.gz

---
+++ packages/pyrandommedia/pyRandomMedia-0.0.2.tar.gz/pyRandomMedia-0.0.2/setup.py
@@ -0,0 +1,24 @@
+import setuptools
+
+with open("README.md", "r", encoding = "utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name = "pyRandomMedia",
+    version = "0.0.2",
+    author = "Stan, Nicholas, Benson, Sangely",
+    description = "A package used to generate random media for website testing. This includes songs, movies, news and Tv shows.",
+    long_description = long_description,
+    long_description_content_type = "text/markdown",
+    url = "https://github.com/software-students-spring2024/3-python-package-exercise-team-kiwi",
+    project_urls = {
+        "Github Repo": "https://github.com/software-students-spring2024/3-python-package-exercise-team-kiwi",
+    },
+    classifiers = [
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+    ],
+    package_dir = {"": "src"},
+    packages = setuptools.find_packages(where="src"),
+    python_requires = ">=3.6"
+)
