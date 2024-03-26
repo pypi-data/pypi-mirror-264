321bc86320c2d8f951c7d2b44632466e979fdfcc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-26 03:04:20 +0000

    Add package udata-front/3.5.5.dev17371/udata_front-3.5.5.dev17371-py2.py3-none-any.whl

---
+++ packages/udata-front/udata_front-3.5.5.dev17371-py2.py3-none-any.whl/udata_front/theme/gouvfr/datagouv-components/src/components/index.ts
@@ -0,0 +1,6 @@
+import { CopyButton } from "./CopyButton";
+import OrganizationNameWithCertificate from "./Organization";
+import Pagination from "./Pagination";
+import { ResourceAccordion, Loader as ResourceAccordionLoader } from "./ResourceAccordion";
+
+export { CopyButton, OrganizationNameWithCertificate, Pagination, ResourceAccordion, ResourceAccordionLoader };
