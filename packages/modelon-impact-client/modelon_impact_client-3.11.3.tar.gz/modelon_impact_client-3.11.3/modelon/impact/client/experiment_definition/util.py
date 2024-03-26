179f7e16db52414525282e4099b9de2b7c0a9e53
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-26 15:04:32 +0000

    Add package modelon-impact-client/3.11.3/modelon_impact_client-3.11.3.tar.gz

---
+++ packages/modelon-impact-client/modelon_impact_client-3.11.3.tar.gz/modelon_impact_client-3.11.3/modelon/impact/client/experiment_definition/util.py
@@ -0,0 +1,25 @@
+from __future__ import annotations
+
+from typing import TYPE_CHECKING, Any, Dict, Optional
+
+from modelon.impact.client.options import BaseExecutionOptions
+
+if TYPE_CHECKING:
+    from modelon.impact.client.entities.case import Case
+
+
+def get_options(default_options: Any, options: Optional[Any]) -> Dict[str, Any]:
+    return (
+        dict(default_options())
+        if options is None
+        else dict(options)
+        if isinstance(options, BaseExecutionOptions)
+        else options
+    )
+
+
+def case_to_identifier_dict(case: Case) -> Dict[str, Any]:
+    return {
+        "caseId": case.id,
+        "experimentId": case.experiment_id,
+    }
