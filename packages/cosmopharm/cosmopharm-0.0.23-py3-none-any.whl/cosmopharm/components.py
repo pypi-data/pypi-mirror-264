360d8fdf3987b731125e70c1a9038c9ad0051f31
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-26 06:03:47 +0000

    Add package cosmopharm/0.0.23/cosmopharm-0.0.23-py3-none-any.whl

---
+++ packages/cosmopharm/cosmopharm-0.0.23-py3-none-any.whl/cosmopharm/components.py
@@ -0,0 +1,30 @@
+from typing import Optional
+from numbers import Number
+
+class Component:
+    def __init__(self, name: Optional[str] = None,
+                 Mw: Optional[Number] = None,
+                 T_fus: Optional[Number] = None,
+                 H_fus: Optional[Number] = None,
+                 Cp_fus_a_fit: Optional[Number] = None,
+                 Cp_fus_bT_fit: Optional[Number] = None,
+                 v_298: Optional[Number] = None,
+                 v_hc: Optional[Number] = None,
+                 ):
+
+        self.name = name
+        self.Mw = Mw  # molar weight in g/mol
+
+        # for SLE calculations
+        self.T_fus = T_fus
+        self.H_fus = H_fus
+        self.Cp_fus_a_fit = Cp_fus_a_fit
+        self.Cp_fus_bT_fit = Cp_fus_bT_fit
+        self.v_298 = v_298
+        self.v_hc = v_hc
+
+    def __repr__(self):
+        return f"<Component('{self.name}')>"
+
+    def __str__(self):
+        return f"{self.name}"
