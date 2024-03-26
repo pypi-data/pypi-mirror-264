0bed9e610d6b7726ee25b52b395fd18b11d7505b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-26 03:03:54 +0000

    Add package PoolController/1.4.0/PoolController-1.4.0.tar.gz

---
+++ packages/PoolController/PoolController-1.4.0.tar.gz/PoolController-1.4.0/setup.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+
+# import os
+# import imp
+from setuptools import setup
+
+release = "1.4.0"
+
+package_dir = {"sardana.PoolController": "python"}
+
+packages = ["sardana.PoolController"]
+
+provides = ['python']
+
+
+setup(name='PoolController',
+      version=release,
+      author="Sardana Controller Developers",
+      author_email="fs-ec@desy.de",
+      maintainer="DESY",
+      maintainer_email="fs-ec@desy.de",
+      url="https://github.com/desy-fsec/sardana-controllers/",
+      packages=packages,
+      package_dir=package_dir,
+      include_package_data=True,
+      provides=provides,
+      )
