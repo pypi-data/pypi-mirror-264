9b85f930eead5b4da98255ff0e6b7edf0058bb6d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-26 17:04:29 +0000

    Add package json-arrays/0.14.1/json_arrays-0.14.1.tar.gz

---
+++ packages/json-arrays/json_arrays-0.14.1.tar.gz/json_arrays-0.14.1/tests/test_utility.py
@@ -0,0 +1,31 @@
+import pytest
+from json_arrays.utility import get_name_of_file, is_jsonl
+
+
+@pytest.mark.parametrize(
+    "filename",
+    [
+        "test.jsonl",
+        "test.jl",
+        "test.jsonl.gz",
+        "test.jl.gz",
+    ],
+)
+def test_filename_is_jsonl(filename: str):
+    assert is_jsonl(filename)
+
+
+@pytest.mark.parametrize(
+    "filename",
+    [
+        "test.json",
+        "test.json.gz",
+    ],
+)
+def test_filename_is_not_jsonl(filename: str):
+    assert not is_jsonl(filename)
+
+
+@pytest.mark.parametrize("fp, expected", [(None, "")])
+def test_get_name_of_file(fp, expected):
+    assert get_name_of_file(fp) == expected
