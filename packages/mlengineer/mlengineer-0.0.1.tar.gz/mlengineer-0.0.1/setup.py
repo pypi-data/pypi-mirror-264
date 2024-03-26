4575a8f315afeadda58f5d7125e6a071d7a55d24
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-26 04:04:07 +0000

    Add package mlengineer/0.0.1/mlengineer-0.0.1.tar.gz

---
+++ packages/mlengineer/mlengineer-0.0.1.tar.gz/mlengineer-0.0.1/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='mlengineer',
+    version='0.0.1',
+    description='Weco AI Python package',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    author='Yuxiang Wu',
+    author_email='yuxiang@weco.ai',
+    packages=find_packages(),
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License:: OSI Approved :: MIT License',
+        'Operat ing System :: OS Independent',
+    ] ,
+    python_requires='>=3.8',
+)
+
