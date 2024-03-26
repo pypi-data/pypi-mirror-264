ea3661c85b501efa800c83fe5be189d066f62058
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-26 14:05:00 +0000

    Add package edc-crf/0.3.72/edc-crf-0.3.72.tar.gz

---
+++ packages/edc-crf/edc-crf-0.3.72.tar.gz/edc-crf-0.3.72/edc_crf/tests/models.py
@@ -0,0 +1,66 @@
+from datetime import date
+
+from django.db import models
+from edc_consent.managers import ConsentObjectsByCdefManager, CurrentSiteByCdefManager
+from edc_identifier.model_mixins import NonUniqueSubjectIdentifierFieldMixin
+from edc_model.models import BaseUuidModel
+from edc_registration.model_mixins import UpdatesOrCreatesRegistrationModelMixin
+from edc_sites.model_mixins import SiteModelMixin
+from edc_utils import get_utcnow
+
+from edc_crf.model_mixins import CrfModelMixin, CrfStatusModelMixin
+
+
+class SubjectConsent(
+    SiteModelMixin,
+    NonUniqueSubjectIdentifierFieldMixin,
+    UpdatesOrCreatesRegistrationModelMixin,
+    BaseUuidModel,
+):
+    consent_datetime = models.DateTimeField(default=get_utcnow)
+
+    version = models.CharField(max_length=25, default="1")
+
+    identity = models.CharField(max_length=25)
+
+    confirm_identity = models.CharField(max_length=25)
+
+    dob = models.DateField(default=date(1995, 1, 1))
+
+
+class SubjectConsentV1(SubjectConsent):
+    objects = ConsentObjectsByCdefManager()
+    on_site = CurrentSiteByCdefManager()
+
+    class Meta:
+        proxy = True
+
+
+class Crf(CrfModelMixin, CrfStatusModelMixin, BaseUuidModel):
+    f1 = models.CharField(max_length=50, null=True, blank=True)
+
+    f2 = models.CharField(max_length=50, null=True, blank=True)
+
+    f3 = models.CharField(max_length=50, null=True, blank=True)
+
+    class Meta(CrfModelMixin.Meta, BaseUuidModel.Meta):
+        pass
+
+
+class CrfReportDatetimeNotRequired(CrfModelMixin, CrfStatusModelMixin, BaseUuidModel):
+    report_datetime = models.DateTimeField(default=get_utcnow, null=True, blank=True)
+
+    f1 = models.CharField(max_length=50, null=True, blank=True)
+
+    f2 = models.CharField(max_length=50, null=True, blank=True)
+
+    f3 = models.CharField(max_length=50, null=True, blank=True)
+
+    def update_reference_on_save(self):
+        pass
+
+    def metadata_update(self):
+        pass
+
+    class Meta(CrfModelMixin.Meta, BaseUuidModel.Meta):
+        pass
