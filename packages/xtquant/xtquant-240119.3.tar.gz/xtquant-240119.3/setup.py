23482d14bf5936c8eac81c309dc373ba71c233a6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-26 17:04:09 +0000

    Add package xtquant/240119.3/xtquant-240119.3.tar.gz

---
+++ packages/xtquant/xtquant-240119.3.tar.gz/xtquant-240119.3/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='xtquant',
+    version='240119.3',
+    packages=find_packages(),
+    description='xtquant_240119b',
+    long_description=open('README.md',encoding="utf-8").read(),
+    long_description_content_type='text/markdown',
+    author='Willows',
+    author_email='972210116@qq.com',
+    url='http://dict.thinktrader.net/?id=7zqjlm',
+    package_data={
+        # 如果你的pyd文件在包的子目录中
+        'xtquant': ['*.*', '**/*.*'],
+    },
+    python_requires='>=3.6, <3.12',
+    license='GNU GPLv3 ',
+    install_requires=[
+        # 依赖列表
+    ],
+)
