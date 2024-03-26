e5b18bca0ef8297d3ca97db03ec8cb205d2e1005
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-26 04:04:16 +0000

    Add package amsdal_cli/0.1.3/amsdal_cli-0.1.3-py3-none-any.whl

---
+++ packages/amsdal_cli/amsdal_cli-0.1.3-py3-none-any.whl/amsdal_cli/commands/cloud/command.py
@@ -0,0 +1,8 @@
+from amsdal_cli.app import app
+from amsdal_cli.commands.cloud.app import cloud_sub_app
+from amsdal_cli.commands.cloud.dependency.command import *  # noqa
+from amsdal_cli.commands.cloud.deploy.command import *  # noqa
+from amsdal_cli.commands.cloud.secret.command import *  # noqa
+from amsdal_cli.commands.cloud.sub_commands import *  # noqa
+
+app.add_typer(cloud_sub_app, name='cloud')
