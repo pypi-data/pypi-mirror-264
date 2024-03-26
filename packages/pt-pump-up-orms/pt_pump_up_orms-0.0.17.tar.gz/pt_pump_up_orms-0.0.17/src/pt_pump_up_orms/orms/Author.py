a2fdad96c9673bb30325ea3d459663d5eeca9877
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-26 03:03:57 +0000

    Add package pt-pump-up-orms/0.0.17/pt_pump_up_orms-0.0.17.tar.gz

---
+++ packages/pt-pump-up-orms/pt_pump_up_orms-0.0.17.tar.gz/pt_pump_up_orms-0.0.17/src/pt_pump_up_orms/orms/Author.py
@@ -0,0 +1,30 @@
+from requests import Response
+from pt_pump_up_orms import ORM
+
+
+class Author(ORM):
+    def __init__(self,
+                 id: int = None,
+                 name: str = None,
+                 institution: str = None,
+                 link=None) -> None:
+
+        super().__init__(id, "author")
+
+        self.name = name
+        self.institution = institution
+        self.link = link
+
+    def serialize(self) -> dict:
+        return {
+            "id": self._id,
+            "name": self.name,
+            "institution": self.institution,
+            "link": self.link.serialize() if self.link else None
+        }
+
+    def deserialize(self, response: Response):
+        self._id = response.json().get("id")
+        self.name = response.json().get("name")
+        self.institution = response.json().get("institution")
+        self.link = self.link.deserialize(response.json().get("link"))
