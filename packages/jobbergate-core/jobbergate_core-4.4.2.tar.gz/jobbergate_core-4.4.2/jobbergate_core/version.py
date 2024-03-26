5f2aa6f67f457acd26e683d20a99009330dbc6de
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-26 06:04:02 +0000

    Add package jobbergate-core/4.4.2/jobbergate_core-4.4.2.tar.gz

---
+++ packages/jobbergate-core/jobbergate_core-4.4.2.tar.gz/jobbergate_core-4.4.2/jobbergate_core/version.py
@@ -0,0 +1,10 @@
+"""
+Provide the version of the package.
+"""
+from importlib import metadata
+
+
+try:
+    __version__ = metadata.version(__package__ or __name__)
+except metadata.PackageNotFoundError:
+    __version__ = "unknown"
