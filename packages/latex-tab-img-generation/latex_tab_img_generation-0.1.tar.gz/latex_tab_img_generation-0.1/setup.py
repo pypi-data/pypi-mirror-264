13d2f3821a061276a93d1578184d351a072c46d6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-26 06:04:10 +0000

    Add package latex-tab-img-generation/0.1/latex_tab_img_generation-0.1.tar.gz

---
+++ packages/latex-tab-img-generation/latex_tab_img_generation-0.1.tar.gz/latex_tab_img_generation-0.1/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="latex_tab_img_generation",
+    version="0.1",
+    packages=find_packages(),
+    description="A library to generate LaTeX format tables and images",
+    author="nikvotya",
+    author_email="no.name340@mail.ru",
+    license="MIT",
+    install_requires=[],
+    zip_safe=False
+)
