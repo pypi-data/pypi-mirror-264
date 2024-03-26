3fe4e03fd0499f8b2907cfca89b3777409ced200
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-26 16:04:12 +0000

    Add package pyrenoweb/2.0.16/pyrenoweb-2.0.16.tar.gz

---
+++ packages/pyrenoweb/pyrenoweb-2.0.16.tar.gz/pyrenoweb-2.0.16/pyrenoweb/__init__.py
@@ -0,0 +1,20 @@
+# ruff: noqa: F401
+"""A Python Wrapper to communicate with RenoWeb API."""
+
+from __future__ import annotations
+
+from pyrenoweb.api import (
+    GarbageCollection,
+    RenowWebGarbageTypeNotFound,
+    RenowWebNotSupportedError,
+    RenowWebNotValidAddressError,
+    RenowWebNoConnection,
+)
+from pyrenoweb.data import PickupEvents, PickupType, RenoWebAddressInfo
+
+from pyrenoweb.const import ICON_LIST, MUNICIPALITIES_ARRAY, NAME_ARRAY, NAME_LIST
+
+__title__ = "pymrenoweb"
+__version__ = "2.0.16"
+__author__ = "briis"
+__license__ = "MIT"
---
+++ packages/pyrenoweb/pyrenoweb-2.0.16.tar.gz/pyrenoweb-2.0.16/setup.py
@@ -0,0 +1,25 @@
+"""Package Setup."""
+
+import setuptools
+
+with open("README.md") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="pyrenoweb",
+    version="2.0.16",
+    author="briis",
+    author_email="bjarne@briis.com",
+    description="Gets garbage collection data from RenoWeb",
+    license="MIT",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/briis/pyrenoweb",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Development Status :: 4 - Beta",
+    ],
+)
