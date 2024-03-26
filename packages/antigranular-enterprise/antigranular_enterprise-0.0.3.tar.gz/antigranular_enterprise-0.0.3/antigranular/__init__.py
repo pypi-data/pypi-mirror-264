49df91d1e0289c2bbaec7cc6b50fca8c4e7c0dcb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-26 16:04:41 +0000

    Add package antigranular-enterprise/0.0.3/antigranular_enterprise-0.0.3.tar.gz

---
+++ packages/antigranular-enterprise/antigranular_enterprise-0.0.3.tar.gz/antigranular_enterprise-0.0.3/antigranular/__init__.py
@@ -0,0 +1,28 @@
+"""
+Antigranular client package
+"""
+
+
+from .client import login
+from .client import read_config
+from .client import write_config
+
+# Package version dunder
+__version__ = "0.0.3"
+
+# Package author dunder
+__author__ = "Oblivious Software Pvt. Ltd."
+
+# Package * imports dunder
+__all__ = ["login", "read_config", "write_config", "__version__", "__author__"]
+
+"""
+Raise an error if client in loaded in a non-Jupyter environment.
+"""
+from IPython.core.getipython import get_ipython
+
+ipython = get_ipython()
+if ipython is None:
+    raise ValueError(
+        "Please ensure antigranuler is loaded in a valid Jupyter environment."
+    )
