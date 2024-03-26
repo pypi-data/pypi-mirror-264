26b091cf53eec33a606f7606a4fa41c7e1a9c6da
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-26 05:05:17 +0000

    Add package enhanced-cnab/0.0.17/enhanced_cnab-0.0.17-py3-none-any.whl

---
+++ packages/enhanced-cnab/enhanced_cnab-0.0.17-py3-none-any.whl/cnab/base/cnab_400/registro2.py
@@ -0,0 +1,14 @@
+from typing import Optional
+from cnab.base.registro import Registro
+from cnab.base.registro_remessa import RegistroRemessa
+
+class CNAB400Registro2(RegistroRemessa):
+    def __init__(self, header: Optional[Registro], parent: Optional[Registro], **kwargs: dict):
+        super().__init__(header, parent, **kwargs)
+        
+        self.init_numero_registro()
+
+    def init_numero_registro(self):
+        self._data['numero_registro'] = self.header.counter
+        self._data['numero_sequencial'] = self.header.counter
+
---
+++ packages/enhanced-cnab/enhanced_cnab-0.0.17-py3-none-any.whl/cnab/core/formatter/__init__.py
@@ -0,0 +1,15 @@
+from .decimal import FormatterDecimal
+from .alfa import FormatterAlfa
+from .alfa2 import FormatterAlfa2
+from .integer import FormatterInteger
+from .date import FormatterDate
+from .time import FormatterTime
+
+__all__ = [
+    'FormatterDecimal',
+    'FormatterAlfa',
+    'FormatterAlfa2',
+    'FormatterInteger',
+    'FormatterDate',
+    'FormatterTime',
+]
