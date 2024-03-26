670ba9476b47ac71547d0a471d5c195b3c2fb9be
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-26 16:04:19 +0000

    Add package qiskit-scaleway/0.1.2/qiskit_scaleway-0.1.2.tar.gz

---
+++ packages/qiskit-scaleway/qiskit_scaleway-0.1.2.tar.gz/qiskit_scaleway-0.1.2/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+with open('README.md', 'r') as f:
+  description = f.read()
+
+setup(
+  name='qiskit_scaleway',
+  version='0.1.2',
+  packages=find_packages(),
+  install_requires=[
+    'qiskit==1.0.2'
+  ],
+  long_description=description,
+  long_description_content_type='text/markdown'
+)
