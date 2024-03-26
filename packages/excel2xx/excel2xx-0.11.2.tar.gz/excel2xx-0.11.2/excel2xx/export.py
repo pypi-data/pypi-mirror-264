05073ebebdfbc79b637bb5f149ff628d4a8e3995
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-26 15:05:12 +0000

    Add package excel2xx/0.11.2/excel2xx-0.11.2.tar.gz

---
+++ packages/excel2xx/excel2xx-0.11.2.tar.gz/excel2xx-0.11.2/excel2xx/export.py
@@ -0,0 +1,69 @@
+# coding: utf-8
+from __future__ import unicode_literals, print_function
+import sys
+import json
+from collections import OrderedDict
+from pprint import pformat
+from mako.template import Template
+from io import open
+from datetime import datetime, date
+
+
+def _defaultSerialize(obj):
+    if isinstance(obj, (datetime, date)):
+        return obj.isoformat()
+    raise TypeError("Type %s not serializable" % type(obj))
+
+
+def toJson(excel, output, encoding="utf-8"):
+    _dict = OrderedDict()
+    for sheet in excel:
+        _dict[sheet.name] = list(sheet)
+
+    if sys.version_info[0] == 2:
+        with open(output, mode="wb") as f:
+            json.dump(
+                _dict,
+                f,
+                ensure_ascii=False,
+                encoding=encoding,
+                default=_defaultSerialize,
+            )
+        return
+
+    with open(output, mode="w", encoding=encoding) as f:
+        json.dump(_dict, f, ensure_ascii=False, indent=4, default=_defaultSerialize)
+    pass
+
+
+def toMako(excel, output, template, encoding="utf-8"):
+    with open(output, mode="w", encoding=encoding) as ouputfile:
+        text = ""
+        with open(template, mode="r", encoding=encoding) as f:
+            text = Template(f.read()).render(excel=excel, format=pformat)
+        ouputfile.write(text)
+    pass
+
+
+def toMsgPack(excel, output, encoding="utf-8"):
+    import msgpack
+
+    _dict = OrderedDict()
+    for sheet in excel:
+        _dict[sheet.name] = list(sheet)
+
+    with open(output, mode="wb") as f:
+        f.write(msgpack.packb(_dict, default=_defaultSerialize))
+    pass
+
+
+def toCSV(excel, output, encoding="utf-8"):
+    import msgpack
+
+    _dict = OrderedDict()
+    for sheet in excel:
+        _dict[sheet.name] = list(sheet)
+
+    with open(output, mode="wb") as f:
+        f.write(msgpack.packb(_dict, default=_defaultSerialize))
+    pass
---
+++ packages/excel2xx/excel2xx-0.11.2.tar.gz/excel2xx-0.11.2/setup.py
@@ -0,0 +1,51 @@
+from setuptools import setup, find_packages
+from io import open
+import os
+
+root_dir = os.path.dirname(__file__)
+
+fpath = os.path.join(root_dir, "README.md")
+readme = open(fpath, "r", encoding="utf-8").read()
+
+
+setup(
+    name="excel2xx",
+    version="0.11.2",
+    packages=find_packages(),
+    url="https://github.com/cupen/excel2xx",
+    license="WTFPL",
+    author="cupen",
+    author_email="xcupen@gmail.com",
+    description="Extract data from excel file, and export to json, msgpack, or any code(mako template).",
+    long_description=readme,
+    long_description_content_type="text/markdown",
+    install_requires=[
+        "xlrd == 1.2.*",
+        "docopt >= 0.6.0",
+        "mako == 1.2.*",
+        "msgpack-python >= 0.4.8",
+        "colorama >= 0.4.6",
+    ],
+    entry_points={
+        "console_scripts": [
+            "excel2xx=excel2xx.main:main_docopt",
+        ],
+    },
+    python_requires=">=3.6,!=3.6.1",
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Environment :: Console",
+        "Intended Audience :: Developers",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+        "Programming Language :: Python :: 3.12",
+        "Programming Language :: Python :: Implementation :: CPython",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+    ],
+)
