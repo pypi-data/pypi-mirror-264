9549d940a4c010f360f1bd151433374326c2f03e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-26 03:04:20 +0000

    Add package ServiceAPI/1.1/ServiceAPI-1.1.tar.gz

---
+++ packages/ServiceAPI/ServiceAPI-1.1.tar.gz/ServiceAPI-1.1/setup.py
@@ -0,0 +1,15 @@
+import setuptools
+
+setuptools.setup(
+	name = 'ServiceAPI',
+	version = '1.1',
+	author='Haytam-Zakaria',
+	description="",
+	packages=['Service'],
+	long_description='',
+	long_description_content_type='text/markdown',
+	classifiers=[
+	'Programming Language :: Python :: 3',
+	'Operating System :: OS Independent',
+	'License :: OSI Approved :: MIT License']
+)
