04d3ef5d4d28b43ec1f390c959ad3cfbda95fa4a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-26 03:04:15 +0000

    Add package arthub-api/1.8.14/arthub_api-1.8.14.tar.gz

---
+++ packages/arthub-api/arthub_api-1.8.14.tar.gz/arthub_api-1.8.14/tests/conftest.py
@@ -0,0 +1,31 @@
+import pytest
+from arthub_api import arthub_api_config
+from arthub_api import OpenAPI
+from arthub_api import Storage
+from . import _utils
+
+
+def pytest_addoption(parser):
+    parser.addoption(
+        "--env", action="store", default="qq",
+        help="api config option: oa or qq or test_oa or test_qq"
+    )
+
+
+@pytest.fixture
+def env(request):
+    return request.config.getoption("--env")
+
+
+@pytest.fixture()
+def open_api(env):
+    _c = _utils.get_config(env)
+    open_api = OpenAPI(_c, False)
+    res = open_api.login(arthub_api_config.account_email, arthub_api_config.password)
+    assert res.is_succeeded()
+    return open_api
+
+
+@pytest.fixture()
+def storage(open_api):
+    return Storage(open_api)
