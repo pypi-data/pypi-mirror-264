1f96fcf74bf0209256b385c050f69cb5a91e64b8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-26 05:04:52 +0000

    Add package communex/0.1.6/communex-0.1.6.tar.gz

---
+++ packages/communex/communex-0.1.6.tar.gz/communex-0.1.6/src/communex/__init__.py
@@ -0,0 +1,11 @@
+"""
+The CommuneX library package.
+
+Submodules:
+    * `communex.client`: A lightweigh yet faster client for the Commune network. 
+    * `.compat`: Compatibility layer for the *classic* `commune` library.
+    * `.types`: CommuneX common types.
+    * `.key`: Key related functions.
+
+.. include:: ../../README.md
+"""
---
+++ packages/communex/communex-0.1.6.tar.gz/communex-0.1.6/src/communex/cli/root.py
@@ -0,0 +1,27 @@
+from typing import Optional
+import typer
+
+from .balance import balance_app
+from .key import key_app
+from .misc import misc_app
+from .module import module_app
+from .network import network_app
+from .subnet import subnet_app
+
+app = typer.Typer()
+
+app.add_typer(key_app, name="key", help="Key operations")
+app.add_typer(balance_app, name="balance", help="Balance operations")
+app.add_typer(misc_app, name="misc", help="Other operations")
+app.add_typer(module_app, name="module", help="Module operations")
+app.add_typer(network_app, name="network", help="Network operations")
+app.add_typer(subnet_app, name="subnet", help="Subnet operations")
+
+
+@app.callback()
+def main(json: Optional[bool] = False):
+    """
+    CommuneX CLI.
+
+    This command line interface is under development and subject to change.
+    """
