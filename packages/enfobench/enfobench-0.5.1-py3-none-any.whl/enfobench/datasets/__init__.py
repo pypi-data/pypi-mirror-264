c5662f6fcd409e9fdfc6a67166ad6a4a0cd21cd1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-26 03:03:49 +0000

    Add package enfobench/0.5.1/enfobench-0.5.1-py3-none-any.whl

---
+++ packages/enfobench/enfobench-0.5.1-py3-none-any.whl/enfobench/datasets/__init__.py
@@ -0,0 +1,8 @@
+from enfobench.datasets.electricity_demand import DemandSubset, ElectricityDemandDataset, MetadataSubset, WeatherSubset
+
+__all__ = [
+    "ElectricityDemandDataset",
+    "DemandSubset",
+    "MetadataSubset",
+    "WeatherSubset",
+]
