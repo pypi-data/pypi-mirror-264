78a3e2926c442468bab5ab7ed4c495d9eccd627a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-26 03:03:45 +0000

    Add package optidata-core/1.5.0/optidata_core-1.5.0-py3-none-any.whl

---
+++ packages/optidata-core/optidata_core-1.5.0-py3-none-any.whl/core/__init__.py
@@ -0,0 +1,5 @@
+from config import constantes, settings, sftp
+from database import mongodb
+from enums import enums
+from services import conciliation, config_rules, cycles, partner, role, type_cycles, users
+from utility import utils, utils_file
