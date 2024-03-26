ac11c61dc83d101a9a3d7473294e0a0ffd383c4a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-26 06:04:22 +0000

    Add package batchalign/0.6.0b6/batchalign-0.6.0b6.tar.gz

---
+++ packages/batchalign/batchalign-0.6.0b6.tar.gz/batchalign-0.6.0b6/batchalign/models/__init__.py
@@ -0,0 +1,5 @@
+from .bert_utterance import BertUtteranceModel
+from .whisper import WhisperASRModel, WhisperFAModel
+from .bert_utterance import BertUtteranceModel
+from .utils import ASRAudioFile
+from .resolve import resolve
