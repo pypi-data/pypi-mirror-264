3149b2b5cb4d853e728ffbca2dd3fedd2a84d35d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-26 06:04:14 +0000

    Add package galileo-observe/1.0.0/galileo_observe-1.0.0.tar.gz

---
+++ packages/galileo-observe/galileo_observe-1.0.0.tar.gz/galileo_observe-1.0.0/galileo_observe/__init__.py
@@ -0,0 +1,7 @@
+"""LLM Monitor"""
+
+from galileo_observe.handlers import GalileoObserveCallback
+from galileo_observe.monitor import GalileoObserve
+from galileo_observe.utils import __version__
+
+__all__ = ["GalileoObserveCallback", "GalileoObserve", "__version__"]
