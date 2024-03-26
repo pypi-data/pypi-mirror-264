da4e12b23c49100b881f9557edda69ed7e691f38
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-26 04:04:08 +0000

    Add package Floom/1.0.4/Floom-1.0.4.tar.gz

---
+++ packages/Floom/Floom-1.0.4.tar.gz/Floom-1.0.4/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+
+with open('requirements.txt') as f:
+    requirements = f.read().splitlines()
+
+setup(
+    name='Floom',  # Floom Python SDK
+    version='1.0.4',
+    packages=find_packages(exclude=['examples*']),  # Excluding examples directories
+    install_requires=requirements,
+    include_package_data=True,
+    author='FloomAI',
+    author_email='dev@floom.ai',
+    description='Floom orchestrates & executes Generative AI pipelines, Empowering Developers and DevOps to focus on '
+                'what matters.',
+    python_requires='>=3.0',
+    url='https://github.com/FloomAI/FloomSDK-Python',
+)
