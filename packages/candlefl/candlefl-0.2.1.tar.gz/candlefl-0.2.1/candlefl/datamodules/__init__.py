81ba50dabd00730c20008ba9fd337ecb6b31c54b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-26 15:04:01 +0000

    Add package candlefl/0.2.1/candlefl-0.2.1.tar.gz

---
+++ packages/candlefl/candlefl-0.2.1.tar.gz/candlefl-0.2.1/candlefl/datamodules/__init__.py
@@ -0,0 +1,6 @@
+"""Sub-package for datamodules used by torchfl."""
+
+__author__ = """Vivek Khimani"""
+__email__ = "vivekkhimani07@gmail.com"
+__version__ = "0.9.0"
+__all__ = ["emnist", "cifar", "fashionmnist"]
