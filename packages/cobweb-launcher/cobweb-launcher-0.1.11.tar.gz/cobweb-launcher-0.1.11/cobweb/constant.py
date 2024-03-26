0ee84f3a41c4d1f2ca2fd16635213c25e19336e8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-26 16:03:58 +0000

    Add package cobweb-launcher/0.1.11/cobweb-launcher-0.1.11.tar.gz

---
+++ packages/cobweb-launcher/cobweb-launcher-0.1.11.tar.gz/cobweb-launcher-0.1.11/cobweb/constant.py
@@ -0,0 +1,24 @@
+
+
+class LauncherModel:
+    task = "launcher model: task"
+    resident = "launcher model: resident"
+
+
+class LogModel:
+    simple = "log model: simple"
+    common = "log model: common"
+    detailed = "log model: detailed"
+
+
+class DealModel:
+    failure = "deal model: failure"
+    success = "deal model: success"
+    polling = "deal model: polling"
+
+
+class Setting:
+    RESET_SCORE = None
+    CHECK_LOCK_TIME = None
+    DEAL_MODEL = None
+    LAUNCHER_MODEL = None
