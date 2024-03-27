77a0283967da77536f97a2412e9986a4dfa2fa46
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-27 15:05:40 +0000

    Add package upsonic/0.20.3/upsonic-0.20.3.tar.gz

---
+++ packages/upsonic/upsonic-0.20.3.tar.gz/upsonic-0.20.3/upsonic/__init__.py
@@ -0,0 +1,38 @@
+import traceback
+
+try:
+    from .core import Upsonic
+    from .core import start_location
+    from .core import HASHES
+    from .core import Upsonic_Serial
+
+except:
+    pass
+
+from .remote import localimport
+from .remote import Upsonic_Remote
+from .remote import Upsonic_On_Prem
+from .remote import Upsonic_Cloud_Free
+from .remote import Upsonic_Cloud_Pro
+from .remote import Upsonic_Cloud_Premium
+from .remote import Upsonic_Cloud_Startup
+from .remote import Upsonic_Cloud_Readonly
+from .remote import Upsonic_Cloud_Generic
+from .remote import no_exception
+from .remote import requires
+from .remote import encrypt
+from .remote import decrypt
+
+from rich.console import Console
+
+from .remote import interface
+
+console = Console()
+
+open_databases = {}
+
+__version__ = '0.20.3'
+
+
+
+
