dde6cb2f1ceb0faf8ac49ed6029586d939d5c820
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-27 04:04:20 +0000

    Add package adpac-auth1/0.0.2/adpac_auth1-0.0.2-py3-none-any.whl

---
+++ packages/adpac-auth1/adpac_auth1-0.0.2-py3-none-any.whl/adpac_auth1/module.py
@@ -0,0 +1,13 @@
+
+def check_license(license) :
+
+    if license == True:
+        return True
+    else :
+        return False
+
+def update_license(license):
+    return True
+
+def check_ken(ken):
+    return True
