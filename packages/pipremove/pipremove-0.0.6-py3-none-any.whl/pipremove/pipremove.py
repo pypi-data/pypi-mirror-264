c5326a3fd284be53362ef4215c6b1bb6fcc858bb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-27 15:05:51 +0000

    Add package pipremove/0.0.6/pipremove-0.0.6-py3-none-any.whl

---
+++ packages/pipremove/pipremove-0.0.6-py3-none-any.whl/pipremove/pipremove.py
@@ -0,0 +1,133 @@
+import os
+
+
+# 白名单，避免被误删
+WHITELIST = ['pip', 'setuptools']
+# 需要删除的模块列表
+REMOVELIST = []
+
+
+def get_requires(module_name):
+    """
+    查看一个模块的依赖
+    :param module_name: 模块名
+    :return: module_name 所依赖的模块的列表
+    """
+    with os.popen('pip show %s' % module_name) as p:
+        requires = p.readlines()
+        if not len(requires):
+            return []
+
+        reqs = ''
+        for line in requires:
+            if not line.startswith('Requires:'):
+                continue
+            reqs = line.strip()
+        
+        requires = reqs.split(':')[-1].replace(' ', '').strip()
+        if requires:
+            requires = requires.split(',')
+        else:
+            requires = []
+        return requires
+
+
+def get_required_bys(module_name):
+    """
+    查看一个模块被哪些模块依赖
+    :param module_name: 模块名
+    :return: module_name 被依赖的模块的列表
+    """
+    with os.popen('pip show %s' % module_name) as p:
+        req_by = p.readlines()
+        if not len(req_by):
+            return []
+
+        required = ''
+        for line in req_by:
+            if not line.startswith('Required-by:'):
+                continue
+            required = line.strip()
+        
+        req_by = required.split(':')[-1].replace(' ', '').strip()
+        if req_by:
+            req_by = req_by.split(',')
+        else:
+            req_by = []
+        return req_by
+
+
+def install(name):
+    os.system(f'pip install {name}')
+
+
+def get_all_requires(module_name):
+    """
+    递归地获取一个模块所有的依赖
+    :param module_name: 模块名称
+    :return: None
+    """
+    dependents = get_requires(module_name)
+    for package in dependents:
+        if package in WHITELIST:
+            #print(f'[{package}] in white list')
+            continue
+        get_all_requires(package)
+    REMOVELIST.append(module_name)
+
+
+def remove(module_name):
+    print('Scanning packages...')
+    get_all_requires(module_name)
+    s_packages =  ','.join(REMOVELIST)
+    print(f'[INFO]Found packages: {s_packages}\n')
+
+    for package in REMOVELIST:
+        req_bys = get_required_bys(package)
+        print('[INFO]removing:', package)
+        #print(package, 'is required by:', req_bys)
+        flag = True
+        for req_by in req_bys:
+            if req_by not in REMOVELIST:
+                flag = False
+                print(f'[NOTICE]As "{package}" is required by "{req_by}" which is not in {REMOVELIST}, skipped...\n')
+                break
+        if flag:
+            print('\n')
+            os.system('pip uninstall -y %s' % package)
+
+
+
+
+def check_tree():
+    os.system('pipdeptree.exe')
+
+def remove_pkg():
+    pkg_name = input('请输入要卸载的第三方模块包:')
+    remove(pkg_name)
+
+
+# 定义菜单选项及对应的操作和说明
+menu_items = {
+    "0": {"function": check_tree, "description": "查看当前依赖树"},
+    "1": {"function": remove_pkg, "description": "卸载第三方模块包"},
+    "2": {"function": exit, "description": "退出程序"}
+}
+
+def main_menu():
+    print("欢迎使用命令行菜单")
+    for key, value in menu_items.items():
+        print(f"{key}. {value['description']}")
+
+    choice = input("请输入选项的数字编号: ")
+
+    if choice in menu_items:
+        menu_items[choice]['function']()
+    else:
+        print("无效的选项，请重新输入")
+        main_menu(menu_items)
+
+
+if __name__ == '__main__':
+    # 启动菜单
+    main_menu()
