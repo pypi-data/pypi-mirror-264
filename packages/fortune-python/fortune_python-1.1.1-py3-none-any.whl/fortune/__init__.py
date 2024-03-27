91b62a07b60a85dc37faa9904a9755b7117853a1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-27 14:04:15 +0000

    Add package fortune-python/1.1.1/fortune_python-1.1.1-py3-none-any.whl

---
+++ packages/fortune-python/fortune_python-1.1.1-py3-none-any.whl/fortune/__init__.py
@@ -0,0 +1,23 @@
+import os
+import random
+import re
+from pathlib import Path
+
+DATFILES = (Path(__file__) / '..' / 'datfiles').resolve()
+
+
+def _get_files():
+    files = os.listdir(DATFILES)
+    files.remove('LICENSE')
+    return [DATFILES / file for file in files]
+
+
+def fortune():
+    paths = _get_files()
+    fortunes = []
+    for path in paths:
+        with open(path, 'r', encoding='utf-8', errors='ignore') as f:
+            text = re.split(r'\r?\n%\r?\n', f.read())
+        text = [fortune for fortune in text if fortune.strip('\n\r')]
+        fortunes += text
+    return random.choice(fortunes)
