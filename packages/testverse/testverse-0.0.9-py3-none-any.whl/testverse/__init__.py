6e2d59fc44b852d1e117527eeab7fbf811c3321a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-27 15:06:10 +0000

    Add package testverse/0.0.9/testverse-0.0.9-py3-none-any.whl

---
+++ packages/testverse/testverse-0.0.9-py3-none-any.whl/testverse/__init__.py
@@ -0,0 +1,17 @@
+from evaluator import Evaluator
+
+
+def extract_version_from_pyproject():
+    with open("pyproject.toml", "r") as f:
+        for line in f:
+            line = line.strip()
+            if line.startswith("version"):
+                # Assuming version is defined as: version = "x.x.x"
+                version = line.split("=")[1].strip().strip('"')
+                return version
+    return None
+
+
+__version__ = extract_version_from_pyproject()
+
+__all__ = [Evaluator]
