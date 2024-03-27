5bc4939e2b0f719d4a564df7b321ff83faaac480
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-27 14:05:20 +0000

    Add package teslagenerator-ok/0.0.3/teslagenerator_ok-0.0.3.tar.gz

---
+++ packages/teslagenerator-ok/teslagenerator_ok-0.0.3.tar.gz/teslagenerator_ok-0.0.3/setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='teslagenerator_ok',
+    version='0.0.3',
+    author='Nikola Tesla',
+    author_email='linhphuong2k5y@gmail.com',
+    description='Generator infomation package. Faker infomation',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/LinhPhuongTesla/generator',
+    packages=find_packages(),
+    install_requires=[
+        'unidecode'
+    ],
+    keywords=["Generator", "Tesla", "TeslaGenerator", "Infomation", "Tesla Information", "Faker"],
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ],
+)
---
+++ packages/teslagenerator-ok/teslagenerator_ok-0.0.3.tar.gz/teslagenerator_ok-0.0.3/teslagenerator_ok/__init__.py
@@ -0,0 +1,10 @@
+from .faker import Faker
+from .crawler import (
+    MediumCrawler,
+    DiscordCrawler,
+    TwitterCrawler,
+    FacebookCrawler,
+    TelegramCrawler
+)
+__author__ = "teslaVinhXuanQuyen"
+__all__ = ["Faker", "MediumCrawler", "DiscordCrawler", "TwitterCrawler", "FacebookCrawler", "TelegramCrawler"]
