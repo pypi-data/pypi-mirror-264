4b011fbb1e9c57f34a4081622bed33ad6ff6db50
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-27 14:04:12 +0000

    Add package jaxonloader/0.2.5/jaxonloader-0.2.5-py3-none-any.whl

---
+++ packages/jaxonloader/jaxonloader-0.2.5-py3-none-any.whl/jaxonloader/__init__.py
@@ -0,0 +1,2 @@
+from jaxonloader._datasets import *  # noqa
+from jaxonloader.dataloader import JITJaxonDataLoader, JaxonDataLoader, make, Index  # noqa
---
+++ packages/jaxonloader/jaxonloader-0.2.5-py3-none-any.whl/jaxonloader/utils.py
@@ -0,0 +1,45 @@
+import os
+import pathlib
+from functools import wraps
+from typing import Any
+
+from loguru import logger
+
+
+JAXONLOADER_PATH = pathlib.Path.home() / ".jaxonloader"
+
+
+def _make_jaxonloader_dir_if_not_exists():
+    if not os.path.exists(JAXONLOADER_PATH):
+        os.makedirs(JAXONLOADER_PATH)
+
+
+def _make_data_dir_if_not_exists(dataset_name: str):
+    data_path = JAXONLOADER_PATH / dataset_name
+    if not os.path.exists(data_path):
+        os.makedirs(data_path)
+
+
+def jaxonloader_cache(dataset_name: str) -> Any:
+    def decorator(func: Any) -> Any:
+        @wraps(func)
+        def wrapper(*args: Any, **kwargs: Any):
+            _make_jaxonloader_dir_if_not_exists()
+            _make_data_dir_if_not_exists(dataset_name)
+            return func(*args, **kwargs)
+
+        return wrapper
+
+    return decorator
+
+
+def deprecation_warning(message: str) -> Any:
+    def decorator(func: Any) -> Any:
+        @wraps(func)
+        def wrapper(*args: Any, **kwargs: Any):
+            logger.warning(message)
+            return func(*args, **kwargs)
+
+        return wrapper
+
+    return decorator
