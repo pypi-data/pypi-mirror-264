003d70255d58a37441d71d1862c4c4315ad9b8ad
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-27 04:05:13 +0000

    Add package WebtoonScraper/3.2.0/webtoonscraper-3.2.0-py3-none-any.whl

---
+++ packages/WebtoonScraper/webtoonscraper-3.2.0-py3-none-any.whl/WebtoonScraper/miscs.py
@@ -0,0 +1,21 @@
+import contextlib
+import logging
+from typing import Any, Callable, Generic, Iterable, TypeAlias, TypeVar
+
+# import coloredlogs
+
+
+__github_user_name__ = __title__ = "WebtoonScraper"
+__github_project_name__ = __author__ = "ilotoki0804"
+__description__ = "Scraping webtoons with ease."
+__url__ = "https://github.com/ilotoki0804/WebtoonScraper"
+__version_info__ = (3, 2, 0)
+__version__ = str.join(".", map(str, __version_info__))
+
+WebtoonId: TypeAlias = (
+    "int | str | tuple[int, int] | tuple[str, int] | tuple[str, str]"  # + ' | NaverPostWebtoonId | NaverBlogWebtoonId'
+)
+EpisodeNoRange: TypeAlias = "tuple[int | None, int | None] | int | None | Iterable[int] | slice"
+
+logger = logging.getLogger("webtoonscraper_logger")
+# coloredlogs.install(logger=logger, datefmt='%H:%M:%S')
