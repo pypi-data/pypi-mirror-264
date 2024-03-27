9d0002bbf204fee56acdfd8cc04736d761a0fa04
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-27 03:05:18 +0000

    Add package stockdatamanager/0.7/stockdatamanager-0.7-py3-none-any.whl

---
+++ packages/stockdatamanager/stockdatamanager-0.7-py3-none-any.whl/stockdatamanager/quant.py
@@ -0,0 +1,9 @@
+class QuantIndicators:
+  pass
+## Greeks
+## Yield curve/convexity bonds
+## Futures pricing
+## Options pricing
+## Commodities pricing
+## CDS pricing
+## credit spread of a company with risk free rate
