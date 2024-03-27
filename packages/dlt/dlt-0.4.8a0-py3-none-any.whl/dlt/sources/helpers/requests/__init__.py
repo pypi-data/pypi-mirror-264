8b6ea7f36b033974731e7e7e276262340d021f11
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-27 03:04:17 +0000

    Add package dlt/0.4.8a0/dlt-0.4.8a0-py3-none-any.whl

---
+++ packages/dlt/dlt-0.4.8a0-py3-none-any.whl/dlt/sources/helpers/requests/__init__.py
@@ -0,0 +1,67 @@
+from tenacity import RetryError
+from requests import (
+    Request,
+    Response,
+    ConnectionError,
+    ConnectTimeout,
+    FileModeWarning,
+    HTTPError,
+    ReadTimeout,
+    RequestException,
+    Timeout,
+    TooManyRedirects,
+    URLRequired,
+)
+from requests.exceptions import ChunkedEncodingError
+from dlt.sources.helpers.requests.retry import Client
+from dlt.sources.helpers.requests.session import Session
+from dlt.sources.helpers.rest_client import paginate
+from dlt.common.configuration.specs import RunConfiguration
+
+client = Client()
+
+get, post, put, patch, delete, options, head, request, paginate = (
+    client.get,
+    client.post,
+    client.put,
+    client.patch,
+    client.delete,
+    client.options,
+    client.head,
+    client.request,
+    paginate,
+)
+
+
+def init(config: RunConfiguration) -> None:
+    """Initialize the default requests client from config"""
+    client.update_from_config(config)
+
+
+__all__ = [
+    "client",
+    "get",
+    "post",
+    "put",
+    "patch",
+    "delete",
+    "options",
+    "head",
+    "request",
+    "init",
+    "Session",
+    "Request",
+    "Response",
+    "ConnectionError",
+    "ConnectTimeout",
+    "FileModeWarning",
+    "HTTPError",
+    "ReadTimeout",
+    "RequestException",
+    "Timeout",
+    "TooManyRedirects",
+    "URLRequired",
+    "ChunkedEncodingError",
+    "RetryErrorClient",
+    "RetryError",
+]
