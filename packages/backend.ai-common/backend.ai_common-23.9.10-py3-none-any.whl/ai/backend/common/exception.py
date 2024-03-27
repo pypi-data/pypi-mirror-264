f55420b29e44e6c12364bf8ed6869f5795401edf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-27 15:06:11 +0000

    Add package backend.ai-common/23.9.10/backend.ai_common-23.9.10-py3-none-any.whl

---
+++ packages/backend.ai-common/backend.ai_common-23.9.10-py3-none-any.whl/ai/backend/common/exception.py
@@ -0,0 +1,90 @@
+from typing import Any, Mapping
+
+
+class ConfigurationError(Exception):
+    invalid_data: Mapping[str, Any]
+
+    def __init__(self, invalid_data: Mapping[str, Any]) -> None:
+        super().__init__(invalid_data)
+        self.invalid_data = invalid_data
+
+
+class UnknownImageReference(ValueError):
+    """
+    Represents an error for invalid/unknown image reference.
+    The first argument of this exception should be the reference given by the user.
+    """
+
+    def __str__(self) -> str:
+        return f"Unknown image reference: {self.args[0]}"
+
+
+class ImageNotAvailable(ValueError):
+    """
+    Represents an error for unavailability of the image in agents.
+    The first argument of this exception should be the reference given by the user.
+    """
+
+    def __str__(self) -> str:
+        return f"Unavailable image in the agent: {self.args[0]}"
+
+
+class UnknownImageRegistry(ValueError):
+    """
+    Represents an error for invalid/unknown image registry.
+    The first argument of this exception should be the registry given by the user.
+    """
+
+    def __str__(self) -> str:
+        return f"Unknown image registry: {self.args[0]}"
+
+
+class InvalidImageName(ValueError):
+    """
+    Represents an invalid string for image name.
+    """
+
+    def __str__(self) -> str:
+        return f"Invalid image name: {self.args[0]}"
+
+
+class InvalidImageTag(ValueError):
+    """
+    Represents an invalid string for image tag.
+    Image tag should be a string of form below
+
+    ```
+    <version-stringA>-<platform-tag-1A>-<platform-tag-2A>-....
+    ```
+    """
+
+    def __str__(self) -> str:
+        return f"Invalid or duplicate image name tag: {self.args[0]}"
+
+
+class AliasResolutionFailed(ValueError):
+    """
+    Represents an alias resolution failure.
+    The first argument of this exception should be the alias given by the user.
+    """
+
+    def __str__(self) -> str:
+        return f"Failed to resolve alias: {self.args[0]}"
+
+
+class InvalidIpAddressValue(ValueError):
+    """
+    Represents an invalid value for ip_address.
+    """
+
+
+class VolumeMountFailed(RuntimeError):
+    """
+    Represents a mount process failure.
+    """
+
+
+class VolumeUnmountFailed(RuntimeError):
+    """
+    Represents a umount process failure.
+    """
