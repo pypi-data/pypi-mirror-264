a8ca0807ece2429556f60df7a43aa215d43afe30
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-27 04:04:13 +0000

    Add package adpac-auth1/0.0.1/adpac_auth1-0.0.1.tar.gz

---
+++ packages/adpac-auth1/adpac_auth1-0.0.1.tar.gz/adpac_auth1-0.0.1/adpac_auth1/module.py
@@ -0,0 +1,10 @@
+
+def check_license(license) :
+
+    if license == True:
+        return True
+    else :
+        return False
+
+def update_license(license):
+    return True
---
+++ packages/adpac-auth1/adpac_auth1-0.0.1.tar.gz/adpac_auth1-0.0.1/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+setup (
+    name ='adpac_auth1',
+    version='0.0.1',
+    description='CORAISE AD PAC AUTH',
+    author ='ken@coraise.co.kr',
+    author_email = 'ken@coraise.co.kr',
+    install_requires = ['Flask'],
+    keywords = [],
+    url = '',
+    python_requires = '>=3.9',
+)
