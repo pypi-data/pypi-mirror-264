cb0324110ee61261b2f73b49754bdb5e509d649d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-27 16:05:36 +0000

    Add package launchable/1.85.0/launchable-1.85.0-py3-none-any.whl

---
+++ packages/launchable/launchable-1.85.0-py3-none-any.whl/tests/test_runners/test_robot.py
@@ -0,0 +1,60 @@
+import gzip
+import json
+import os
+from pathlib import Path
+from unittest import mock
+
+import responses  # type: ignore
+
+from tests.cli_test_case import CliTestCase
+
+
+class RobotTest(CliTestCase):
+    test_files_dir = Path(__file__).parent.joinpath(
+        '../data/robot/').resolve()
+
+    @responses.activate
+    @mock.patch.dict(os.environ, {"LAUNCHABLE_TOKEN": CliTestCase.launchable_token})
+    def test_subset(self):
+        result = self.cli('subset', '--target', '10%', '--session',
+                          self.session, 'robot', str(self.test_files_dir) + "/dryrun.xml")
+        self.assert_success(result)
+
+        payload = json.loads(gzip.decompress(responses.calls[0].request.body).decode())
+
+        expected = self.load_json_from_file(self.test_files_dir.joinpath('subset_result.json'))
+
+        self.assert_json_orderless_equal(expected, payload)
+
+    @ responses.activate
+    @mock.patch.dict(os.environ, {"LAUNCHABLE_TOKEN": CliTestCase.launchable_token})
+    def test_record_test(self):
+
+        result = self.cli('record', 'tests', '--session', self.session,
+                          'robot', str(self.test_files_dir) + "/output.xml")
+        self.assert_success(result)
+
+        payload = json.loads(gzip.decompress(responses.calls[1].request.body).decode())
+        for e in payload["events"]:
+            del e["created_at"]
+        expected = self.load_json_from_file(self.test_files_dir.joinpath("record_test_result.json"))
+        self.assert_json_orderless_equal(expected, payload)
+
+    # for #637
+    @ responses.activate
+    @mock.patch.dict(os.environ, {"LAUNCHABLE_TOKEN": CliTestCase.launchable_token})
+    def test_record_test_executed_only_one_file(self):
+
+        result = self.cli('record', 'tests', '--session', self.session,
+                          'robot', str(self.test_files_dir) + "/single-output.xml")
+        self.assert_success(result)
+
+        print(result.output)
+
+        payload = json.loads(gzip.decompress(responses.calls[1].request.body).decode())
+
+        for e in payload["events"]:
+            del e["created_at"]
+
+        expected = self.load_json_from_file(self.test_files_dir.joinpath("record_test_executed_only_one_file_result.json"))
+        self.assert_json_orderless_equal(expected, payload)
