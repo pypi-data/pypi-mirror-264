06e39af13a2f17895d18780c686ad7e9e4669ce3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-27 14:04:40 +0000

    Add package langkit/0.0.28.dev0/langkit-0.0.28.dev0.tar.gz

---
+++ packages/langkit/langkit-0.0.28.dev0.tar.gz/langkit-0.0.28.dev0/langkit/metrics/embeddings_types.py
@@ -0,0 +1,19 @@
+from functools import lru_cache
+from typing import Protocol, Tuple
+
+import torch
+from sentence_transformers import SentenceTransformer
+
+
+class TransformerEmbeddingAdapter:
+    def __init__(self, transformer: SentenceTransformer):
+        self._transformer = transformer
+
+    @lru_cache(maxsize=6, typed=True)
+    def encode(self, text: Tuple[str, ...]) -> "torch.Tensor":
+        return torch.as_tensor(self._transformer.encode(sentences=list(text)))  # type: ignore[reportUnknownMemberType]
+
+
+class EmbeddingEncoder(Protocol):
+    def encode(self, text: Tuple[str, ...]) -> "torch.Tensor":
+        ...
