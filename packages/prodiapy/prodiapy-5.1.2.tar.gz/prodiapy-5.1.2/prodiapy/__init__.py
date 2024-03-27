9fab2627618909a258603fe384f67f3636863af2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-27 16:04:29 +0000

    Add package prodiapy/5.1.2/prodiapy-5.1.2.tar.gz

---
+++ packages/prodiapy/prodiapy-5.1.2.tar.gz/prodiapy-5.1.2/prodiapy/__init__.py
@@ -0,0 +1,10 @@
+from . import _exceptions, resources, aio
+from .resources.logger import logger
+from ._client import Prodia
+
+__all__ = [
+    "_exceptions",
+    "resources",
+    "Prodia",
+    "aio"
+]
