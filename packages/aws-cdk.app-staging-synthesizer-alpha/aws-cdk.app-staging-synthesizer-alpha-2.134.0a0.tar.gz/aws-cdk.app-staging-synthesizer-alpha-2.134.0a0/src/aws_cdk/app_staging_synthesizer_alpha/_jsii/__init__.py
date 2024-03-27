0294faf60d92ff12c7b2c2344e647d5aa20b9fb7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-27 06:04:32 +0000

    Add package aws-cdk.app-staging-synthesizer-alpha/2.134.0a0/aws-cdk.app-staging-synthesizer-alpha-2.134.0a0.tar.gz

---
+++ packages/aws-cdk.app-staging-synthesizer-alpha/aws-cdk.app-staging-synthesizer-alpha-2.134.0a0.tar.gz/aws-cdk.app-staging-synthesizer-alpha-2.134.0a0/src/aws_cdk/app_staging_synthesizer_alpha/_jsii/__init__.py
@@ -0,0 +1,27 @@
+import abc
+import builtins
+import datetime
+import enum
+import typing
+
+import jsii
+import publication
+import typing_extensions
+
+from typeguard import check_type
+
+import aws_cdk._jsii
+import constructs._jsii
+
+__jsii_assembly__ = jsii.JSIIAssembly.load(
+    "@aws-cdk/app-staging-synthesizer-alpha",
+    "2.134.0-alpha.0",
+    __name__[0:-6],
+    "app-staging-synthesizer-alpha@2.134.0-alpha.0.jsii.tgz",
+)
+
+__all__ = [
+    "__jsii_assembly__",
+]
+
+publication.publish()
