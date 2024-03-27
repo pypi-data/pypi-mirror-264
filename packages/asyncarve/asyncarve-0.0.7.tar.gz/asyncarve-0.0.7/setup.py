874976f52d60079b9155f002438b35cfab433370
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-27 03:04:15 +0000

    Add package asyncarve/0.0.7/asyncarve-0.0.7.tar.gz

---
+++ packages/asyncarve/asyncarve-0.0.7.tar.gz/asyncarve-0.0.7/setup.py
@@ -0,0 +1,13 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="asyncarve",
+    version="0.0.8 ",
+    description="Simple Arve library",
+    package_dir={"": "app"},
+    packages=find_packages(where="app"),
+    long_description="Asynchronous Python client to communicate with Arve devices",
+    long_description_content_type="text/markdown",
+    install_requires=["orjson>=2.0.1", "mashumaro>=3.12", "aiohttp>=3.9.3", "yarl>=1.9.4"],
+    extras_require={"dev": ["pytest", "aresponses", "twine"]}
+)
