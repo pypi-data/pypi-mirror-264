4d041574036ee4bcf437f9cd0e6e2232cd9cc4c6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-27 03:05:10 +0000

    Add package pytorch-sidu/1.0.1/pytorch-sidu-1.0.1.tar.gz

---
+++ packages/pytorch-sidu/pytorch-sidu-1.0.1.tar.gz/pytorch-sidu-1.0.1/setup.py
@@ -0,0 +1,35 @@
+import setuptools
+import os
+
+with open(os.path.join(os.path.dirname(os.path.realpath(__file__)), "README.md"), "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="pytorch-sidu",
+    version="0.0.0",
+    author="Marco Parola",
+    author_email="marcoparola96@gmail.com",
+    description="SIDU: SImilarity Difference and Uniqueness method for explainable AI",
+    url="https://github.com/MarcoParola/pytorch-sidu",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    packages=["pytorch_sidu"],
+    #packages=setuptools.find_packages(),
+    install_requires=[
+        "torch",
+        "torchvision",
+        "numpy",
+    ],
+    setuptools_git_versioning={
+        "enabled": True,
+    },
+    setup_requires=[
+        "setuptools-git-versioning<2"
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        "Operating System :: OS Independent",
+        "Development Status :: 4 - Beta"
+    ],
+)
