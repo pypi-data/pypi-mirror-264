2e1c5ff0985f9859728352038e29b153ae85bed4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-27 04:04:22 +0000

    Add package hmus/0.7.1/hmus-0.7.1.tar.gz

---
+++ packages/hmus/hmus-0.7.1.tar.gz/hmus-0.7.1/setup.py
@@ -0,0 +1,42 @@
+from setuptools import setup, find_packages
+
+"""
+python setup.py sdist bdist_wheel
+twine upload dist/*
+"""
+
+setup(
+				name='hmus',
+				version='0.7.1',
+				author='LG125YT',
+				author_email='lg125yt@gmail.com',
+				description='An asynchronous API wrapper for Hummus by Ziad87',
+				long_description=open('README.md').read(),
+				long_description_content_type='text/markdown',
+				url='https://gitlab.com/lg125yt/hummus.py',
+				packages=find_packages(),
+				install_requires=[
+								"discord.py",
+								"asyncio",
+								"fake_useragent",
+								"websockets",
+								"requests",
+								"requests_toolbelt",
+								"filetype",
+								"Pillow",
+				],
+				classifiers=[
+								'Development Status :: 3 - Alpha',
+								'Intended Audience :: Developers',
+								'License :: OSI Approved :: MIT License',
+								'Operating System :: OS Independent',
+								'Programming Language :: Python',
+								'Programming Language :: Python :: 3',
+								'Programming Language :: Python :: 3.6',
+								'Programming Language :: Python :: 3.7',
+								'Programming Language :: Python :: 3.8',
+								'Programming Language :: Python :: 3.9',
+								'Programming Language :: Python :: 3.10',
+								'Programming Language :: Python :: 3.11',
+				],
+)
