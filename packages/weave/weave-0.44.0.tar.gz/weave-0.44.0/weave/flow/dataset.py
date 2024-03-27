1ad2f42f1d96cba2d1e5bc7addb989d58e3360cf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-27 14:04:13 +0000

    Add package weave/0.44.0/weave-0.44.0.tar.gz

---
+++ packages/weave/weave-0.44.0.tar.gz/weave-0.44.0/weave/flow/dataset.py
@@ -0,0 +1,12 @@
+from typing import Union, Any
+import weave
+
+from weave.flow.obj import Object
+
+
+class Dataset(Object):
+    rows: Union[weave.Table, list[Any]]
+
+    def model_post_init(self, __context: Any) -> None:
+        if not isinstance(self.rows, weave.Table):
+            self.__dict__["rows"] = weave.Table(self.rows)
