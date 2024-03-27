4341ee00f4ff9d57fa9229ac8227a51b9ce16736
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-27 03:05:06 +0000

    Add package aicamera/1.3.1/aicamera-1.3.1.tar.gz

---
+++ packages/aicamera/aicamera-1.3.1.tar.gz/aicamera-1.3.1/src/aicamera/lcd.py
@@ -0,0 +1,21 @@
+
+'''
+Date: 2024-02-23 19:04:54
+author: zjs
+description: 屏幕宽
+'''
+
+
+def get_width():
+    return 320
+
+
+'''
+Date: 2024-02-23 19:04:54
+author: zjs
+description: 屏幕高
+'''
+
+
+def get_height():
+    return 386
