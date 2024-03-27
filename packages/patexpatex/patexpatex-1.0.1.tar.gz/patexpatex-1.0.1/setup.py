75055946643905b46ded4e01170c292dbc56240f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-27 05:05:04 +0000

    Add package patexpatex/1.0.1/patexpatex-1.0.1.tar.gz

---
+++ packages/patexpatex/patexpatex-1.0.1.tar.gz/patexpatex-1.0.1/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+from glob import glob
+
+setup(
+    name="patexpatex",
+    version="1.0.1",
+    author="Mark Baushenko",
+    author_email="m.baushenko@gmail.com",
+    description="test desc",
+    license="MIT",
+    url="https://github.com/e0xextazy/python-advanced",
+    packages=find_packages(),
+    install_requires=[],
+    keywords="key word",
+)
