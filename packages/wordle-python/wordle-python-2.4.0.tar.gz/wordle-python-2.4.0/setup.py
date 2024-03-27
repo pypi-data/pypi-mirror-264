d0a7f887578d72595c11eeb37103518748085180
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-27 06:04:57 +0000

    Add package wordle-python/2.4.0/wordle-python-2.4.0.tar.gz

---
+++ packages/wordle-python/wordle-python-2.4.0.tar.gz/wordle-python-2.4.0/setup.py
@@ -0,0 +1,30 @@
+from setuptools import setup, find_packages
+
+VERSION = "2.4.0"
+DESCRIPTION = (
+    "The popular word game recreated in Python, deployable with custom answers."
+)
+
+def get_read_me():
+    with open("README.md", "r") as f:
+        return f.read()
+
+# Setting up
+setup(
+    name="wordle-python",
+    version=VERSION,
+    author="Prerit Das",
+    author_email="<preritdas@gmail.com>",
+    description=DESCRIPTION,
+    long_description=get_read_me(),
+    long_description_content_type="text/markdown",
+    packages=find_packages(),
+    install_requires=[],
+    keywords=["python", "games", "wordle", "english", "word games"],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: Unix",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows",
+    ],
+)
