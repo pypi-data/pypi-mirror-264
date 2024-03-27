55216fa39c78e905f246c9a3dfe017593461d04a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-27 14:05:12 +0000

    Add package py-dbchat/0.0.2rc66.post1/py_dbchat-0.0.2rc66.post1.tar.gz

---
+++ packages/py-dbchat/py_dbchat-0.0.2rc66.post1.tar.gz/py_dbchat-0.0.2rc66.post1/playground.py
@@ -0,0 +1,5 @@
+from db_chat.ask import ask_ai
+
+connection_string = "postgresql://dbchatuser:dbchatuser@localhost:5432/dbchatdb"
+question = "Find the user who got the most likes"
+ask_ai(connection_string, question)
