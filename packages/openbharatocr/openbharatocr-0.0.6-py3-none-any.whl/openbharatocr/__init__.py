3f50c16447baa66b414bdf3e97e25f0a84714f11
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-27 15:06:07 +0000

    Add package openbharatocr/0.0.6/openbharatocr-0.0.6-py3-none-any.whl

---
+++ packages/openbharatocr/openbharatocr-0.0.6-py3-none-any.whl/openbharatocr/__init__.py
@@ -0,0 +1,9 @@
+# author:    Kunal Kumar Kushwaha
+# website:   http://www.essentia.dev
+
+# import the required packages
+from openbharatocr.ocr.api import pan
+from openbharatocr.ocr.api import front_aadhaar, back_aadhaar
+
+
+__all__ = ["pan", "front_aadhaar", "back_aadhaar"]
