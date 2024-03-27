e6ac23889e8aec61fd053fbfb27372fbee23620e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-27 15:06:11 +0000

    Add package netbox-acls/1.4.0/netbox_acls-1.4.0-py3-none-any.whl

---
+++ packages/netbox-acls/netbox_acls-1.4.0-py3-none-any.whl/netbox_acls/__init__.py
@@ -0,0 +1,24 @@
+"""
+Define the NetBox Plugin
+"""
+
+from extras.plugins import PluginConfig
+
+from .version import __version__
+
+
+class NetBoxACLsConfig(PluginConfig):
+    """
+    Plugin specifc configuration
+    """
+
+    name = "netbox_acls"
+    verbose_name = "Access Lists"
+    version = __version__
+    description = "Manage simple ACLs in NetBox"
+    base_url = "access-lists"
+    min_version = "3.6.0"
+    max_version = "3.6.99"
+
+
+config = NetBoxACLsConfig
