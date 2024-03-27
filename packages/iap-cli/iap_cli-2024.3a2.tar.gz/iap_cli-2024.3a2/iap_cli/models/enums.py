65bcf11a5327334c7e60ab53fc05d22c217bf678
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-27 04:04:48 +0000

    Add package iap-cli/2024.3a2/iap_cli-2024.3a2.tar.gz

---
+++ packages/iap-cli/iap_cli-2024.3a2.tar.gz/iap_cli-2024.3a2/iap_cli/models/enums.py
@@ -0,0 +1,31 @@
+from enum import Enum
+
+
+class Order(Enum):
+    integer__1 = -1
+    integer_1 = 1
+
+
+class LogLevel(Enum):
+    error = "error"
+    warn = "warn"
+    info = "info"
+    debug = "debug"
+    trace = "trace"
+    spam = "spam"
+
+
+class Method(Enum):
+    get = "get"
+    delete = "delete"
+    patch = "patch"
+    post = "post"
+    put = "put"
+
+
+class State(Enum):
+    DEAD = "DEAD"
+    STOPPED = "STOPPED"
+    RUNNING = "RUNNING"
+    DELETED = "DELETED"
+    STARTING = "STARTING"
