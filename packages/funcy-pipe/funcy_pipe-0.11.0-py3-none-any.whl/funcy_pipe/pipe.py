99d1f2dd2707b471be769d78928bc5fb0dce0faa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-27 07:05:00 +0000

    Add package funcy-pipe/0.11.0/funcy_pipe-0.11.0-py3-none-any.whl

---
+++ packages/funcy-pipe/funcy_pipe-0.11.0-py3-none-any.whl/funcy_pipe/pipe.py
@@ -0,0 +1,71 @@
+# https://github.com/JulienPalard/Pipe/blob/main/pipe.py
+import functools
+
+
+class PipeFirst:
+    """
+    Takes the input into `|` and passes to the first argument of the function
+
+    Described as:
+    >>> first = PipeFirst(lambda iterable: next(iter(iterable)))
+
+    and used as:
+    >>> print([1, 2, 3] | first)
+    1
+
+    Or represent a Pipeable Function :
+    It's a function returning a Pipe
+    Described as :
+    >>> select = PipeFirst(lambda iterable, pred: (pred(x) for x in iterable))
+
+    and used as:
+    >>> print(list([1, 2, 3] | select(lambda x: x * 2)))
+    [2, 4, 6]
+    """
+
+    def __init__(self, function):
+        self.function = function
+        functools.update_wrapper(self, function)
+
+    def __ror__(self, other):
+        return self.function(other)
+
+    def __call__(self, *args, **kwargs):
+        return PipeFirst(
+            lambda iterable, *args2, **kwargs2: self.function(
+                iterable, *args, *args2, **kwargs, **kwargs2
+            )
+        )
+
+
+class PipeSecond:
+    """
+    Represent a Pipeable Element :
+    Described as :
+    first = Pipe(lambda iterable: next(iter(iterable)))
+    and used as :
+    print [1, 2, 3] | first
+    printing 1
+
+    Or represent a Pipeable Function :
+    It's a function returning a Pipe
+    Described as :
+    select = Pipe(lambda iterable, pred: (pred(x) for x in iterable))
+    and used as :
+    print [1, 2, 3] | select(lambda x: x * 2)
+    # 2, 4, 6
+    """
+
+    def __init__(self, function):
+        self.function = function
+        functools.update_wrapper(self, function)
+
+    def __ror__(self, other):
+        return self.function(other)
+
+    def __call__(self, *args, **kwargs):
+        return PipeSecond(
+            lambda iterable, *args2, **kwargs2: self.function(
+                *args, *args2, iterable, **kwargs, **kwargs2
+            )
+        )
