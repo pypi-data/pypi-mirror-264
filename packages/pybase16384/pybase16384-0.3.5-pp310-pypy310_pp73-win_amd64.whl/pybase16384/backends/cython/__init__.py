29be2d50ad75f4406b824ea229595c6ad994632c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-27 05:04:48 +0000

    Add package pybase16384/0.3.5/pybase16384-0.3.5-pp310-pypy310_pp73-win_amd64.whl

---
+++ packages/pybase16384/pybase16384-0.3.5-pp310-pypy310_pp73-win_amd64.whl/pybase16384/backends/cython/__init__.py
@@ -0,0 +1,18 @@
+"""
+Copyright (c) 2008-2021 synodriver <synodriver@gmail.com>
+"""
+from pybase16384.backends.cython._core import (
+    _decode,
+    _decode_into,
+    _encode,
+    _encode_into,
+    decode_fd,
+    decode_file,
+    decode_len,
+    decode_local_file,
+    encode_fd,
+    encode_file,
+    encode_len,
+    encode_local_file,
+    is_64bits,
+)
