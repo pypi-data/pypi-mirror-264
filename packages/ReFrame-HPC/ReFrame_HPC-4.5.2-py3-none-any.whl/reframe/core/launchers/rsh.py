c0c5b6efee8a5bdc000896f240ec6ec5d39934ed
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-27 06:04:45 +0000

    Add package ReFrame-HPC/4.5.2/ReFrame_HPC-4.5.2-py3-none-any.whl

---
+++ packages/ReFrame-HPC/ReFrame_HPC-4.5.2-py3-none-any.whl/reframe/core/launchers/rsh.py
@@ -0,0 +1,33 @@
+# Copyright 2016-2024 Swiss National Supercomputing Centre (CSCS/ETH Zurich)
+# ReFrame Project Developers. See the top-level LICENSE file for details.
+#
+# SPDX-License-Identifier: BSD-3-Clause
+
+from reframe.core.backends import register_launcher
+from reframe.core.launchers import JobLauncher
+
+# Remote shell launchers
+
+
+@register_launcher('clush')
+class ClushLauncher(JobLauncher):
+    def command(self, job):
+        return ['clush', *job.sched_access]
+
+
+@register_launcher('pdsh')
+class PdshLauncher(JobLauncher):
+    def command(self, job):
+        return ['pdsh', *job.sched_access]
+
+
+@register_launcher('ssh')
+class SSHLauncher(JobLauncher):
+    def command(self, job):
+        hostname = job.sched_access[-1]
+        ssh_opts = list(job.sched_access[:-1]) + self.options
+        return ['ssh', '-o BatchMode=yes'] + ssh_opts + [hostname]
+
+    def run_command(self, job):
+        # self.options is processed specially above
+        return ' '.join(self.command(job))
