064347a890fab28145e9960453f52dcacd3be077
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-27 16:04:29 +0000

    Add package pylips/0.0.9/pylips-0.0.9-py3-none-any.whl

---
+++ packages/pylips/pylips-0.0.9-py3-none-any.whl/pylips/speech/client.py
@@ -0,0 +1,38 @@
+from .robot_face import RobotFace
+
+if __name__ == '__main__':
+    aimee = RobotFace(robot_name='aimee', tts_method='system', voice_id='com.apple.voice.premium.en-US.Zoe')
+    aimee.tts.list_voices()
+
+    nathan = RobotFace(robot_name='nathan', voice_id='Matthew')
+
+    # send_message(request)
+    while True:
+        message = input("who will we make talk?: ")
+
+        aimee.update_face({
+            'background_color': '#ddc1e3',
+            'eyeball_color': '#ffffff',
+            'iris_color': '#5a8594',
+            'eye_size': 150,
+            'eye_height': 80,
+            'eye_separation': 400,
+            'iris_size': 90,
+            'pupil_scale': 0,
+            'eye_shine': True,
+            'eyelid_color': '#ddc1e3',
+            'nose_color': '#9bbce0',
+            'nose_vertical_position': 0,
+            'nose_width': 0,
+            'nose_height': 0,
+            'mouth_color': '#9bbce0',
+            'mouth_width': 480,
+            'mouth_height': 30,
+            'mouth_thickness': 18,
+            'brow_color': '#9bbce0',
+            'brow_width': 140,
+            'brow_height': 120,
+            'brow_thickness': 20
+        })
+            
+        
