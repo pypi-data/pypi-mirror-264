ed00915800d3487ef2cc8095d8ab0a1bfac46c25
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-27 03:04:19 +0000

    Add package iwashi/2.2.0/iwashi-2.2.0-py3-none-any.whl

---
+++ packages/iwashi/iwashi-2.2.0-py3-none-any.whl/iwashi/visitors/pagelink.py
@@ -0,0 +1,27 @@
+from __future__ import annotations
+
+import re
+import urllib.parse
+
+from iwashi.helper import HTTP_REGEX
+from iwashi.visitor import Context, SiteVisitor
+
+
+class PageLink(SiteVisitor):
+    NAME = "PageLink"
+    URL_REGEX: re.Pattern = re.compile(
+        HTTP_REGEX + r"(?P<host>\w+)\.page\.link/\?link=(?P<link>[^&]+)", re.IGNORECASE
+    )
+
+    async def normalize(self, context: Context, url: str) -> str:
+        match = self.URL_REGEX.match(url)
+        if match is None:
+            return url
+        return f'https://{match.group("host")}.page.link/?link={match.group("link")}'
+
+    async def visit(self, url, context: Context, host: str, link: str):
+        context.create_result(
+            "PageLink",
+            url=url,
+        )
+        context.enqueue_visit(urllib.parse.unquote(link))
