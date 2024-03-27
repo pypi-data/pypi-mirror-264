ca6141a2eaedd7dd7f409ff80b3c43c853fae875
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-27 16:05:48 +0000

    Add package oak-cli/0.2.2/oak_cli-0.2.2-py3-none-any.whl

---
+++ packages/oak-cli/oak_cli-0.2.2-py3-none-any.whl/oak_cli/main.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+# PYTHON_ARGCOMPLETE_OK
+
+from oak_cli.args_parser.main import parse_arguments_and_execute
+
+
+def main():
+    parse_arguments_and_execute()
+
+
+if __name__ == "__main__":
+    main()
