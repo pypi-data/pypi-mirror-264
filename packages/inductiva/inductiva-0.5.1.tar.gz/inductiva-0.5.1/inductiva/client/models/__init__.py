0b49364f6833ae94a335e50065d84d69827fc8bc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-27 16:05:34 +0000

    Add package inductiva/0.5.1/inductiva-0.5.1.tar.gz

---
+++ packages/inductiva/inductiva-0.5.1.tar.gz/inductiva-0.5.1/inductiva/client/models/__init__.py
@@ -0,0 +1,39 @@
+# coding: utf-8
+
+# flake8: noqa
+
+# import all models into this package
+# if you have many models here with many references from one model to another this may
+# raise a RecursionError
+# to avoid this, import only the models that you directly need like:
+# from inductiva.client.model.pet import Pet
+# or import this package, but before doing it, use:
+# import sys
+# sys.setrecursionlimit(n)
+
+from inductiva.client.model.backend_version import BackendVersion
+from inductiva.client.model.base_machine_type import BaseMachineType
+from inductiva.client.model.base_vm_group import BaseVMGroup
+from inductiva.client.model.body_upload_task_input import BodyUploadTaskInput
+from inductiva.client.model.default_machine_group import DefaultMachineGroup
+from inductiva.client.model.executer import Executer
+from inductiva.client.model.executer_tracker_api_connection_info import ExecuterTrackerAPIConnectionInfo
+from inductiva.client.model.executer_tracker_register_info import ExecuterTrackerRegisterInfo
+from inductiva.client.model.file_info import FileInfo
+from inductiva.client.model.http_validation_error import HTTPValidationError
+from inductiva.client.model.machine_type import MachineType
+from inductiva.client.model.output_archive_info import OutputArchiveInfo
+from inductiva.client.model.provider import Provider
+from inductiva.client.model.providers import Providers
+from inductiva.client.model.quota import Quota
+from inductiva.client.model.task import Task
+from inductiva.client.model.task_request import TaskRequest
+from inductiva.client.model.task_status import TaskStatus
+from inductiva.client.model.task_status_code import TaskStatusCode
+from inductiva.client.model.user import User
+from inductiva.client.model.user_api_key import UserApiKey
+from inductiva.client.model.user_create import UserCreate
+from inductiva.client.model.vm_group_config import VMGroupConfig
+from inductiva.client.model.vm_group_lifecycle_config import VMGroupLifecycleConfig
+from inductiva.client.model.validation_error import ValidationError
+from inductiva.client.model.version_comparison_result import VersionComparisonResult
