25da3f75e94dfd3461a45360ed3a80e00c37164a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-27 05:06:31 +0000

    Add package unique-sdk/0.7.0/unique_sdk-0.7.0.tar.gz

---
+++ packages/unique-sdk/unique_sdk-0.7.0.tar.gz/unique_sdk-0.7.0/unique_sdk/api_resources/_search.py
@@ -0,0 +1,46 @@
+from typing import ClassVar, List, Literal, Optional, cast
+
+from typing_extensions import NotRequired, Unpack
+
+from unique_sdk._api_resource import APIResource
+from unique_sdk._request_options import RequestOptions
+
+
+class Search(APIResource["Search"]):
+    OBJECT_NAME: ClassVar[Literal["search.search"]] = "search.search"
+
+    class CreateParams(RequestOptions):
+        chatId: str
+        searchString: str
+        searchType: Literal["VECTOR", "COMBINED"]
+        scopeIds: NotRequired[List[str]]
+        chatOnly: NotRequired[bool]
+        limit: NotRequired[int]
+        page: NotRequired[int]
+
+    id: str
+    chunkId: str
+    text: str
+    createdAt: str
+    updatedAt: str
+    url: Optional[str]
+    title: Optional[str]
+    key: Optional[str]
+    order: int
+    startPage: int
+    endPage: int
+
+    @classmethod
+    def create(
+        cls, user_id: str, company_id: str, **params: Unpack["Search.CreateParams"]
+    ) -> "Search":
+        return cast(
+            "Search",
+            cls._static_request(
+                "post",
+                "/search/search",
+                user_id,
+                company_id,
+                params=params,
+            ),
+        )
