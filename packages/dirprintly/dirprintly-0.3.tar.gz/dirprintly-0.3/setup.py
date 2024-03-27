0f2661749d5cedfbc4bc804165600b0eb67395d6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-27 07:04:44 +0000

    Add package dirprintly/0.3/dirprintly-0.3.tar.gz

---
+++ packages/dirprintly/dirprintly-0.3.tar.gz/dirprintly-0.3/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='dirprintly',
+    version='0.3',  # Ensure this version number is incremented if re-uploading.
+    packages=find_packages(),
+    install_requires=[
+        'colorama',
+    ],
+    entry_points={
+        'console_scripts': [
+            'dirprintly=dirprintly.file_printer:explore_directory_cli',
+        ],
+    },
+)
