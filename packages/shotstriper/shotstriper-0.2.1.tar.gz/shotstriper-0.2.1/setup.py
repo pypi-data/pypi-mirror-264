e13b1d1ce2e6de758823611d98ea7e094ebc5159
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-27 06:04:46 +0000

    Add package shotstriper/0.2.1/shotstriper-0.2.1.tar.gz

---
+++ packages/shotstriper/shotstriper-0.2.1.tar.gz/shotstriper-0.2.1/setup.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['shotstriper']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['aggdraw>=1.3.18.post0,<2.0.0',
+ 'pillow>=10.2.0,<11.0.0',
+ 'typer[all]>=0.10.0,<0.11.0']
+
+entry_points = \
+{'console_scripts': ['shotstriper = shotstriper.main:app']}
+
+setup_kwargs = {
+    'name': 'shotstriper',
+    'version': '0.2.1',
+    'description': 'Add beautiful, striped backgrounds to your screenshots',
+    'long_description': '# Shot-Striper\n\nAdd beautiful, striped backgrounds to your screenshots.\n\n![Shot-Striper](https://raw.githubusercontent.com/psyonara/shotstriper/master/imgs/headline.jpg)\n\n## Installation\n\n### Pipx\n\n```shell\npipx install shotstriper\n```\n\n### Pip\n\n```shell\npip install shotstriper\n```\n\n## Usage\n\n### Quick Start\n\n```shell\nshotstriper add-background screengrab.jpg --palette-name="winter.planar-fugue"\n```\n\nThis creates a new file "out.jpg", with a striped background applied, the colors used being from the "planar-fugue" palette in the "winter" category.\n\n### Palettes\n\nTo get a list of palette categories, run:\n\n```shell\nshotstriper palette-categories\n```\n\nFrom the list, pick one category. Now browse through the palettes in that categories:\n\n```shell\nshotstriper browse-palettes --category sunset\n```\n\nThe palettes are displayed page by page. Note the name of your favourite palette.\n\nNote: you can browse all categories by omitting the category name.\n\n### Adding Backgrounds\n\n#### Using an image from the clipboard\n\n```shell\nshotstriper --from-clipboard --palette-name="sunset.quick-vignette" --output-file="new_screen.jpg"\n```\n\n### More Options\n\n```shell\nshotstriper add-background --help\n```\n',
+    'author': 'Helmut Irle',
+    'author_email': 'me@helmut.dev',
+    'maintainer': 'None',
+    'maintainer_email': 'None',
+    'url': 'https://github.com/psyonara/shotstriper',
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'entry_points': entry_points,
+    'python_requires': '>=3.10,<4.0',
+}
+
+
+setup(**setup_kwargs)
