4eb389aa67f762c11f1efed3b3541336784c0a8f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-27 14:05:21 +0000

    Add package gamechanger/0.2/gamechanger-0.2.tar.gz

---
+++ packages/gamechanger/gamechanger-0.2.tar.gz/gamechanger-0.2/setup.py
@@ -0,0 +1,14 @@
+# setup.py
+
+from setuptools import setup, find_packages
+
+setup(
+    name="gamechanger",
+    version="0.2",
+    packages=find_packages(),
+    install_packages=[],
+    author="Abdellatif BELMADY",
+    author_email="abdellatif.belmady@gmail.com",
+    description="A sample gamechanger library for gamification",
+    url="https://github.com/abdellatif-belmady/gamechanger",
+)
