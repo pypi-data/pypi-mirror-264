78a9df8d5764095e56b8fe0a82d9694a5f3e21e6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-27 05:05:47 +0000

    Add package mediqbox-md2pptx/0.0.6/mediqbox-md2pptx-0.0.6.tar.gz

---
+++ packages/mediqbox-md2pptx/mediqbox-md2pptx-0.0.6.tar.gz/mediqbox-md2pptx-0.0.6/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_namespace_packages
+
+with open("README.md", "r") as fp:
+  long_description = fp.read()
+
+setup(
+  name='mediqbox-md2pptx',
+  version='0.0.6',
+  description="A mediqbox component to generate pptx from markdown",
+  long_description=long_description,
+  long_description_content_type="text/markdown",
+  package_dir={"": "src"},
+  packages=find_namespace_packages(
+    where="src", include=["mediqbox.*"]
+  ),
+  install_requires=[
+    "mediqbox-abc >= 0.0.4",
+    "python-pptx",
+    "cairosvg",
+    "graphviz"
+  ]
+)
