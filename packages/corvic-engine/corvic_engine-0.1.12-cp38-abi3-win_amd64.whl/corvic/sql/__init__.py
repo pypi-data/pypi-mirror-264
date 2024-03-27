6126f4f8d4ac91e6855d0743bbe40a3992a3b084
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-27 04:05:26 +0000

    Add package corvic-engine/0.1.12/corvic_engine-0.1.12-cp38-abi3-win_amd64.whl

---
+++ packages/corvic-engine/corvic_engine-0.1.12-cp38-abi3-win_amd64.whl/corvic/sql/__init__.py
@@ -0,0 +1,38 @@
+"""Library supporting python code around SQL."""
+
+from sqlglot import column, condition
+from sqlglot.errors import ParseError
+from sqlglot.expressions import (
+    Condition,
+    Except,
+    ExpOrStr,
+    From,
+    Limit,
+    Select,
+    func,
+    select,
+)
+
+from corvic.sql.parse_ops import StagingQueryGenerator, parse_op_graph
+from corvic.sql.rockset import Dialect as RocksetDialect
+from corvic.sql.rockset import collection as rockset_collection
+from corvic.sql.rockset import format_query as format_rockset_query
+
+__all__ = [
+    "Condition",
+    "Except",
+    "ExpOrStr",
+    "From",
+    "Limit",
+    "ParseError",
+    "RocksetDialect",
+    "Select",
+    "StagingQueryGenerator",
+    "column",
+    "condition",
+    "format_rockset_query",
+    "func",
+    "parse_op_graph",
+    "rockset_collection",
+    "select",
+]
