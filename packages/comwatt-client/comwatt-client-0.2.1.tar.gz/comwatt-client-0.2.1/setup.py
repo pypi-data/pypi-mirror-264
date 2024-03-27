32332e7b7373a867b2eef780961c508230262c1a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-27 05:05:12 +0000

    Add package comwatt-client/0.2.1/comwatt-client-0.2.1.tar.gz

---
+++ packages/comwatt-client/comwatt-client-0.2.1.tar.gz/comwatt-client-0.2.1/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='comwatt-client',
+    version='0.2.1',
+    author='Matéo Greil',
+    author_email='contact@greil.fr',
+    description='Python Client for Comwatt API',
+    packages=find_packages(),
+    install_requires=[
+        'requests',
+    ],
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+    ],
+)
