7fa73ebc00542066f262d5efe416dd255bbc02c4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-27 16:04:25 +0000

    Add package x-stock/0.1/x_stock-0.1-py3-none-any.whl

---
+++ packages/x-stock/x_stock-0.1-py3-none-any.whl/data/akshare.py
@@ -0,0 +1,20 @@
+# import akshare as ak
+# class AkShare:
+#     """_summary_
+#     文档: https://akshare.akfamily.xyz/introduction.html
+#     更新: pip install akshare --upgrade -i https://pypi.org/simple
+
+#     """
+#     def __init__(self) -> None:
+#         pass
+    
+#     def get_stock_zh_a_spot(self):
+#         """_summary_
+#         获取A股实时行情数据
+#         """
+#         return ak.stock_zh_a_spot()
+
+import akshare as ak
+
+futures_hog_supply_df = ak.futures_hog_supply(symbol="猪肉批发价")
+print(futures_hog_supply_df)
---
+++ packages/x-stock/x_stock-0.1-py3-none-any.whl/data/tushare.py
@@ -0,0 +1,12 @@
+
+
+
+class TuShare:
+    
+    """_summary_
+    https://tushare.pro/document/2
+    """
+    def __init__(self) -> None:
+        pass
+    
+    
