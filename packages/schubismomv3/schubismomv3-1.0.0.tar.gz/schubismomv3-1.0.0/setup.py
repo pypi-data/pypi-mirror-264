9b5d93556b9f924adcf7d285e7e2e4c1126e090c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-27 05:05:20 +0000

    Add package schubismomv3/1.0.0/schubismomv3-1.0.0.tar.gz

---
+++ packages/schubismomv3/schubismomv3-1.0.0.tar.gz/schubismomv3-1.0.0/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+
+VERSION = '1.0.0'
+DESCRIPTION = 'Package for black people'
+LONG_DESCRIPTION = 'This is a very long description yesyes'
+
+print("Hello World")
+
+setup(
+    name="schubismomv3",
+    version=VERSION,
+    author="John Hammond",
+    author_email="nig@ger.co",
+    description=DESCRIPTION,
+    long_description_content_type="text/markdown",
+    long_description=LONG_DESCRIPTION,
+    packages=find_packages(),
+    install_requires=['fernet', 'requests'],
+    keywords=['ratting'],
+    classifiers=[
+        "Operating System :: Microsoft :: Windows",
+    ]
+)
