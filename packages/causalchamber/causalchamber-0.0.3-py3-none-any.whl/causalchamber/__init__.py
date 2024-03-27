ef72a3461620f5034982b9ebc794a961388078f1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-27 04:04:52 +0000

    Add package causalchamber/0.0.3/causalchamber-0.0.3-py3-none-any.whl

---
+++ packages/causalchamber/causalchamber-0.0.3-py3-none-any.whl/causalchamber/__init__.py
@@ -0,0 +1,12 @@
+citation = """
+@article{gamella2024chamber,
+  title={The Causal Chambers: Real Physical Systems as a Testbed for AI Methodology},
+  author={Gamella, Juan L. and B\"uhlmann, Peter and Peters, Jonas},
+  journal={(work in progress)},
+  year={2023}
+}
+"""
+
+print(
+    f"If you use our datasets or models for your work please consider citing:\n{citation}"
+)
