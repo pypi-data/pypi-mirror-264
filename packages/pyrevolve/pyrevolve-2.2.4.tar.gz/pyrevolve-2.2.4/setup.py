668a228f74da923595dbef3d3c1fd61640a7adf9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-27 06:04:58 +0000

    Add package pyrevolve/2.2.4/pyrevolve-2.2.4.tar.gz

---
+++ packages/pyrevolve/pyrevolve-2.2.4.tar.gz/pyrevolve-2.2.4/setup.py
@@ -0,0 +1,61 @@
+from setuptools import setup, Extension, find_packages
+import versioneer
+
+
+class lazy_cythonize(list):
+    def __init__(self, callback):
+        self._list, self.callback = None, callback
+
+    def c_list(self):
+        if self._list is None:
+            self._list = self.callback()
+        return self._list
+
+    def __iter__(self):
+        for e in self.c_list():
+            yield e
+
+    def __getitem__(self, ii):
+        return self.c_list()[ii]
+
+    def __len__(self):
+        return len(self.c_list())
+
+
+def extensions():
+    from Cython.Build import cythonize
+    ext = Extension("pyrevolve.crevolve", sources=["pyrevolve/schedulers/crevolve.pyx",
+                                                   "src/revolve_c.cpp",
+                                                   "src/revolve.cpp"],
+                    include_dirs=[".", "pyrevolve"],
+                    language="c++")
+    return cythonize([ext])
+
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+s_required = ["cython>=0.17,<=3.0.0", "versioneer", "flake8"]
+i_required = ["contexttimer"]
+
+configuration = {
+    'name': 'pyrevolve',
+    'packages': find_packages(exclude=['examples', 'tests']),
+    'setup_requires': s_required,
+    'install_requires': i_required,
+    'extras_require': {'compression': ['blosc2', 'pyzfp']},
+    'ext_modules': lazy_cythonize(extensions),
+    'version': versioneer.get_version(),
+    'cmdclass': versioneer.get_cmdclass(),
+    'description': "Python wrapper for Revolve checkpointing",
+    'long_description': long_description,
+    'long_description_content_type': 'text/markdown',
+    'url': 'https://github.com/opesci/pyrevolve/',
+    'author': "Imperial College London",
+    'author_email': 'g.gorman@imperial.ac.uk',
+    'license': 'MIT',
+    'zip_safe': False
+}
+
+
+setup(**configuration)
