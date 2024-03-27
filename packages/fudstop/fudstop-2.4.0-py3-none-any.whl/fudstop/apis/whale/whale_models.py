1d3c979a2aa0f4823fdfac78e7ad5d3ab2579ae9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-27 15:05:43 +0000

    Add package fudstop/2.4.0/fudstop-2.4.0-py3-none-any.whl

---
+++ packages/fudstop/fudstop-2.4.0-py3-none-any.whl/fudstop/apis/whale/whale_models.py
@@ -0,0 +1,52 @@
+import pandas as pd
+
+
+class MarketTide:
+    def __init__(self, data):
+
+        self.date = [i.get('date') for i in data]
+        self.net_call_premium = [i.get('net_call_premium') for i in data]
+        self.net_put_premium = [i.get('net_put_premium') for i in data]
+        self.net_volume = [i.get('net_volume') for i in data]
+        self.timestamp = [i.get('timestamp') for i in data]
+
+
+
+        self.data_dict = { 
+            'date': self.date,
+            'call_premium': self.net_call_premium,
+            'put_premium': self.net_put_premium,
+            'volume': self.net_volume,
+            'timestamp': self.timestamp
+        }
+
+
+
+        self.as_dataframe = pd.DataFrame(self.data_dict)
+
+
+
+class AllTide:
+    def __init__(self, data):
+
+        self.date = [i.get('date') for i in data]
+        self.net_call_premium = [i.get('net_call_premium') for i in data]
+        self.net_put_premium = [i.get('net_put_premium') for i in data]
+        self.net_volume = [i.get('net_volume') for i in data]
+        self.timestamp = [i.get('timestamp') for i in data]
+        self.price = [i.get('spy_price') for i in data]
+
+
+
+        self.data_dict = { 
+            'date': self.date,
+            'price': self.price,
+            'call_premium': self.net_call_premium,
+            'put_premium': self.net_put_premium,
+            'volume': self.net_volume,
+            'timestamp': self.timestamp
+        }
+
+
+
+        self.as_dataframe = pd.DataFrame(self.data_dict)
