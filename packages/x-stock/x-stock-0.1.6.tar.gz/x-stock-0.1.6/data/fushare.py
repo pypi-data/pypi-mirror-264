939ce0ec25f94c3f3e48de12edf24b6d54383882
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-27 16:05:40 +0000

    Add package x-stock/0.1.6/x-stock-0.1.6.tar.gz

---
+++ packages/x-stock/x-stock-0.1.6.tar.gz/x-stock-0.1.6/data/fushare.py
@@ -0,0 +1,9 @@
+class FuShare:
+    
+    """_summary_
+    https://tushare.pro/document/2
+    """
+    def __init__(self) -> None:
+        pass
+    
+    
---
+++ packages/x-stock/x-stock-0.1.6.tar.gz/x-stock-0.1.6/test/test_pakg.py
@@ -0,0 +1,4 @@
+import x_stock 
+
+from x_stock import TuShare
+import pandas as pd
