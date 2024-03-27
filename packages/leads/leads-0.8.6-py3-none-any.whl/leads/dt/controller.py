ac1082b1ca1b988090cf00ac3fe97db7bf051b6f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-27 14:04:18 +0000

    Add package leads/0.8.6/leads-0.8.6-py3-none-any.whl

---
+++ packages/leads/leads-0.8.6-py3-none-any.whl/leads/dt/controller.py
@@ -0,0 +1,33 @@
+from typing import override as _override, overload as _overload
+
+from leads.dt.device import Device
+
+
+class Controller(Device):
+    @_overload
+    def __init__(self, *args, **kwargs) -> None:  # real signature unknown
+        ...
+
+    def __init__(self) -> None:
+        super().__init__()
+        self._devices: dict[str, Device] = {}
+
+    def level(self) -> int:
+        return len(self._parent_tags)
+
+    def _attach_device(self, tag: str, device: Device) -> None:
+        self._devices[tag] = device
+        device.tag(tag)
+
+    def devices(self) -> list[Device]:
+        return list(self._devices.values())
+
+    def device(self, tag: str, device: Device | None = None) -> Device | None:
+        if device is None:
+            return self._devices[tag]
+        self._attach_device(tag, device)
+
+    @_override
+    def initialize(self, *parent_tags: str) -> None:
+        for device in self._devices.values():
+            device.initialize(*self._parent_tags, self._tag)
