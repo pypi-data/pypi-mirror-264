fcd5eb707b57338275724f28c93845765a2b7110
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-27 06:04:25 +0000

    Add package aws-cdk.aws-kinesisfirehose-destinations-alpha/2.134.0a0/aws_cdk.aws_kinesisfirehose_destinations_alpha-2.134.0a0-py3-none-any.whl

---
+++ packages/aws-cdk.aws-kinesisfirehose-destinations-alpha/aws_cdk.aws_kinesisfirehose_destinations_alpha-2.134.0a0-py3-none-any.whl/aws_cdk/aws_kinesisfirehose_destinations_alpha/_jsii/__init__.py
@@ -0,0 +1,28 @@
+import abc
+import builtins
+import datetime
+import enum
+import typing
+
+import jsii
+import publication
+import typing_extensions
+
+from typeguard import check_type
+
+import aws_cdk._jsii
+import aws_cdk.aws_kinesisfirehose_alpha._jsii
+import constructs._jsii
+
+__jsii_assembly__ = jsii.JSIIAssembly.load(
+    "@aws-cdk/aws-kinesisfirehose-destinations-alpha",
+    "2.134.0-alpha.0",
+    __name__[0:-6],
+    "aws-kinesisfirehose-destinations-alpha@2.134.0-alpha.0.jsii.tgz",
+)
+
+__all__ = [
+    "__jsii_assembly__",
+]
+
+publication.publish()
