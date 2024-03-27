475496f981a83149b0c9aecb6e62995f245ed5bf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-27 04:04:58 +0000

    Add package mteb/2.0.0/mteb-2.0.0-py3-none-any.whl

---
+++ packages/mteb/mteb-2.0.0-py3-none-any.whl/mteb/tasks/BitextMining/__init__.py
@@ -0,0 +1,8 @@
+from __future__ import annotations
+
+from .da.BornholmskBitextMining import *
+from .multilingual.BUCCBitextMining import *
+from .multilingual.DiaBLaBitextMining import *
+from .multilingual.FloresBitextMining import *
+from .multilingual.NorwegianCourtsBitextMining import *
+from .multilingual.TatoebaBitextMining import *
