e89f756cc5e24e46329f3ad088c2d9e9827f2572
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-27 03:05:11 +0000

    Add package lcax/1.5.0/lcax-1.5.0.tar.gz

---
+++ packages/lcax/lcax-1.5.0.tar.gz/lcax-1.5.0/tests/test_lcax.rs
@@ -0,0 +1,33 @@
+#[cfg(test)]
+mod tests {
+    use lcax::project::Location;
+
+    #[test]
+    fn test_lcax() -> Result<(), String> {
+        lcax::project::LCAxProject {
+            id: "2e257f19-50cc-47de-b54e-2af1add052eb".to_string(),
+            name: "Test".to_string(),
+            description: Some("Test Project".to_string()),
+            comment: None,
+            location: Location {
+                country: "Denmark".to_string(),
+                city: None,
+                address: None,
+            },
+            owner: None,
+            format_version: "1.1.0".to_string(),
+            lcia_method: None,
+            classification_system: None,
+            reference_study_period: None,
+            life_cycle_stages: vec![],
+            impact_categories: vec![],
+            results: None,
+            project_info: None,
+            project_phase: Default::default(),
+            software_info: Default::default(),
+            meta_data: None,
+            assemblies: Default::default(),
+        };
+        Ok(())
+    }
+}
