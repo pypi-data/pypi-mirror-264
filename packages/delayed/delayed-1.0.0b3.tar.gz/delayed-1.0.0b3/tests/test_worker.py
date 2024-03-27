2244446cfbd3c2982cd8f52dec9ff685583bcbc0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-27 16:05:34 +0000

    Add package delayed/1.0.0b3/delayed-1.0.0b3.tar.gz

---
+++ packages/delayed/delayed-1.0.0b3.tar.gz/delayed-1.0.0b3/tests/test_worker.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+
+import os
+import signal
+
+from delayed.task import PyTask
+
+from .common import CONN, NOTI_KEY, PROCESSING_KEY, QUEUE, QUEUE_NAME, WORKER
+
+
+def stop(pid):
+    os.kill(pid, signal.SIGHUP)
+
+
+class TestWorker(object):
+    def test_run(self):
+        CONN.delete(QUEUE_NAME, NOTI_KEY, PROCESSING_KEY)
+
+        task = PyTask(stop, (os.getpid(),))
+        QUEUE.enqueue(task)
+        WORKER.run()
+
+        CONN.delete(QUEUE_NAME, NOTI_KEY, PROCESSING_KEY)
