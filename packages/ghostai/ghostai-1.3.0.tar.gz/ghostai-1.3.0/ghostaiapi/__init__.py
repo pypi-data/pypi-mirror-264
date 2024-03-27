0f026040171c0c1dea55bef572951923f4eefeaf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-27 15:05:51 +0000

    Add package ghostai/1.3.0/ghostai-1.3.0.tar.gz

---
+++ packages/ghostai/ghostai-1.3.0.tar.gz/ghostai-1.3.0/ghostaiapi/__init__.py
@@ -0,0 +1,3 @@
+from .main import chat, chat_with_image
+
+__all__ = ["chat, chat_with_image"]
---
+++ packages/ghostai/ghostai-1.3.0.tar.gz/ghostai-1.3.0/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+
+# Read the contents of your README.md file
+with open('README.md', encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(
+    name='ghostai',
+    version='1.3.0',
+    description='A Python library for interacting with the GhostAI API',
+    long_description=long_description,
+    long_description_content_type='text/markdown',  # This indicates markdown content
+    url='https://github.com/The-UnknownHacker/ghostaiapi',
+    author='CyberZenDev',
+    author_email='cyberzendev@gmail.com',
+    license='MIT',
+    packages=find_packages(),
+    install_requires=[
+        'requests>=2.26.0'
+    ],
+    python_requires='>=3.6',
+)
