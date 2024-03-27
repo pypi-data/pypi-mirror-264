4c5e94bbacef3d83a22029518ee6dd9980f9ba7e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-27 14:05:46 +0000

    Add package navconfig/1.6.5/navconfig-1.6.5-cp311-cp311-manylinux_2_17_x86_64.manylinux2014_x86_64.whl

---
+++ packages/navconfig/navconfig-1.6.5-cp311-cp311-manylinux_2_17_x86_64.manylinux2014_x86_64.whl/navconfig/__init__.py
@@ -0,0 +1,47 @@
+"""
+NavConfig.
+
+Main object for Configuration of several Navigator-related Tools.
+"""
+import os
+import sys
+from pathlib import Path
+from .config import Kardex  # noqa
+from .utils import project_root
+from .version import __version__
+
+# PROJECT PATH IS DEFINED?
+SITE_ROOT = project_root(__file__)
+BASE_DIR = os.getenv("BASE_DIR", None)
+if not BASE_DIR:
+    BASE_DIR = SITE_ROOT
+else:
+    BASE_DIR = Path(BASE_DIR).resolve()
+
+# configuration of the environment type:
+# environment type can be a file (.env) an encrypted file (crypt)
+ENV_TYPE = os.getenv("ENV_TYPE", "file")
+
+# check if create is True (default: false), create the required directories:
+CREATE = os.getenv("CONFIG_CREATE", None)
+
+"""
+Loading main Configuration Object.
+"""
+config = Kardex(SITE_ROOT, env_type=ENV_TYPE, create=CREATE)
+
+# ENV version (dev, prod, staging)
+ENV = config.ENV
+
+# DEBUG VERSION
+DEBUG = config.debug
+
+# Settings Directory
+SETTINGS_DIR = BASE_DIR.joinpath("settings")
+
+# SECURITY WARNING: keep the secret keys used in production secret!
+PRODUCTION = config.getboolean("PRODUCTION", fallback=bool(not DEBUG))
+
+# Add Path Navigator to Sys path
+sys.path.append(str(BASE_DIR))
+sys.path.append(str(SETTINGS_DIR))
