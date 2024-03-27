38ef20f50fc91163bc9e0701c46c44ed780e1ce9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-27 06:05:02 +0000

    Add package PyRAG/0.0.2/PyRAG-0.0.2.tar.gz

---
+++ packages/PyRAG/PyRAG-0.0.2.tar.gz/PyRAG-0.0.2/README.md
@@ -0,0 +1,15 @@
+# PyRAG
+
+A library that makes it easy to get started with RAG applications with SingleStore.
+
+## Installetion
+
+To install the package run:
+
+```sh
+pip install pyrag
+```
+
+## Usage Example
+
+Examples of use are given at this [link](https://github.com/singlestore-labs/pyrag/tree/main/examples).
---
+++ packages/PyRAG/PyRAG-0.0.2.tar.gz/PyRAG-0.0.2/pyrag/files/manager.py
@@ -0,0 +1,26 @@
+from os import environ
+from pyrag.db.database import Database
+from pyrag.embeddings.embeddings import Embeddings
+from pyrag.files.source.s3 import S3FilesSource
+from pyrag.files.source.url import URLFilesSource
+
+
+class FilesManager:
+    def __init__(self, db: Database, embeddings: Embeddings):
+        self._db = db
+        self._embeddings = embeddings
+        self.url = URLFilesSource(self._db, self._embeddings)
+
+    def s3(
+        self,
+        access_key_id: str = environ.get('AWS_ACCESS_KEY_ID', ''),
+        secret_access_key: str = environ.get('AWS_SECRET_ACCESS_KEY', ''),
+        bucket_name: str = environ.get('AWS_BUCKET_NAME', ''),
+    ):
+        return S3FilesSource(
+            db=self._db,
+            embeddings=self._embeddings,
+            access_key_id=access_key_id,
+            secret_access_key=secret_access_key,
+            bucket_name=bucket_name,
+        )
