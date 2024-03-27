fa00aef083935f227584dcb7a73f42135404d43e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-27 15:05:51 +0000

    Add package eidolon-ai-sdk/0.1.23/eidolon_ai_sdk-0.1.23.tar.gz

---
+++ packages/eidolon-ai-sdk/eidolon_ai_sdk-0.1.23.tar.gz/eidolon_ai_sdk-0.1.23/eidolon_ai_sdk/util/str_utils.py
@@ -0,0 +1,11 @@
+import os
+
+
+def replace_env_var_in_string(s, **defaults):
+    """
+    Replace all occurrences of '${VAR}' with the value of the VAR environment variable.
+    """
+    for key in {*os.environ, *defaults.keys()}:
+        replacement = os.environ.get(key, defaults[key]) if key in defaults else os.environ[key]
+        s = s.replace(f"${{{key}}}", replacement)
+    return s
