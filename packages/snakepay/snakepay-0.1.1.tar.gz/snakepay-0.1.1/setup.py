26942cf94c42335c69d61093c4e65a7d6e6fda85
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-27 07:04:52 +0000

    Add package snakepay/0.1.1/snakepay-0.1.1.tar.gz

---
+++ packages/snakepay/snakepay-0.1.1.tar.gz/snakepay-0.1.1/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='snakepay',
+    version='0.1.1',
+    packages=find_packages(),
+    install_requires=[
+        'bitcoinlib',
+    ],
+    author='F4k3r22',
+    author_email='fredyriveraacevedo13@gmail.com',
+    description='Transacciones de bitcoin en una sola linea de código ',
+    long_description='Una biblioteca para realizar transacciones de Bitcoin.',
+    url='https://f4k3r22.github.io/Snake-Pay-Docs/',
+    classifiers=[
+        'Programming Language :: Python :: 3.10',
+        'License :: OSI Approved :: GNU General Public License (GPL)',
+        'Operating System :: OS Independent',
+    ],
+)
