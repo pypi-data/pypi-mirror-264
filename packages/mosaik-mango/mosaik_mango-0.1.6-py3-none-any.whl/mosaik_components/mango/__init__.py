4c99cb0560ef4e5b0ecc66c382a8a2c61fd0d32c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-27 04:04:10 +0000

    Add package mosaik-mango/0.1.6/mosaik_mango-0.1.6-py3-none-any.whl

---
+++ packages/mosaik-mango/mosaik_mango-0.1.6-py3-none-any.whl/mosaik_components/mango/__init__.py
@@ -0,0 +1,4 @@
+from mosaik_components.mango.simulator import AgentSetup as AgentSetup
+from mosaik_components.mango.simulator import AgentSpec as AgentSpec
+from mosaik_components.mango.simulator import MangoSimulator as MangoSimulator
+from mosaik_components.mango.simulator import MosaikAgent as MosaikAgent
