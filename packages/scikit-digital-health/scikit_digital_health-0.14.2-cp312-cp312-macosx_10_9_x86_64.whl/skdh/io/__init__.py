df8d9411ab28e85f5ea994a54cf8044f24e36485
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-27 04:05:00 +0000

    Add package scikit-digital-health/0.14.2/scikit_digital_health-0.14.2-cp312-cp312-macosx_10_9_x86_64.whl

---
+++ packages/scikit-digital-health/scikit_digital_health-0.14.2-cp312-cp312-macosx_10_9_x86_64.whl/skdh/io/__init__.py
@@ -0,0 +1,54 @@
+"""
+Binary File Reading (:mod:`skdh.io`)
+====================================
+
+.. currentmodule:: skdh.io
+
+Device Specific IO
+------------------
+
+These processes are designed to quickly read in data from various different
+wearable devices from their default binary file format.
+
+.. autosummary::
+    :toctree: generated/
+
+    ReadCwa
+    ReadBin
+    ReadApdmH5
+
+General Data IO
+---------------
+
+These processes are generic and not limited to a specific device/manufacturer's format.
+
+.. autosummary::
+    :toctree: generated/
+
+    ReadNumpyFile
+    ReadCSV
+"""
+from skdh.io.axivity import ReadCwa
+from skdh.io import axivity
+from skdh.io.geneactiv import ReadBin
+from skdh.io import geneactiv
+from skdh.io.apdm import ReadApdmH5
+from skdh.io import apdm
+from skdh.io.numpy_compressed import ReadNumpyFile
+from skdh.io import numpy_compressed
+from skdh.io.csv import ReadCSV
+from skdh.io import csv
+from skdh.io.utility import FileSizeError
+
+__all__ = (
+    "ReadCwa",
+    "ReadBin",
+    "ReadApdmH5",
+    "ReadNumpyFile",
+    "ReadCSV",
+    "axivity",
+    "geneactiv",
+    "apdm",
+    "numpy_compressed",
+    "csv",
+)
