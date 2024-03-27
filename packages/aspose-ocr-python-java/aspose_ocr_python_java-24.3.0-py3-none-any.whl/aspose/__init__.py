29e0efe127394c5ea619e2f505bad682a09d3fb5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-27 04:04:59 +0000

    Add package aspose-ocr-python-java/24.3.0/aspose_ocr_python_java-24.3.0-py3-none-any.whl

---
+++ packages/aspose-ocr-python-java/aspose_ocr_python_java-24.3.0-py3-none-any.whl/aspose/__init__.py
@@ -0,0 +1,24 @@
+import jpype
+import os
+from aspose import asposeocr, models, recognitionsettings, recognitionresult, license
+
+__asposeocr_dir__ = os.path.dirname(__file__)
+__ocr_jar_path__ = __asposeocr_dir__ + "/jlib/aspose-ocr-24.3.0.jar"
+__onnx_jar_path__ = __asposeocr_dir__ + "/jlib/onnxruntime-1.16.0.jar"
+
+jpype.startJVM('-ea', classpath=[__ocr_jar_path__, __onnx_jar_path__])
+
+
+__all__ = ['asposeocr', 'models', 'recognitionsettings', 'recognitionresult', 'license', 'helper']
+
+from .recognitionsettings import CarPlateRecognitionSettings
+from .recognitionsettings import IDCardRecognitionSettings
+from .recognitionsettings import InvoiceRecognitionSettings
+from .recognitionsettings import PassportRecognitionSettings
+from .recognitionsettings import ReceiptRecognitionSettings
+from .recognitionsettings import *
+from .recognitionresult import *
+from .asposeocr import AsposeOcr
+from .asposeocr import Resources
+from .license import *
+
