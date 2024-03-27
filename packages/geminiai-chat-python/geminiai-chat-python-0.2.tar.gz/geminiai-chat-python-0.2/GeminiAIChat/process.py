39442b00f04a7f79bf6ed2bd71f77b65cbe2dafe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-27 03:04:56 +0000

    Add package geminiai-chat-python/0.2/geminiai-chat-python-0.2.tar.gz

---
+++ packages/geminiai-chat-python/geminiai-chat-python-0.2.tar.gz/geminiai-chat-python-0.2/GeminiAIChat/process.py
@@ -0,0 +1,31 @@
+import os
+import httpx
+from dotenv import load_dotenv
+import json
+load_dotenv()
+
+class GET:
+    def __init__(self,api):
+        self.KEY =  f"{os.environ.get('KEY')}"
+        self.API_URL =  f"{os.environ.get('API_URL') + self.KEY}"
+        self.PROMPT =  f"{os.environ.get('PROMPT')}"
+        self.PROMPT = json.loads(self.PROMPT)
+
+        
+    def sendResponse(self,prompt):
+        try:
+            self.PROMPT['contents'][0]['parts'][0]['text'] = prompt
+            self.response = httpx.post(
+                self.API_URL,
+                headers={'Content-Type': 'application/json'},
+                json=self.PROMPT)
+            return self.response
+        except:
+            raise ValueError({"message":"check internet connection"})
+            
+
+    def retrieveResponse(self,response):
+        try:
+            return response['candidates'][0]['content']['parts'][0]['text']
+        except:
+            return {"message":"violat gemini ai safety rules"}
