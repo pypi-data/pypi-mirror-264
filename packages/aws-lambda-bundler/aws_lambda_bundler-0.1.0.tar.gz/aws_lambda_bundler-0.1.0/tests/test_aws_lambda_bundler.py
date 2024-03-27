b93ce8aa3903a6c7cf11d3922cfc24584a63d901
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-27 07:04:41 +0000

    Add package aws-lambda-bundler/0.1.0/aws_lambda_bundler-0.1.0.tar.gz

---
+++ packages/aws-lambda-bundler/aws_lambda_bundler-0.1.0.tar.gz/aws_lambda_bundler-0.1.0/tests/test_aws_lambda_bundler.py
@@ -0,0 +1,24 @@
+import sys
+import tempfile
+from pathlib import Path
+
+import aws_lambda_bundler
+
+
+def test_install_to_dir():
+    dependencies = ["requests"]
+    python = sys.executable
+    scratch_dir = Path(tempfile.mkdtemp())
+    aws_lambda_bundler._install_to_dir(python, scratch_dir, dependencies)
+    assert len(list(scratch_dir.glob("*")))
+
+
+def test_zip_dir():
+    scratch_dir = Path(tempfile.mkdtemp())
+    output = Path(tempfile.mkdtemp()) / "out.zip"
+
+    with open(scratch_dir / "test", "w") as handle:
+        handle.write("hello")
+
+    aws_lambda_bundler._zip_dir(output=output, scratch_dir=scratch_dir)
+    assert len(list(output.parent.glob("*")))
