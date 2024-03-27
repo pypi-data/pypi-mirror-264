d75de16f254d6b608ca87b455426743c03f72f65
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-27 14:04:09 +0000

    Add package weave/0.44.0/weave-0.44.0-py3-none-any.whl

---
+++ packages/weave/weave-0.44.0-py3-none-any.whl/weave/frontend/assets/Component.4d41d27e.js
@@ -0,0 +1,2 @@
+import{ah as i,u as o,R as e}from"./index.b6574bb3.js";const n=l=>{const a=l.input,r=i({file:a}),t=o(r);return e.createElement("div",null,t.loading?e.createElement("div",null):e.createElement("img",{style:{maxWidth:"100%"},alt:"cool-alt",src:t.result}))};export{n as default};
+//# sourceMappingURL=Component.4d41d27e.js.map
