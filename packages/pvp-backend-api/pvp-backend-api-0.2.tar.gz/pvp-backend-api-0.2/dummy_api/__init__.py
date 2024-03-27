3f0228f0d7daf2c8280abce38e9d856c899f53be
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-27 15:06:10 +0000

    Add package pvp-backend-api/0.2/pvp-backend-api-0.2.tar.gz

---
+++ packages/pvp-backend-api/pvp-backend-api-0.2.tar.gz/pvp-backend-api-0.2/dummy_api/__init__.py
@@ -0,0 +1,17 @@
+# dummy_api/__init__.py
+
+import click
+import os
+from .generator import generate_template
+
+@click.command()
+@click.option('--output', '-o', default='.', help='Output path for the template')
+def cli(output):
+    generate_template(output)
+
+
+
+if __name__ == "__main__":
+    cli()
+
+
