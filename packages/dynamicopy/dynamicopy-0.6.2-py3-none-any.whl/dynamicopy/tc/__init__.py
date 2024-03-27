d0046ac2c18550535ac66f37b921b725bb5223c1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-27 04:05:02 +0000

    Add package dynamicopy/0.6.2/dynamicopy-0.6.2-py3-none-any.whl

---
+++ packages/dynamicopy/dynamicopy-0.6.2-py3-none-any.whl/dynamicopy/tc/__init__.py
@@ -0,0 +1,16 @@
+from .ibtracs import load_ibtracs
+from .load_tracks import *
+from .maps import plot_tracks, plot_polar, ax_med, plot_tracks_med
+from .matching import *
+from .metrics import *
+from .utils import *
+from .ET import *
+from .STJ import *
+from .lifecycle import *
+
+import seaborn as sns
+
+pal_algo = {"UZ":sns.color_palette("colorblind")[0],
+          "OWZ":sns.color_palette("colorblind")[1],
+          "TRACK":sns.color_palette("colorblind")[2],
+          "CNRM": sns.color_palette("colorblind")[4]}
