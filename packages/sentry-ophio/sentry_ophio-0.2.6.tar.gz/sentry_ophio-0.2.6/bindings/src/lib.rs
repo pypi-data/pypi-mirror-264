f19eb405a296ba26a3f9537f9513cb56e212afae
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-27 16:05:32 +0000

    Add package sentry-ophio/0.2.6/sentry_ophio-0.2.6.tar.gz

---
+++ packages/sentry-ophio/sentry_ophio-0.2.6.tar.gz/sentry_ophio-0.2.6/bindings/src/lib.rs
@@ -0,0 +1,16 @@
+use pyo3::prelude::*;
+
+mod enhancers;
+mod proguard;
+
+#[pymodule]
+fn _bindings(_py: Python, m: Bound<PyModule>) -> PyResult<()> {
+    m.add_class::<enhancers::Cache>()?;
+    m.add_class::<enhancers::Component>()?;
+    m.add_class::<enhancers::Enhancements>()?;
+    m.add_class::<enhancers::AssembleResult>()?;
+    m.add_class::<proguard::JavaStackFrame>()?;
+    m.add_class::<proguard::ProguardMapper>()?;
+
+    Ok(())
+}
