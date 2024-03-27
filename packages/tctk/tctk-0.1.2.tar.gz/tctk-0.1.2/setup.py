112fce775edf04815ad5b485824a2b7da143a87b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-27 15:05:50 +0000

    Add package tctk/0.1.2/tctk-0.1.2.tar.gz

---
+++ packages/tctk/tctk-0.1.2.tar.gz/tctk-0.1.2/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup
+
+setup(
+    name='tctk',
+    version='0.1.2',
+    packages=['src.tctk'],
+    url='https://github.com/nhgritctran/tctk',
+    license='GPL-3.0',
+    author='Tam Tran',
+    author_email='tam.c.tran@outlook.com',
+    description='A collection of mini tools.',
+    install_requires=[
+        "connectorx",
+        "google-cloud-bigquery",
+        "hail",
+        "polars",
+        "pyarrow",
+        "tqdm"
+    ]
+)
