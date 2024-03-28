de64fe67b4a64e2406d8233995d2e20d2e1a8029
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-28 06:05:06 +0000

    Add package xbasic/1.2.0/xbasic-1.2.0.tar.gz

---
+++ packages/xbasic/xbasic-1.2.0.tar.gz/xbasic-1.2.0/xbasic/main.py
@@ -0,0 +1,60 @@
+import click
+import webbrowser
+import platform
+import datetime
+from .init_interp import run
+
+
+@click.group()
+def cli():
+    pass
+
+
+@cli.command()
+def report():
+    """Open the GitHub page."""
+    click.echo("Going to the Git page...")
+    webbrowser.open("https://github.com/vivekkdagar/xbasic")
+
+
+def entry_shell():
+    while True:
+        text = input('>> ')
+        if text.strip() == "":
+            continue
+        result, error = run('<stdin>', text)
+
+        if error:
+            print(error.as_string())
+        elif result:
+            print("The process returned: ", result)
+
+
+@cli.command()
+@click.option('-f', type=click.Path(exists=True), default=None, help='Specify a file to execute within the shell.')
+def shell(f):
+    """Start an interactive shell."""
+    print_intro()
+    if f:
+        if not f.endswith('.bsx'):
+            click.echo("Error: File must end with '.bsx'.")
+            return
+        result, error = run('<stdin>', f'RUN("{f}")')
+        if error:
+            print(error.as_string())
+        elif result:
+            print("The process returned ", result)
+    else:
+        entry_shell()
+
+
+def print_intro():
+    version = "1.2.0"
+    current_date_time = datetime.datetime.now().strftime("%b %d %Y, %H:%M:%S")
+    os_name = platform.system()
+    intro = f"XBasic {version} ({current_date_time}) on {os_name.lower()}"
+    click.echo(intro)
+
+
+if __name__ == '__main__':
+    cli()
