cd9af4eeeb4ccd8381201b88d7d57f030a407b7c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-28 05:04:48 +0000

    Add package pyembroidery/1.5.1/pyembroidery-1.5.1-py2.py3-none-any.whl

---
+++ packages/pyembroidery/pyembroidery-1.5.1-py2.py3-none-any.whl/pyembroidery/__init__.py
@@ -0,0 +1,14 @@
+name = "pyembroidery"
+
+# items available at the top level (e.g. pyembroidery.read)
+from .EmbConstant import *
+from .EmbFunctions import *
+from .EmbMatrix import EmbMatrix
+from .EmbPattern import EmbPattern
+from .EmbThread import EmbThread
+from .EmbCompress import compress, expand
+import pyembroidery.GenericWriter as GenericWriter
+
+# items available in a sub-heirarchy (e.g. pyembroidery.PecGraphics.get_graphic_as_string)
+from .PecGraphics import get_graphic_as_string
+from .PyEmbroidery import *
