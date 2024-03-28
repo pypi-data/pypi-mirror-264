813e6e0fce2da4a6359c082647b648afa0112669
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-28 03:07:01 +0000

    Add package MONSDA/1.2.3/MONSDA-1.2.3-py3-none-any.whl

---
+++ packages/MONSDA/MONSDA-1.2.3-py3-none-any.whl/MONSDA-1.2.3.data/data/share/MONSDA/scripts/Shells/UniqueSam_woPicard.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+in=$1
+out=$2
+threads=$3
+bwa="${4:-}"
+
+if [[ "$1" == *.gz* ]]
+then
+    samtools view -H <(zcat $in) | grep '@HD' | pigz -p $threads -f > $out
+    samtools view -H <(zcat $in) | grep '@SQ' | sort -t$'\t' -k1,1 -k2,2V | pigz -p $threads -f >> $out
+    samtools view -H <(zcat $in) | grep '@RG' | pigz -p $threads -f >> $out
+    samtools view -H <(zcat $in) | grep '@PG' | pigz -p $threads -f >> $out
+else
+    samtools view -H <(cat $in)|grep '@HD' | pigz -p $threads -f > $out
+    samtools view -H <(cat $in)|grep '@SQ' | sort -t$'\t' -k1,1 -k2,2V |pigz -p $threads -f >> $out
+    samtools view -H <(cat $in)|grep '@RG' | pigz -p $threads -f >> $out
+    samtools view -H <(cat $in)|grep '@PG' | pigz -p $threads -f >> $out
+fi
+
+if [[ "$1" == *bwa* ]] || [[ -n "$bwa" ]]
+then
+    if [[ "$1" == *.gz* ]]
+    then
+        zcat $in | grep -v "^@"| grep -v -e $'\t''XA:Z:' -e $'\t''SA:Z:' | pigz -p $threads -f >> $out
+    else
+        cat $in | grep -v "^@"| grep -v -e $'\t''XA:Z:' -e $'\t''SA:Z:' | pigz -p $threads -f >> $out
+    fi
+else
+    if [[ "$1" == *.gz* ]]
+    then
+        zcat $in | grep -v "^@" | grep -w -P "NH:i:1|tp:A:P" | pigz -p $threads -f >> $out
+    else
+        cat $in | grep -v "^@" | grep -w -P "NH:i:1|tp:A:P" | pigz -p $threads -f >> $out
+    fi
+fi
