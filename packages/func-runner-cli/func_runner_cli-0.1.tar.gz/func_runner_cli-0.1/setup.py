bddd7b33894d309b2a711d3708b9f989c678e512
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-28 04:05:40 +0000

    Add package func-runner-cli/0.1/func_runner_cli-0.1.tar.gz

---
+++ packages/func-runner-cli/func_runner_cli-0.1.tar.gz/func_runner_cli-0.1/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+setup(
+   name='func_runner_cli',
+   version='0.1',
+   python_requires='>=3.7',
+   packages=find_packages(),
+   install_requires=[
+      'typer>=0.9.0',  # Assuming you want to match the version specified in Poetry
+      'pyyaml>=6.0.1',
+      'pydantic>=2.6.4',
+      'pydantic-settings>=2.2.1',
+      'requests>=2.31.0',
+   ],
+   entry_points='''
+      [console_scripts]
+      funcrunner=func_runner_cli.main:app
+      ''',
+)
