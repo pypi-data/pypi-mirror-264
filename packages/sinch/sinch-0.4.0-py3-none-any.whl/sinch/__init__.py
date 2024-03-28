26674e4c3c34d8211a01cb4a64c72a6dd9bd2101
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-28 06:05:19 +0000

    Add package sinch/0.4.0/sinch-0.4.0-py3-none-any.whl

---
+++ packages/sinch/sinch-0.4.0-py3-none-any.whl/sinch/__init__.py
@@ -0,0 +1,9 @@
+""" Sinch Python SDK
+To access Sinch resources, use the Sync or Async version of the Sinch Client.
+"""
+__version__ = "0.4.0"
+
+from sinch.core.clients.sinch_client_sync import SinchClient
+from sinch.core.clients.sinch_client_async import SinchClientAsync
+
+__all__ = (SinchClient, SinchClientAsync)
