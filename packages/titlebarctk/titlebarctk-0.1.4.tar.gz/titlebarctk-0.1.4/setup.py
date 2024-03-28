46858f2c83c2c6a72960a567d37e4427c0cf3366
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-28 05:04:45 +0000

    Add package titlebarctk/0.1.4/titlebarctk-0.1.4.tar.gz

---
+++ packages/titlebarctk/titlebarctk-0.1.4.tar.gz/titlebarctk-0.1.4/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+from pathlib import Path
+
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.txt").read_text()
+
+setup(
+    name='titlebarctk',
+    version='0.1.4',
+    long_description=long_description,
+    long_description_content_type="text/plain"
+)
