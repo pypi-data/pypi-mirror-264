85d3ad9f1f9f09fd5f6630a16323db479ea5a4c5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-28 03:07:02 +0000

    Add package processing-pypelines/0.0.30/processing-pypelines-0.0.30.tar.gz

---
+++ packages/processing-pypelines/processing-pypelines-0.0.30.tar.gz/processing-pypelines-0.0.30/src/pypelines/tasks.py
@@ -0,0 +1,48 @@
+from functools import wraps
+from typing import TYPE_CHECKING
+
+if TYPE_CHECKING:
+    from .pipelines import Pipeline
+    from .steps import BaseStep
+
+
+class BaseStepTaskManager:
+    step: "BaseStep"
+    backend: "BaseTaskBackend"
+
+    def __init__(self, step, backend):
+        self.step = step
+        self.backend = backend
+
+    def start(self, session, *args, **kwargs):
+        if not self.backend:
+            raise NotImplementedError
+
+
+class BaseTaskBackend:
+
+    task_manager_class = BaseStepTaskManager
+    success: bool = False
+
+    def __init__(self, parent: "Pipeline", **kwargs):
+        self.parent = parent
+
+    def __bool__(self):
+        return self.success
+
+    def create_task_manager(self, step) -> "BaseStepTaskManager":
+        return self.task_manager_class(step, self)
+
+    #     wrapped_step = getattr(step, "queue", None)
+    #     if wrapped_step is None:
+    #         # do not register
+    #         pass
+    #     # registration code here
+
+    # def wrap_step(self, step):
+
+    #     @wraps(step.generate)
+    #     def wrapper(*args, **kwargs):
+    #         return step.generate(*args, **kwargs)
+
+    #     return wrapper
