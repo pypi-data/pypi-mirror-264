06751f5482be96e0883750db6b138ccdc52d86d2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-28 03:07:39 +0000

    Add package curvey/0.0.0/curvey-0.0.0-py3-none-any.whl

---
+++ packages/curvey/curvey-0.0.0-py3-none-any.whl/curvey/__init__.py
@@ -0,0 +1,12 @@
+"""
+TODO
+    - Pip install, etc
+    - separate environment.yml
+"""
+
+from __future__ import annotations
+
+from .curve import Curve
+from .curves import Curves
+
+__all__ = ("Curve", "Curves")
