9be19d3424bc6cefe62e2d8fae30db0a367b51d9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-28 05:04:33 +0000

    Add package homa/0.15/homa-0.15.tar.gz

---
+++ packages/homa/homa-0.15.tar.gz/homa-0.15/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup
+from setuptools import find_packages
+
+with open("README.md") as fh:
+    description = fh.read()
+
+setup(
+    name="homa",
+    version="0.15",
+    packages=find_packages(),
+    install_requires=[
+        #
+    ],
+    long_description=description,
+    long_description_content_type="text/markdown"
+)
