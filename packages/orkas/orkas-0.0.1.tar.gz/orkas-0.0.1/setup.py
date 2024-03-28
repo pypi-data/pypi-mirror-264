d1ac23873dac38636ca3c927027d9b15d014ebf6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-28 06:04:59 +0000

    Add package orkas/0.0.1/orkas-0.0.1.tar.gz

---
+++ packages/orkas/orkas-0.0.1.tar.gz/orkas-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name='orkas',
+    version='0.0.1',
+    description="DataFrame dialect",
+    url='https://github.com/kszucs',
+    author='kszucs',
+    author_email='kszucs@kszucs.me',
+    packages=[],
+)
