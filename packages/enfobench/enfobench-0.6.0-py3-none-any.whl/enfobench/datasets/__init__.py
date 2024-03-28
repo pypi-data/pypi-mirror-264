ac172c19ad9da0e2c9289c512fac2f315f71efc5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-28 06:05:28 +0000

    Add package enfobench/0.6.0/enfobench-0.6.0-py3-none-any.whl

---
+++ packages/enfobench/enfobench-0.6.0-py3-none-any.whl/enfobench/datasets/__init__.py
@@ -0,0 +1,7 @@
+from enfobench.datasets.electricity_demand import ElectricityDemandDataset
+from enfobench.datasets.pv_generation import PVGenerationDataset
+
+__all__ = [
+    "ElectricityDemandDataset",
+    "PVGenerationDataset",
+]
