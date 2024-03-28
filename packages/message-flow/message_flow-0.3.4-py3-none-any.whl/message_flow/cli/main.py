6889e19258cd3b2aa3f76257597340be17c2ab55
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-28 05:04:48 +0000

    Add package message-flow/0.3.4/message_flow-0.3.4-py3-none-any.whl

---
+++ packages/message-flow/message_flow-0.3.4-py3-none-any.whl/message_flow/cli/main.py
@@ -0,0 +1,60 @@
+import typer
+
+from ._async_api_schema import serve_schema
+from .cli_app import CLIApp
+from .logging_level import LoggingLevel
+
+__all__ = ["cli"]
+
+cli = typer.Typer(pretty_exceptions_short=True)
+
+
+@cli.callback()
+def callback():
+    """
+    Message Flow
+    """
+
+
+@cli.command()
+def dispatch(
+    app: str = typer.Argument(
+        ...,
+        help="[python_module:MessageFlow] - path to your application",
+    ),
+    log_level: LoggingLevel = typer.Option(
+        LoggingLevel.INFO,
+        case_sensitive=False,
+        show_default=False,
+        help="[INFO] default",
+    ),
+):
+    """
+    Starts message dispatching
+    """
+    cli_app = CLIApp(app, log_level)
+
+    cli_app.dispatch()
+
+
+@cli.command()
+def docs(
+    app: str = typer.Argument(
+        ...,
+        help="[python_module:MessageFlow] - path to your application",
+    ),
+    host: str = typer.Option(
+        "localhost",
+        help="documentation hosting address",
+    ),
+    port: int = typer.Option(
+        8000,
+        help="documentation hosting port",
+    ),
+):
+    """
+    Starts Async API schema serving
+    """
+    cli_app = CLIApp(app)
+
+    serve_schema(schema=cli_app.make_async_api_schema(), host=host, port=port)
