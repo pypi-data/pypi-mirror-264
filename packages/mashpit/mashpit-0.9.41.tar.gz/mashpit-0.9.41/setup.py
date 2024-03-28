e99a21d5fdbb27336a8fb15cc7633e75a84c42ff
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-28 04:05:41 +0000

    Add package mashpit/0.9.41/mashpit-0.9.41.tar.gz

---
+++ packages/mashpit/mashpit-0.9.41.tar.gz/mashpit-0.9.41/setup.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+
+from setuptools import setup
+
+with open("README.md", "r") as rm:
+    long_description = rm.read()
+
+setup(
+    name='mashpit',
+    version='0.9.41',
+    url='https://github.com/tongzhouxu/mashpit',
+    author='Tongzhou Xu',
+    author_email='tongzhou.xu@uga.edu',
+    description='A sketch-based surveillance platform',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    packages=['mashpit'],
+    include_package_data=True,
+    entry_points={'console_scripts':['mashpit=mashpit.mashpit:main']},
+    install_requires=[
+        'sourmash~=4.6.1',
+        'ncbi-datasets-pylib~=14.6.2',
+        'pandas',
+        'biopython',
+        'scikit-bio',
+        'tqdm',
+        'flask',
+        'dask[dataframe]',
+    ]
+)
