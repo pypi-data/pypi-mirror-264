e45fe7ab7f46923c48a7bdc90b40f82f50b78a41
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-28 06:05:13 +0000

    Add package epiccrypto/0.1.2.5/epiccrypto-0.1.2.5.tar.gz

---
+++ packages/epiccrypto/epiccrypto-0.1.2.5.tar.gz/epiccrypto-0.1.2.5/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+setup(
+name='epiccrypto',
+version='0.1.2.5',
+author='Epic099',
+author_email='',
+description='Encryption is cool',
+packages=find_packages(),
+classifiers=[
+'Programming Language :: Python :: 3',
+'License :: OSI Approved :: MIT License',
+'Operating System :: OS Independent',
+],
+python_requires='>=3.6',
+install_requires=[
+    "epicfilemanager"   
+]
+)
