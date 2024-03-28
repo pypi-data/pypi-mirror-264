18be4d32972cfa4e6c7831df3e9dfc27dd979e20
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-28 05:05:43 +0000

    Add package buildrunner/3.6.974/buildrunner-3.6.974-py3-none-any.whl

---
+++ packages/buildrunner/buildrunner-3.6.974-py3-none-any.whl/buildrunner/config/fetch/http.py
@@ -0,0 +1,15 @@
+"""
+Copyright 2021 Adobe
+All Rights Reserved.
+
+NOTICE: Adobe permits you to use, modify, and distribute this file in accordance
+with the terms of the Adobe license agreement accompanying it.
+"""
+
+
+def fetch_file(parsed_url, config: dict):
+    """
+    Fetch files using HTTP.
+    """
+
+    raise NotImplementedError("Not Implemented: fetch.http.file_fetch")
