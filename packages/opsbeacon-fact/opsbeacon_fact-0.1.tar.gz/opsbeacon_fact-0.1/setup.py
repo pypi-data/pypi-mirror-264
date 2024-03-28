261de173fc04bca0c5c8c05350a033e169f5577c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-28 14:05:31 +0000

    Add package opsbeacon-fact/0.1/opsbeacon_fact-0.1.tar.gz

---
+++ packages/opsbeacon-fact/opsbeacon_fact-0.1.tar.gz/opsbeacon_fact-0.1/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='opsbeacon_fact',
+    version='0.1',
+    packages=find_packages(),
+    author='obsevket',
+    author_email='mserdogmus@opsbeacon.com',
+    description='A simple package to calculate factorial',
+)
