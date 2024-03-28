c1a2e8bdeba7b10826007ec31ae0689e166bf49c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-28 15:04:52 +0000

    Add package FindOrg/0.2/FindOrg-0.2.0.tar.gz

---
+++ packages/FindOrg/FindOrg-0.2.0.tar.gz/FindOrg-0.2.0/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r") as f:
+    description = f.read()
+
+setup(
+    name='FindOrg',
+    version='0.2.0',
+    packages=find_packages(),
+    install_requires=[
+        # Add dependencies here
+        # e.g. 'numpy>=1.11.1'
+    ],
+    long_description=description,
+    long_description_content_type="text/markdown",
+)
