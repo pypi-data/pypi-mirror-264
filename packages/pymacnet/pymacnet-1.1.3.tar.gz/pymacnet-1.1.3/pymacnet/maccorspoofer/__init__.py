4428abbf23d79f2c2bb5e463cd8b42f70636b33e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-28 05:06:02 +0000

    Add package pymacnet/1.1.3/pymacnet-1.1.3.tar.gz

---
+++ packages/pymacnet/pymacnet-1.1.3.tar.gz/pymacnet-1.1.3/pymacnet/maccorspoofer/__init__.py
@@ -0,0 +1,1 @@
+from .MaccorSpoofer import MaccorSpoofer
---
+++ packages/pymacnet/pymacnet-1.1.3.tar.gz/pymacnet-1.1.3/setup.py
@@ -0,0 +1,27 @@
+import setuptools
+
+with open('requirements.txt') as f:
+    requirements = f.read().splitlines()
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="pymacnet",
+    version="1.1.3",
+    author="Bing Syuan Wang, Chintan Pathak and Zander Nevitt",
+    author_email="info@battgenie.life",
+    description="A class based python interface for communication and control of Maccor cyclers over Macnet.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/BattGenie/pymacnet.git",
+    packages=setuptools.find_packages(),
+    install_requires=requirements,
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+    license='MIT',
+)
