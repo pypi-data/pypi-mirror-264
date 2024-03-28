116062ff82cc65d8f74af7451ddaa1fe900c53d3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-28 04:05:28 +0000

    Add package dataferry/0.0.4/dataferry-0.0.4-py3-none-any.whl

---
+++ packages/dataferry/dataferry-0.0.4-py3-none-any.whl/dataferry/__init__.py
@@ -0,0 +1,13 @@
+from dataferry.connections import (
+    SqlServerConnection
+    , DbxConnection
+)
+
+from dataferry.transfers import (
+        Etl
+)
+
+from dataferry.execution import(
+    run_sql_server_code
+    , execute_sql_file
+)
