d94af0db1a58be9a6c3872cbb0625302d3c08916
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-28 03:07:44 +0000

    Add package data-horizon/0.1.1/data_horizon-0.1.1.tar.gz

---
+++ packages/data-horizon/data_horizon-0.1.1.tar.gz/data_horizon-0.1.1/horizon/commons/errors/base.py
@@ -0,0 +1,31 @@
+# SPDX-FileCopyrightText: 2023-2024 MTS (Mobile Telesystems)
+# SPDX-License-Identifier: Apache-2.0
+
+from typing import Any, Generic, TypeVar
+
+from pydantic import BaseModel
+from pydantic import __version__ as pydantic_version
+from pydantic.generics import GenericModel
+
+from horizon.commons.dto.unset import Unset
+
+
+class BaseErrorSchema(BaseModel):
+    code: str
+    message: str
+    details: Any
+
+
+ErrorModel = TypeVar("ErrorModel", bound=BaseErrorSchema)
+
+
+class APIErrorSchema(GenericModel, Generic[ErrorModel]):
+    error: ErrorModel
+
+    if pydantic_version < "2":
+
+        class Config:
+            # https://github.com/pydantic/pydantic/issues/2277
+            json_encoders = {
+                Unset: str,
+            }
