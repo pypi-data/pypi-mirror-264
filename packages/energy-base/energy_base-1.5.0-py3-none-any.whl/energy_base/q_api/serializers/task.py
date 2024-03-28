ae633bfb91e9ac1397b781903f3b3056e1e1c6ed
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-28 03:07:12 +0000

    Add package energy-base/1.5.0/energy_base-1.5.0-py3-none-any.whl

---
+++ packages/energy-base/energy_base-1.5.0-py3-none-any.whl/energy_base/q_api/serializers/task.py
@@ -0,0 +1,17 @@
+from django_q.models import Task
+from rest_framework import serializers
+
+
+class TaskSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Task
+        fields = '__all__'
+
+    def to_representation(self, instance):
+        if isinstance(instance, str):
+            instance = Task.objects.get(pk=instance)
+        data = super(TaskSerializer, self).to_representation(instance)
+        data['args'] = instance.args
+        data['kwargs'] = instance.kwargs
+        data['result'] = instance.result
+        return data
