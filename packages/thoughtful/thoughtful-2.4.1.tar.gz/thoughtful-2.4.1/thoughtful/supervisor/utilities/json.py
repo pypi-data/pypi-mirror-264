879947e241c62e176dcb1a0247eef6ed16a3c357
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-28 04:04:51 +0000

    Add package thoughtful/2.4.1/thoughtful-2.4.1.tar.gz

---
+++ packages/thoughtful/thoughtful-2.4.1.tar.gz/thoughtful-2.4.1/thoughtful/supervisor/utilities/json.py
@@ -0,0 +1,20 @@
+import json
+import math
+
+
+class JSONEncoder(json.JSONEncoder):
+    """
+    A JSON encoder that converts NaNs to None.
+    """
+
+    def encode(self, obj, *args, **kwargs):
+        return super().encode(self._nan_to_none(obj), *args, **kwargs)
+
+    def _nan_to_none(self, obj):
+        if isinstance(obj, dict):
+            return {k: self._nan_to_none(v) for k, v in obj.items()}
+        elif isinstance(obj, list):
+            return [self._nan_to_none(v) for v in obj]
+        elif isinstance(obj, float) and math.isnan(obj):
+            return None
+        return obj
