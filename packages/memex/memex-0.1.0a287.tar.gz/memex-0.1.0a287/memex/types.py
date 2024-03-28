d0d7495ce07a4bb4aa40a5514a6982c0d30866da
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-28 04:04:46 +0000

    Add package memex/0.1.0a287/memex-0.1.0a287.tar.gz

---
+++ packages/memex/memex-0.1.0a287.tar.gz/memex-0.1.0a287/memex/types.py
@@ -0,0 +1,52 @@
+import inspect
+import re
+from typing import Type, get_origin, get_args
+from pydantic import BaseModel
+
+
+def serialize_type(typ: Type) -> dict:
+    if isinstance(typ, type) and not get_origin(typ) and issubclass(typ, BaseModel):
+        return {"kind": "pydantic", "name": typ.__name__, "schema": typ.model_json_schema()}
+    elif get_origin(typ):
+        inner_types = [serialize_type(t) for t in get_args(typ)]
+        return {"kind": "generic", "origin": get_origin(typ).__name__, "args": inner_types}
+    else:
+        return {"kind": "basic", "name": getattr(typ, "__name__", str(typ))}
+
+
+def _udf_payload(func, prompt, resources=None, remote=False):
+    # eval_str=True is needed in case the user's code has `from __future__ import annotations` which will cause
+    # the type hints to be evaluated as strings
+    sig = inspect.signature(func, eval_str=True)
+    parameter_types = [serialize_type(param.annotation) for param in sig.parameters.values()]
+    param_names = [param.name for param in sig.parameters.values()]
+    return_type = serialize_type(sig.return_annotation)
+    if not prompt:
+        from dill.source import getsource
+
+        source = getsource(func)
+        # Exclude any function decorator from the source
+        source = re.sub(r"@.*\n", "", source)
+        payload = dict(
+            name=func.__name__,
+            content="",
+            variables=param_names,
+            source=source,
+            parameter_types=parameter_types,
+            return_type=return_type,
+            native=True,
+            remote=remote,
+        )
+    else:
+        docstring = func.__doc__
+        payload = dict(
+            name=func.__name__,
+            content=docstring,
+            variables=param_names,
+            parameter_types=parameter_types,
+            return_type=return_type,
+            native=False,
+        )
+    if resources:
+        payload["resources"] = resources
+    return payload
