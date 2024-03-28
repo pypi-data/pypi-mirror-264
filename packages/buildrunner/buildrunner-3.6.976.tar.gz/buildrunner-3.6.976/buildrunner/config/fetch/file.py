bdb7aa0d0b2bc35fbcd1198b9c175b5e6929cb0f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-28 06:04:45 +0000

    Add package buildrunner/3.6.976/buildrunner-3.6.976.tar.gz

---
+++ packages/buildrunner/buildrunner-3.6.976.tar.gz/buildrunner-3.6.976/buildrunner/config/fetch/file.py
@@ -0,0 +1,20 @@
+"""
+Copyright 2021 Adobe
+All Rights Reserved.
+
+NOTICE: Adobe permits you to use, modify, and distribute this file in accordance
+with the terms of the Adobe license agreement accompanying it.
+"""
+
+import codecs
+from typing import Optional
+
+
+def fetch_file(parsed_url, _: Optional[dict]):
+    """
+    Pull files from the local file system.
+    """
+    with codecs.open(parsed_url.path, "r", encoding="utf-8") as _file:
+        contents = "".join(_file.readlines())
+
+    return contents
