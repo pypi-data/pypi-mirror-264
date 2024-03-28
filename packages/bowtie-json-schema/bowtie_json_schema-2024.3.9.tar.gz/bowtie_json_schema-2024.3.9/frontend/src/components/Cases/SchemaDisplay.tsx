b8a9efeb8a70336fc6f57d8e8505d02a35adee57
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-28 05:05:27 +0000

    Add package bowtie-json-schema/2024.3.9/bowtie_json_schema-2024.3.9.tar.gz

---
+++ packages/bowtie-json-schema/bowtie_json_schema-2024.3.9.tar.gz/bowtie_json_schema-2024.3.9/frontend/src/components/Cases/SchemaDisplay.tsx
@@ -0,0 +1,56 @@
+import CopyToClipboard from "../CopyToClipboard";
+import { Prism as SyntaxHighlighter } from "react-syntax-highlighter";
+import { atomDark } from "react-syntax-highlighter/dist/esm/styles/prism";
+
+const SchemaDisplay = ({
+  schema,
+  instance,
+}: {
+  instance: unknown;
+  schema: Record<string, unknown> | boolean;
+}) => {
+  const schemaFormatted = JSON.stringify(schema, null, 2);
+  const instanceFormatted = JSON.stringify(instance, null, 2);
+  return (
+    <div className="card mb-3 mw-100">
+      <div className="d-flex">
+        <div className="col-md-8 col-6 pe-0">
+          <div className="d-flex align-items-center highlight-toolbar ps-3 pe-2 py-1 border-0 border-top border-bottom">
+            <small className="font-monospace text-body-secondary text-uppercase">
+              Schema
+            </small>
+            <div className="d-flex ms-auto"></div>
+          </div>
+          <div className="card-body position-relative">
+            <CopyToClipboard
+              textToCopy={schemaFormatted}
+              style="position-absolute top-0 end-0 mt-4 me-3"
+            />
+            <SyntaxHighlighter language="json" style={atomDark}>
+              {schemaFormatted}
+            </SyntaxHighlighter>
+          </div>
+        </div>
+        <div className="col-md-4 col-6 border-start px-0">
+          <div className="d-flex align-items-center highlight-toolbar ps-3 pe-2 py-1 border-0 border-top border-bottom">
+            <small className="font-monospace text-body-secondary text-uppercase pe-4">
+              Instance
+            </small>
+            <div className="d-flex ms-auto"></div>
+          </div>
+          <div id="instance-info" className="card-body position-relative">
+            <CopyToClipboard
+              textToCopy={instanceFormatted}
+              style="position-absolute top-0 end-0 mt-4 me-3"
+            />
+            <SyntaxHighlighter language="json" style={atomDark}>
+              {instanceFormatted}
+            </SyntaxHighlighter>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default SchemaDisplay;
---
+++ packages/bowtie-json-schema/bowtie_json_schema-2024.3.9.tar.gz/bowtie_json_schema-2024.3.9/frontend/src/components/CopyToClipboard.tsx
@@ -0,0 +1,45 @@
+import { useState } from "react";
+import { Check, Copy } from "react-bootstrap-icons";
+import Button from "react-bootstrap/Button";
+import OverlayTrigger from "react-bootstrap/OverlayTrigger";
+import Tooltip from "react-bootstrap/Tooltip";
+
+interface CopyProps {
+  textToCopy: string;
+  style?: string;
+}
+
+function CopyToClipboard({ textToCopy, style }: CopyProps) {
+  const [copied, setCopied] = useState(false);
+
+  const handleCopy = () => {
+    navigator.clipboard
+      .writeText(textToCopy)
+      .then(() => {
+        setCopied(true);
+        setTimeout(() => setCopied(false), 1500);
+      })
+      .catch((error) => {
+        console.error("Error copying text to clipboard:", error);
+      });
+  };
+
+  return (
+    <>
+      <OverlayTrigger
+        placement="top"
+        overlay={
+          <Tooltip id="tooltip-top">
+            {copied ? "Copied!" : "Copy to clipboard"}
+          </Tooltip>
+        }
+      >
+        <Button variant="dark" className={style} onClick={handleCopy}>
+          {copied ? <Check /> : <Copy />}
+        </Button>
+      </OverlayTrigger>
+    </>
+  );
+}
+
+export default CopyToClipboard;
