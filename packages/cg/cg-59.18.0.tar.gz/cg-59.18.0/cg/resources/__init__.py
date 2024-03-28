b50e2d103c7a1345c1f3ec917a77130e2f42d23c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-28 03:06:24 +0000

    Add package cg/59.18.0/cg-59.18.0.tar.gz

---
+++ packages/cg/cg-59.18.0.tar.gz/cg-59.18.0/cg/resources/__init__.py
@@ -0,0 +1,19 @@
+from pathlib import Path
+
+import pkg_resources
+
+VALID_INDEXES_FILE: str = Path("resources", "20181012_Indices.csv").as_posix()
+
+VALID_INDEXES_PATH: Path = Path(pkg_resources.resource_filename("cg", VALID_INDEXES_FILE))
+
+RNAFUSION_BUNDLE_FILENAMES: str = Path("resources", "rnafusion_bundle_filenames.csv").as_posix()
+
+TAXPROFILER_BUNDLE_FILENAMES: str = Path("resources", "taxprofiler_bundle_filenames.csv").as_posix()
+
+RNAFUSION_BUNDLE_FILENAMES_PATH: Path = Path(
+    pkg_resources.resource_filename("cg", RNAFUSION_BUNDLE_FILENAMES)
+)
+
+TAXPROFILER_BUNDLE_FILENAMES_PATH: Path = Path(
+    pkg_resources.resource_filename("cg", TAXPROFILER_BUNDLE_FILENAMES)
+)
