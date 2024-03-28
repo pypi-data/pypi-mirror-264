bdd0087bf0e9eb09a0444dfdc1326dddcec9a1e1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-28 06:05:31 +0000

    Add package homa/0.17/homa-0.17.tar.gz

---
+++ packages/homa/homa-0.17.tar.gz/homa-0.17/homa/repositories/RandomImageRepository.py
@@ -0,0 +1,2 @@
+class RandomImageRepository:
+    pass
---
+++ packages/homa/homa-0.17.tar.gz/homa-0.17/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup
+from setuptools import find_packages
+
+with open("README.md") as fh:
+    description = fh.read()
+
+setup(
+    name="homa",
+    version="0.17",
+    packages=find_packages(),
+    install_requires=[
+        #
+    ],
+    long_description=description,
+    long_description_content_type="text/markdown",
+    package_data={
+        "homa": ["./wordlists/*"]
+    }
+)
