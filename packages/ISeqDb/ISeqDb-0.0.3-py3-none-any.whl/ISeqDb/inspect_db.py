91464de2e09e80ad1e7af5b43b2f704ef5d9bb66
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-28 04:05:04 +0000

    Add package ISeqDb/0.0.3/ISeqDb-0.0.3-py3-none-any.whl

---
+++ packages/ISeqDb/ISeqDb-0.0.3-py3-none-any.whl/ISeqDb/inspect_db.py
@@ -0,0 +1,85 @@
+# ISeqDb - Identify Sequences in Databases
+# version 0.0.3
+# module inspect_db
+# Nico Salmaso, FEM, nico.salmaso@fmach.it
+
+import os
+import tarfile
+import sys
+
+
+def run(args):
+
+    print('_____________________________________')
+    print(' ')
+    print("Database:", args.targetdb)
+    print('')
+    print('_____________________________________')
+    print(' ')
+
+    dirtarget, file_name_db = os.path.split(args.targetdb)
+    if len(dirtarget) == 0:
+        print(" ")
+        print("db directory not indicated")
+        print(" ")
+        sys.exit()
+    if len(file_name_db) == 0:
+        print(" ")
+        print("database.tar.gz name not indicated")
+        print(" ")
+        sys.exit()
+
+    # open and extract database
+    datseqtar = tarfile.open(args.targetdb)
+    # extracting file
+    datseqtar.extractall(dirtarget)
+    datseqtar.close()
+
+    # remove extensions
+    datseq = args.targetdb.split('.')[0]
+    datseq_name = file_name_db.split('.')[0]
+
+    define_cmd_1: str = (
+        f'blastdbcmd -db {datseq} -entry all -outfmt %f'
+    )
+    os.system(define_cmd_1)
+
+    define_cmd_2: str = (
+        f'blastdbcmd -db {datseq} -info'
+    )
+    os.system(define_cmd_2)
+
+    if args.savedb != "nosave":
+        file_name = os.path.join(args.savedb, datseq_name + '.fasta')
+        file_name = str(file_name)
+        define_cmd_3: str = (
+            f'blastdbcmd -db {datseq} -entry all -outfmt %f > {file_name}'
+        )
+        os.system(define_cmd_3)
+        print(" ")
+        print("db saved: ", file_name)
+        print(" ")
+    else:
+        print(" ")
+        print("db not saved")
+        print(" ")
+
+    # cleaning
+    if os.path.isfile(datseq + '.njs'):
+        chartask = ".n"
+    elif os.path.isfile(datseq + '.pjs'):
+        chartask = ".p"
+    else:
+        print(" ")
+        print("unidentified molecule")
+        sys.exit()
+    os.remove("".join([datseq, chartask, 'db']))
+    os.remove("".join([datseq, chartask, 'hr']))
+    os.remove("".join([datseq, chartask, 'in']))
+    os.remove("".join([datseq, chartask, 'js']))
+    os.remove("".join([datseq, chartask, 'og']))
+    os.remove("".join([datseq, chartask, 'os']))
+    os.remove("".join([datseq, chartask, 'ot']))
+    os.remove("".join([datseq, chartask, 'sq']))
+    os.remove("".join([datseq, chartask, 'tf']))
+    os.remove("".join([datseq, chartask, 'to']))
