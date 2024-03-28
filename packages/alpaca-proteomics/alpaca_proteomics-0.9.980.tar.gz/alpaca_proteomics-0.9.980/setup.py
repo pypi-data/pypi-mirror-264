a2bf253658424bf39950fca71514a35d0dd614a0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-28 05:04:39 +0000

    Add package alpaca-proteomics/0.9.980/alpaca_proteomics-0.9.980.tar.gz

---
+++ packages/alpaca-proteomics/alpaca_proteomics-0.9.980.tar.gz/alpaca_proteomics-0.9.980/setup.py
@@ -0,0 +1,22 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='alpaca_proteomics',
+    packages=find_packages(include=['alpaca_proteomics']),
+    version='0.9.980',
+    description='Absolute Protein Quantification python library',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    author='Borja Ferrero Bordera',
+    author_email='b.ferrero@med.uni-muenchen.de',
+    project_urls={'Author':'https://www.linkedin.com/in/borjaferrero/'},
+    license='MIT',
+    install_requires= [
+            'matplotlib',
+            'numpy',
+            'pandas',
+            'scikit-learn',
+            'scipy',
+            'seaborn',
+            'XlsxWriter']
+)
