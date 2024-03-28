541f1773407ce8308c12310b7aaf411f5886ee8c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-28 14:05:20 +0000

    Add package train-route/0.4.0/train_route-0.4.0.tar.gz

---
+++ packages/train-route/train_route-0.4.0.tar.gz/train_route-0.4.0/train_route/rotation.py
@@ -0,0 +1,11 @@
+from math import radians, sin ,cos, atan2, degrees
+
+def spheric_rotation(x0, y0, x1, y1):
+    "assumes x, y is latitude, longitude"
+    lat1, lon1, lat2, lon2 = [radians(phi) for phi in [x0, y0, x1, y1]]
+    dLon = lon2 - lon1
+    x = sin(dLon) * cos(lat2)
+    y = cos(lat1) * sin(lat2) - sin(lat1) * cos(lat2) * cos(dLon)
+    initial_bearing = atan2(x, y)
+    initial_bearing_degrees = degrees(initial_bearing)
+    return initial_bearing_degrees
