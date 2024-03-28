7e39b1597ea4e57a8635f6984994323beed94f54
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-28 04:05:31 +0000

    Add package nxstools/3.67.0/nxstools-3.67.0.tar.gz

---
+++ packages/nxstools/nxstools-3.67.0.tar.gz/nxstools-3.67.0/.ci/install.sh
@@ -0,0 +1,72 @@
+#!/usr/bin/env bash
+
+# workaround for a bug in debian9, i.e. starting mysql hangs
+if [ "$1" = "debian11" ] || [ "$1" = "debian12" ]; then
+    docker exec --user root ndts service mariadb restart
+else
+    docker exec --user root ndts service mysql stop
+    if [ "$1" = "ubuntu20.04" ] || [ "$1" = "ubuntu20.10" ] || [ "$1" = "ubuntu21.04" ] || [ "$1" = "ubuntu22.04" ]|| [ "$1" = "ubuntu23.10" ]; then
+	docker exec --user root ndts /bin/bash -c 'usermod -d /var/lib/mysql/ mysql'
+    fi
+    docker exec --user root ndts service mysql start
+    # docker exec  --user root ndts /bin/bash -c '$(service mysql start &) && sleep 30'
+fi
+
+docker exec --user root ndts /bin/bash -c 'export DEBIAN_FRONTEND=noninteractive; apt-get -qq update; apt-get -qq install -y   tango-db tango-common; sleep 10'
+if [ "$?" != "0" ]; then exit 255; fi
+
+if [ "$1" = "ubuntu20.04" ] || [ "$1" = "ubuntu20.10" ] || [ "$1" = "ubuntu21.04" ] || [ "$1" = "ubuntu21.10" ] || [ "$1" = "ubuntu22.04" ] || [ "$1" = "ubuntu23.10" ]; then
+    # docker exec  --user tango ndts /bin/bash -c '/usr/lib/tango/DataBaseds 2 -ORBendPoint giop:tcp::10000  &'
+    docker exec  --user root ndts /bin/bash -c 'echo -e "[client]\nuser=root\npassword=rootpw" > /root/.my.cnf'
+    docker exec  --user root ndts /bin/bash -c 'echo -e "[client]\nuser=tango\nhost=127.0.0.1\npassword=rootpw" > /var/lib/tango/.my.cnf'
+fi
+docker exec  --user root ndts service tango-db restart
+if [ "$?" != "0" ]; then exit 255; fi
+
+docker exec  --user root ndts mkdir -p /tmp/runtime-tango
+docker exec  --user root ndts chown -R tango:tango /tmp/runtime-tango
+
+if [ "$1" = "debian9" ] ||  [ "$2" = "2" ] ; then
+    echo "start Xvfb :99 -screen 0 1024x768x24 &"
+    docker exec  --user root ndts /bin/bash -c 'export DISPLAY=":99.0"; Xvfb :99 -screen 0 1024x768x24 &'
+    if [ "$?" != "0" ]; then exit 255; fi
+fi
+
+echo "install tango servers"
+docker exec --user root ndts /bin/bash -c 'export DEBIAN_FRONTEND=noninteractive;  apt-get -qq update; apt-get -qq install -y  tango-starter tango-test'
+if [ "$?" != "0" ]; then exit 255; fi
+
+docker exec --user root ndts service tango-starter restart
+
+
+echo "install nxsconfigserver"
+docker exec --user root ndts /bin/sh -c 'export DEBIAN_FRONTEND=noninteractive;  apt-get -qq update; apt-get -qq install -y  nxsconfigserver-db; sleep 10'
+if [ "$?" != "0" ]; then exit 255; fi
+
+
+if [ "$2" = "2" ]; then
+    echo "install python-pytango ..."
+    docker exec --user root ndts /bin/sh -c 'apt-get -qq update; export DEBIAN_FRONTEND=noninteractive; apt-get -qq install -y  python-pytango python-nxswriter nxswriter python-nxsconfigserver nxsconfigserver'
+else
+    echo "install python3-pytango ..."
+    if [ "$1" = "debian10" ] || [ "$1" = "ubuntu20.04" ] || [ "$1" = "ubuntu22.04" ] || [ "$1" = "ubuntu23.10" ] || [ "$1" = "ubuntu20.10" ]  || [ "$1" = "debian11" ] || [ "$1" = "debian12" ] ; then
+	docker exec --user root ndts /bin/sh -c 'apt-get -qq update; export DEBIAN_FRONTEND=noninteractive; apt-get -qq install -y   python3-tango python3-nxswriter nxswriter python3-nxsconfigserver nxsconfigserver'
+    else
+	docker exec --user root ndts /bin/sh -c 'apt-get -qq update; export DEBIAN_FRONTEND=noninteractive; apt-get -qq install -y   python3-pytango python3-nxswriter nxswriter3 python3-nxsconfigserver nxsconfigserver3'
+    fi
+fi
+if [ "$?" != "0" ]; then exit 255; fi
+
+
+if [ "$2" = "2" ]; then
+    echo "install python-nxstools"
+    docker exec --user root ndts chown -R tango:tango .
+    docker exec  ndts python setup.py build
+    docker exec --user root ndts python setup.py  install
+else
+    echo "install python3-nxstools"
+    docker exec --user root ndts chown -R tango:tango .
+    docker exec  ndts python3 setup.py build
+    docker exec --user root ndts python3 setup.py  install
+fi
+if [ "$?" != "0" ]; then exit 255; fi
---
+++ packages/nxstools/nxstools-3.67.0.tar.gz/nxstools-3.67.0/doc/nxstools.rst
@@ -0,0 +1,131 @@
+nxstools package
+================
+
+Submodules
+----------
+
+nxstools.nxscollect module
+--------------------------
+       
+.. automodule:: nxstools.nxscollect
+    :members:
+    :undoc-members:
+    :show-inheritance:
+
+nxstools.nxsconfig module
+-------------------------
+
+.. automodule:: nxstools.nxsconfig
+    :members:
+    :undoc-members:
+    :show-inheritance:
+
+nxstools.nxscreate module
+-------------------------
+
+.. automodule:: nxstools.nxscreate
+    :members:
+    :undoc-members:
+    :show-inheritance:
+
+nxstools.nxsdata module
+-----------------------
+
+.. automodule:: nxstools.nxsdata
+    :members:
+    :undoc-members:
+    :show-inheritance:
+
+nxstools.nxsetup module
+-----------------------
+
+.. automodule:: nxstools.nxsetup
+    :members:
+    :undoc-members:
+    :show-inheritance:
+
+nxstools.nxscreator module
+--------------------------
+
+.. automodule:: nxstools.nxscreator
+    :members:
+    :undoc-members:
+    :show-inheritance:
+
+nxstools.nxsfileinfo module
+---------------------------
+
+.. automodule:: nxstools.nxsfileinfo
+    :members:
+    :undoc-members:
+    :show-inheritance:
+
+nxstools.nxsfileparser module
+-----------------------------
+
+.. automodule:: nxstools.nxsfileparser
+    :members:
+    :undoc-members:
+    :show-inheritance:
+
+nxstools.nxsargparser module
+----------------------------
+
+.. automodule:: nxstools.nxsargparser
+    :members:
+    :undoc-members:
+    :show-inheritance:
+
+nxstools.nxsparser module
+-------------------------
+
+.. automodule:: nxstools.nxsparser
+    :members:
+    :undoc-members:
+    :show-inheritance:
+
+nxstools.nxsxml module
+----------------------
+
+.. automodule:: nxstools.nxsxml
+    :members:
+    :undoc-members:
+    :show-inheritance:
+
+
+nxstools.nxsdevicetools module
+------------------------------
+
+.. automodule:: nxstools.nxsdevicetools
+    :members:
+    :undoc-members:
+    :show-inheritance:
+
+nxstools.filenamegenerator module
+---------------------------------
+
+.. automodule:: nxstools.filenamegenerator
+    :members:
+    :undoc-members:
+    :show-inheritance:
+
+nxstools.release module
+-----------------------
+
+.. automodule:: nxstools.release
+    :members:
+    :undoc-members:
+    :show-inheritance:
+
+Module contents
+---------------
+
+.. automodule:: nxstools
+    :members:
+    :undoc-members:
+    :show-inheritance:
+
+.. automodule:: nxstools.xmltemplates
+    :members:
+    :undoc-members:
+    :show-inheritance:
