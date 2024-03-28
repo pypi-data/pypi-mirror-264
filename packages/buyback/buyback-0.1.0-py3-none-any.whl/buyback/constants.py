bb06d43b03201d370a0851bfa5ae5e9b7089e1ac
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-28 03:05:55 +0000

    Add package buyback/0.1.0/buyback-0.1.0-py3-none-any.whl

---
+++ packages/buyback/buyback-0.1.0-py3-none-any.whl/buyback/constants.py
@@ -0,0 +1,19 @@
+HIGH_COL = "High"
+LOW_COL = "Low"
+CLOSE_COL = "Close"
+
+SHARES_COL = "Daily Shares"
+PRICE_COL = "Share Price"
+COST_COL = "Cost"
+CUM_COST_COL = "Cumulative Cost"
+CUM_SHARES_COL = "Cumulative Shares"
+REM_BUDGET_COL = "Remaining Budget"
+REM_SHARES_COL = "Remaining Shares"
+
+TYPICAL_PRICE = "Typical Price"
+
+WEEK = 5
+MOTNH = 21
+QUARTER = 63
+HALF_YEAR = 126
+YEAR = 252
