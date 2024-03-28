75a097098d38651ba3d4e3db7264c6cd16219723
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-28 03:07:12 +0000

    Add package mybitmlabprog/5/mybitmlabprog-5.tar.gz

---
+++ packages/mybitmlabprog/mybitmlabprog-5.tar.gz/mybitmlabprog-5/mybitmlabprog/__init__.py
@@ -0,0 +1,1 @@
+from .main import ai1,ai2,ai4,ai5,ai6,ai7,ai8,dbms1,dbms2,dbms3,dbms4,dbms5
---
+++ packages/mybitmlabprog/mybitmlabprog-5.tar.gz/mybitmlabprog-5/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup, find_packages
+
+setup(
+      name='mybitmlabprog',
+      version='5',
+      packages=find_packages(),
+      install_requires=[],    )
