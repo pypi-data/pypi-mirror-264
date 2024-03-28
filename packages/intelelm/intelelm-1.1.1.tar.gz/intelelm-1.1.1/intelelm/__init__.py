7b433ec98c615e01efc43982a75f8d5015707c06
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-28 03:06:10 +0000

    Add package intelelm/1.1.1/intelelm-1.1.1.tar.gz

---
+++ packages/intelelm/intelelm-1.1.1.tar.gz/intelelm-1.1.1/intelelm/__init__.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+# Created by "Thieu" at 15:23, 10/08/2023 ----------%
+#       Email: nguyenthieu2102@gmail.com            %                                                    
+#       Github: https://github.com/thieu1995        %                         
+# --------------------------------------------------%
+
+__version__ = "1.1.1"
+
+from intelelm.utils.scaler import DataTransformer
+from intelelm.utils.data_loader import Data, get_dataset
+from intelelm.model.mha_elm import MhaElmRegressor, MhaElmClassifier
+from intelelm.model.standard_elm import ElmRegressor, ElmClassifier
