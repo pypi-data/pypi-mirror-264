ec6f4c791f7b31751679c0cb1bbe9b2d764202f6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-28 05:05:14 +0000

    Add package mltemp/0.2/mltemp-0.2.tar.gz

---
+++ packages/mltemp/mltemp-0.2.tar.gz/mltemp-0.2/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='mltemp',
+    version='0.2',
+    packages=find_packages(),
+    entry_points={
+        'console_scripts': [
+            'mltemp=mltemplate:main',
+        ],
+    },
+)
