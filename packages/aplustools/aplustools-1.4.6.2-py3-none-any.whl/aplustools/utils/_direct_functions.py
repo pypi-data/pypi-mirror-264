0a70cb2b533a752d902b4bcce0901417dae3d784
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-28 05:04:48 +0000

    Add package aplustools/1.4.6.2/aplustools-1.4.6.2-py3-none-any.whl

---
+++ packages/aplustools/aplustools-1.4.6.2-py3-none-any.whl/aplustools/utils/_direct_functions.py
@@ -0,0 +1,25 @@
+import subprocess
+import sys
+
+
+def install_dependencies():
+    def execute_python_command(arguments: list = None, *args, **kwargs) -> subprocess.CompletedProcess[str]:
+        if arguments is None:
+            arguments = []
+        print(' '.join([sys.executable] + arguments))
+        # Added to remain consistent with executing in same python environment
+        return subprocess.run([sys.executable] + arguments, *args, **kwargs)
+    for dep in ["Pillow==10.2.0",
+                "rich==13.7.0",
+                "pycryptodome==3.20.0",
+                "brotli==1.1.0",
+                "zstandard==0.22.0",
+                "py7zr==0.21.0"]:
+        try:
+            proc = execute_python_command(arguments=["-m", "pip", "install", dep])
+            if proc.returncode != 0:
+                raise
+        except Exception as e:
+            print("An error occurred:" + str(e))
+
+    print("Done, all possible dependencies for the utils module installed ...")
