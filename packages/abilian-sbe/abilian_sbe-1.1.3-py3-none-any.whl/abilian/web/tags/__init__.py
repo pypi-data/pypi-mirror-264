2d513c31d0b44c989bbab43833d8cc53b53daecc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-28 03:05:49 +0000

    Add package abilian-sbe/1.1.3/abilian_sbe-1.1.3-py3-none-any.whl

---
+++ packages/abilian-sbe/abilian_sbe-1.1.3-py3-none-any.whl/abilian/web/tags/__init__.py
@@ -0,0 +1,14 @@
+""""""
+
+from __future__ import annotations
+
+from flask import Flask
+
+from .criterion import TagCriterion
+from .extension import TagsExtension
+
+__all__ = ["TagCriterion", "TagsExtension"]
+
+
+def register_plugin(app: Flask):
+    TagsExtension(app)
