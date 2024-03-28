20e6303ae4fd55ac45a8146f89ebd13c3c92917f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-28 03:06:05 +0000

    Add package geo-api-gouv-fr/1.7.1/geo_api_gouv_fr-1.7.1.tar.gz

---
+++ packages/geo-api-gouv-fr/geo_api_gouv_fr-1.7.1.tar.gz/geo_api_gouv_fr-1.7.1/geo_api_gouv_fr/tests/test_cache.py
@@ -0,0 +1,27 @@
+import os
+import time
+from unittest import TestCase
+
+import requests
+
+from .. import RegionApi
+
+WAIT_TIME = 0.2
+
+
+class TestRegion(TestCase):
+    def setUp(self) -> None:
+        self.api = RegionApi()
+        return super().setUp()
+
+    def test_regions(self) -> requests.Response:
+        time.sleep(WAIT_TIME)
+
+        if not os.environ.get("REQUEST_CACHE"):
+            # deactivate test if locally defined sqlite or
+
+            r = self.api.regions(limit=5)
+            assert not r.from_cache
+
+        r = self.api.regions(limit=5)
+        assert r.from_cache
