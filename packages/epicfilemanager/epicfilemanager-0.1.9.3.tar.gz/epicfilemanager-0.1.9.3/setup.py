7da9d4f99ccb58f0bdc0be6822ec5defcbbc01e3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-28 06:04:59 +0000

    Add package epicfilemanager/0.1.9.3/epicfilemanager-0.1.9.3.tar.gz

---
+++ packages/epicfilemanager/epicfilemanager-0.1.9.3.tar.gz/epicfilemanager-0.1.9.3/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+setup(
+name='epicfilemanager',
+version='0.1.9.3',
+author='Epic099',
+author_email='',
+description='File manager',
+packages=find_packages(),
+classifiers=[
+'Programming Language :: Python :: 3',
+'License :: OSI Approved :: MIT License',
+'Operating System :: OS Independent',
+],
+python_requires='>=3.6',
+)
