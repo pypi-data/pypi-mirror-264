ee10e385e3511a436f3e6f2bc407a190aa9a674f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-28 03:06:35 +0000

    Add package ns-asphalt9/3.0.0/ns_asphalt9-3.0.0.tar.gz

---
+++ packages/ns-asphalt9/ns_asphalt9-3.0.0.tar.gz/ns_asphalt9-3.0.0/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup
+
+setup(
+    name="ns_asphalt9",
+    include_package_data=True,
+    long_description_content_type="text/markdown",
+    install_requires=[
+        "beautifulsoup4==4.11.2",
+        "customtkinter==5.2.0",
+        "nxbt==0.1.4",
+        "Pillow==9.5.0",
+        "pytesseract==0.3.10",
+        "Requests==2.31.0",
+    ],
+    extra_require={"dev": ["pytest"]},
+)
