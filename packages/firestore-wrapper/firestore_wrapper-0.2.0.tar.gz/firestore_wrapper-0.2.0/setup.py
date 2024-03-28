48e2efba518d028176cf974a68e71b0fabb5c46c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-28 04:04:58 +0000

    Add package firestore-wrapper/0.2.0/firestore_wrapper-0.2.0.tar.gz

---
+++ packages/firestore-wrapper/firestore_wrapper-0.2.0.tar.gz/firestore_wrapper-0.2.0/setup.py
@@ -0,0 +1,23 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='firestore_wrapper',
+    version='0.2.0',
+    packages=find_packages(),
+    description='A custom wrapper for Google Firestore.',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    author='Antonio Ventilii',
+    author_email='antonioventilii@gmail.com',
+    license='MIT',
+    install_requires=[
+        'google-cloud-firestore>=2.1.0',
+        'schema',
+    ],
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.7',
+)
