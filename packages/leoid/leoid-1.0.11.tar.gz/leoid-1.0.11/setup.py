ca20841f4b2dfe948ea0f50819cb3087806a4173
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-29 14:07:02 +0000

    Add package leoid/1.0.11/leoid-1.0.11.tar.gz

---
+++ packages/leoid/leoid-1.0.11.tar.gz/leoid-1.0.11/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup
+
+setup(
+    name = 'leoid',
+    version = '1.0.11',
+    author = 'Jay Ticku',
+    description = 'LEOID Python Package',
+    long_descrition =
+                    """## Python Package for Machine Learning Model LEOID (Level-based Ensemble for Overcoming Inbalanced Data)
+                    \nMore information about LEOID and example usage is availabe on Kaggle.
+                    \n<www.kaggle.com/models/jayticku/leoid>""",
+    long_description_content_type='text/markdown',
+    install_requires = [
+        'imbalanced-learn==0.12.0'
+    ],
+    classifiers=[
+    'Development Status :: 5 - Production/Stable',
+    'Programming Language :: Python :: 3',
+    'License :: OSI Approved :: Apache Software License',
+    'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.8',
+    include_package_data = True
+)
