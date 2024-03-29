f5d2d6bd05b1286a882e9a2f35431cc6aafd56a4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-29 03:06:30 +0000

    Add package cloudicorn-cli/0.2/cloudicorn-cli-0.2.tar.gz

---
+++ packages/cloudicorn-cli/cloudicorn-cli-0.2.tar.gz/cloudicorn-cli-0.2/setup.py
@@ -0,0 +1,52 @@
+#!/usr/bin/python3
+
+# -*- coding: utf-8 -*-
+
+import setuptools, re
+from setuptools import setup
+
+url = 'https://github.com/jumidev/cloudicorn-cli'
+
+md_regex = r"\[([^\[]+)\](\(.*\))"
+
+
+with open("../README.md", "r") as fh:
+    long_description = fh.read()
+    
+
+# convert links in readme to absolute paths
+matches = re.finditer(md_regex, long_description, re.MULTILINE)
+
+for matchNum, match in enumerate(matches, start=1):     
+    replace = '('+ url + '/' + match.groups(1)[1][1:-1] + ')'
+    long_description = long_description.replace(match.groups(1)[1], replace)
+
+
+with open("requirements.txt", "r") as fh:
+    install_requires = fh.readlines()
+
+setup(name='cloudicorn-cli',
+    version='0.2',
+    description='Taking Infrastructure As Code to the next level',
+    long_description=long_description,
+    long_description_content_type="text/markdown",      
+    url=url,
+    author='krezreb',
+    author_email='josephbeeson@gmail.com',
+    license='MIT',
+    packages=["."],
+    zip_safe=False,
+    install_requires=install_requires,
+    entry_points = {
+              'console_scripts': [
+                  'cloudicorn=cloudicorn:cli_entrypoint',              
+                  'cloudicorn_setup=cloudicorn_setup:cli_entrypoint'              
+                ]     
+          },
+
+    )
+
+
+
+
+
