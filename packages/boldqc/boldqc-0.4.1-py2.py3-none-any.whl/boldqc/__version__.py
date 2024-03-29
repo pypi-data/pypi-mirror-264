9e1e04cc1d50902db2522412f39377576ec66dca
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-29 06:05:23 +0000

    Add package boldqc/0.4.1/boldqc-0.4.1-py2.py3-none-any.whl

---
+++ packages/boldqc/boldqc-0.4.1-py2.py3-none-any.whl/boldqc/__version__.py
@@ -0,0 +1,6 @@
+__title__ = 'boldqc'
+__description__ = 'BOLD QC'
+__url__ = 'https://github.com/harvard-nrg/boldqc'
+__version__ = '0.4.1'
+__author__ = 'Neuroinformatics Research Group'
+__author_email__ = 'info@neuroinfo.org'
