ca93ea7ab85550f26c83246a2497b3340bdcd8be
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-29 03:06:44 +0000

    Add package LnkAnalyser/0.0.1/LnkAnalyser-0.0.1.tar.gz

---
+++ packages/LnkAnalyser/LnkAnalyser-0.0.1.tar.gz/LnkAnalyser-0.0.1/tests/test_LnkAnalyser.py
@@ -0,0 +1,10 @@
+import unittest
+from lnkanalyser import lnkanalyser
+
+class TestLnkAnalyser(unittest.TestCase):
+
+    def test_Init(self):
+        self.assertEqual('foo'.upper(), 'FOO')
+
+    def test_shortcut(self):
+        shortcut = lnkanalyser.go("Desktop.lnk")
