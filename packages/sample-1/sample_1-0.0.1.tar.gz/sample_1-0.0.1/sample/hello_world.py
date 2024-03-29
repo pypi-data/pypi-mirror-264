b36f08bc99d635215efe91ea38a2cdcb17219a23
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-29 05:05:10 +0000

    Add package sample-1/0.0.1/sample_1-0.0.1.tar.gz

---
+++ packages/sample-1/sample_1-0.0.1.tar.gz/sample_1-0.0.1/sample/hello_world.py
@@ -0,0 +1,6 @@
+class HelloWorld:
+    def __init__(self) -> None:
+        pass
+
+    def say_hello(self):
+        return "Hello World!!!"
---
+++ packages/sample-1/sample_1-0.0.1.tar.gz/sample_1-0.0.1/setup.py
@@ -0,0 +1,15 @@
+import pathlib
+from setuptools import setup, find_packages
+
+README = (pathlib.Path(__file__).parent / "README.md").read_text()
+
+setup(
+    name="sample_1",
+    version="0.0.1",
+    description="A user-friendly wrapper for the Kite Connect library",
+    long_description=README,
+    url="https://github.com/AST-LW/kite_connect_lite",
+    packages=find_packages(exclude=["venv", "tests"]),
+    install_requires=["kiteconnect", "pandas"],
+    include_package_data=True,
+)
