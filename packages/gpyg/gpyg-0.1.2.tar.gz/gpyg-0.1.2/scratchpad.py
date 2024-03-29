a52d0f6d6aab5897b5f93a3280bafeaf9f114f14
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-29 04:06:05 +0000

    Add package gpyg/0.1.2/gpyg-0.1.2.tar.gz

---
+++ packages/gpyg/gpyg-0.1.2.tar.gz/gpyg-0.1.2/scratchpad.py
@@ -0,0 +1,29 @@
+import os
+import shutil
+from tempfile import TemporaryDirectory
+import time
+from gpyg import GPG, StatusInteractive, ProcessSession
+
+if os.path.exists("./tmp"):
+    shutil.rmtree("./tmp")
+
+os.makedirs("./tmp", exist_ok=True)
+with TemporaryDirectory(dir="tmp", delete=False) as tmpdir:
+    gpg = GPG(homedir=tmpdir, kill_existing_agent=True)
+    with gpg.smart_card() as card:
+        print(card.active)
+        if card.active:
+            card.reset()
+            card.set_usage_info("decrypt", True, "12345678")
+            # print(card.active.model_dump_json(indent=4))
+
+    """gpg = GPG(homedir=tmpdir, kill_existing_agent=True)
+    key = gpg.keys.generate_key("Bongus", passphrase="test")
+    key_other = gpg.keys.generate_key("Bingus", passphrase="test")
+
+    print("BONGUS:", key.fingerprint)
+    print("BINGUS:", key_other.fingerprint)
+    with key.edit(passphrase="test", run_as=key_other.fingerprint) as editor:
+        print(editor.sign())
+
+    key.reload()"""
