fab58a894316787d0d30eeef7f323629481d24cb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-29 06:05:41 +0000

    Add package laser-learning-environment/0.2.0/laser_learning_environment-0.2.0-cp310-cp310-manylinux_2_17_aarch64.manylinux2014_aarch64.whl

---
+++ packages/laser-learning-environment/laser_learning_environment-0.2.0-cp310-cp310-manylinux_2_17_aarch64.manylinux2014_aarch64.whl/lle/types.py
@@ -0,0 +1,13 @@
+Position = tuple[int, int]
+"""
+Represents a position (i, j) in the gridworld.
+
+This is a semantic type wrapper around tuple[int, int].
+"""
+
+AgentId = int
+"""
+The integer identifier of an agent.
+
+This is a semantic type wrapper around int.
+"""
