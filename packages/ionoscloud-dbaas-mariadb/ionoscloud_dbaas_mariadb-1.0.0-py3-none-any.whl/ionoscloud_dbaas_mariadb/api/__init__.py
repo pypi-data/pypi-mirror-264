359ac4cf6a9b96f8237fae77ded276f0660582ca
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-29 03:06:07 +0000

    Add package ionoscloud-dbaas-mariadb/1.0.0/ionoscloud_dbaas_mariadb-1.0.0-py3-none-any.whl

---
+++ packages/ionoscloud-dbaas-mariadb/ionoscloud_dbaas_mariadb-1.0.0-py3-none-any.whl/ionoscloud_dbaas_mariadb/api/__init__.py
@@ -0,0 +1,6 @@
+# flake8: noqa
+
+# import apis into api package
+from ionoscloud_dbaas_mariadb.api.backups_api import BackupsApi
+from ionoscloud_dbaas_mariadb.api.clusters_api import ClustersApi
+
