7a9bf63baafaebb5b9f0a1521b3c9d16f50c5796
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-29 04:05:00 +0000

    Add package spai/2024.3.28/spai-2024.3.28.tar.gz

---
+++ packages/spai/spai-2024.3.28.tar.gz/spai-2024.3.28/spai/data/satellite/__init__.py
@@ -0,0 +1,2 @@
+from .download import download_satellite_image, download_dem, download_cloud_mask
+from .explore import explore_satellite_images
---
+++ packages/spai/spai-2024.3.28.tar.gz/spai-2024.3.28/spai/storage/decorators.py
@@ -0,0 +1,40 @@
+def with_rio(func):
+    def wrapper(self, *args, **kwargs):
+        try:
+            import rasterio as rio
+
+            return func(self, rio, *args, **kwargs)
+        except ImportError:
+            raise ImportError(
+                "The rasterio package is required for this function. Please install it with 'pip install rasterio' and try again."
+            )
+
+    return wrapper
+
+
+def with_xarray(func):
+    def wrapper(self, *args, **kwargs):
+        try:
+            import xarray as xr
+
+            return func(self, xr, *args, **kwargs)
+        except ImportError:
+            raise ImportError(
+                "The xarray package is required for this function. Please install it with 'pip install xarray' and try again."
+            )
+
+    return wrapper
+
+
+def with_geopandas(func):
+    def wrapper(self, *args, **kwargs):
+        try:
+            import geopandas as gpd
+
+            return func(self, gpd, *args, **kwargs)
+        except ImportError:
+            raise ImportError(
+                "The geopandas package is required for this function. Please install it with 'pip install geopandas' and try again."
+            )
+
+    return wrapper
