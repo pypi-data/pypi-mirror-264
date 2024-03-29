39af298858ed86a615d6598ffa14f116b254d5fc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-29 06:06:25 +0000

    Add package cellxgene-census/1.12.0/cellxgene-census-1.12.0.tar.gz

---
+++ packages/cellxgene-census/cellxgene-census-1.12.0.tar.gz/cellxgene-census-1.12.0/src/cellxgene_census/experimental/__init__.py
@@ -0,0 +1,8 @@
+"""Experimental API for the CELLxGENE Discover Census."""
+
+from ._embedding import get_embedding, get_embedding_metadata
+
+__all__ = [
+    "get_embedding",
+    "get_embedding_metadata",
+]
