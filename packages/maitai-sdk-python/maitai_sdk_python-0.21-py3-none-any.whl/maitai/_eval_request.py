9d79d31e81947c2e4b5240523a9aa446be0f6e12
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-29 03:06:47 +0000

    Add package maitai-sdk-python/0.21/maitai_sdk_python-0.21-py3-none-any.whl

---
+++ packages/maitai-sdk-python/maitai_sdk_python-0.21-py3-none-any.whl/maitai/_eval_request.py
@@ -0,0 +1,23 @@
+from json import JSONEncoder
+
+from maitai._loadable import Loadable
+
+
+class EvalRequest(Loadable):
+
+    def __init__(self):
+        super().__init__()
+        self.id = -1
+        self.date_created = -1
+        self.application_id = -1
+        self.session_id = -1
+        self.evaluation_content_type = ''
+        self.evaluation_content = ''
+
+
+class EvalRequestEncoder(JSONEncoder):
+    def default(self, o):
+        try:
+            return o.__dict__
+        except Exception as e:
+            return o
