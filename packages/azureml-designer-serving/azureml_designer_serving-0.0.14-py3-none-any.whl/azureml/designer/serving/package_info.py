2173b002a91b1952d8c76a135242c2e6744ad162
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-29 06:06:26 +0000

    Add package azureml-designer-serving/0.0.14/azureml_designer_serving-0.0.14-py3-none-any.whl

---
+++ packages/azureml-designer-serving/azureml_designer_serving-0.0.14-py3-none-any.whl/azureml/designer/serving/package_info.py
@@ -0,0 +1,4 @@
+VERSION = '0.0.14'
+PACKAGE_NAME = 'azureml-designer-serving'
+DESCRIPTION = 'Provide functionalities to invoke Azure Machine Learning designer ' \
+              'built-in modules in deployment service.'
