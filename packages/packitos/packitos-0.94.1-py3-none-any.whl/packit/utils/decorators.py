8edf2546f5a27cf8cd4a59dd6914812400f1e924
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-29 04:05:22 +0000

    Add package packitos/0.94.1/packitos-0.94.1-py3-none-any.whl

---
+++ packages/packitos/packitos-0.94.1-py3-none-any.whl/packit/utils/decorators.py
@@ -0,0 +1,40 @@
+# Copyright Contributors to the Packit project.
+# SPDX-License-Identifier: MIT
+
+import functools
+import logging
+from typing import Any, Callable, Union
+
+logger = logging.getLogger(__name__)
+
+
+def fallback_return_value(
+    fallback_value: Any,
+    exceptions: Union[type[Exception], tuple[type[Exception]]] = Exception,
+) -> Any:
+    """
+    The function of this decorator is to return some fallback value in case an exception was raised
+    during a function call.
+
+    :param fallback_value: a value which will be returned if a specified exception was raised
+    :param exceptions: exception(s) which should be handled, by default all exceptions are caught
+    :return: func original return value if no exception was raised
+            fallback_return_value if a specified exception was caught
+    """
+
+    def decorator(func: Callable) -> Callable:
+        @functools.wraps(func)
+        def inner(*args: Any, **kwargs: Any) -> Any:
+            try:
+                return func(*args, **kwargs)
+            except exceptions as e:
+                logger.warning(
+                    f"Fallback return value used while calling {func.__name__} because of "
+                    f"{type(e).__name__}"
+                    f"{': ' + str(e) if str(e) else ''} ",
+                )
+                return fallback_value
+
+        return inner
+
+    return decorator
