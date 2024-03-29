424382b59e6a0e9e5c5cc732a8fb43cf17d247fb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-29 05:05:27 +0000

    Add package typedparser/0.11.7/typedparser-0.11.7.tar.gz

---
+++ packages/typedparser/typedparser-0.11.7.tar.gz/typedparser-0.11.7/src/typedparser/__init__.py
@@ -0,0 +1,32 @@
+from attrs import define as attrs_define
+
+from ._typedattr import definenumpy, attrs_from_dict, NamedTupleMixin
+from ._typedparser import add_argument, TypedParser, VerboseQuietArgs
+from .custom_format import CustomArgparseFmt
+from .objects import get_attr_names
+
+__all__ = [
+    "definenumpy",
+    "attrs_from_dict",
+    "NamedTupleMixin",
+    "add_argument",
+    "TypedParser",
+    "VerboseQuietArgs",
+    "CustomArgparseFmt",
+    "get_attr_names",
+]
+
+
+def define(*args, **kwargs):
+    print(
+        f"Importing define from typedparser is deprecated, please import from attrs directly. "
+        f"This import will stop working in the future."
+    )
+    return attrs_define(
+        *args,
+        **kwargs,
+    )
+
+
+__version__ = "0.11.7"
+
