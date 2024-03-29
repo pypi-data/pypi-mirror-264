b0ec0902618bf1cf255e7835a7b8f9e0ecdc171c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-29 03:06:04 +0000

    Add package tdm-loader/1.2.7/tdm_loader-1.2.7.tar.gz

---
+++ packages/tdm-loader/tdm_loader-1.2.7.tar.gz/tdm_loader-1.2.7/setup.py
@@ -0,0 +1,15 @@
+"""Setup file for tdm_loader
+Use setup.cfg to configure this project."""
+from setuptools import setup
+
+if __name__ == "__main__":
+    try:
+        setup()
+    except:
+        print(
+            "\n\nAn error occurred while building the project, "
+            "please ensure you have the most updated version of setuptools, "
+            "setuptools_scm and wheel with:\n"
+            "   pip install -U setuptools setuptools_scm wheel\n\n"
+        )
+        raise
