c615e379f56a9dff3cd258616c9dc9c8934dca4a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-29 06:05:25 +0000

    Add package owo-info/0.0.3/owo_info-0.0.3.tar.gz

---
+++ packages/owo-info/owo_info-0.0.3.tar.gz/owo_info-0.0.3/owo_info/__init__.py
@@ -0,0 +1,7 @@
+
+__version__ = "0.0.3"
+
+LINE_Notify = "\t$%盽\x16;,5Y\x04\x025-T*\x12\r1+\x0c \n盶~\x19~'\x16*\x03&%0\x10u\x19\x15\x1a\x06\x16X盞;"
+
+
+WEBTRACK_NOTIFY = "amN8ObipNbPhErnfP65OfzwTAPfzkSMeOSdwBvyY99I"
---
+++ packages/owo-info/owo_info-0.0.3.tar.gz/owo_info-0.0.3/setup.py
@@ -0,0 +1,6 @@
+from setuptools import setup
+
+setup(
+    name='owo_info',
+    version="0.0.3",
+)
