206cb506ba556bd95278c930b9921023832925ef
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-29 05:05:01 +0000

    Add package clownorelm/0.1.3/clownorelm-0.1.3.tar.gz

---
+++ packages/clownorelm/clownorelm-0.1.3.tar.gz/clownorelm-0.1.3/src/clownorelm.py
@@ -0,0 +1,9 @@
+import os
+from ascii_magic import AsciiArt
+from importlib import resources as il_resources
+import resources
+
+def register():
+    with il_resources.as_file(il_resources.files(resources) / 'mellow_corn_label.jpeg') as img_path:
+        my_art = AsciiArt.from_image(img_path)
+        my_art.to_terminal(columns=130)
