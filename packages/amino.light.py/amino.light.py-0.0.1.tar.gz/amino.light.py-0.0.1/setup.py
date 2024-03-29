946f6613a0e6af0b0f00ea810442e8fd61a3443d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-29 14:05:55 +0000

    Add package amino.light.py/0.0.1/amino.light.py-0.0.1.tar.gz

---
+++ packages/amino.light.py/amino.light.py-0.0.1.tar.gz/amino.light.py-0.0.1/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name="amino.light.py",
+    version="0.0.1",
+    description="Best Amino.py alternative",
+    author="AugustLight",
+    packages=["AminoLightPy"],
+    install_requires=["requests", "msgspec"]
+)
