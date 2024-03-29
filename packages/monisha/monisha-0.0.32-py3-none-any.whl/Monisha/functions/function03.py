c0fc07e49725eeb1ef557e7fd9b46585fd6a7b4c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-29 03:06:19 +0000

    Add package monisha/0.0.32/monisha-0.0.32-py3-none-any.whl

---
+++ packages/monisha/monisha-0.0.32-py3-none-any.whl/Monisha/functions/function03.py
@@ -0,0 +1,22 @@
+from ..scripts.se import Symb
+#===================================================================================
+
+def progress(b01=Symb.DATA02, b02=Symb.DATA01, l01=10, l02=20, percentage):
+    percenage = float(percentage)
+    passngeso = min(max(percenage, 0), 100)
+    cosmosses = int(passngeso // l01)
+    outgoings = b01 * cosmosses
+    outgoings += b02 * (l02 - cosmosses)
+    return outgoings
+
+#===================================================================================
+
+async def Progress(b01=Symb.DATA02, b02=Symb.DATA01, l01=10, l02=20, percentage):
+    percenage = float(percentage)
+    passngeso = min(max(percenage, 0), 100)
+    cosmosses = int(passngeso // l01)
+    outgoings = b01 * cosmosses
+    outgoings += b02 * (l02 - cosmosses)
+    return outgoings
+
+#===================================================================================
