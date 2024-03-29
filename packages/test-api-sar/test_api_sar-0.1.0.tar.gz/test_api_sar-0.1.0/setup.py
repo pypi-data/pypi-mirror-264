fbf0fc5a51df845f825ac121182a56c1b90c6f92
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-29 04:05:27 +0000

    Add package test-api-sar/0.1.0/test_api_sar-0.1.0.tar.gz

---
+++ packages/test-api-sar/test_api_sar-0.1.0.tar.gz/test_api_sar-0.1.0/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='test_api_sar',  # This is the package name as you want it to appear on PyPI.
+    version='0.1.0',
+    packages=find_packages(),
+    description='A brief description of what your package does.',
+    long_description_content_type='text/markdown',
+    author='Your Name',
+    author_email='your.email@example.com',
+    # More metadata like license, classifiers, etc.
+)
