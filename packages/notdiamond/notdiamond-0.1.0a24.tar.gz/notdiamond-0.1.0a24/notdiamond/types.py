c230e334102431e0dad586b766c44387a54f045b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-29 14:05:31 +0000

    Add package notdiamond/0.1.0a24/notdiamond-0.1.0a24.tar.gz

---
+++ packages/notdiamond/notdiamond-0.1.0a24.tar.gz/notdiamond-0.1.0a24/notdiamond/types.py
@@ -0,0 +1,42 @@
+from pydantic import BaseModel, field_validator
+from typing import Dict, List, TypedDict
+
+from notdiamond.exceptions import MissingApiKey, InvalidApiKey
+
+
+class NDApiKeyValidator(BaseModel):
+    api_key: str
+
+    @field_validator('api_key', mode='before')
+    @classmethod
+    def api_key_must_be_a_string(cls, v) -> str:
+        if type(v) is not str:
+            raise InvalidApiKey("ND API key should be a string")
+        return v
+
+    @field_validator('api_key', mode="after")
+    @classmethod
+    def string_must_not_be_empty(cls, v):
+        if len(v) == 0:
+            raise MissingApiKey("ND API key should be longer than 0")
+        return v
+
+
+class RequestProvider(TypedDict):
+    provider: str
+    model: str
+
+
+class ModelSelectRequestPayload(BaseModel):
+    prompt_template: str
+    formatted_prompt: str
+    components: Dict[str, Dict]
+    llm_providers: List[Dict]
+    metric: str
+    max_model_depth: int
+
+
+class FeedbackRequestPayload(BaseModel):
+    session_id: str
+    provider: RequestProvider
+    feedback: Dict[str, int]
