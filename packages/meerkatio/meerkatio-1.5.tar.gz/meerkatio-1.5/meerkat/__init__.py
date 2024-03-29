1819c7299d3cdd0f8daaa77160f008f237cdb73b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-29 14:05:26 +0000

    Add package meerkatio/1.5/meerkatio-1.5.tar.gz

---
+++ packages/meerkatio/meerkatio-1.5.tar.gz/meerkatio-1.5/meerkat/__init__.py
@@ -0,0 +1,41 @@
+import os
+import time
+from playsound import playsound
+
+from meerkat.api import send_meerkat_notification, get_user_token
+
+module_runtime = int(time.time()*1000)
+
+if not os.environ.get("MEERKAT_TOKEN"):
+    try:
+        with open(os.path.expanduser("~") + "/.meerkat") as file:
+            os.environ["MEERKAT_TOKEN"] = file.read()
+    except Exception as e:
+        print(e)
+        pass
+
+#
+# User Management Functions
+#
+def login(email: str, password: str) -> bool:
+    token = get_user_token(email, password)
+
+    if not token:
+        print("Invalid Meerkat Token!")
+        return False
+
+    os.environ["MEERKAT_TOKEN"] = token
+    return True
+
+#
+# Notification Functions
+#
+def email(token=None, message=""):
+    if token == None:
+        token = os.environ.get("MEERKAT_TOKEN")
+
+    if not token:
+        print("No Meerkat token found in the environment")
+        return
+    
+    return send_meerkat_notification("email", token, message)
---
+++ packages/meerkatio/meerkatio-1.5.tar.gz/meerkatio-1.5/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='meerkatio',
+    version='1.5',
+    packages=find_packages(),
+    package_data={'meerkat': ['ping_sounds/*.mp3']},
+    include_package_data=True,
+    install_requires=[
+        "requests",
+        "click"
+    ],
+    entry_points='''
+        [console_scripts]
+        meerkat=meerkat.cli:meerkat
+    ''',
+    author="meerkat.io",
+    description="Simple notification tool for multi-tasking developers"
+    # Add other metadata such as author, author_email, description, etc.
+)
