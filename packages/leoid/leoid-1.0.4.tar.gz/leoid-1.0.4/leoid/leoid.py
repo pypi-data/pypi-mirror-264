203be45929bec0a04860ae5abec10c47f2fa443b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-29 06:05:22 +0000

    Add package leoid/1.0.4/leoid-1.0.4.tar.gz

---
+++ packages/leoid/leoid-1.0.4.tar.gz/leoid-1.0.4/leoid/leoid.py
@@ -0,0 +1,32 @@
+import pickle
+import numpy as np
+import os
+
+class LEOID():
+    """Level-based Ensemble for Overcoming Inbalanced Data"""
+
+    def __init__(self):
+        with open("leoid/leoid/stage_1.pkl", "rb") as f:
+            self.model_1 = pickle.load(f)
+
+        with open("leoid/leoid/stage_2.pkl, "rb") as f:
+            self.model_2 = pickle.load(f)
+    
+    def predict(self, X):
+        X = np.array(X)
+        if X.ndim == 1:
+            X = X.reshape(1, -1)
+            
+        model_1_predictions = self.model_1.predict(X)
+        predictions = []
+        for index, model_1_prediction in enumerate(model_1_predictions):
+            if model_1_prediction == 1:
+                model_2_prediction = self.model_2.predict(X[index].reshape(1, -1))[0]
+                if model_2_prediction == 1:
+                    predictions.append(2)
+                else:
+                    predictions.append(1)
+            else:
+                predictions.append(model_1_prediction)
+                
+        return np.array(predictions)
