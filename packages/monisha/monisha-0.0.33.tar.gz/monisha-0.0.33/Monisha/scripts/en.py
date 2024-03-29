3cdd91599ca0b979efd75b629100c396dfe7ebff
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-29 04:04:58 +0000

    Add package monisha/0.0.33/monisha-0.0.33.tar.gz

---
+++ packages/monisha/monisha-0.0.33.tar.gz/monisha-0.0.33/Monisha/scripts/en.py
@@ -0,0 +1,11 @@
+
+class Scripted(object):
+    DATA01 = ""
+    DATA02 = " "
+    DATA03 = "ㅤ"
+    DATA04 = "--"
+    DATA05 = "NA"
+    DATA06 = "0𝚜𝚎𝚌"
+    DATA07 = "0sec"
+    DATA08 = "0 𝙱𝚢𝚝𝚎𝚜"
+    DATA09 = "0 Bytes"
