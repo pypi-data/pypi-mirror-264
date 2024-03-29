4d0ba7a32afb1cf75aa6340be1d011fdbf5d3fd9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-29 06:05:42 +0000

    Add package itkdb-gtk/0.0.18/itkdb_gtk-0.0.18-py3-none-any.whl

---
+++ packages/itkdb-gtk/itkdb_gtk-0.0.18-py3-none-any.whl/itkdb_gtk/__init__.py
@@ -0,0 +1,52 @@
+__version__ = "0.0.18"
+
+
+def dash_board():
+    """Launches the dash board."""
+    from .dashBoard import main
+    dashBoard.main()
+
+
+def getShipments():
+    """getShipments."""
+    from .GetShipments import main
+    main()
+
+
+def glueWeight():
+    """glue weight."""
+    from .GlueWeight import main
+    main()
+
+
+def groundVITests():
+    """GND/VI tests."""
+    from .GroundVITests import main
+    main()
+
+
+def sendShipments():
+    """Send items."""
+    from .SendShipments import main
+    main()
+
+def uploadTest():
+    """Upload a single tests."""
+    from .UploadTest import main
+    main()
+
+
+def uploadMultipleTests():
+    """Upload multiple tests."""
+    from .UploadMultipleTests import main
+    main()
+
+def uploadModuleIV():
+    """Upload IV files of single and double modules"""
+    from .UploadModuleIV import main
+    main()
+
+def wirebondTest():
+    """Inputs data and eventually upload wirebod test."""
+    from .WireBondGui import main
+    main()
