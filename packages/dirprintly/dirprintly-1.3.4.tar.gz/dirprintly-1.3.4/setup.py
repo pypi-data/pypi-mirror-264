642d55dc7e74492075c4f24ca2dda933175e9742
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-29 14:05:40 +0000

    Add package dirprintly/1.3.4/dirprintly-1.3.4.tar.gz

---
+++ packages/dirprintly/dirprintly-1.3.4.tar.gz/dirprintly-1.3.4/setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setup(
+    name='dirprintly',
+    version='1.3.4',
+    packages=find_packages(),
+    install_requires=[
+        'colorama',
+    ],
+    entry_points={
+        'console_scripts': [
+            'dirprintly=dirprintly.file_printer:explore_directory_cli',
+        ],
+    },
+    url='https://github.com/PasanAbeysekara/dirprintly',
+    project_urls={
+        'Bug Tracker': 'https://github.com/PasanAbeysekara/dirprintly/issues',
+    },
+    author='Pasan Abeysekara',
+    author_email='pasankavindaabey@gmail.com',
+    description='📂 Easily view file contents in your terminal with a simple command. No need to open an editor! 💻',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+)
