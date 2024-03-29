9b6b85803d4c875eb608babfe5a90a4285261b24
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-29 06:05:49 +0000

    Add package formant/1.162.42/formant-1.162.42-py2.py3-none-any.whl

---
+++ packages/formant/formant-1.162.42-py2.py3-none-any.whl/formant/sdk/cloud/v2/src/resources/annotations/__init__.py
@@ -0,0 +1,5 @@
+
+from formant.sdk.cloud.v2.formant_admin_api_client.models import AnnotationStreamType, AnnotationTags, AnnotationFieldValuesResponse, AnnotationFieldValue, AnnotationFieldValuesRequest, TagParameters, AnnotationMetadata, AnnotationFieldValuesRequestTags, UserParameters, ScopeFilterTypesItem, ScopeFilter, AnnotationType, AnnotationTemplateTags, UserParametersRolesItem, AnnotationTemplateListResponse, AnnotationTemplate, AnnotationFieldType, AnnotationFieldValueTags, SheetParameters, AnnotationField, Annotation
+from formant.sdk.cloud.v2.formant_admin_api_client.api.annotation import annotation_controller_post
+from formant.sdk.cloud.v2.formant_admin_api_client.api.annotation_template import annotation_template_controller_list, annotation_template_controller_get_one
+from formant.sdk.cloud.v2.formant_admin_api_client.types import *
---
+++ packages/formant/formant-1.162.42-py2.py3-none-any.whl/formant/sdk/cloud/v2/src/resources/commands/__init__.py
@@ -0,0 +1,4 @@
+
+from formant.sdk.cloud.v2.formant_admin_api_client.models import CommandTemplateListResponse, CommandDeliveryEventTags, CommandQuery, CommandStreamType, CommandDeliveryEventStreamType, CommandListResponse, CommandRequest, CommandTemplateParameterMeta, CommandTags, CommandTemplate, ScopeFilterTypesItem, ScopeFilter, CommandResponseTags, CommandParameterMeta, CommandDeliveryEventType, CommandParameter, CommandDeliveryEventMetadata, CommandDeliveryEvent, CommandResponseStreamType, CommandRequestListResponse, CommandResponse, Command, CommandTemplateTags, CommandProgress, FileInfo
+from formant.sdk.cloud.v2.formant_admin_api_client.api.command import command_controller_post, command_controller_query
+from formant.sdk.cloud.v2.formant_admin_api_client.types import *
