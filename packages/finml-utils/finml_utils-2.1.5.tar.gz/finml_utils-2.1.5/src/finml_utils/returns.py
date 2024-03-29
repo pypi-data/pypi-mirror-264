89a49ec3df5b6890a9438184fb201d84e3b22e2b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-29 03:06:21 +0000

    Add package finml-utils/2.1.5/finml_utils-2.1.5.tar.gz

---
+++ packages/finml-utils/finml_utils-2.1.5.tar.gz/finml_utils-2.1.5/src/finml_utils/returns.py
@@ -0,0 +1,30 @@
+from typing import TypeVar
+
+import numpy as np
+import pandas as pd
+
+from .stats import compsum
+
+T = TypeVar("T", pd.Series, pd.DataFrame)
+
+
+def to_log_returns(data: T) -> T:
+    return np.log1p(to_returns(data))
+
+
+def to_returns(data: T) -> T:
+    if isinstance(data, pd.DataFrame):
+        return data.apply(to_returns)
+    if data.min() < 0:
+        return (data.diff() / data.shift(1).abs()).fillna(0.0)
+    return (data / data.shift(1) - 1).fillna(0.0)
+
+
+
+TPandas = TypeVar("TPandas", pd.DataFrame, pd.Series)
+
+
+def to_prices(returns: pd.Series, base=1.0) -> pd.Series:
+    """Arithcmetic returns to price series"""
+    returns = returns.copy().fillna(0).replace([np.inf, -np.inf], float("NaN"))
+    return base + base * compsum(returns)
