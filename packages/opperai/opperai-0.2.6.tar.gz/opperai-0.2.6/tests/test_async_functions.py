643a1bb5939c13e653a0c8116457c40cff8b49c3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-29 04:05:57 +0000

    Add package opperai/0.2.6/opperai-0.2.6.tar.gz

---
+++ packages/opperai/opperai-0.2.6.tar.gz/opperai-0.2.6/tests/test_async_functions.py
@@ -0,0 +1,200 @@
+import pytest
+from opperai import AsyncClient
+from opperai.types import CacheConfiguration, ChatPayload, FunctionDescription, Message
+
+DEFAULT_NAME = "test_function"
+DEFAULT_DESCRIPTION = "Test function"
+DEFAULT_INSTRUCTIONS = "Do something"
+
+
+@pytest.fixture
+async def function(
+    request: pytest.FixtureRequest,
+    aclient: AsyncClient,
+):
+    name = DEFAULT_NAME
+    description = DEFAULT_DESCRIPTION
+    instructions = DEFAULT_INSTRUCTIONS
+
+    try:
+        name = request.param.get("name", DEFAULT_NAME)
+        description = request.param.get("description", DEFAULT_DESCRIPTION)
+        instructions = request.param.get("instructions", DEFAULT_INSTRUCTIONS)
+    except AttributeError:
+        pass
+
+    fdesc = FunctionDescription(
+        path=f"test/sdk/{name}",
+        description=description,
+        instructions=instructions,
+    )
+
+    return await aclient.functions.create(fdesc)
+
+
+@pytest.mark.asyncio(scope="module")
+async def test_create_function(function, vcr_cassette):
+    fid = await function
+    assert fid is not None
+
+
+@pytest.mark.parametrize("function", [{"name": "test_get_by_id"}], indirect=True)
+@pytest.mark.asyncio(scope="module")
+async def test_get_by_id(aclient: AsyncClient, function, vcr_cassette):
+    fid = await function
+    f_by_id = await aclient.functions.get_by_id(fid)
+    assert f_by_id.path == "test/sdk/test_get_by_id"
+
+
+@pytest.mark.parametrize("function", [{"name": "test_get_by_path"}], indirect=True)
+@pytest.mark.asyncio(scope="module")
+async def test_get_by_path(aclient: AsyncClient, function, vcr_cassette):
+    await function
+    f_by_path = await aclient.functions.get_by_path("test/sdk/test_get_by_path")
+    assert f_by_path.path == "test/sdk/test_get_by_path"
+
+
+@pytest.mark.parametrize("function", [{"name": "test_get"}], indirect=True)
+@pytest.mark.asyncio(scope="module")
+async def test_get(aclient: AsyncClient, function, vcr_cassette):
+    fid = await function
+    f_by_id = await aclient.functions.get(id=fid)
+    assert f_by_id.path == "test/sdk/test_get"
+
+    f_by_id = await aclient.functions.get(path="test/sdk/test_get")
+    assert f_by_id.path == "test/sdk/test_get"
+
+
+@pytest.mark.parametrize("function", [{"name": "test_update_function"}], indirect=True)
+@pytest.mark.asyncio(scope="module")
+async def test_update_function(aclient: AsyncClient, function, vcr_cassette):
+    fid = await function
+    f = await aclient.functions.get_by_id(fid)
+    f.instructions = "Do something else"
+    fn_id = await aclient.functions.update(f)
+    f1 = await aclient.functions.get_by_id(fn_id)
+
+    assert f1.instructions == "Do something else"
+
+
+@pytest.mark.parametrize(
+    "function", [{"name": "test_delete_function_by_id"}], indirect=True
+)
+@pytest.mark.asyncio(scope="module")
+async def test_delete_function_by_id(aclient: AsyncClient, function, vcr_cassette):
+    fid = await function
+    f = await aclient.functions.get(id=fid)
+    assert f is not None
+    await aclient.functions.delete(id=f.id)
+    f = await aclient.functions.get(id=f.id)
+    assert f is None
+
+
+@pytest.mark.parametrize(
+    "function", [{"name": "test_delete_function_by_path"}], indirect=True
+)
+@pytest.mark.asyncio(scope="module")
+async def test_delete_function_by_path(aclient: AsyncClient, function, vcr_cassette):
+    fid = await function
+    f = await aclient.functions.get(id=fid)
+    assert f is not None
+    await aclient.functions.delete(path=f.path)
+    f = await aclient.functions.get(path=f.path)
+    assert f is None
+
+
+@pytest.mark.parametrize(
+    "function",
+    [{"name": "test_async_chat", "instructions": "translate to french"}],
+    indirect=True,
+)
+@pytest.mark.asyncio(scope="module")
+async def test_async_chat(function, aclient: AsyncClient, vcr_cassette):
+    fid = await function
+    f = await aclient.functions.get_by_id(fid)
+    resp = await aclient.functions.chat(
+        f.path, ChatPayload(messages=[Message(role="user", content="hello")])
+    )
+
+    assert resp.message == "Bonjour"
+
+
+@pytest.mark.parametrize(
+    "function",
+    [{"name": "test_async_chat_stream", "instructions": "translate to french"}],
+    indirect=True,
+)
+@pytest.mark.asyncio(scope="module")
+async def test_async_chat_stream(function, aclient: AsyncClient, vcr_cassette):
+    fid = await function
+    f = await aclient.functions.get_by_id(fid)
+    gen = await aclient.functions.chat(
+        f.path,
+        ChatPayload(messages=[Message(role="user", content="hello")]),
+        stream=True,
+    )
+
+    resp = "".join([message.delta async for message in gen if message.delta])
+
+    assert resp == "Bonjour"
+
+
+@pytest.mark.asyncio(scope="module")
+async def test_create_function_with_cache(aclient: AsyncClient, vcr_cassette):
+    fdesc = FunctionDescription(
+        path="test/sdk/test_create_function_with_cache_async",
+        description="Test function",
+        instructions="Do something",
+        cache_configuration=CacheConfiguration(exact_match_cache_ttl=10),
+    )
+
+    fid = await aclient.functions.create(fdesc)
+    assert fid is not None
+
+    res = await aclient.functions.chat(
+        fdesc.path, ChatPayload(messages=[Message(role="user", content="hello")])
+    )
+    print(res)
+    assert not res.cached
+
+    res = await aclient.functions.chat(
+        fdesc.path, ChatPayload(messages=[Message(role="user", content="hello")])
+    )
+    print(res)
+    assert res.cached
+
+    await aclient.functions.delete(path=fdesc.path)
+
+
+@pytest.mark.asyncio(scope="module")
+async def test_create_function_with_cache_flush(aclient: AsyncClient, vcr_cassette):
+    fdesc = FunctionDescription(
+        path="test/sdk/test_create_function_with_cache_async_flush",
+        description="Test function",
+        instructions="Do something",
+        cache_configuration=CacheConfiguration(exact_match_cache_ttl=10),
+    )
+
+    fid = await aclient.functions.create(fdesc)
+    assert fid is not None
+
+    res = await aclient.functions.chat(
+        fdesc.path, ChatPayload(messages=[Message(role="user", content="hello")])
+    )
+    print(res)
+    assert not res.cached
+
+    res = await aclient.functions.chat(
+        fdesc.path, ChatPayload(messages=[Message(role="user", content="hello")])
+    )
+    print(res)
+    assert res.cached
+
+    await aclient.functions.flush_cache(id=fid)
+    res = await aclient.functions.chat(
+        fdesc.path, ChatPayload(messages=[Message(role="user", content="hello")])
+    )
+    print(res)
+    assert not res.cached
+
+    await aclient.functions.delete(path=fdesc.path)
---
+++ packages/opperai/opperai-0.2.6.tar.gz/opperai-0.2.6/tests/test_functions.py
@@ -0,0 +1,236 @@
+from unittest.mock import MagicMock, patch
+
+import pytest
+import random
+from opperai import Client
+from opperai.types import ChatPayload, FunctionDescription, Message, CacheConfiguration
+
+api_key = "key"
+
+
+@patch("opperai._http_clients._http_client.do_request")
+def test_chat(mock_do_request):
+    mock_do_request.return_value = MagicMock(
+        status_code=200, json=lambda: {"message": "Bonjour"}
+    )
+    client = Client(api_key=api_key)
+    resp = client.functions.chat(
+        "french", ChatPayload(messages=[Message(role="user", content="hello")])
+    )
+
+    assert resp.message == "Bonjour"
+    mock_do_request.assert_called_once()
+
+
+@pytest.mark.asyncio
+@patch("opperai._http_clients._http_client.stream")
+async def test_chat_stream(mock_stream):
+    def gen():
+        yield {"delta": "Bon"}
+        yield {"delta": "Jour"}
+
+    mock_stream.return_value = gen()
+    client = Client(api_key="op-dev-api-key", api_url="http://localhost:8000")
+    gen = client.functions.chat(
+        "french",
+        ChatPayload(messages=[Message(role="user", content="hello")]),
+        stream=True,
+    )
+    resp = ""
+    for message in gen:
+        resp += message.delta
+
+    assert resp == "BonJour"
+
+
+@pytest.mark.asyncio
+@patch("opperai._http_clients._http_client.do_request")
+async def test_create_function(mock_do_request):
+    mock_do_request.side_effect = [
+        MagicMock(
+            status_code=404,
+        ),  # Response for get_function_by_path
+        MagicMock(status_code=200, json=lambda: {"id": 1}),  # Response for create
+    ]
+    client = Client(api_key="op-dev-api-key", api_url="http://localhost:8000")
+    function = FunctionDescription(
+        path="test/path", description="Test function", instructions="Do something"
+    )
+    fn_id = client.functions.create(function)
+    assert fn_id == 1
+    assert mock_do_request.call_count == 2
+    mock_do_request.assert_any_call(
+        "GET",
+        "/api/v1/functions/by_path/test/path",
+    )
+    mock_do_request.assert_any_call(
+        "POST",
+        "/api/v1/functions",
+        json=function.model_dump(),
+    )
+
+
+@pytest.mark.asyncio
+@patch("opperai._http_clients._http_client.do_request")
+async def test_update_function(mock_do_request):
+    mock_do_request.return_value = MagicMock(status_code=200, json=lambda: {"id": 1})
+    client = Client(api_key="op-dev-api-key", api_url="http://localhost:8000")
+    function = FunctionDescription(
+        id=1,
+        path="test/path",
+        description="Updated Test function",
+        instructions="Do something else",
+    )
+    fn_id = client.functions.update(function)
+    assert fn_id == function.id
+    mock_do_request.assert_called_once()
+
+
+@pytest.mark.asyncio
+@patch("opperai._http_clients._http_client.do_request")
+async def test_get(mock_do_request):
+    mock_do_request.side_effect = [
+        MagicMock(
+            status_code=200,
+            json=lambda: {
+                "id": 1,
+                "path": "test/path",
+                "description": "Test function",
+                "instructions": "Do something",
+            },
+        ),
+        MagicMock(
+            status_code=200,
+            json=lambda: {
+                "id": 1,
+                "path": "test/path",
+                "description": "Test function",
+                "instructions": "Do something",
+            },
+        ),
+    ]
+    client = Client(api_key="op-dev-api-key", api_url="http://localhost:8000")
+    function_description = client.functions.get(path="test/path")
+    assert function_description.id == 1
+    assert function_description.path == "test/path"
+    assert function_description.description == "Test function"
+    mock_do_request.assert_called_once_with(
+        "GET",
+        "/api/v1/functions/by_path/test/path",
+    )
+
+    mock_do_request.reset_mock()
+
+    function_description = client.functions.get(id=1)
+    assert function_description.id == 1
+    assert function_description.path == "test/path"
+    assert function_description.description == "Test function"
+    mock_do_request.assert_called_once_with(
+        "GET",
+        "/api/v1/functions/1",
+    )
+
+
+@pytest.mark.asyncio
+@patch("opperai._http_clients._http_client.do_request")
+async def test_get_function_by_path(mock_do_request):
+    mock_do_request.return_value = MagicMock(
+        status_code=200,
+        json=lambda: {
+            "id": 1,
+            "path": "test/path",
+            "description": "Test function",
+            "instructions": "Do something",
+        },
+    )
+    client = Client(api_key="op-dev-api-key", api_url="http://localhost:8000")
+    function_description = client.functions.get_by_path("test/path")
+    assert function_description.id == 1
+    assert function_description.path == "test/path"
+    assert function_description.description == "Test function"
+    mock_do_request.assert_called_once_with(
+        "GET",
+        "/api/v1/functions/by_path/test/path",
+    )
+
+
+@pytest.mark.asyncio
+@patch("opperai._http_clients._http_client.do_request")
+async def test_get_function_by_id(mock_do_request):
+    mock_do_request.return_value = MagicMock(
+        status_code=200,
+        json=lambda: {
+            "id": 1,
+            "path": "test/path",
+            "description": "Test function",
+            "instructions": "Do something",
+        },
+    )
+    client = Client(api_key="op-dev-api-key", api_url="http://localhost:8000")
+    function_description = client.functions.get_by_id("1")
+    assert function_description.id == 1
+    assert function_description.path == "test/path"
+    assert function_description.description == "Test function"
+    mock_do_request.assert_called_once_with(
+        "GET",
+        "/api/v1/functions/1",
+    )
+
+
+def test_create_function_with_cache(client: Client, vcr_cassette):
+    fdesc = FunctionDescription(
+        path="test/sdk/test_create_function_with_cache_sync",
+        description="Test function",
+        instructions="Do something",
+        cache_configuration=CacheConfiguration(exact_match_cache_ttl=10),
+    )
+
+    fid = client.functions.create(fdesc)
+    assert fid is not None
+
+    res = client.functions.chat(
+        fdesc.path, ChatPayload(messages=[Message(role="user", content="hello")])
+    )
+    print(res)
+    assert not res.cached
+
+    res = client.functions.chat(
+        fdesc.path, ChatPayload(messages=[Message(role="user", content="hello")])
+    )
+    print(res)
+    assert res.cached
+
+    client.functions.delete(path=fdesc.path)
+
+
+def test_create_function_with_cache_flush(client: Client, vcr_cassette):
+    fdesc = FunctionDescription(
+        path="test/sdk/test_create_function_with_cache_sync_flush",
+        description="Test function",
+        instructions="Do something",
+        cache_configuration=CacheConfiguration(exact_match_cache_ttl=10),
+    )
+
+    fid = client.functions.create(fdesc)
+    assert fid is not None
+
+    res = client.functions.chat(
+        fdesc.path, ChatPayload(messages=[Message(role="user", content="hello")])
+    )
+    print(res)
+    assert not res.cached
+
+    res = client.functions.chat(
+        fdesc.path, ChatPayload(messages=[Message(role="user", content="hello")])
+    )
+    print(res)
+    assert res.cached
+
+    client.functions.flush_cache(id=fid)
+    res = client.functions.chat(
+        fdesc.path, ChatPayload(messages=[Message(role="user", content="hello")])
+    )
+    print(res)
+    assert not res.cached
+
+    client.functions.delete(path=fdesc.path)
