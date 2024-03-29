9a9c32178b1f53bf4cc272740607511c42b15ad5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-29 14:05:27 +0000

    Add package GriffinX/1.2/GriffinX-1.2.tar.gz

---
+++ packages/GriffinX/GriffinX-1.2.tar.gz/GriffinX-1.2/GriffinX/yolov8.py
@@ -0,0 +1,29 @@
+import torch
+import os
+import cv2
+from ultralytics import YOLO
+
+def initialize_yolov8():
+    print("hellov8!!!!!")
+    global model_yolov8
+    model_yolov8 = YOLO('yolov8n.pt')
+
+def detect_and_save_cars_v8(partition, selected_partition_index, output_folder):
+    # Detect cars using YOLOv8
+    print("hellov8 detect!")
+    results = model_yolov8(partition.copy())  # copy of the partition to avoid altering 
+
+    for r in results.pred:
+        boxes = r['boxes']
+        for box in boxes:
+            left, top, right, bottom = map(int, box[:4])
+
+        # Draw rectangle and label on the frame
+            cv2.rectangle(partition, (left, top), (right, bottom), (0, 255, 0), 2) 
+            label = model_yolov8.names[int(box[5])]
+            confidence = float(box[4])
+            text = f"{label} {confidence:.2f}"
+            cv2.putText(partition, text, (left, bottom + 20), cv2.FONT_HERSHEY_SIMPLEX, 0.6, (0, 255, 0), 1, cv2.LINE_AA) 
+
+    # Save the partition with detected cars
+    cv2.imwrite(os.path.join(output_folder, f"partition_{selected_partition_index}_yolov8.jpg"), partition)
---
+++ packages/GriffinX/GriffinX-1.2.tar.gz/GriffinX-1.2/setup.py
@@ -0,0 +1,31 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="GriffinX",
+    version="1.2",
+    packages=find_packages(),
+    install_requires=[
+        # List your project's dependencies here.
+        # For example: 'numpy>=1.18.1'
+        "torch",
+        "numpy",
+        "ultralytics",
+        "opencv-python>=4.9.0.80",
+        "pillow>=10.2.0"
+        #"PIL>=7.1.2",
+    ],
+    # Metadata
+    author="Percom@UMD",
+    author_email="kirandav@umich.edu",
+    description="GriffinX is a middleware package for real time event detection",
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',  # This is important for making the README render correctly on PyPI
+    url="https://github.com/kiran-collab/Griffin-Drone-Car-Collaboration",
+    license="MIT",
+    classifiers=[
+        # Trove classifiers
+        # Full list: https://pypi.org/classifiers/
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+    ],
+)
