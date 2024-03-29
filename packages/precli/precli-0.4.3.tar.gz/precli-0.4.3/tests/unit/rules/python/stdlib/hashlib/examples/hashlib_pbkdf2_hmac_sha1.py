afa452665bbc637cf5346024347dfcdc25a6279f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-29 05:05:50 +0000

    Add package precli/0.4.3/precli-0.4.3.tar.gz

---
+++ packages/precli/precli-0.4.3.tar.gz/precli-0.4.3/tests/unit/rules/python/stdlib/hashlib/examples/hashlib_pbkdf2_hmac_sha1.py
@@ -0,0 +1,10 @@
+# level: ERROR
+# start_line: 10
+# end_line: 10
+# start_column: 0
+# end_column: 19
+import hashlib
+
+
+our_app_iters = 500_000
+hashlib.pbkdf2_hmac("sha1", b"password", b"bad salt" * 2, our_app_iters)
