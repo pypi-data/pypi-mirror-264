eefc97035109af7453d5fb260b34249c1c91286c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-29 06:06:59 +0000

    Add package bsb-nest/4.0.0rc2/bsb_nest-4.0.0rc2.tar.gz

---
+++ packages/bsb-nest/bsb_nest-4.0.0rc2.tar.gz/bsb_nest-4.0.0rc2/bsb_nest/exceptions.py
@@ -0,0 +1,18 @@
+class KernelWarning(Warning):
+    pass
+
+
+class NestError(Exception):
+    pass
+
+class NestKernelError(NestError):
+    pass
+
+class NestModuleError(NestKernelError):
+    pass
+
+class NestModelError(NestError):
+    pass
+
+class NestConnectError(NestError):
+    pass
