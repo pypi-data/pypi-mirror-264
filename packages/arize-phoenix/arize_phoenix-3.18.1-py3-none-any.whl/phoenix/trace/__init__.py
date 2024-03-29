945fcb248102976a3f8447fa5141cfc2b97dc0cb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-29 06:06:55 +0000

    Add package arize-phoenix/3.18.1/arize_phoenix-3.18.1-py3-none-any.whl

---
+++ packages/arize-phoenix/arize_phoenix-3.18.1-py3-none-any.whl/phoenix/trace/__init__.py
@@ -0,0 +1,35 @@
+import contextlib
+from typing import Iterator
+
+from .projects import using_project
+from .span_evaluations import DocumentEvaluations, Evaluations, SpanEvaluations, TraceEvaluations
+from .trace_dataset import TraceDataset
+
+__all__ = [
+    "Evaluations",
+    "TraceDataset",
+    "SpanEvaluations",
+    "DocumentEvaluations",
+    "TraceEvaluations",
+    "using_project",
+]
+
+
+@contextlib.contextmanager
+def suppress_tracing() -> Iterator[None]:
+    """
+    Context manager to pause OpenTelemetry instrumentation.
+
+    Examples:
+        with suppress_tracing():
+            # No tracing will occur within this block
+            ...
+    """
+    try:
+        from opentelemetry.context import _SUPPRESS_INSTRUMENTATION_KEY, attach, detach, set_value
+    except ImportError:
+        yield
+        return
+    token = attach(set_value(_SUPPRESS_INSTRUMENTATION_KEY, True))
+    yield
+    detach(token)
