5dc2b49f8c563805c294ea761f6f0c8b3309220a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-29 14:05:13 +0000

    Add package dirprintly/1.3.2/dirprintly-1.3.2.tar.gz

---
+++ packages/dirprintly/dirprintly-1.3.2.tar.gz/dirprintly-1.3.2/setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup, find_packages
+
+with open("README.md","r") as f:
+    description = f.read()
+
+setup(
+    name='dirprintly',
+    version='1.3.2',
+    packages=find_packages(),
+    install_requires=[
+        'colorama',
+    ],
+    entry_points={
+        'console_scripts': [
+            'dirprintly=dirprintly.file_printer:explore_directory_cli',
+        ],
+    },
+    url='https://github.com/PasanAbeysekara/dirprintly',  # GitHub Repo URL
+    project_urls={
+        'Bug Tracker': 'https://github.com/PasanAbeysekara/dirprintly/issues',
+    },
+    author='Pasan Abeysekara',
+    author_email='pasankavindaabey@gmail.com',
+
+    long_description=description,
+    long_description_content_type="text/markdown"
+)
